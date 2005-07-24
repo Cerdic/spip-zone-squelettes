3b9591f6eb88a55ef97a9ee8cb3094db0493202d
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-07-24 17:08:35 +0000

    et un moteur de recherche intégré à sedna
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@467 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sedna/sedna.php
+++ sedna/sedna.php
@@ -4,6 +4,25 @@
     $flag_preserver=true;
   } else
     $fond='sedna';
+
+  // critere {contenu}
+  function critere_contenu($idb, &$boucles, $crit) {
+    $boucle = &$boucles[$idb];
+
+    $boucle->hash =  '
+    // RECHERCHE
+    if ($r = addslashes($GLOBALS["recherche"])) $s="(syndic_articles.descriptif LIKE \'%$r%\'
+      OR syndic_articles.titre LIKE \'%$r%\'
+      OR syndic_articles.url LIKE \'%$r%\'
+      OR syndic_articles.lesauteurs LIKE \'%$r%\')";
+      else $s=1;
+    
+    ';
+
+    // un peu trop rapide, ca... le compilateur exige mieux
+    $boucle->where[] = '$s';
+  }
+
   $delais=0;
   include('inc-public.php3');
 ?>
