a694655ed3c611fc1f54b4c12216ea0239597bc9
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-07-26 21:26:11 +0000

    simplification du machin à style
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@519 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/sedna.js
@@ -0,0 +1,68 @@
+/*
+	Excerpted from (c) Paul Sowden
+	http://www.alistapart.com/articles/alternate/
+	http://www.alistapart.com/d/alternate/styleswitcher.js
+*/
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+
+/*
+ * Fonctions pour Sedna (c) Fil -- GNU/GPL
+ */
+
+/* une fonction pour modifier la couleur des intertitres du meme site */
+function highlight_site(id) {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("h2")[i]); i++) {
+    a.style.background = 'transparent url(sedna.gif)';
+  }
+  for(i=0; (a = document.getElementsByName(id)[i]); i++) {
+    a.style.background = '#004080';
+  }
+}
+
+/* gerer le cookie des articles lus */
+/* attention on ne doit pas depasser 3ko */
+function jai_lu(id) {
+  var cookie = readCookie("sedna_lu");
+  cookie = cookie ? (cookie+','+id) : (''+id);
+  cookie = cookie.substring(-3000);
+  createCookie("sedna_lu", cookie, 365);
+  a = document.getElementById('news'+id);
+  a.className='linkvu'; /* ce lien change de style */
+}
+
+/* Afficher ou masquer les descriptifs */
+function style_desc(aff) {
+  createCookie("sedna_style", aff, 365);
+  for(i=0; (a = document.getElementsByName('desc')[i]); i++) {
+   a.className="desc_"+aff;
+  }
+  if (aff == 'masquer') {
+    document.getElementById('desc_masquer').className='selected';
+    document.getElementById('desc_afficher').className='';
+  } else {
+    document.getElementById('desc_masquer').className='';
+    document.getElementById('desc_afficher').className='selected';
+  }  
+}
