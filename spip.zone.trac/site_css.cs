11c189ae87bfa9f43fbbbe6487e8e4084ba08e35
izo@aucuneid.net <izo@aucuneid.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-09-19 18:08:16 +0000

    Bones  dotspip 1.8 ... enfin !
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip.zone.trac/site_css.cs
+++ spip.zone.trac/site_css.cs
@@ -1,4 +1,8 @@
 <?cs
 ##################################################################
 # Site CSS - Place custom CSS, including overriding styles here.
-?>
+<style type="text/css">
+		@import url( /template/css/trac.css );
+	   @import url(/template/css/wiki.css);
+		body { margin: 0 !important; }
+	</style>?>
