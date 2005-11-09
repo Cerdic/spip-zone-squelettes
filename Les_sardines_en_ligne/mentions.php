0646b34fe7a3c04f8999e24e5426f1be7d7d7d69
karim@glums.com <karim@glums.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-11-09 08:45:55 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@1262 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Les_sardines_en_ligne/mentions.php
@@ -0,0 +1,7 @@
+<?php
+$fond = "mentions";
+$delais = 2 * 3600;
+
+include ("inc-public.php");
+
+?>
---
+++ Les_sardines_en_ligne/mentionsRub.php
@@ -0,0 +1,8 @@
+<?php
+
+$fond = "mentionsRub";
+$delais = 24 * 3600;
+
+include ("inc-public.php");
+
+?>
