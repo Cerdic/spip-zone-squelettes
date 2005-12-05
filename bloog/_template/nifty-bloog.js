8c418ab54502c519649d1294e4c60411b2e8a55c
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-12-05 23:25:05 +0000

    un squelette d article css en marche.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@1380 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bloog/_template/nifty-bloog.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+if(!NiftyCheck())
+    return;
+RoundedTop("div#container","#ededf5","#ededf5");
+RoundedBottom("div#container","#ededf5","#fff");
+RoundedTop("ul#nav li","transparent","#fff");
+Rounded("div.boite","#ededf5","#FFF");
+Rounded("div.grande_boite","#ededf5","#FFF");
+Rounded("div#minipics li","#ededf5","#FFF");
+RoundedTop("div.discussion","#ededf5","#fff");
+RoundedBottom("div.discussion","#ededf5","#fff");
+}
