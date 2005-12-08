22eb4d457d405398f0d32383348aca9e8b80f6a2
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-12-08 21:52:02 +0000

    Avec des forums patraques. css a revoir.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@1390 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bloog/_template/nifty-bloog.js
+++ bloog/_template/nifty-bloog.js
@@ -5,8 +5,10 @@ RoundedTop("div#container","#ededf5","#ededf5");
 RoundedBottom("div#container","#ededf5","#fff");
 RoundedTop("ul#nav li","transparent","#fff");
 Rounded("div.boite","#ededf5","#FFF");
+Rounded("div.avatar","#ededf5","#FFF");
+Rounded("div.discussion","#ededf5","#FFF");
 Rounded("div.grande_boite","#ededf5","#FFF");
 Rounded("div#minipics li","#ededf5","#FFF");
-RoundedTop("div.discussion","#ededf5","#fff");
-RoundedBottom("div.discussion","#ededf5","#fff");
+RoundedTop("div.footer","#ededf5","#fff");
+RoundedBottom("div.footer","#ededf5","#fff");
 }
