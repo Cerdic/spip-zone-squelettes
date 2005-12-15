27a99df12cbb7984df57cad387abdbd891b413a7
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-12-15 18:23:30 +0000

    Encore une amelioration du css et du nifty.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@1432 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bloog/_template/nifty-bloog.js
+++ bloog/_template/nifty-bloog.js
@@ -1,8 +1,11 @@
 window.onload=function(){
 if(!NiftyCheck())
     return;
-RoundedTop("div#container","#ededf5","#ededf5");
-RoundedBottom("div#container","#ededf5","#fff");
+RoundedTop("div#container","#f5f5f9","#e0e0ee");
+RoundedBottom("div#container","#f5f5f9","#e0e0ee");
+RoundedTop("div#menu","#e0e0ee","#c5c5df");
+RoundedTop("div#surcontent","#c5c5df","#ededf5");
+RoundedBottom("div#surcontent","#c5c5df","#ededf5");
 RoundedTop("ul#nav li","transparent","#fff");
 Rounded("div.boite","#ededf5","#FFF");
 Rounded("div.avatar","#ededf5","#FFF");
@@ -10,6 +13,5 @@ Rounded("div.reponse","#ededf5","#FFF");
 Rounded("div.discussion","#ededf5","#FFF");
 Rounded("div.grande_boite","#ededf5","#FFF");
 Rounded("div#minipics li","#ededf5","#FFF");
-RoundedTop("div.footer","#ededf5","#fff");
-RoundedBottom("div.footer","#ededf5","#fff");
+RoundedBottom("div#footer","#e0e0ee","#c5c5df");
 }
