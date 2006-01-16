5ab8941f58404db9e783eb4bf2f9e49363ed2c31
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-16 21:38:33 +0000

    la meme définie par un squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@1713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- hBones/balises.php
+++ hBones/balises.php
@@ -1,31 +1,18 @@
 <?php
 
-
 //
 // Dans n'importe quel contexte (article, breve, etc)
-// #MF_TAGS liste les mots-cles dans l'ordre ou ils ont
-// ete poses, au microformat relTag
+// on liste les mots-cles dans l'ordre ou ils ont ete
+// poses, au microformat relTag
 // <http://www.microformats.org/wiki/reltag>
+// On appelle le squelette mf_tags.html
 //
 function sql_tags($id, $type) {
-	$tags = array();
-
-	$s = spip_query("SELECT mots.id_mot, mots.titre, mots.descriptif
-		FROM spip_mots AS mots, spip_mots_".table_objet($type)." AS lien
-		WHERE lien.id_$type = $id
-		AND mots.id_mot = lien.id_mot");
-	while ($t = spip_fetch_array($s)) {
-		$tag = "<a rel='tag' href='"
-			.generer_url_mot($t['id_mot'])
-			."'";
-		if ($title = couper(textebrut(propre($t['descriptif'])), 36))
-			$tag .= ' title="'.texte_backend($title).'"';
-		$tag .= ">".typo($t['titre']).'</a>';
-		
-		$tags[] = $tag;
-	}
-
-	return join(', ',$tags);
+	$page = cherche_page('' /* chemin_cache */,
+		array('id_'.$type => $id) /* contexte_inclus */,
+			'mf_tags' /* fond */,
+			false);
+	return $page['texte'];
 }
 function balise_MF_TAGS_dist($p) {
 	$nom = $p->id_boucle;
@@ -35,7 +22,5 @@ function balise_MF_TAGS_dist($p) {
 	$p->code = 'sql_tags('.$_id.",'".$type."')";
 	return $p;
 }
-
-
 
 ?>
