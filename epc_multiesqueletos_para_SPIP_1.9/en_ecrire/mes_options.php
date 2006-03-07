541414e6b05543a2abcb15bc248a701c5884253f
joseluis@digital77.com <joseluis@digital77.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-07 22:23:43 +0000

    pequenos cambios
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@2272 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- epc_multiesqueletos_para_SPIP_1.9/en_ecrire/mes_options.php
+++ epc_multiesqueletos_para_SPIP_1.9/en_ecrire/mes_options.php
@@ -1,8 +1,6 @@
 <?php
 
-
-if ($ce=="si") {  // comprueba que el esqueleto existe en la carpeta "esqueletos"
-
+if ($ce=="si") {
 // Funci—n para purgar la cache y que todo el sitio funcione con el nuevo esqueleto 
 // si se produce un cambio
 // de 3615MARLENE (http://marlene.c3ew.com/article.php?id_article=18)
@@ -28,7 +26,7 @@ function purge($dir, $age='ignore', $regexp = '') {
 // Guarda en una cookie el esqueleto seleccionado por un a–o o lo utiliza si existe
 // Comprueba qu exista y si no deja el "por_defecto"
 
-if (isset($esqueleto)) {
+if (isset($esqueleto) and ($esqueleto!=$dossier_squelettes)) {
  	purge(CACHE, 0);     // vaciar la cache
 	$dossier_squelettes = "esqueletos/".$esqueleto;
 	setcookie("esqueleto_selec","$esqueleto",time()+365*24*3600);
@@ -44,10 +42,48 @@ else {
 $dossier_squelettes = "esqueletos/por_defecto";
 }
 
-// Si sigue sin haber esqueleto seleccionado coge esqueletos/por_defecto como esqueleto
+// Si  no hay esqueleto seleccionado coge esqueletos/por_defecto como esqueleto
 
 if ($dossier_squelettes=="" ) {
 $dossier_squelettes = "esqueletos/por_defecto";
 }
+
+//
+// Definition de tous les extras possibles
+//
+
+echo $comprobar_esqueleto;
+
+$champs_extra = true;
+
+	$GLOBALS['champs_extra'] = Array (
+		'auteurs' => Array (
+				"abo" => "radio|brut|Opciones|Html,Texto,Darse de baja|html,texte,non"
+
+			),
+			
+		'articles' => Array (
+				'squelette' => 'bloc|propre|Bibliographie'
+
+			)
+
+		);
+		
+		$GLOBALS['champs_extra_proposes'] = Array (
+'auteurs' => Array (
+		'tous' => 'abo',
+		'inscription' => 'abo'
+	        ),
+'articles' => Array (
+		'0' => 'squelette',
+		'tous' => ''
+		
+                )
+				
+);
+
+
+include('options_spip_listes.php3');
+
 
 ?>
