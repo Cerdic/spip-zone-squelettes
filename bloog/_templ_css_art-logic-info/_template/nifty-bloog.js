97c6d01743efca43434c594aca2886c05e4f053a
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-19 12:03:47 +0000

    Le squelette css de www.Art-logic.info en l etat.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@2733 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bloog/_templ_css_art-logic-info/_template/nifty-bloog.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+Nifty("div#container","big");
+Nifty("div#header","top,big");
+Nifty("ul#deco h4,ul#nav li","top");
+Nifty("div#surcontent","bottom,big");
+Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.grande_boite,div#content");
+Nifty("div#footer");
+Nifty("ul#minipics li");
+Nifty("ul#deco li p","bottom");
+Nifty("ul#cel");
+}
