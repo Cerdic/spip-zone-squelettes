53227334a6cce57d596bf3a61b54e6c3efdf8ad4
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-09 09:01:43 +0000

    ajout: - recherche - recherche mots croisees - pagination
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@3221 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ annu/lang/local_fr.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// 404
+'404_1' => 'D&eacute;sol&eacute; aucun article, brève ou rubrique disponible sur cette page',
+'404_2' => 'Où vous voulez aller ?',
+'404_home' => 'Page d\'accueil',
+'404_lastpage' => 'Dernière page consult&eacute;e',
+
+// recherche
+'aucun_resultat' => 'D&eacute;sol&eacute;, aucun r&eacute;sultat disponible pour',
+'indiquer_recherche' => 'Indiquer ce que vous voulez chercher',
+'recherche_avancee' => 'Vous pouvez aussi essayer ',
+'recherche_avancee2' => 'la recherche avancée',
+
+
+
+
+);
+?>
