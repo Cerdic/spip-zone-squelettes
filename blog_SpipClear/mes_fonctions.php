7cba88cdf761633a3c529f51b91fe967aa40be87
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-18 23:00:53 +0000

    l'apprenti sorcier
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4694 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blog_SpipClear/mes_fonctions.php
+++ blog_SpipClear/mes_fonctions.php
@@ -3,17 +3,16 @@
 	/***
 	* declaration doctype et entete html, a appliquer a #CHARSET
 	***/
+		define("ENVOI_XML", false); //false par defaut, mettre a true (vrai) dans ecrire/mes_options.php3 si on est sur de soi
 
 	function doctype($charset) {
-		global $envoi_xml;
-		tester_variable("envoi_xml", false); //false par défaut, mettre à true (vrai) dans ecrire/mes_options.php3 si on est sûr de soi
-		# En-têtes et prologue
-		# Envoie des en-tête HTTP
+		# En-tetes et prologue
+		# Envoie des en-tete HTTP
 		$accept_xml =
 			!empty($_SERVER['HTTP_ACCEPT']) &&
 			strpos($_SERVER['HTTP_ACCEPT'],'application/xhtml+xml') !== false;
 
-		if ($envoi_xml && $accept_xml) {
+		if (ENVOI_XML && $accept_xml) {
 			header('Content-Type: application/xhtml+xml');
 		} else {
 			header('Content-Type: text/html; charset='.$charset);
@@ -30,30 +29,8 @@
 		return affdate_base($date, 'annee').'-'.affdate_base($date, 'mois').(($type == 'jour')?'-'.affdate($date, 'd'):'');
 	}
 
-	// Gestion des parametres des urls
-	/*function parametre_url($url, $parametre, $valeur = '__global__') {
-		$link = new Link(str_replace('&amp;', '&', $url));
-		if($valeur == '__global__')
-			$valeur = $GLOBALS[$parametre];
-		if(empty($valeur)) $link->DelVar($parametre);
-		else $link->AddVar($parametre, $valeur);
-		return quote_amp($link->getUrl());
-	}*/
-
-	function generer_url_base($fond, $delais = 0) {
-		$url = "page.php3";
-		$url = parametre_url($url, 'fond', $fond);
-		$url = parametre_url($url, 'delais', $delais);
-		//if ($fond == 'rss' || $fond == 'atom') $url = parametre_url($url, 'flag_preserver', 1);
-		return $url;
-	}
-	
-	function generer_url_rss($id, $type = 'rubrique') {
-		return parametre_url(generer_url_base('rss'), "id_".$type, $id);
-	}
-
-	function generer_url_atom($id_secteur) {
-		return parametre_url(generer_url_base('atom'), 'id_rubrique', $id_secteur);
+	function affdate_long($date) {
+		return affdate_base($date, 'nom_jour').' '.affdate_base($date, 'entier');
 	}
 
 	//
@@ -64,17 +41,5 @@
 		$p->interdire_scripts = false;
 		return $p;
 	}
-
-	//
-	// Themes de blog
-	//
-	function theme($id_secteur) {
-		return $GLOBALS['scEstUnBlog'][$id_secteur];
-	}
-
-	function affdate_long($date) {
-		return affdate_base($date, 'nom_jour').' '.affdate_base($date, 'entier');
-	}
-
 
 ?>
