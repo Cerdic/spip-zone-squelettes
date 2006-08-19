25538d71488a3759b5966bdca43edda62cbc7656
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-19 15:57:02 +0000

    Pfff... j'oublie le fichier de langues !
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-net-fantastik/local_fr.php
@@ -0,0 +1,31 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// Special pour spip.net
+
+// Barre de navigation de spip.net :
+'documentation' => 'Documentation',
+'glossaire' => 'Glossaire',
+'contribuer' => 'Contribuer',
+'aide' => 'Aide',
+
+
+
+
+// Des trucs qui manquent, quelque soit le site...
+
+// pour la navigation :
+'accueil' => 'Accueil',
+'lire_suite' => 'Lire la suite',
+'liens_utiles' => 'Liens utiles'
+
+// Voir aussi : http://fraichdist.online.fr/spip.php?article292
+
+);
+
+
+?>
