125260a7fe077be48a8ce5708a9374b126c9d8c8
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-20 10:09:27 +0000

    Broutilles...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-net-fantastik/local_fr.php
+++ spip-net-fantastik/local_fr.php
@@ -4,8 +4,9 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-
+//
 // Special pour spip.net
+//
 
 // Barre de navigation de spip.net :
 'documentation' => 'Documentation',
@@ -13,19 +14,34 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'contribuer' => 'Contribuer',
 'aide' => 'Aide',
 
+// autre...
+'dernieres_modifs' => 'Derni&egrave;res modifications',
+'maj' => 'maj', // abbreviation de 'mise a jour'
+'sites_realises_avec_spip' => 'Sites r&eacute;alis&eacute;s avec SPIP',
+'derniers_sites_realises_avec_spip' => 'Derniers sites r&eacute;alis&eacute;s avec SPIP',
+'sites_references' => 'Sites r&eacute;f&eacute;renc&eacute;s',
 
-
-
+//
 // Des trucs qui manquent, quelque soit le site...
+//
 
 // pour la navigation :
 'accueil' => 'Accueil',
 'lire_suite' => 'Lire la suite',
-'liens_utiles' => 'Liens utiles'
+'liens_utiles' => 'Liens utiles',
+
+// autres...
+'quoideneuf' => 'Quoi de neuf ?',
+'FAQ' => 'FAQ',
+
+// specifique SPIP :
+'squelette' => 'Squelette',
+'squelettes' => 'Squelettes',
+'squelette_voir' => 'Voir le squelette de cette page',
+'squelettes_dossier' => 'Dossier squelettes'
 
 // Voir aussi : http://fraichdist.online.fr/spip.php?article292
 
 );
-
 
 ?>
