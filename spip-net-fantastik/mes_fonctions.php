0c2d550e2a8aa5bdabff40b7f8967b0906a5a4da
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-21 21:52:22 +0000

    Ne gardons que l'essentiel...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-net-fantastik/mes_fonctions.php
+++ spip-net-fantastik/mes_fonctions.php
@@ -3,48 +3,5 @@
 	# pour la page www.spip.fr/download
 	define('VERSION_STABLE', '1.9.0');
 	define('VERSION_STABLE_URL', 'spip-dev/DISTRIB/SPIP-v1-9-0.zip');
-
-	$type_urls = 'trad';
-
-	$plugins[] = 'ancres';
-
-
-	# raccourcis [->spip19] etc
-	function calculer_url_spip($id, $texte, $ancre) {
-	$spip = array( 
-		1 => 1309,
-		10 => 1309,
-		103 => 1309,
-		104 => 1309,
-		105 => 1309,
-		12 => 1310,
-		121 => 1310,
-		13 => 1253,
-		14 => 1832,
-		15 => 1911,
-		16 => 1965,
-		17 => 2102,
-		171 => 2102,
-		172 => 2102,
-		18 => 2991,
-		181 => 2991,
-		182 => 3173,
-		183 => 3333,
-		19 => 3368
-	);
-
-	if (isset($spip[$id])) {
-		$p= calculer_url_article($spip[$id], $texte, $ancre);
-		$p[1] = 'spip'; # class
-		return $p;
-	} else {
-		return array('/', 'spip', 'version inconnue');
-	}
-	}
-	# indisppensable ? ou pas ?
-	function generer_url_spip($id) {
-		$p= calculer_url_spip($id, $texte, $ancre);
-		return $p[0];
-	}
 	
 ?>
