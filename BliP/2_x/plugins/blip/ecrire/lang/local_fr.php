bea94054899474a72e44e96b62ab249e0bb3f2b4
petit.bazar@laposte.ne <petit.bazar@laposte.ne@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-28 19:52:27 +0000

    BliP : Déplacement du dossier ecrire vers sa localisation finale.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4924 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/2_x/plugins/blip/ecrire/lang/local_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'blip_config' => 'Configuration du squelette BLiP',
+    'blip_info' => 'infos &agrave; propos de la configuration de BLiP',
+    'configure_blip' => 'Configurer BLiP'
+
+);
+
+?>
