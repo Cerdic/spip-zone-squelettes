1a0a612e8e45cc469ede71eac80ec4f50a35fa99
petit.bazar@laposte.net <petit.bazar@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-28 21:03:57 +0000

    TAG BliP 2.2
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4937 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/tags/2.2/plugins/blip/lang/local_en.php
@@ -0,0 +1,145 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP -- Pour les squelettes Blip
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+// A
+
+
+ 'blip_accueil' => 'Home page',
+ 'blip_afficher_les_derniers' => 'Post the last',
+ 'blip_articles' => 'Articles',
+ 'blip_articles_aucun_publies' => 'No article published',
+ 'blip_article_aucun_publie' => 'No article published',
+ 'blip_article_de' => 'Articles of',
+ 'blip_articles_derniers_publies' => 'Lastest articles published',
+ 'blip_article_propos_de_cet' => 'In connection with this article',
+ 'blip_article_liste_des' => 'List articles',
+ 'blip_article_n' => 'the articles n&deg;',
+ 'blip_nest_plus_disponible_il_a_probablement_ete_deplace_ou_supprime_vous_avez_ete_redirige_sur_la_page_listant_les_articles_publies' => 'It is not available any more probably was moved or removed. You were redirected on the page listing the articles published',
+ 'blip_articles_publies' => 'articles published', 
+ 'blip_populaires_articles' => 'Popular articles',
+ 'blip_article_statistiques_de' => 'Statistics of the article',
+ 'blip_articles_trier_par' => 'Sort the articles by',
+ 'blip_auteur_aucun' => 'No author',
+ 'blip_auteurs_liste_des' => 'List authors',
+ 'blip_auteur_n' => 'The authors n&deg;',
+ 'blip_nexiste_pas_vous_avez_ete_redirige_sur_la_page_listant_les_auteurs_du_site' => 'does not exist. You were redirected on the page listing the authors of the site',
+ 'blip_articles_derniers_publies' => 'Latest articles published',
+
+
+
+// C
+
+ 'blip_commentaires_aucun_publie' => 'No comment published',
+ 'blip_commentaires_derniers' => 'Last comments',
+ 'blip_commentaires_derniers_poste' => 'Last comments posted',
+ 'blip_commentaires_postes' => 'Posted comments',
+ 'blip_commentaires_postes2' => 'posted comments',
+ 'blip_commentaire_poster_un' => 'To post a comments',
+ 'blip_commentaires_de_larticle' => 'Comments of the article',
+
+
+// D
+
+ 'blip_dans' => 'in',
+ 'blip_date_de_mise_a_jour' => 'updated on',
+ 'blip_date_de_publication' => 'published on',
+ 'blip_depuis_cette_page_vous_pouvez_personnaliser_laffichage_de_ce_site_internet' => 'Since this page, you can personalize the posting of this Internet site',
+ 'blip_derniere_mise_a_jour_le' => 'Last update on',
+ 'blip_derniere_mise_a_jour' => 'Last update',
+ 'blip_documents' => 'Documents',
+ 'blip_document_aucun_publie' => 'No document published;',
+ 'blip_document_dernier_publie' => 'Last documents published',
+ 'blip_documents_joints_a_larticle' => 'Documents joined to the article',
+ 'blip_document_publie_le' => 'document published on',
+ 'blip_document_publie' => 'document published',
+
+
+// E
+
+ 'blip_envoyer_un_message_a' => 'Send a message to',
+ 'blip_encyclopedie_wikipedia' => 'The Wikipedia encyclopaedia',
+
+
+// F
+
+ 'blip_fois' => 'time',
+
+
+// G
+
+ 'blip_galerie_dimages' => 'Gallery of images',
+ 
+
+// J
+
+ 'blip_jeux_de_couleurs_disponibles' => 'Sets of colors available',
+
+
+// L
+
+ 'blip_le' => 'The',
+ 'blip_lu' => 'Read',
+
+
+// M
+
+ 'blip_mis_a_jour_en' => 'Updated in',
+ 'blip_mis_a_jour_le' => 'Updated on',
+ 'blip_modifier_les_couleurs_du_site' => 'Modify the colors of the site',
+
+
+// P
+ 
+ 'blip_page_de_tests' => 'Page of tests',
+ 'blip_par' => 'by',
+ 'blip_popularite' => 'popularity',
+ 'blip_publie_en' => 'Published in',
+
+
+// R
+
+ 'blip_recherche_externe' => 'External research',
+ 'blip_recherche_sur_des_moteurs_de' => 'on search engines',
+ 'blip_recherche_votre' => 'Your research',
+ 'blip_rubriques' => 'Sections',
+ 'blip_rubrique_articles_de_la' => 'Articles of the section',
+ 'blip_rubrique_aucune' => 'No section',
+ 'blip_rubrique_dans_la_meme' => 'In the same section',
+ 'blip_rubrique_documents_de_la' => 'Documents of section',
+ 'blip_rubriques_du_site' => 'Section of the site',
+ 'blip_rubriques_liste_des' => 'List section',
+ 'blip_rubrique_liens_externe_de_la_rubrique' => 'External link of the section',
+ 'blip_rubrique_n' => 'The section n&deg;',
+ 'blip_nest_plus_disponible_elle_a_probablement_ete_deplacee_ou_supprimre_vous_avez_ete_redirige_sur_la_page_listant_les_rubriques_du_site' => 'is not available any more, it probably was moved or removed. You were redirected on the page listing the sections of the site',
+ 'blip_rubriques_sous' => 'Under sections',
+
+
+// S
+
+ 'blip_sites' => 'Sites',
+
+
+// V
+
+ 'blip_visiteurs_nombre_de' => 'number of visitors',
+ 'blip_visiteurs_par_jour_cumul' => 'Visitors per day (office plurality)',
+ 'blip_visiteurs_quotidiens' => 'daily visitors',
+
+
+);
+
+?>
