a4493ae34d4b4551bb5c0257958dfc0920bc4531
petit.bazar@laposte.ne <petit.bazar@laposte.ne@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-28 19:37:15 +0000

    Identification BliP 2.0 (dépréciée).
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@4920 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/tags/2.0/squelettes/inc-css_couleur.php
@@ -0,0 +1,231 @@
+<?php
+
+// Ce fichier defini les classes CSS en tant que variable php, pour permettre un changement de theme graphique comparable a celui de sedna
+
+/*
+Remplacer '8' par rand (0 ,9) dans la ligne 16 -> Switch autonome couleur (puis vider le cache)
+*/
+
+	// CHOIX CSS PAR DEFAUT DE BliP. Modifications possibles : remplacer les valeurs numériques par d'autres valeurs numériques, à condition que la déclaration CSS existe sinon plantage.
+$css_blip = array(	
+	'couleur' => '8',
+	'structure' => '0',
+	'tete' => '0',
+	'menu' => '0',
+	'corps' => '0',
+	'pied' => '0',
+	'lateral' => '0',
+	'divers' => '0',	
+); 
+
+	// COULEURS ET PROPORTIONS DU SITE
+$css_blip_couleur_1_nom = 'n°01 : Vert acidulé et orange pétillant';
+$css_blip_couleur_1 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#85c329',
+	'couleur|1|clair' => '#bcde89',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+);
+
+$css_blip_couleur_10_nom = 'n°10 : Test en cours';
+$css_blip_couleur_10 = array(
+	// Jeu de couleur n°10
+	'gris|fonce' => '#aaaaaa',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#333333',
+	'couleur|1' => '#1a8ab3',
+	'couleur|1|clair' => '#c2e2ee',
+	'couleur|2' => '#c43219',
+	'couleur|2|clair' => '#dc715e',
+	'couleur|neutre' => '#000000',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_2_nom = 'n°02 : Violet intense et orange pétillant';
+$css_blip_couleur_2 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#600051',
+	'couleur|1|clair' => '#925F6E',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_3_nom = 'n°03 : Bleu azur et orange pétillant';
+$css_blip_couleur_3 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#009999',
+	'couleur|1|clair' => '#66DEFF',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_4_nom = 'n°04 : Rouge intense et orange pétillant';
+$css_blip_couleur_4 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#D00000',
+	'couleur|1|clair' => '#DD7875',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_5_nom = 'n°05 : Rose bonbon et orange pétillant';
+$css_blip_couleur_5 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#cc0066',
+	'couleur|1|clair' => '#ff6666',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_6_nom = 'n°06 : Vert kaki et orange pétillant';
+$css_blip_couleur_6 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#666633',
+	'couleur|1|clair' => '#999999',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_7_nom = 'n°07 : Orange pétillant et bleu océan';
+$css_blip_couleur_7 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#fb9622',
+	'couleur|1|clair' => '#ffd600',
+	'couleur|2' => '#1e73cd',
+	'couleur|2|clair' => '#c0daef',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_8_nom = 'n°08 : Bleu océan et rouge intense';
+$css_blip_couleur_8 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#1a8ab3',
+	'couleur|1|clair' => '#c2e2ee',
+	'couleur|2' => '#c43219',
+	'couleur|2|clair' => '#dc715e',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+$css_blip_couleur_9_nom = 'n°09 : Bleu intense et orange pétillant';
+$css_blip_couleur_9 = array(
+	// Jeu de couleur n°1
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#1e73cd',
+	'couleur|1|clair' => '#c0daef',
+	'couleur|2' => '#fb9622',
+	'couleur|2|clair' => '#ffd600',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+);  
+?>
