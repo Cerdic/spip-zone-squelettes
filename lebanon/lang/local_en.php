d15f249ffd741c2eb040fde25797706f50a44555
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-13 14:19:58 +0000

    Squelettes Lebanon pour SPIP 1.9.0 Explications : http://beyrouthenguerre.free.fr/squelettes
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@5307 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lebanon/lang/local_en.php
@@ -0,0 +1,23 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Creer portfolio
+'info_creer_portolio' => 'When you add images as attached documents to an article, SPIP can automatically create vignettes (thumbnails) from inserted images. This will allow, for instance, the automated creation of a gallery or a portfolio.<br />
+This template use <a href="http://www.huddletogether.com/projects/lightbox2">Lightbox&nbsp;JS&nbsp;v2.0</a> : it\'s a simple, unobtrusive script used to overlay images on the current page. It\'s a snap to setup and works on all modern browsers.',
+
+// Bagde flickr
+'badge_flickr' => 'Flickr badge',
+'badge_flickr_info' => 'This badge show public photos from <a href="http://www.flickr.com">www.<strong style="color: #3993ff;">flick<span style="color: #ff1c92;">r</span></strong>.com</a> tagged with <a href="http://www.flickr.com/photos/tags/lebanon">lebanon</a>. Make your own badge <a href="http://www.flickr.com/badge_new.gne">here</a>.',
+
+// Navigation
+'articles' => 'articles',
+'info_articles_trouves' => 'articles found',
+'articles_precedents' => 'Show the articles',
+'squelettes' => 'Template'
+
+);
+
+?>
---
+++ lebanon/lang/local_fr.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Creer portfolio
+'info_creer_portolio' => 'Pour cr&eacute;er une galerie d\'images (portfolio), ajoutez vos images en tant que documents joints &agrave; un article. Si l\'option est activ&eacute;e, SPIP peut cr&eacute;er pour vous, automatiquement, des vignettes (miniatures) des images ins&eacute;r&eacute;es.<br />De plus, ces squelettes utilisent le script &#171;&nbsp;<a href="http://www.huddletogether.com/projects/lightbox2">Lightbox&nbsp;JS&nbsp;v2.0</a>&nbsp;&#187; qui permet de visualiser les images du portfolio en diaporama.',
+
+// Bagde flickr
+'badge_flickr' => 'Badge flickr',
+'badge_flickr_info' => 'Un badge pr&eacute;sente les derni&egrave;res photos d&eacute;pos&eacute;es sur <a href="http://www.flickr.com">www.<strong style="color: #3993ff;">flick<span style="color: #ff1c92;">r</span></strong>.com</a> avec le tag &#171;&nbsp;<a href="http://www.flickr.com/photos/tags/lebanon">Lebanon</a>&nbsp;&#187;. Vous pouvez supprimer ou remplacer ce badge en modifiant le code correspondant du squelette <tt>inc-navigation.html</tt>.',
+
+// Navigation
+'articles' => 'articles',
+'info_articles_trouves' => 'articles trouv&eacute;s',
+'articles_precedents' => 'Articles pr&eacute;c&eacute;dents',
+'squelettes' => 'Squelettes'
+
+);
+
+?>
