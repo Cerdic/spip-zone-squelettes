1713ce1c103d8fed401503b7dbb379c66959bd25
jsb92@laposte.net <jsb92@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-16 19:58:32 +0000

    mise à jour accessibilité
    ajout tabindex, accesskey, hreflang, title et alt
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@6366 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scolaspip/plugins/scola/optionsscola.php
+++ scolaspip/plugins/scola/optionsscola.php
@@ -17,9 +17,6 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-// réécriture des URLs
-
-$type_urls='propres2';
 
 // critère {edito}
 // permet d'affecter une rubrique spécifique pour les éditos
