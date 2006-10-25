6a5e7ebcf59151b23ccde45d6300241f17564fea
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-25 14:48:43 +0000

    Avec gestion de version?
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@6617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _bloog_/bloog_css/_template/nifty-bloog-bz.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+if(!NiftyCheck())
+    return;
+RoundedTop("div#container","#f5f5f9","#e0e0ee");
+RoundedBottom("div#container","#f5f5f9","#e0e0ee");
+RoundedTop("div#menu","#e0e0ee","#c5c5df");
+RoundedTop("div#surcontent","#c5c5df","#ededf5");
+RoundedBottom("div#surcontent","#c5c5df","#ededf5");
+RoundedTop("ul#nav li","transparent","#fff");
+Rounded("div.boite","#ededf5","#FFF");
+Rounded("div.avatar","#ededf5","#FFF");
+Rounded("div.reponse","#ededf5","#FFF");
+Rounded("div.discussion","#ededf5","#FFF");
+Rounded("div.grande_boite","#ededf5","#FFF");
+Rounded("div#minipics li","#ededf5","#FFF");
+RoundedBottom("div#footer","#e0e0ee","#c5c5df");
+}
