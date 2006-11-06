b6c85f248c33c4accffc41c9b4f6d605450cf698
petit.bazar@laposte.net <petit.bazar@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-06 20:10:39 +0000

    BliP : mize à jour de la draduction allemande.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@7042 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- BliP/2_x/plugins/blip/lang/blip_de.php
+++ BliP/2_x/plugins/blip/lang/blip_de.php
@@ -1,159 +1,144 @@
 <?php
 
 // This is a BliP language file  --  Ceci est le fichier langue ALLEMAND du squelette BliP pour SPIP
-//  Ce fichier langue a été compilé par Stéphane COTTIN pour BliP 2.2
+//  Ce fichier langue a ÈtÈ compilÈ par StÈphane COTTIN pour BliP 2.2
 // Documentation sur le multilinguisme : http://www.cent20.net/spip.php?article79 et http://www.cent20.net/spip.php?article67
-// Les mises à jour sont suivies et assurées par ? 
+// Les mises ‡ jour sont suivies et assurÈes par ?
 // Pour contacter la personne responsable du fichier langue, vous devez passer par http://groups.google.com/group/blip, sauf mentions contraires dans l'articles http://www.cent20.net/spip.php?article79
 
 $GLOBALS['i18n_blip_de'] = array(
- 
- 
+
 // A
 
-
  'accueil' => 'Hauptseite',
- 'afficher_les_derniers' => 'Zeigt die letzte',
+ 'actualite' => 'aktuell',
+ 'afficher_les_derniers' => 'Zeigt letzte',
  'articles' => 'Artikel',
  'articles_aucun_publies' => 'Unter dieser Adresse gibt es keinen Artikel',
  'article_aucun_publie' => 'Unter dieser Adresse gibt es keinen Artikel',
  'article_de' => 'Artikel von',
- 'articles_derniers_publies' => 'Letzte Artikel',
- 'article_propos_de_cet' => 'In Verbindung mit diesem Artikel',
+ 'articles_derniers_publies' => 'Artikel',
+ 'article_propos_de_cet' => 'verbunden mit diesem Artikel',
  'article_liste_des' => 'Liste der Artikel',
  'article_numero' => 'Der Artikel n&deg;',
- 'n_est_plus_disponible' => 'ist nicht mehr verf&uuml;gbar, er ist wahrscheinlich verschoben oder abgeschafft worden. Sie sind jetzt auf der Seite, die die ver&ouml;ffentlichten Artikel auflistet',
- 'articles_publies' => 'ver&ouml;ffentlichte Artikel', 
- 'populaires_articles' => 'Beliebtesten Artikel',
- 'article_statistiques_de' => 'Statistiken des Artikels',
- 'articles_trier_par' => 'Die Artikel sortieren im',
- 'auteur_aucun' => 'Kein Autor',
+ 'n_est_plus_disponible' => 'ist nicht mehr verf&uuml;gbar, er ist wahrscheinlich verschoben oder abgeschafft. Sie sind jetzt auf der Seite, die die ver&ouml;ffentlichten Artikel auflistet',
+ 'articles_publies' => 'Artikel',
+ 'populaires_articles' => 'Beliebteste Artikel',
+ 'article_statistiques_de' => 'Statistik der Artikel',
+ 'articles_trier_par' => 'Artikel sortieren nach',
+ 'auteur_aucun' => 'Niemand',
  'auteurs_liste_des' => 'Liste der Autoren',
  'auteur_numero' => 'Der Autor n&deg;',
  'auteur_n_existe_pas' => 'ist nicht mehr verf&uuml;gbar. Sie sind jetzt auf der Seite, die die ver&ouml;ffentlichten Autoren auflistet',
  'articles_derniers_publies' => 'Letzte Artikel',
 
-
-
 // C
 
  'commentaires_aucun_publie' => 'Kein ver&ouml;ffentlichter Kommentar',
- 'commentaires_derniers' => 'Letzte Kommentar',
+ 'commentaires_derniers' => 'letzte Kommentare',
  'commentaires_derniers_poste' => 'Letzte ver&ouml;ffentlichte Kommentare',
- 'commentaires_postes' => 'Ver&ouml;ffentlichte Kommentare',
- 'commentaires_postes2' => 'ver&ouml;ffentlichte Kommentare',
- 'commentaire_poster_un' => 'Hinterlassen einen Kommentar',
+ 'commentaires_postes' => 'Kommentare',
+ 'commentaires_postes2' => 'Kommentare',
+ 'commentaire_poster_un' => 'Hinterlassen eines Kommentars',
  'commentaires_de_larticle' => 'Kommentare des Artikels',
-
 
 // D
 
  'dans' => 'im',
  'date_de_mise_a_jour' => 'Aktualisierungsdatum',
  'date_de_publication' => 'Ver&ouml;ffentlichungsdatum ',
- 'depuis_cette_page_vous_pouvez_personnaliser_laffichage_de_ce_site_internet' => 'Von dieser Seite k&ouml;nnen Sie den Anschlag dieses Website personalisieren ',
+ 'depuis_cette_page_vous_pouvez_personnaliser_laffichage_de_ce_site_internet' => 'Von dieser Seite k&ouml;nnen Sie die Anzeige dieser Webseiten personalisieren ',
  'derniere_mise_a_jour_le' => 'Letzte Aktualisierung am',
  'derniere_mise_a_jour' => 'Letzte Aktualisierung',
- 'documents' => 'Dokumenten',
- 'document_aucun_publie' => 'kein ver&ouml;ffentlichte Dokument',
- 'document_dernier_publie' => 'letzt ver&ouml;ffentlichte Dokument',
- 'documents_joints_a_larticle' => '',
- 'document_publie_le' => 'document publi&eacute; le',
- 'document_publie' => 'ver&ouml;ffentlichte Dokument',
-
+ 'documents' => 'Dokumente',
+ 'document_aucun_publie' => 'kein ver&ouml;ffentlichtes Dokument',
+ 'document_dernier_publie' => 'ver&ouml;ffentlichte Dokumente',
+ 'documents_joints_a_larticle' => 'Beilagen',
+ 'document_publie_le' => 'Dokumkent publiziert am',
+ 'document_publie' => 'Dokumente',
 
 // E
 
- 'envoyer_un_message_a' => 'Nachricht senden zu',
+ 'envoyer_un_message_a' => 'Nachricht senden an',
  'encyclopedie_wikipedia' => 'Wikipedia Enzyklop&auml;die',
-
 
 // F
 
  'fois' => 'mals',
 
-
 // G
 
- 'galerie_dimages' => 'Bildgalerie',
- 
+ 'galerie_dimages' => 'Galerie',
 
 // J
 
- 'jeux_de_couleurs_disponibles' => 'Verf&uuml;gbare Farbspiele',
-
+ 'jeux_de_couleurs_disponibles' => 'Verf&uuml;gbare Farben',
 
 // L
 
- 'le' => 'Am',
- 'lu' => 'Geredet',
-
+ 'le' => 'am',
+ 'lu' => 'gelesen',
 
 // M
 
- 'mis_a_jour_en' => 'Aktualisierung im',
+ 'mis_a_jour_en' => 'Aktualisiert in',
  'mis_a_jour_le' => 'Aktualisierung am',
- 'modifier_les_couleurs_du_site' => '&Auml;ndern die Farben des Website',
- 'liste_des_mots_cles' => 'Schlagworte',
- 'afficher_les_mots_cles_par' => 'Zeigt die Schlagworte in',
+ 'modifier_les_couleurs_du_site' => 'die Farben der Webseiten &Auml;ndern',
+ 'liste_des_mots_cles' => 'Stichworte',
+ 'afficher_les_mots_cles_par' => 'Zeigt Stichworte nach',
 
 // N
 
- 'nouveaute' => 'Neuigkeit',
- 'mis_a_jour_en' => 'Mis &agrave; jour en',
- 
- 
+ 'nouveaute' => 'Neu',
+ 'mis_a_jour_en' => 'Aktualisiert in',
+
 // P
- 
+
  'page_de_tests' => 'Testseite',
  'par' => 'von',
  'popularite' => 'Beliebtheit',
- 'publie_en' => 'Ver&ouml;ffentlicht im',
- 
- 
+ 'publie_en' => 'Ver&ouml;ffentlicht unter',
+
 // O
- 'ordre_alphabetique' => 'alphabetischer Reihenfolge',
 
-
+ 'ordre_alphabetique' => 'alphabetische Reihenfolge',
 
 // R
 
- 'recherche_externe' => 'Externe Forschung',
- 'recherche_sur_des_moteurs_de' => 'auf Forschungsmotoren',
- 'recherche_votre' => 'Ihre Forschung',
+ 'recherche_externe' => 'Externe Suche',
+ 'recherche_sur_des_moteurs_de' => 'auf Suchmaschinen',
+ 'recherche_votre' => 'Ihre Suche',
  'rubriques' => 'Rubriken',
  'rubrique_articles_de_la' => 'Artikel der Rubrik',
  'rubrique_aucune' => 'keine Rubrik',
- 'rubrique_dans_la_meme' => 'In die selbe Rubrik',
+ 'rubrique_dans_la_meme' => 'In derselben Rubrik',
  'rubrique_documents_de_la' => 'Dokumente der Rubrik',
- 'rubriques_du_site' => 'Rubriken des website',
+ 'rubriques_du_site' => 'Rubriken',
  'rubriques_liste_des' => 'Liste der Rubriken',
- 'rubrique_liens_externe_de_la_rubrique' => 'Externe Verbindungen der Rubrik',
+ 'rubrique_liens_externe_de_la_rubrique' => 'Externe Links der Rubrik',
  'rubrique_numero' => 'Die Rubrik n&deg;',
- 'nest_plus_disponible' => 'ist nicht mehr verfügbar, sie ist wahrscheinlich verschoben oder abgeschafft worden. Sie sind jetzt auf der Seite, die die ver&ouml;ffentlichten Rubriken auflistet',
+ 'nest_plus_disponible' => 'ist nicht mehr verf¸gbar, sie ist wahrscheinlich verschoben oder abgeschafft. Sie sind jetzt auf der Seite, die die ver&ouml;ffentlichten Rubriken auflistet',
  'rubriques_sous' => 'Unterrubriken',
-
 
  // S
 
- 'sites' => 'Websites',
- 
- // T
- 'theme' => 'thema',
+ 'sites' => 'Webseiten',
 
+ // T
+
+ 'theme' => 'Thema',
 
  // V
 
- 'visiteurs_nombre_de' => 'Anzahl der Besucher',
+ 'visiteurs_nombre_de' => 'Anzahl Besucher',
  'visiteurs_par_jour_cumul' => 'Besucher pro Tag (Kumulierung)',
  'visiteurs_quotidiens' => 't&auml;gliche Besucher',
- 'visiteurs_aujourd_hui' => 'Heute Besucher',
+ 'visiteurs_aujourd_hui' => 'Besucher heute',
  'visiteurs_cumules' => 'kumulierte Besucher',
 
-
  // DEBUG
- 'z_debug' => 'NE RIEN METTRE SOUS CETTE LIGNE',
 
+ 'z_debug' => 'NE RIEN METTRE SOUS CETTE LIGNE',
 
 );
 
