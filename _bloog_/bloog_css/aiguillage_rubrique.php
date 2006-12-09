b7c484f9e0066438e8154f47f4028d1c6a7140ea
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-09 22:56:13 +0000

    déplacement mes options + remis aiguillage + encore des modifs sur les inclures
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@7847 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _bloog_/bloog_css/aiguillage_rubrique.php
@@ -0,0 +1,31 @@
+<?php
+//on analyse la valeur de la variable $squelette_rubrique
+if($squelette_rubrique){
+      if($restreint){
+	  $squelette_rubrique="-".$squelette_rubrique."-restreint";
+	  }else{
+	  $squelette_rubrique="-".$squelette_rubrique;}
+}else{
+      if($restreint){
+	  $squelette_rubrique="-restreint";
+	  }else{
+	  $squelette_rubrique="-normal";
+}}
+
+if($squelette_rubrique=="-forum"){
+
+//on construit $fond en fonction de $squelette_rubrique
+$fond = "squelettes/__rubrique".$squelette_rubrique;
+$delais = 60 ;
+include ("inc-public.php");
+
+} else {
+
+//on construit $fond en fonction de $squelette_rubrique
+$fond = "squelettes/__rubrique".$squelette_rubrique;
+$delais = 3600*24*7 ;
+include ("inc-public.php");
+
+}
+
+?>
