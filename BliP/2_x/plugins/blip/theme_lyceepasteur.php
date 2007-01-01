6569c89d379a044b256294844b6395adba6a5ac0
postmaster@cent20.net <postmaster@cent20.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-01 23:07:47 +0000

    Un nouveau commit (J'aurais bien tout commité d'un coup, mais mon client svn il me déteste) : Thème graphique supplémentaire.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@8404 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/2_x/plugins/blip/theme_lyceepasteur.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ce fichier defini les classes CSS en tant que variable php, pour permettre une  surcharge de la feuille de style
+
+$css_blip["body|background|color"] = ${$css_blip_couleur}['couleur|1|clair'];
+$css_blip["#page|width"] = '900px';
+$css_blip["#page|border|style"] = 'solid';
+$css_blip["#page|border|width"] = '0 2px';
+$css_blip["#page|border|color"] = ${$css_blip_couleur}['couleur|1'];
+$css_blip["#page|background|color"] = ${$css_blip_couleur}['couleur|neutre'];
+$css_blip[".titre_principal|width"] = '900px';
+$css_blip[".titre_principal|margin"] = '0';
+$css_blip[".titre_principal|padding"] = '0';
+$css_blip[".sous_titre|width"] = '880px';
+$css_blip["..sous_titre|margin"] = '0';
+$css_blip[".sous_titre|padding"] = '10px 0 10px 20px';
+
+?>
