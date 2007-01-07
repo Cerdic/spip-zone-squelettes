98595a1057b302552d30a30ccb0d02702259e251
petit.bazar@laposte.net <petit.bazar@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-07 17:25:55 +0000

    BliP : nettoyage anti-vide.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@8634 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- BliP/2_x/plugins/blip/exec/blip_contenu.php
+++ BliP/2_x/plugins/blip/exec/blip_contenu.php
@@ -20,15 +20,15 @@ function exec_blip_contenu() {
 	global $options, $changer_config, $spip_display;
 	global $liste_meta_navigation;
 	$surligne = "";
-  
+
 	if ($connect_statut != '0minirezo' OR !$connect_toutes_rubriques) {
 		debut_page(_T('blipconfig:blip_config'), "administration", "BLiP");
 		echo _T('avis_non_acces_page');
 		fin_page();
 		exit;
 	}
-	
-	$liste_meta_navigation = array(			
+
+	$liste_meta_navigation = array(
 		'blip_accueil',
 		'blip_sommaire',
 		'blip_sommaire_afficher',
@@ -52,37 +52,37 @@ function exec_blip_contenu() {
 		'blip_espaceprive',
 		'blip_switch'
 	);
-	
+
 	if ($changer_config == 'oui') {
 		ecrire_meta("blip_prefixe", $_POST['blip_prefixe']);
 		ecrire_meta("blip_couleur", $_POST['blip_couleur']);
 		ecrire_metas();
 		foreach($liste_meta_navigation as $i)
 			if ($_POST[$i]!=NULL)
-				ecrire_meta($i, $_POST[$i]);		
-		ecrire_metas();	
+				ecrire_meta($i, $_POST[$i]);
+		ecrire_metas();
 	}
-	
+
 	lire_metas();
-	
-	
-	
+
+
+
 	debut_page(_T('blipconfig:blip_config'), "administration", "BLiP");
 	echo "<br/>";
-	
+
 	gros_titre(_T('blipconfig:blip_config'));
-	
+
 	if (BliP_verifier_base())
 	{
         echo barre_onglets("blip", "options");
     }
-	
+
 	debut_gauche();
-	
+
 	debut_boite_info();
 	echo _T('blipconfig:blip_fonctions_info');
 	fin_boite_info();
-	
+
 	debut_raccourcis();
 	echo _T('blipconfig:blip_raccourcis_documentation');
 	fin_raccourcis();
@@ -91,11 +91,11 @@ function exec_blip_contenu() {
 
 	//
 	// Modifications
-	//	
-	
+	//
+
 	echo generer_url_post_ecrire('blip_contenu');
 	echo "<input type='hidden' name='changer_config' value='oui'>";
-	
+
 	debut_cadre_couleur();
 
 	debut_cadre_relief("", false, "", _T('blipconfig:blip_couleur_du_site'));
@@ -103,8 +103,8 @@ function exec_blip_contenu() {
 	echo "<input type='text' name='blip_couleur' value=\"$blip_couleur\" size='40' CLASS='forml'>";
 	echo "<div style='text-align:right;'><input type='submit' name='Valider' value='"._T('bouton_enregistrer')."' CLASS='fondo'></div>";
 	echo "Saisir un num&eacute;ro entre 1 et 10.";
-	fin_cadre_relief();	
-	
+	fin_cadre_relief();
+
 	debut_cadre_relief("", false, "", _T('blipconfig:blip_theme_graphique'));
 	$blip_prefixe = entites_html($GLOBALS['meta']["blip_prefixe"]);
 	echo "<input type='text' name='blip_prefixe' value=\"$blip_prefixe\" size='40' CLASS='forml'>";
@@ -112,11 +112,8 @@ function exec_blip_contenu() {
 	echo "<br />";
 	echo "Dans votre dossier 'blip' vous trouverez des fichiers theme_XXXX.php. Saisir : XXXX";
 	fin_cadre_relief();
+	fin_cadre_couleur();
 
-	fin_cadre_couleur();	
-
-	
-	
 	debut_cadre_trait_couleur("mot-cle-24.gif", false, "", _T('blipconfig:blip_options'));
 
 	//
@@ -132,7 +129,7 @@ function exec_blip_contenu() {
 
 	// J'ai modifié le formulaire ci-dessous et cela ne marche plus, les autres continues de marcher ... Je ne sais pas pourquoi.
 	// Normalement, ces formulaires me permettent d'activer ou désactiver par oui - non les valeurs des metas, definies par BliP_installer_blip_meta
-	
+
 	foreach($liste_meta_navigation as $i)
 		{
 			echo "<TR>";
@@ -142,8 +139,8 @@ function exec_blip_contenu() {
 			echo "<TD ALIGN='$spip_lang_left' class='verdana2'>";
 			afficher_choix($i, $GLOBALS['meta'][$i],
 				array('oui' => _T('item_oui'), 'non' => _T('item_non')), " &nbsp; ");
-			echo "</TD></TR>\n";		
-		}	
+			echo "</TD></TR>\n";
+		}
 
 	echo "<TR><TD style='text-align: $spip_lang_right;' COLSPAN=2>";
 	echo "<INPUT TYPE='submit' NAME='Valider' VALUE='"._T('bouton_valider')."' CLASS='fondo'>";
@@ -153,12 +150,9 @@ function exec_blip_contenu() {
 	fin_cadre_relief();
 
 	fin_cadre_trait_couleur();
-	
+
 	echo "</form>";
-	
-	
-	
- 
+
 	echo fin_page();
 
 }
