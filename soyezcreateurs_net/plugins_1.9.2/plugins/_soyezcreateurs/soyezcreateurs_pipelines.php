e4f3a34b229c78cc55b0964f8ef089432ff36278
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-17 10:33:41 +0000

    Avec un define plutôt qu'un find_in_path, c'est plus rapide.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@9725 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_pipelines.php
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_pipelines.php
@@ -3,7 +3,7 @@
 function SoyezCreateurs_ajouterOnglets($flux) {
   if($flux['args']=='configuration')
 	$flux['data']['soyez_createurs']= new Bouton(
-											  find_in_path('soyezcreateurs.png'), 'Param&egrave;tres SoyezCreateurs',
+											  '../'._DIR_PLUGIN_SOYEZCREATEURS.'/img_pack/soyezcreateurs.png', 'Param&egrave;tres SoyezCreateurs',
 											  generer_url_ecrire('cfg','cfg=soyezcreateurs'));
   return $flux;
 }
