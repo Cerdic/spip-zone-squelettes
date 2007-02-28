7b778f103caaa87d1a9f36bfdf3db9fc34ee0779
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-28 22:07:56 +0000

    Quelques chaines en plus
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@10001 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/lang/cfgpyrat_fr.php
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/lang/cfgpyrat_fr.php
@@ -6,6 +6,10 @@ $GLOBALS['i18n_cfgpyrat_fr'] = array(
 
 // pour cfg_soyezcreateurs_layout
 'layout_gala' => 'Agencement des colonnes sur la page',
+'layout_agencement' => 'Agencement',
+'layout_defaut' => 'Par d&eacute;faut',
+'layout_accueil' => 'Accueil',
+'layout_autres' => 'Autres pages',
 
 // pour cfg_soyezcreateurs_google
 'parametres_google' => 'Param&egrave;tres pour Google',
