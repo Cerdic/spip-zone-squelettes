7ed25cad2bff44990981a810e9d9c0eca2286b8c
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-28 21:59:58 +0000

    Chaines de langues pour LayoutGala
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@9994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/lang/cfgpyrat_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
+
+$GLOBALS['i18n_cfgpyrat_fr'] = array(
+
+'layout_gala' => 'Agencement des colonnes sur la page'
+
+);
+
+?>
