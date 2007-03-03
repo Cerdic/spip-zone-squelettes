e6254567512108d27f286947fd8b080a1563e4ed
postmaster@cent20.net <postmaster@cent20.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-03 13:45:32 +0000

    Thème maths trigo pour BliP
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@10087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/2_x/plugins/blip/theme_maths_trigo.php
@@ -0,0 +1,32 @@
+<?php
+
+// Ce fichier defini les classes CSS en tant que variable php, pour permettre une  surcharge de la feuille de style
+
+$css_blip["body|background"] = $css_blip_couleur['couleur|1|clair'];
+$css_blip["body|margin"] = '25px 0';
+$css_blip["#page|border|style"] = 'solid';
+$css_blip["#page|width"] = '950px';
+$css_blip["#page|border|width"] = '2px';
+$css_blip["#page|border|color"] = $css_blip_couleur['couleur|1'];
+$css_blip["#page|background"] = '#ffffff url(plugins/blip/theme_maths_trigo_logo.gif) top right no-repeat';
+$css_blip['#tete|height'] = '100px';
+$css_blip['#tete|width'] = '600px';
+$css_blip['#tete|border|width'] = '0 0 2px 0';
+$css_blip[".titre_principal|margin"] = '0';
+$css_blip[".titre_principal|padding"] = '20px 20px';
+$css_blip[".sous_titre|margin"] = '0';
+$css_blip[".sous_titre|padding"] = '0 0 10px 20px';
+
+$css_blip[".barre_laterale|background"] = 'url(plugins/blip/theme_maths_trigo_logo2.gif) bottom right no-repeat';
+$css_blip[".barre_laterale|padding"] = $css_blip_couleur['taille|espacement_bloc'].' 0 250px 0';
+
+$css_blip[".barre_laterale|background|color"] = '';
+
+$css_blip["#pied|background"] = '#ffffff';
+$css_blip["#pied|height"] = '20px';
+$css_blip["#pied|border|width"] = '0px';
+
+
+
+
+?>
