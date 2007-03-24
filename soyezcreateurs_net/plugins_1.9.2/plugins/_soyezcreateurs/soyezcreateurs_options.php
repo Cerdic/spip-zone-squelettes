7331a318ba97664069c00ea1e6e334bde51498ff
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-24 10:07:35 +0000

    Il FAUT modifier inc_version.php pour faire réellement foncer find_in_path
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@10671 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_options.php
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_options.php
@@ -32,10 +32,6 @@ $GLOBALS['fin_intertitre_5'] = '</h6>';
 // Ne pas permettre de passer en interface simplifiee
 $_GET["set_options"] = $GLOBALS["set_options"] = 'avancees';
 
-# si vous n'avez aucun fichier .php3, redefinissez a ""
-# ca fera foncer find_in_path
-define('_EXTENSION_PHP', '');
-
 # La liste des webmestres n'ayant pas besoin d'autentification FTP pour les actions avancées de SPIP
 # Séparer les id_auteurs par ':'
 define('_ID_WEBMESTRES', '1');
