4f6068da5413b9a0ff788bee0cef1b8c51a80dcb
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-06 22:42:02 +0000

    La config de SoyezCreateurs ne quitte plus la page de configuration (merci à Fil d'avoir ouvert la voie avec le plugin Autorité !). Elle devient en plus réservée aux webmestres seuls.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@11908 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_pipelines.php
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_pipelines.php
@@ -1,11 +1,22 @@
 <?php
 
-function SoyezCreateurs_ajouterOnglets($flux) {
-  if($flux['args']=='configuration')
-	$flux['data']['soyez_createurs']= new Bouton(
-											  '../'._DIR_PLUGIN_SOYEZCREATEURS.'/img_pack/soyezcreateurs.png', 'Param&egrave;tres SoyezCreateurs',
-											  generer_url_ecrire('cfg','cfg=soyezcreateurs'));
-  return $flux;
+// Ajoute le bouton d'amin aux webmestres
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+function SoyezCreateurs_ajouter_onglets($flux) {
+	if($flux['args']=='configuration'
+	AND autoriser('webmestre')) {
+		// on voit le bouton dans la barre "configurer"
+		$flux['data']['cfg_soyezcreateurs']= 
+			new Bouton(
+			'../'._DIR_PLUGIN_SOYEZCREATEURS.'/img_pack/soyezcreateurs.png', // icone
+			'SoyezCreateurs',	// titre
+			generer_url_ecrire('cfg','cfg=soyezcreateurs'),
+			NULL,
+			'cfg_soyezcreateurs'
+			);
+	}
+ 	return $flux;
 }
 
 ?>
