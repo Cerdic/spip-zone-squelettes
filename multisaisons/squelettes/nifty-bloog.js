5924bcb038b62b6a87d8da39e68d2d82861d0d9d
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-11 18:42:12 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@12072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multisaisons/squelettes/nifty-bloog.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+Nifty("div.boite,div.avatar,div.reponse,div.discussion,p.interne");
+Nifty("ul#minipics li,div.grande_boite,div#content,div#footer");
+Nifty("div#container,div.interforum,div.interforumgrand","big");
+Nifty("div#header","top,big");
+Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
+Nifty("div#surcontent","bottom,big");
+Nifty("#cel p","bottom,same-height");
+Nifty("#celbas p","bottom");
+}
