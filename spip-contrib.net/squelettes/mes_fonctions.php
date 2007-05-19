bad08664a4a7d95123e6ea12735cecf1caa11f84
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-19 16:15:11 +0000

    reecriture du hack des boucles  wiki sur spip-contrib, de facon a ce que les crayons puissent s'afficher correctement y compris dans la partie ignoree des boucles (on considere que sur une action -- crayons_html ou crayons_store -- on ne veut pas de restriction anti- secteur 607
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@12301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-contrib.net/squelettes/mes_fonctions.php
+++ spip-contrib.net/squelettes/mes_fonctions.php
@@ -3,6 +3,22 @@
 
 define('SECTEUR_WIKI', 607);
 
+// true quand on vient du wiki
+// true dans l'espace prive
+// true dans les crayons
+// false dans les autres pages (publiques hors wiki)
+function boucle_exclure_secteur() {
+	return
+	(
+		_DIR_RESTREINT==''
+		OR _request('action')
+		OR (defined('RUBRIQUE_WIKI_OK') AND RUBRIQUE_WIKI_OK)
+	)
+		? '0'
+		: SECTEUR_WIKI
+	;
+}
+
 //
 // <BOUCLE(ARTICLES)> sans le wiki (secteur 607)
 //
@@ -10,15 +26,7 @@ function boucle_ARTICLES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 
-	// true quand on vient du wiki
-	// true dans l'espace prive
-	// false dans les autres pages (publiques hors wiki)
-	define ('RUBRIQUE_WIKI_OK', _DIR_RESTREINT=='');
-
-	if (!RUBRIQUE_WIKI_OK) {
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = array("'!='", "'id_secteur'", "'".SECTEUR_WIKI."'");
-	}
+	$boucle->where[] = array("'!='", "'id_secteur'", '_q(boucle_exclure_secteur())');
 
 	return boucle_ARTICLES_dist($id_boucle, $boucles);
 }
@@ -30,12 +38,7 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 
-	define ('RUBRIQUE_WIKI_OK', _DIR_RESTREINT=='');
-
-	if (!RUBRIQUE_WIKI_OK) {
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = array("'!='", "'id_secteur'", "'".SECTEUR_WIKI."'");
-	}
+	$boucle->where[] = array("'!='", "'id_secteur'", '_q(boucle_exclure_secteur())');
 
 	return boucle_RUBRIQUES_dist($id_boucle, $boucles);
 }
