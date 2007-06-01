51c93da533b6bf323335de5c31c0e0b34172d152
rpapanicola@gmail.com <rpapanicola@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-01 20:08:52 +0000

    mise a jour - fenetre auteur flottante (revoir css formulaire) - fenetre mot flottante (mini nuage de tag) - ajout mes_fonctions.php (nuage de tags)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@12643 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ post-it_Webtoo/squelettes/mes_fonctions.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ *   +----------------------------------+
+ *    Nom des Filtres :    noop, filtre_max, coef et repeat
+ *   +----------------------------------+
+ *    Date : 23 Mars 2005
+ *    Auteur :  Pierre Andrews (mortimer.pa@free.fr)
+ *   +-------------------------------------+
+ *    Fonctions de ces filtres : ces filtres permettent
+ *   de faire un affichage variant en fonction de l'importance
+ *    de l'objet.  Vois la contrib pour plus d'informations.
+ *   +-------------------------------------+
+ *
+ * Pour toute suggestion, remarque, proposition d'ajout
+ * reportez-vous au forum de l'article :
+ * http://www.spip_contrib.net/article.php3?id_article=879
+*/
+
+function noop($texte) {
+  return '';
+}
+
+function filtre_max($texte, $id='tout') {
+  static $max = array();
+  if($max[$id] < $texte) {
+    $max[$id] = $texte;
+  }
+  return $max[$id];
+}
+
+function coef($max,$nbr,$nbrMax=6) {
+  return ceil(($nbr/$max*$nbrMax));
+}
+
+function repeat($nombre,$texte,$avant,$apres,$min = 0) {
+  if($nombre > $min) {
+    for($i=0;$i < $nombre;$i++) {
+      $texte = $avant.$texte.$apres;
+    }
+    return $texte;
+  } else
+    return '';
+}
+?>
