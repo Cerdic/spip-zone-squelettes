0bfc23d3a9dfcd904e9683ba91c922965e3b07bb
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-25 11:25:18 +0000

    Suivre le plugin CFG
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@13838 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/fonds/cfg_soyezcreateurs_layout_fonctions.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Prepare les messages d'aide de la page de configuration du plugin
-if (!defined("_ECRIRE_INC_VERSION")) return;
-include_spip('inc/presentation'); // pour compat cfg 1.0.1
-?>
