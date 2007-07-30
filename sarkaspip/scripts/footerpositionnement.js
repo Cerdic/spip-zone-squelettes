4d34fb227c6bace32fa2c6e8049fd30eee9fa291
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-30 12:39:06 +0000

    Correction des erreurs js quand l'id de la colonne de droite ou gauche est introuvable. Ajout d'un test d'existence.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@13998 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/scripts/footerpositionnement.js
+++ sarkaspip/scripts/footerpositionnement.js
@@ -1,8 +1,18 @@
 function setFooter() {
-	var gaucheHeight=document.getElementById('gauche').offsetHeight;
-	var centreHeight=document.getElementById('centre').offsetHeight;
-	var droiteHeight=document.getElementById('droite').offsetHeight;
-	var heightGDMax = Math.max(droiteHeight, gaucheHeight)
+	var gaucheHeight=0;
+	var centreHeight=0;
+	var droiteHeight=0;
+	var heightGDMax;
+
+	if (document.getElementById('gauche')) {
+		gaucheHeight=document.getElementById('gauche').offsetHeight;
+	}
+	if (document.getElementById('droite')) {
+		droiteHeight=document.getElementById('droite').offsetHeight;
+	}
+	centreHeight=document.getElementById('centre').offsetHeight;
+
+	heightGDMax = Math.max(droiteHeight, gaucheHeight)
 	if (heightGDMax>centreHeight){
 	   document.getElementById('centre').style.height=(heightGDMax)+'px';
 	}
