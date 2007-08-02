d90d6c3aab3ccf1cfe4187314683009fda5b1200
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-02 00:13:39 +0000

    On vire le quote au coin rond du javascript. On transforme le sommaire pour avoir profiter d'un blog.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@14116 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multisaisons/squelettes/nifty-bloog.js
+++ multisaisons/squelettes/nifty-bloog.js
@@ -1,6 +1,6 @@
 window.onload=function(){
 Nifty("div.boite,div.avatar,div.reponse,div.discussion,p.interne");
-Nifty("ul#minipics li,div.grande_boite,div#content,div#footer,blockquote.spip");
+Nifty("ul#minipics li,div.grande_boite,div#content,div#footer");
 Nifty("div#container,div.interforum,div.interforumgrand","big");
 Nifty("div#header","top,big");
 Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
