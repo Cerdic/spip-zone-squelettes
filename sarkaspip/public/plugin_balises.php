f3f40dece8f433817792109a46ff325c4ef6845c
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-16 14:06:12 +0000

    Encore une maj de la balise #PLUGIN. Cohérence avec SPIP du futur...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@14639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/public/plugin_balises.php
+++ sarkaspip/public/plugin_balises.php
@@ -3,87 +3,36 @@
 // Balise : #PLUGIN
 // =======================================================================================================================================
 // Auteur: Smellup
-// Fonction : retourne une info d'un plugin donne
+// Fonction : retourne une info d'un plugin actif donne
 // =======================================================================================================================================
 //
 include_spip('inc/plugin');
-function balise_PLUGIN($p) {
 
+function balise_PLUGIN_dist($p) {
 	$plugin = interprete_argument_balise(1,$p);
-	if (!isset($plugin)) {
-		$p->code = "''";
-	}
-	else {
-		$type_info = interprete_argument_balise(2,$p);
-		if (!isset($type_info)) $type_info = 'est_actif';
-		$p->code = 'calcul_info_plugin('.$plugin.', '.$type_info.')';
-	}
+	$plugin = isset($plugin) ? str_replace('\'', '"', $plugin) : '""';
+	$type_info = interprete_argument_balise(2,$p);
+	$type_info = isset($type_info) ? str_replace('\'', '"', $type_info) : '"est_actif"';
+
+	$p->code = 'calcul_info_plugin('.$plugin.', '.$type_info.')';
 	$p->interdire_scripts = false;
 	return $p;
 }
 
 function calcul_info_plugin($plugin, $type_info) {
-	$dir_tous_plugins = array();
-	$inf_tous_plugins = array();
-	$plugins_valides = array();
-	$plugins_actifs = array();
-	$plugin_infos = array();
-	
-	// Validite du prefixe
-	$dir_tous_plugins = liste_plugin_files();
-	$plugins_valides = liste_plugin_valides($dir_tous_plugins, $inf_tous_plugins);
-	if (!array_key_exists(strtoupper($plugin), $plugins_valides)) 
-		return $valeur_info;
-
-	// Determination du plugin dans la liste des plugins valides. Si plusieurs plugins de meme prefixe on choisit dans l'ordre
-	// - 1. Le plugin actif (il y en a forcement qu'un seul actif)
-	// - 2. Le plugin inactif recense dans la liste des plugins valides
+	$plugin = strtoupper($plugin);
+	$type_info = strtolower($type_info);
 	$plugins_actifs = liste_plugin_actifs();
-	if (isset($plugins_actifs[strtoupper($plugin)])) {
-		$plugin_infos = $inf_tous_plugins[$plugins_actifs[strtoupper($plugin)]['dir']];
-		$plugin_infos['est_actif'] = 'oui';
-	}
-	else {
-		$plugin_infos = $inf_tous_plugins[$plugins_valides[strtoupper($plugin)]['dir']];
-		$plugin_infos['est_actif'] = 'non';
-	}
 
-	// Renvoi de l'information demandee
-	switch (strtoupper($type_info)) {
-		case 'NOM':
-		    $valeur_info = propre($plugin_infos['nom']);
-		    break;
-		case 'VERSION':
-		    $valeur_info = $plugin_infos['version'];
-		    break;
-		case 'REVISION':
-		    $valeur_info = $plugin_infos['revision'];
-		    break;
-		case 'AUTEUR':
-		    $valeur_info = propre($plugin_infos['auteur']);
-		    break;
-		case 'ICONE':
-		    $valeur_info = $plugin_infos['icon'];
-		    break;
-		case 'DESCRIPTION':
-		    $valeur_info = propre($plugin_infos['description']);
-		    break;
-		case 'LIEN':
-			$valeur_info = formate_lien_plugin($plugin_infos['lien']);
-		    break;
-		case 'ETAT':
-		    $valeur_info = formate_etat_plugin($plugin_infos['etat']);
-		    break;
-		case 'EST_ACTIF':
-		    $valeur_info = $plugin_infos['est_actif'];
-		    break;
-		case 'TOUT':
-		    $valeur_info = serialize($plugin_infos);
-		    break;
-		default:
-			$valeur_info = '';
-	}
-	return $valeur_info;
+	if(!$plugin)
+		return serialize(array_keys($plugins_actifs));
+	if(!empty($plugins_actifs[$plugin]))
+		if($type_info == 'est_actif')
+			return $plugins_actifs[$plugin] ? 1 : 0;
+		else {
+			$plugins_valides = liste_plugin_valides(liste_plugin_files(), $inf_tous_plugins);
+			return $inf_tous_plugins[$plugins_actifs[$plugin]['dir']][$type_info];
+		}
 }
 
 function formate_lien_plugin($lien) {
@@ -117,5 +66,6 @@ function formate_etat_plugin($etat) {
 	}
 	return $ret;
 }
+
 
 ?>
