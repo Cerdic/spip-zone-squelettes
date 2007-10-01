93c53e4d996936836ff559098db73826b0f51d8b
postmaster@cent20.net <postmaster@cent20.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-01 19:47:23 +0000

    Deux mois de mise à jours mineures ... corrections de mini bugs, trois nouveaux thèmes graphiques : mapemonde, papillon et oiseau ...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@15667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ BliP/2_x/plugins/blip/couleur_theme_oiseau.php
@@ -0,0 +1,23 @@
+<?php
+	// Jeux de couleurs Thème Fleur
+$css_blip_couleur = array(
+	
+	'gris|fonce' => '#333333',
+	'gris|moyen' => '#666666',
+	'gris|clair' => '#aaaaaa',
+	'couleur|1' => '#6C9CDC',
+	'couleur|1|clair' => '#DFE4EA',
+	'couleur|2' => '#966B58',
+	'couleur|2|clair' => '#B4AD90',
+	'couleur|neutre' => '#ffffff',
+	'couleur|neutre|bis' => 'transparent',
+	'taille|espacement_bloc' => '3em',
+	'taille|geant' => '1.5em',
+	'taille|grand' => '1.25em',
+	'taille|moyen' => '1em',
+	'taille|normal' => '0.75em',
+	'taille|petit' => '0.5em',
+	'taille|bordure' => '0.25em',
+); 
+
+?>
