252dcb9e4ee90b8360db14e1f949cb89112f4240
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-03 16:01:24 +0000

    Suivre la SVN (http://trac.rezo.net/trac/spip/changeset/10491)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@15731 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs/public/composer.php
+++ soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs/public/composer.php
@@ -325,7 +325,7 @@ function calculer_hierarchie($id_rubrique, $exclure_feuille = false) {
 // http://doc.spip.org/@calcul_exposer
 function calcul_exposer ($id, $prim, $reference, $parent, $type) {
 	static $exposer;
-	static $ref_precedente;
+	static $ref_precedente =-1;
 
 	// Que faut-il exposer ? Tous les elements de $reference
 	// ainsi que leur hierarchie ; on ne fait donc ce calcul
@@ -339,6 +339,7 @@ function calcul_exposer ($id, $prim, $reference, $parent, $type) {
 			if ($type == 'id_mot')
 				$exposer['id_groupe'][$parent] = true;
 			if ($type != 'id_groupe') {
+			  if (!$parent) $parent = $principal;
 			  $a = split(',',calculer_hierarchie($parent));
 			  foreach($a as $n) $exposer['id_rubrique'][$n] = true;
 			}
