5bf0f6d4bcc08709562b5f1059babab3f324e094
jeanphilippe.bliek@gmail.com <jeanphilippe.bliek@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-19 16:34:26 +0000

    Cosmétique et validation
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@16148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Durzy/_1_9_3_Dev/squelettes/javascript/menu.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+function hover(obj){
+  if(document.all){
+    UL = obj.getElementsByTagName('ul');
+    if(UL.length > 0){
+      sousMenu = UL[0].style;
+      if(sousMenu.display == 'none' || sousMenu.display == ''){
+        sousMenu.display = 'block';
+      }else{
+        sousMenu.display = 'none';
+      }
+    }
+  }
+}
+
+function setHover(){
+  LI = document.getElementById('menu').getElementsByTagName('li');
+  nLI = LI.length;
+  for(i=0; i < nLI; i++){
+    LI[i].onmouseover = function(){
+      hover(this);
+    }
+    LI[i].onmouseout = function(){
+      hover(this);
+    }
+  }
+}
+</script>
