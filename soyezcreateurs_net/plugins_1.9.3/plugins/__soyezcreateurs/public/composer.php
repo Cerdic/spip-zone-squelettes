feaa9cb86b2c17d882a596710652b037888f5602
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-29 09:57:38 +0000

    Suivre la SVN (http://trac.rezo.net/trac/spip/changeset/10699)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@16414 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs/public/composer.php
+++ soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs/public/composer.php
@@ -32,7 +32,8 @@ include_spip('public/interfaces');
 // http://doc.spip.org/@public_composer_dist
 function public_composer_dist($squelette, $mime_type, $gram, $source, $connect) {
 
-	$nom = $mime_type . ($connect ?  "_$connect" : '') . '_' . md5($squelette);
+	$nom = $mime_type . ($connect ?  "_$connect" : '') . '_'
+		. md5($GLOBALS['spip_version_code'].' * '.$squelette);
 
 	// si squelette est deja en memoire (INCLURE  a repetition)
 	if (function_exists($nom))
@@ -45,7 +46,6 @@ function public_composer_dist($squelette, $mime_type, $gram, $source, $connect) 
 	AND lire_fichier ($phpfile, $contenu,
 	array('critique' => 'oui', 'phpcheck' => 'oui'))) 
 		eval('?'.'>'.$contenu);
-
 	if (@file_exists($fonc = $squelette . '_fonctions'.'.php')
 	OR @file_exists($fonc = $squelette . '_fonctions'.'.php3')) {
 		include_once $fonc;
@@ -460,20 +460,6 @@ function calcule_logo_document($id_document, $doubdoc, &$doublons, $flag_fichier
 		$logo = "<a href='$lien' type='$mime'>$logo</a>";
 	}
 	return $logo;
-}
-
-
-// les balises dynamiques et EMBED ont des filtres sans arguments 
-// car en fait ce sont des arguments pas des filtres.
-// Si le besoin s'en fait sentir, il faudra recuperer la 2e moitie du tableau 
-
-// http://doc.spip.org/@argumenter_balise
-function argumenter_balise($fonctions, $sep) {
-  $res = array();
-  if ($fonctions)
-		foreach ($fonctions as $f)
-			$res[] = str_replace('\'', '\\\'', str_replace('\\', '\\\\',$f[0]));
-  return ("'" . join($sep, $res) . "'");
 }
 
 // fonction appelee par la balise #NOTES
