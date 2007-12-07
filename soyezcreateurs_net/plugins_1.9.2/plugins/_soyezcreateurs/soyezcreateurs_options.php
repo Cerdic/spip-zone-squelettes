f97606d59854458357493a1e7906c0ce60033b4f
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-07 13:31:01 +0000

    Sur une info de Cédric, assujettir le cache de SPIP aux changements de configuration du site dans cfg.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@17291 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_options.php
+++ soyezcreateurs_net/plugins_1.9.2/plugins/_soyezcreateurs/soyezcreateurs_options.php
@@ -1,6 +1,9 @@
 <?php
 $GLOBALS['type_urls'] = 'propres2';
 
+// Recalculer le cache si la config du site change
+$GLOBALS['marqueur'] .= ":".md5($GLOBALS['meta']['soyezcreateurs_layout'].$GLOBALS['meta']['soyezcreateurs'].$GLOBALS['meta']['soyezcreateurs_google'].$GLOBALS['meta']['bte']); // Sur un conseil de Cedric : http://permalink.gmane.org/gmane.comp.web.spip.zone/6258
+
 define('_DUREE_CACHE_DEFAUT', 30*24*3600); // pris en compte à partir de http://trac.rezo.net/trac/spip/changeset/10121
 define('_URLS_PROPRES_MAX', 60); // pris en compte à partire de http://trac.rezo.net/trac/spip/changeset/10346 
 
