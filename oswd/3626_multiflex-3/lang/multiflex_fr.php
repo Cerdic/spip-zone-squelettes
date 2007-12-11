ccce67c808fa25cd34b68043ff4724396bd8b9d3
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-11 19:58:45 +0000

    Ajout du Squelette Multiflex-3 de OSWD.
    Mis sous forme de plugin, donc à installer dans le répertoire plugins afin de pouvoir surcharger facilement les squelettes. Il y a tout un tas d'options possibles d'une part dans CFG, d'autre part dans les noisettes.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@17369 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oswd/3626_multiflex-3/lang/multiflex_fr.php
@@ -0,0 +1,106 @@
+<?php
+
+$GLOBALS['i18n_multiflex_fr'] = array(
+
+// pour entete
+'aller_accueil' => 'Aller sur la page d\'accueil',
+'a_propos' => 'A propos',
+'contact' => 'Contact',
+'contactez_nous' => 'Contactez-nous',
+'ok' => 'GO!',
+'qui_sommes_nous' => 'Qui sommes nous ?',
+
+// pour navigation
+'navigation' => 'Navigation',
+
+// pour centre
+'a_la_une' => 'A la une',
+'autres_sites' => 'Autres sites',
+'actualites' => 'Actualit&eacute;s',
+'commentaire' => 'commentaire',
+'commentaires' => 'commentaires',
+'documents_joints' => 'Documents joints',
+'dans_rubrique' => '//',
+'dans_groupe' => '//',
+'derniers_sites' => 'Derniers sites',
+'lire_suite' => 'Lire la suite&nbsp;&raquo;',
+'mots_cles' => 'Mots cl&eacute;s',
+'sur' => '//',
+'voir_site' => 'Voir le site&nbsp;&raquo;',
+
+// pied
+'designer_page' => 'Site internet du designer',
+'habillage_de' => 'Habillage de ',
+'modifie_par' => 'Modifi&eacute; par ',
+'modifieur_page' => 'Site internet du modifieur',
+'propulse_par' => 'Propuls&eacute; par',
+'site_internet_spip' => 'Site internet de SPIP',
+
+// pour cfg_multiflex
+'afficher_nombre_commentaires' => 'Afficher le nombre de commentaires',
+'apparence_layout' => 'Choix du corps&nbsp;:',
+'apparence_header' => 'Choix de l\'ent&ecirc;te&nbsp;:',
+'apparence_navigation' => 'Choix de l\'apparence de la navigation latérale (si présente dans le corps)',
+
+'blocs' => 'Blocs',
+
+'coefficients_teinte' => 'Coefficients entre -1 et 1',
+'configuration_multiflex' => 'Configuration du Squelette Multiflex',
+'configuration_principale' => 'Configuration principale',
+'complement_nom_site' => 'Compl&eacute;ment au nom du site (&eacute;crit en petit)',
+'couleurs' => 'Couleurs',
+'couleur_bloc_bleu_titre' => 'Bleus, titre',
+'couleur_bloc_bleu_fond' => 'Bleus, fond',
+'couleur_bloc_orange_titre' => 'Oranges, titre',
+'couleur_bloc_orange_fond' => 'Oranges, fond',
+'couleur_bloc_vert_titre' => 'Verts, titre',
+'couleur_bloc_vert_fond' => 'Verts, fond',
+'couleur_actuelle' => 'Couleur actuelle',
+'couleur_liens' => 'Liens',
+'couleur_liens_survoles' => 'Liens survol&eacute;s',
+'couleur_origine' => 'Couleur d\'origine',
+
+'decoration' => 'D&eacute;coration',
+'description_entete' => 'Description dans le bandeau de l\'en-t&ecirc;te (si pr&eacute;sent dans le corps)',
+'descriptifs_optionnels' => 'Descriptifs optionnels',
+'descriptions_site' => 'Descriptions du site pour l\'en-t&ecirc;te',
+'divers' => 'Divers',
+'dominante' => 'Teinte dominante',
+
+'generale' => 'Teinte g&eacute;n&eacute;rale',
+'grille' => 'Grille',
+
+'header1' => 'Titre + bandeau + menu',
+'header2' => 'Titre + bandeau',
+'header3' => 'Titre + menu',
+'header4' => 'Bandeau + menu',
+'header5' => 'Titre',
+'header6' => 'Bandeau',
+'header7' => 'Menu',
+
+
+'layouts' => 'Headers et Layouts',
+
+'layout1' => '1 colonne',
+'layout2' => '2 colonnes',
+'layout3' => '2 colonnes invers&eacute;es',
+'layout4' => '3 colonnes',
+'layout5' => '3 colonnes invers&eacute;es',
+
+'non' => 'Non',
+'nouvelle_couleur' => 'Nouvelle couleur',
+
+'oui' => 'Oui',
+
+'positionnements_generaux' => 'Positionnements g&eacute;n&eacute;raux',
+'puces' => 'Puces',
+
+'sans_puces' => 'Sans puces',
+'sous_titre_nom_site' => 'Sous-titre au nom du site',
+
+'teintes' => 'Teintes',
+
+'valeur' => 'Valeur'
+);
+
+?>
---
+++ oswd/3626_multiflex-3/squelette_multiflex_fonctions.php
@@ -0,0 +1,214 @@
+<?php
+
+
+/*
+ * #K_RVB{240,240,240,1,1,0.7}
+ * renvoie : rvb{240,240,237}
+ * Applique un coefficient 
+ * sur une couleur rvb
+ * 
+ */ 
+function balise_KRVB($p){
+	$r = interprete_argument_balise(1, $p);
+	$v = interprete_argument_balise(2, $p);
+	$b = interprete_argument_balise(3, $p);
+	$kr = interprete_argument_balise(4, $p);
+	$kv = interprete_argument_balise(5, $p);
+	$kb = interprete_argument_balise(6, $p);
+	
+	$p->code = "calculer_balise_KRVB($r, $v, $b, $kr, $kv, $kb)";
+	return $p;
+}
+
+function calculer_balise_KRVB($r,$v,$b,$kr,$kv,$kb){
+	include_spip('inc/filtres_images');
+	
+	if ($r == 255 && $v == 255 && $b == 255) return '#ffffff';
+	if ($r == 0 && $v == 0 && $b == 0) return '#000000';
+	
+	if ($kr >  1) $kr =  1;
+	if ($kr < -1) $kr = -1;
+	if ($kv >  1) $kv =  1;
+	if ($kv < -1) $kv = -1;
+	if ($kb >  1) $kb =  1;
+	if ($kb < -1) $kb = -1;
+			
+	$r = ($kr>=0) ? $r + $kr * (255-$r) : $r + $kr * $r;
+	$v = ($kv>=0) ? $v + $kv * (255-$v) : $v + $kv * $v;
+	$b = ($kv>=0) ? $b + $kb * (255-$b) : $b + $kb * $b;
+	
+	if ($r>255) $r=255;
+	if ($v>255) $v=255;
+	if ($b>255) $b=255;
+	
+	if ($r<0) $r=0;
+	if ($v<0) $v=0;
+	if ($b<0) $b=0;
+	
+	
+	return '#'.couleur_dec_to_hex(round($r), round($v), round($b));
+}
+
+
+
+//http://www.spip-contrib.net/balise-TITRE-PARENT
+include_spip('public/balises');
+
+// Le filtre [(#ID_RUBRIQUE|titre_parent)]
+if (!function_exists('calcul_titre_parent')){
+	function calcul_titre_parent($id_rubrique) {
+		$fetch = function_exists('sql_fetch') ? 'sql_fetch' : 'spip_fetch_array';
+		if(!($id_rubrique = intval($id_rubrique))) return '';
+		$q = 'SELECT titre FROM spip_rubriques WHERE id_rubrique='.$id_rubrique;
+		if($r = spip_query($q))
+			if($row = $fetch($r))
+				return $row['titre'];
+		return '';
+	}
+}
+
+// La balise
+if (!function_exists('balise_TITRE_PARENT_dist')){
+	function balise_TITRE_PARENT_dist($p) {
+		$id_rubrique = champ_sql('id_rubrique', $p);
+		$p->code = "calcul_titre_parent(".$id_rubrique.")";
+		return $p;
+	}
+}
+
+// Positionner les filtres standards en recopiant ceux de #TITRE
+// attention, ca ne positionne pas la langue_typo (mais tant pis)
+include_spip('public/interfaces');
+global $table_des_traitements;
+if (!isset($table_des_traitements['TITRE_PARENT'])) {
+	$table_des_traitements['TITRE_PARENT'] = $table_des_traitements['TITRE'];
+}
+
+
+
+
+// balise ID_RUBRIQUE_PARENT new !
+// Le filtre [(#ID_RUBRIQUE|titre_parent)]
+/*
+if (!function_exists('calcul_id_rubrique_parent')){
+	function calcul_id_rubrique_parent($id_rubrique) {
+		$fetch = function_exists('sql_fetch') ? 'sql_fetch' : 'spip_fetch_array';
+		if(!($id_rubrique = intval($id_rubrique))) return '';
+		$q = 'SELECT titre FROM spip_rubriques WHERE id_rubrique='.$id_rubrique;
+		if($r = spip_query($q))
+			if($row = $fetch($r))
+				return $row['titre'];
+		return '';
+	}
+}*/
+
+// La balise
+if (!function_exists('balise_ID_RUBRIQUE_OBJET_dist')){
+	function balise_ID_RUBRIQUE_OBJET_dist($p) {
+		$id_rubrique = champ_sql('id_rubrique', $p);
+		$p->code = "$id_rubrique";
+		//$p->code = "'coucou'";
+		return $p;
+	}
+}
+
+
+
+
+/* balises pour doublons entre #inclure (idee _renato_) */
+
+/*
+ *
+ *  Gestion des doublons entre #inclure
+ * 
+ *  B.html , C.html :
+ *  --------------
+ *  #LOAD_DOUBLONS
+ *  <BOUCLE />
+ *  #SAVE DOUBLONS
+ * 
+ * 
+ *  A.html :
+ *  --------------
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{C}{_unique = #HASH_DOUBLONS})]
+ * 
+ * 
+ * 
+ *  Un peu plus complexe (plusieurs valeurs de doublons)
+ * 
+ *  B.html, C.html :
+ *  ----------------
+ *  [(#SET{doublons, [(#ENV{_doublons}|sinon{#DOUBLON_ALEA})]})]
+ *  #LOAD_DOUBLONS
+ *  <BOUCLE(){doublons = #GET{doublons}} />
+ *  #SAVE DOUBLONS 
+ * 
+ *  A.html :
+ *  ----------------
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})] // affiche la même chose car doublon aleatoire
+ * 
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = toto})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = toto})] // suite
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = plouf})] 
+ * 
+ *  [(#INCLURE{C}{_unique = #HASH_DOUBLONS}{_doublons = plouf})]  // suite de plouf
+ * 
+ */
+
+
+/* 
+ * Sauvegarde la table des doublons dans une globale.
+ */
+if (!function_exists('balise_SAVE_DOUBLONS')){
+	function balise_SAVE_DOUBLONS($p) {
+	  $p->code = "vide(\$GLOBALS['recherche_doublons'] = \$doublons)";
+
+	  return $p; 
+	}
+}
+
+/* 
+ * Restaure la globale dans la table des doublons.
+ */
+if (!function_exists('balise_LOAD_DOUBLONS')){
+	function balise_LOAD_DOUBLONS($p) {
+	  if(!isset($GLOBALS['recherche_doublons'])) $GLOBALS['recherche_doublons'] = array();
+	  $p->code = "vide(\$doublons = \$GLOBALS['recherche_doublons'])";
+
+	  return $p; 
+	}
+}
+
+
+/*
+ * Genere un id unique pour le tableau de doublons
+ */ 
+if (!function_exists('balise_HASH_DOUBLONS')){
+	function balise_HASH_DOUBLONS($p) {
+	  #$p->code = "md5(serialize(\$GLOBALS['recherche_doublons'])) . vide(spip_log('#GET_DOUBLONS : ' . serialize(\$GLOBALS['recherche_doublons']) , 'spip_doublons'))";
+	  $p->code = "md5(serialize(\$GLOBALS['recherche_doublons']))";
+
+	  return $p; 
+	}
+}
+
+
+/*
+ * Genere un id unique pour un doublon
+ */ 
+if (!function_exists('balise_DOUBLON_ALEA')){
+	function balise_DOUBLON_ALEA($p) {
+	  $p->code = "calculer_balise_DOUBLON_ALEA()";
+	  return $p; 
+	}
+}
+if (!function_exists('balise_DOUBLON_ALEA')){
+	function balise_DOUBLON_ALEA(){
+	  return substr(rand().rand(),0,8);
+	}
+}
+
+?>
