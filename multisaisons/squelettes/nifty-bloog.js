b8a2c61806363a8326a065c681267da8fef42376
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-11 19:35:43 +0000

    Des corrections pour ie6 dans les rubriques. Des coins carrés et ronds sur les logos d'articles dans les rubriques. Ajout d'un PS dans l'article agenda.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@17963 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multisaisons/squelettes/nifty-bloog.js
+++ multisaisons/squelettes/nifty-bloog.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-Nifty("div.boite,div.avatar,div.reponse,div.discussion,p.interne,div.interne,span.pixlogo");
+Nifty("div.boite,div.avatar,div.reponse,div.discussion,p.interne,div.interne");
 Nifty("ul#minipics li,div.grande_boite,div#content,div#footer");
 Nifty("div.interforum,div.interforumgrand","big");
 Nifty("div#header","top,big,transparent");
@@ -7,4 +7,5 @@ Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
 Nifty("div#surcontent","bottom,big");
 Nifty("#cel p","bottom,same-height");
 Nifty("#celbas p","bottom");
+Nifty("span.pixlogo","br tl,transparent");
 }
