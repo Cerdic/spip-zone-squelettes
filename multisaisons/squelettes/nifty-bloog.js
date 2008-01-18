d84e2967d1085ada6c80aaaef5b165b1c9b23e7a
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-18 23:45:46 +0000

    Un débug d'une 404 qui nous faisait perdre une seconde de chargement sur toutes les pages. Un bel agenda, avec des beaux titres typo et de jolies boîtes. Une transparence de mieux dans les boîtes pour ajouter des fonds un peu partout. Il manquait les boîtes rondes sur les logos d'articles ordinaires des forums.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@18072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multisaisons/squelettes/nifty-bloog.js
+++ multisaisons/squelettes/nifty-bloog.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.interne,span.logorond");
+Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.interne,p.interne,span.logorond","transparent");
 Nifty("ul#minipics li,div.grande_boite,div#content,div#footer");
 Nifty("div.interforum,div.interforumgrand","big");
 Nifty("div#header","top,big,transparent");
