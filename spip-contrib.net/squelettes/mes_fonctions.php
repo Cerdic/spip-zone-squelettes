2c0a95fed9c115e19a805f2b260eda06e80a8f53
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-20 20:49:26 +0000

    date_rfc822  age_style ... 2 filtres pour le backend, surement perdu lors de l'upgrade
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@18089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-contrib.net/squelettes/mes_fonctions.php
+++ spip-contrib.net/squelettes/mes_fonctions.php
@@ -44,4 +44,32 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 }
 
 
+function date_rfc822($date_heure) {
+  list($annee, $mois, $jour) = recup_date($date_heure);
+  list($heures, $minutes, $secondes) = recup_heure($date_heure);
+  $time = mktime($heures, $minutes, $secondes, $mois, $jour, $annee);
+  return gmdate("D, d M Y H:i:s +0100", $time);
+}
+
+
+function age_style($date) {
+ 	
+// $decal en secondes
+$decal = date("U") - date("U", strtotime($date));
+			 
+// 3 jours = vieux
+$decal = min(1.0, sqrt($decal/(3*24*3600)));
+			 
+// Quand $decal = 0, c'est tout neuf : couleur vive
+// Quand $decal = 1, c'est vieux : bleu pale
+$red = ceil(128+127*(1-$decal));
+$blue = ceil(130+60*$decal);
+$green = ceil(200+55*(1-$decal));
+										 
+$couleur = dec2hex($red).dec2hex($green).dec2hex($blue);
+											 
+return 'background-color: #'.$couleur.';';
+}
+
+
 ?>
