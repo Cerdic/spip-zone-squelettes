84e5d149c807fa5698db848fd35dca93df7a07fb
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-01 10:52:58 +0000

    Correction xhtml des noisettes. Utilisation de lire_fichier dans la calcul de la version du squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@18881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip_dev_21/public/sarkaspip_balises.php
+++ sarkaspip_dev_21/public/sarkaspip_balises.php
@@ -16,8 +16,8 @@ function calcul_version_squelette() {
 
 	$version = NULL;
 	
-	$contenu = spip_file_get_contents(_DIR_PLUGIN_SARKASPIP.'/plugin.xml');
-	if (preg_match('/<version>([0-9.]+)<\/version>/', $contenu, $match))
+	if (lire_fichier(_DIR_PLUGIN_SARKASPIP.'/plugin.xml', $contenu)
+	&& preg_match('/<version>([0-9.]+)<\/version>/', $contenu, $match))
 		$version .= trim($match[1]);
 
 	$revision = version_svn_courante(_DIR_PLUGIN_SARKASPIP);
