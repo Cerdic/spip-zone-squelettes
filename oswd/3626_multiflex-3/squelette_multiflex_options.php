d9753bc8385aca66cb32657b88f923071524fa25
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-04 18:07:46 +0000

    - mettre un mot pour supprimer les numéros dans mes_options (Nicolas Dubail)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@18972 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oswd/3626_multiflex-3/squelette_multiflex_options.php
+++ oswd/3626_multiflex-3/squelette_multiflex_options.php
@@ -1,3 +1,11 @@
 <?php
+/*
+ * Quelques notations que vous pouvez utiliser dans
+ * votre fichier mes_options.php
+ * 
+ * Enlever le # pour decommenter la ligne
+ */
 
+// Supprimer les numeros sur les titres
+# $GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s))';
 ?>
