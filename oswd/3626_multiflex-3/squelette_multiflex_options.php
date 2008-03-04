e510762705db43a170927a137fcae85521a33b8f
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-04 18:20:17 +0000

    - un mot pour les webmasters aussi dans les options
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@18974 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oswd/3626_multiflex-3/squelette_multiflex_options.php
+++ oswd/3626_multiflex-3/squelette_multiflex_options.php
@@ -6,6 +6,11 @@
  * Enlever le # pour decommenter la ligne
  */
 
+// ID des webmasters, habilités à executer des taches administratives
+// sans vérification de l'identité par FTP
+// Ici, auteurs 1 et 2
+# define('_ID_WEBMESTRES', '1:2');
+
 // Supprimer les numeros sur les titres
 # $GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s))';
 ?>
