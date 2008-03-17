61c596c0e909bcdc085489a484cc829523dc9d23
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-17 17:07:36 +0000

    Mise à jour de l'installation automatique de SPIP et des greffons (plugins) nécessaires à ce squelettes.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@19189 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.3/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_1.9.3/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -5,38 +5,36 @@ svn checkout svn://zone.spip.org/spip-zone/_squelettes_/gribouille
 #Installation des plugins
 mkdir plugins
 cd plugins
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/compat193
+svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/acronymes
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/agenda/1_9_3 ./agenda/
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/balise_session
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/autorite
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/barre_typo_generalisee
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/barre_typo_v2 
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/enluminures_typographiques_v2
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/boutonstexte
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/gestion_documents
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/recherche_etendue
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/sitemap
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/typo_exposants
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/typo_guillemets
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/w3c_go_home
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/widget_calendar
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/forms/forms_et_tables_1_9_1
-#svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/thickbox
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/sedna
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/clevermail/1_9_2
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/compat193
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/couteau_suisse
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/crayons
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/enluminures_typographiques_v2
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/fonctions_images
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/forms/forms_et_tables_1_9_1
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/Lecteur_multimedia
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/sedna
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/sitemap
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/thickbox2
+svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/widget_calendar
 
 
 cd ..
 
 #Installation du squelette SoyezCreateur : il faut le faire dans un sous dossier
 #car sinon, conflit avec le .svn de SPIP
-svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.2/ ./__sc/
+svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.3/ ./__sc/
 #Deplacement du squelette au bon endroit
 mv ./__sc/htaccess.txt ./.htaccess
-mv ./__sc/plugins/_soyezcreateurs ./plugins/_soyezcreateurs
-svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.3/ ./__sc193/
-mv ./__sc193/plugins/__soyezcreateurs ./plugins/__soyezcreateurs
 
 #Effacer ses traces
 #rm -r ./__sc
