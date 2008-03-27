906e853322704efe1b85e6cfa31f92acedbc56fb
jeanphilippe.bliek@gmail.com <jeanphilippe.bliek@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-27 11:19:52 +0000

    Mise en conformité des fonds cfg pour pouvoir porter les squelettes en d'autres langues. (Début...)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@19577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Durzy/_1_9_3_Dev/squelettes/lang/durzy_fr.php
@@ -0,0 +1,54 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// description du plugin
+'description_durzy' => '
+	<h4>Configuration des squelettes Durzy</h4>
+	<p>Utilisez cette page pour configurer l&rsquo;apparence des squelettes.<br />
+		Assurez-vous d&rsquo;avoir activ&eacute; le plugin jQuery avec la biblioth&egrave;que "jquery.ui-1.0" afin d&rsquo;obtenir un affichage correct des onglets
+	</p>
+	<br />
+	<a href="http://www.durzy-spip.fr/spip.php?article1" class="spip_out">Documentation provisoire</a>',
+
+'description_durzy_squelettes' => '
+	<p>
+		Ce sont des squelettes plut&ocirc;t orient&eacute;s &eacute;tablissement scolaire, m&ecirc;me s&rsquo;ils ont &eacute;t&eacute; adopt&eacute;s pour d&rsquo;autres usages (associations, club etc, ...). 
+		<a href="http://www.durzy-spip.fr/spip.php?article2" class="spip_out">Voir quelques exemples.</a>
+	</p>
+	
+	<p>Cette nouvelle version des squelettes Durzy s&rsquo;appuie notamment sur 2 plugins (dont la pr&eacute;sence est obligatoire) :</p>
+	<ul>
+		<li>Crayons offre la possibilit&eacute; d&rsquo;&eacute;dition directement depuis l&rsquo;interface publique.</li>
+		<li>Cfg permet un param&eacute;trage complet de l&rsquo;interface (jeu de couleur, choix des &eacute;l&eacute;ments graphiques, largeur du site,...) &agrave; l&rsquo;aide d&rsquo;un formulaire dans l&rsquo;interface priv&eacute;e.</li>
+	</ul>
+
+	<p> D&rsquo;autres plugins (optionnels) pourront enrichir les fonctionnalit&eacute;s du site avec bonheur : </p>
+	<ul>
+		<li>Couteau suisse offre de multiples petits r&eacute;glages fort utiles.</li>
+		<li>Acc&egrave;s restreint par groupe pour d&eacute;finir des zones rserv&eacute;es (aux professeurs par exemple).</li>
+		<li>Autorit&eacute; pour modifier certains droits par d&eacute;faut des utilisateurs.</li>
+		<li>Thickbox pour l&rsquo;affichage des images du portfolio.</li>
+	</ul>',
+
+'description_cadres' => '
+<h4>Configuration des cadres</h4>
+<p>Ici vous pouvez modifier l&rsquo;organisation, la position, et la forme des cadres des colonnes gauche et droite des squelettes.</p>
+<p><i>Fonctionnel sur la page sommaire uniquement pour l&rsquo;instant.</i></p>
+En attendant la documentation sur <a href="http://www.spip-contrib.net">Spip-Contrib</a>, consultez le <a href="http://www.durzy-spip.fr/spip.php?article1"> wiki</a>',
+
+'description_gen' => '
+<h4>Configuration g&eacute;n&eacute;rale</h4>
+<p>Choisissez ici l&rsquo;apparence g&eacute;n&eacute;rale de votre site.</p>
+En attendant la documentation sur <a href="http://www.spip-contrib.net">Spip-Contrib</a>, consultez le <a href="http://www.durzy-spip.fr/spip.php?article1"> wiki</a>',
+
+'description_couleurs' => '
+<h4>Configuration des couleurs</h4>
+<p>Coloriez ici les squelettes Durzy de votre site.</p>
+En attendant la documentation sur <a href="http://www.spip-contrib.net">Spip-Contrib</a>, consultez le <a href="http://www.durzy-spip.fr/spip.php?article1"> wiki</a>',
+
+'description_mail' => 'L&rsquo;adresse mail pour &ecirc;tre contact&eacute; via la barre d&rsquo;outil.',
+'mail' => 'Courriel',
+'desc_mail' => 'Le courriel est l&rsquo;adresse de courrier electronique &agrave; laquelle vous souhaitez que vos visiteurs puissent vous contacter via la barre d&rsquo;outil.',
+
+);
+?>
