dc530a4791b902c34c4be26ce3e64d321d5892c0
sarkarchimed@gmail.com <sarkarchimed@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-27 19:38:43 +0000

    Un petit bug sous IE : le menu déroulant ne se déroulait plus depuis les modifs d'Eric
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@20205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip_dev_21/scripts/menuderoulant.js
+++ sarkaspip_dev_21/scripts/menuderoulant.js
@@ -12,8 +12,8 @@ function hout(obj) {
 	}
 }
 function setHover(){
-	if (document.getElementById('nav1')) {
-		LI = document.getElementById('nav1').getElementsByTagName('li');
+	if (document.getElementById('menu_rubriques')) {
+		LI = document.getElementById('menu_rubriques').getElementsByTagName('li');
 		nLI = LI.length;
 		for(i=0; i < nLI; i++){
 			LI[i].onmouseover = function(){hover(this);}
