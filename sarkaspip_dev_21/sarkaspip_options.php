ecee30b209aa64e38d96f78f09474392d2f29379
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-25 12:41:09 +0000

    Ajax pour le plan du site et nettoyage du code. En particulier, la partie vive de la page doit en général etre incluse dans un div avec un id et une classe (<div id="plan" class="plan" à l'intérieur de content et donc incluant la pagination)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@20665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip_dev_21/sarkaspip_options.php
+++ sarkaspip_dev_21/sarkaspip_options.php
@@ -1,6 +1,6 @@
 <?php
 // Liste des rubriques specialisees standard du squelette
-// Pour ajouter des rubriques perso, dŽfinir de la mme faon les constantes _PERSO_XXX
+// Pour ajouter des rubriques perso, definir de la meme facon les constantes _PERSO_XXX
 // dans le fichier mes_options.php
 define(_SARKASPIP_MOT_RUBRIQUES_SPE, 'agenda:galerie:annonce:herbier');
 define(_SARKASPIP_TYPE_RUBRIQUES_SPE, 'config:motcle:motcle:config');
