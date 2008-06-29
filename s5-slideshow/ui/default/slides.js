a7b5fc7ca60cecc29e6d9e014dc2dc10bc9ad05f
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-29 11:43:28 +0000

    jquerisation  * slideLabel()
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@21165 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- s5-slideshow/ui/default/slides.js
+++ s5-slideshow/ui/default/slides.js
@@ -73,30 +73,15 @@ function nodeValue(node) {
 }
 
 function slideLabel() {
-	var slideColl = GetElementsWithClassName('*','slide');
-	var list = document.getElementById('jumplist');
-	smax = slideColl.length;
-	for (var n = 0; n < smax; n++) {
-		var obj = slideColl[n];
-
-		var did = 'slide' + n.toString();
-		obj.setAttribute('id',did);
-		if (isOp) continue;
-
-		var otext = '';
-		var menu = obj.firstChild;
-		if (!menu) continue; // to cope with empty slides
-		while (menu && menu.nodeType == 3) {
-			menu = menu.nextSibling;
-		}
-	 	if (!menu) continue; // to cope with slides with only text nodes
-
-		var menunodes = menu.childNodes;
-		for (var o = 0; o < menunodes.length; o++) {
-			otext += nodeValue(menunodes[o]);
-		}
-		list.options[list.length] = new Option(n + ' : '  + otext, n);
-	}
+	$('.slide').each(function(i) {
+	    $(this).attr('id','slide'+i);
+        $('#jumplist').append('<option value="'+i+'">'+i+' : '+$(this).children('h1:eq(0)').text()+'</option>');
+        smax = i;
+	});
+    
+    //nombre de slide
+	// +1 : ne pas oublier le slide0
+    smax += 1;
 }
 
 function currentSlide() {
@@ -528,8 +513,11 @@ function trap(e) {
 }
 
 function startup() {
+    //charge les configuration meta
 	defaultCheck();
-	if (!isOp) createControls();
+	//initialise la barre de navigation
+	createControls();
+	//initialise la liste des slides
 	slideLabel();
 	fixLinks();
 	externalLinks();
