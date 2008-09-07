869a1f770617089dcd5d81cf4437ea778ad29c70
m.spiprezo@team-santonum.com <m.spiprezo@team-santonum.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-07 10:06:23 +0000

    Certains fichiers rassemblés n'avaient pas été versionnés donc pas ajoutés sur le SVN. kDiff3 c'est génial pour comparer des fichiers !!!
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@22411 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jaycee/trunk/jayceeWeb/plugins/jaycee/styles/js/wz_tooltip_config_jaycee.js
@@ -0,0 +1,10 @@
+
+//===================  GLOBAL TOOPTIP CONFIGURATION  =========================//
+config. Duration		= 5000 		// Time span in ms after which the tooltip disappears; 0 for infinite duration
+config. FadeIn			= 500 		// Fade-in duration in ms, e.g. 400; 0 for no animation
+config. FadeOut 		= 500
+config. FollowMouse	= false		// false or true - tooltip follows the mouse
+config. Shadow			= true 	// false or true
+config. Sticky			= true 	// Do NOT hide tooltip on mouseout? false or true
+
+
