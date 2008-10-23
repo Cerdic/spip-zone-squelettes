ff14733412cb5e73112cf483d58957abeb9e59cc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-23 18:41:45 +0000

    "ne pas oublier les ' dans le test defined(_DIR_PLUGIN_xxx) : tant que _DIR_PLUGIN_xxx n'est pas definie c'est equivalent (mais plus lent) mais apres ca bug !"
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@23586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/public/sarkaspip_criteres.php
+++ sarkaspip3/public/sarkaspip_criteres.php
@@ -6,7 +6,7 @@
 // Fonction : permet d'éviter une erreur quand le plugin ACCES RESTREINT n'est pas actif. Le critère ne fait rien
 // =======================================================================================================================================
 //
-if (!defined(_DIR_PLUGIN_ACCESRESTREINT)) {
+if (!defined('_DIR_PLUGIN_ACCESRESTREINT')) {
 	function critere_tout_voir_dist($idb, &$boucles, $crit) {
 		return NULL;
 	}
