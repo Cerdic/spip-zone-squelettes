679ed45b881ce05c475311c9b30390de56a846b9
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-06 20:08:41 +0000

    Une correction rapide pour éviter d'avoir le bouton menant à la page des messages du forum
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@24062 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/sarkaspip_options.php
+++ sarkaspip3/sarkaspip_options.php
@@ -2,9 +2,9 @@
 // Liste des rubriques specialisees standard du squelette
 // Pour ajouter des rubriques perso, definir de la meme facon les constantes _PERSO_XXX
 // dans le fichier mes_options.php
-define('_SARKASPIP_MOT_SECTEURS_SPECIALISES', 'agenda:galerie:annonce:herbier');
-define('_SARKASPIP_TYPE_SECTEURS_SPECIALISES', 'config:config:config:config');
-define('_SARKASPIP_FOND_SECTEURS_SPECIALISES', 'sarkaspip_agenda:sarkaspip_galerie:sarkaspip_noisettes:sarkaspip_herbier');
+define('_SARKASPIP_MOT_SECTEURS_SPECIALISES', 'agenda:galerie:annonce:herbier:forum');
+define('_SARKASPIP_TYPE_SECTEURS_SPECIALISES', 'config:config:config:config:motcle');
+define('_SARKASPIP_FOND_SECTEURS_SPECIALISES', 'sarkaspip_agenda:sarkaspip_galerie:sarkaspip_noisettes:sarkaspip_herbier:-');
 
 // Liste des donnees de configuration du squelette non CFG
 // -- Pour les documents joints et portfolio d'images
