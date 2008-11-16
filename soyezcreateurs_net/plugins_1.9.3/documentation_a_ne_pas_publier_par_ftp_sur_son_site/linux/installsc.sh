79795098bed0d9b3859379b9f7e689dd37028dd1
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-16 21:09:04 +0000

    Limiter l'impact sur la zone en rajoutant un peu de respirations.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@24396 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_1.9.3/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_1.9.3/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -1,41 +1,66 @@
+#! /bin/sh
 #Installation de SPIP
 svn checkout svn://trac.rezo.net/spip/branches/spip-2.0.0 ./
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_squelettes_/gribouille
+sleep 1;
 #Si besoin de la mutualisation, decomenter la ligne suivante
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/mutualisation
+#sleep 1;
 
 #Installation des plugins
 mkdir plugins
 cd plugins
 svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.3/plugins/__soyezcreateurs
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/acronymes
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/agenda/2_0_0 ./agenda/
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/autorite
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/barre_typo_generalisee
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/barre_typo_v2 
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/boutonstexte
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/clevermail/1_9_3 ./clevermail/
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/couteau_suisse
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/crayons
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/enluminures_typographiques_v2
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/fonctions_images
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/forms/forms_et_tables_1_9_3 ./forms_et_tables/
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/Lecteur_multimedia
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_test_/sedna
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/sitemap
+sleep 1;
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/thickbox2
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/nyroceros
+sleep 1;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/spip-bonux-2
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/_stable_/woopra
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/_dev_/notation
+sleep 1;
 
 cd ..
 
 #Installation du squelette SoyezCreateur : il faut le faire dans un sous dossier
 #car sinon, conflit avec le .svn de SPIP
 svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_1.9.3/ ./__sc/
+sleep 1;
 #Deplacement du squelette au bon endroit
 mv ./__sc/htaccess.txt ./.htaccess
 
@@ -46,7 +71,7 @@ mv ./__sc/htaccess.txt ./.htaccess
 mkdir squelettes
 
 #mettre les droits idoines pour les dossiers
-chmod 777 config
-chmod 777 IMG
-chmod 777 local
-chmod 777 tmp
+chmod 755 config
+chmod 755 IMG
+chmod 755 local
+chmod 755 tmp
