e040e8091419006644de955f7ba03e31254a2357
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-19 17:02:44 +0000

    Fin de préparation d'eva_bonus
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@24465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_bonus/lang/evabonus_fr.php
+++ eva-web/4_0/eva_bonus/lang/evabonus_fr.php
@@ -6,10 +6,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'lieu'=>"Lieu d'application",
 'descriptif'=>"Descriptif",
 'auteur'=>"Auteur(s)",
-'evabonus_nom'=>"eva-bonus",
+'evabonus_nom'=>"EVA-bonus",
 'nom_plugin_bonus' => "EVA-bonus : des noisettes suppl&eacute;mentaires pour EVA-web",
 'descriptif_plugin_bonus' => "<p>Ce plugin contient des noisettes (bouts de squelettes) suppl&eacute;mentaires non incluses dans la distribution de base EVA-web et que vous pouvez ajouter dans vos pages gr&acirc;ce au module d'insersion de noisettes personnelles.</p>
-<p>Notez le nom de la noisette qui vous int&eacute;sse et son domaine d'application avant de l'inscrire dans la page <em>structure</em> du plugin eva-habillage</p>",
+<p>Notez le nom de la noisette qui vous int&eacute;resse et son domaine d'application avant de l'inscrire dans la page <em>structure</em> du plugin eva-habillage.</p>
+<p>Les noisettes disponibles dans ce plugin sont le fruit d'une mutualisation entre les utilisateurs d'eva-web.<br />N'h&eacute;sitez pas &agrave; proposer vos propres noisettes personnelles !</p>",
+'evahabillage_block_evabonus'=>"Vous pouvez ins&eacute;rer des noisettes suppl&eacute;mentaires fournies par le plugin eva-bonus. En voici la liste (&agrave; ins&eacute;rer dans les parties <em>Ins&eacute;rer un squelette personnel ...</em>) :",
 
 );
 ?>
