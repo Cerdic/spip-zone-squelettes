338040cc350aee07fc188b36ccbbe4a75e929512
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-27 22:37:45 +0000

    Un bloc formulaire de recherche pour eva_bonus
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@24701 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_bonus/inc/evabonus.php
+++ eva-web/4_0/eva_bonus/inc/evabonus.php
@@ -3,6 +3,7 @@ function eva_bonus_tab() {
 $retour=array(
 array('menu_tout_deplie','sommaire rubrique article breve site auteur','Un menu de navigation enti&egrave;rement d&eacute;pli&eacute;','Olivier Gautier'),
 array('nuage_mot_cle','sommaire','Ajoute un nuage de mots cl&eacute; (plugin nuage indispensable)','Olivier Gautier'),
+array('bloc_recherche','sommaire rubrique article breve site auteur','Bloc formulaire de recherche','Olivier Gautier'),
 
 );
 return $retour;
