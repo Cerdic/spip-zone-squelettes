26eb63e839b2bdd836aaec50061fedc8e17c7c1d
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-06 16:58:49 +0000

    Référence à eva_news dans eva_bonus
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@24995 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_bonus/inc/evabonus.php
+++ eva-web/4_0/eva_bonus/inc/evabonus.php
@@ -4,6 +4,7 @@ $retour=array(
 array('menu_tout_deplie','sommaire rubrique article breve site auteur','Un menu de navigation enti&egrave;rement d&eacute;pli&eacute;','Olivier Gautier'),
 array('nuage_mot_cle','sommaire','Ajoute un nuage de mots cl&eacute; (plugin nuage indispensable)','Olivier Gautier'),
 array('bloc_recherche','sommaire rubrique article breve site auteur','Bloc formulaire de recherche','Olivier Gautier'),
+array('articles_news','sommaire','Ajoute un bloc des nouveaut&eacute;s avec effet JavaScript d&eacute;filant (plugin eva_news indispensable)','Samuel Bocharel'),
 
 );
 return $retour;
