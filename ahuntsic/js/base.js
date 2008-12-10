fedb1998af9e8a6cdbf212f233cd6a36455b319f
andre@alternatives.ca <andre@alternatives.ca@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-10 08:43:18 +0000

    L'agenda et le resume (archives) du site ne se dépliait plus. On simplifie : on déploie tout.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@25061 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ahuntsic/js/base.js
+++ ahuntsic/js/base.js
@@ -1,13 +1,3 @@
-function deplier(objet){
-	d = document.getElementById(objet);
-	var etat = d.style.display;
-	if (etat!='none') {
-	d.style.display = 'none';
-	} else {
-	d.style.display = 'block';
-	}
-}
-
 /* si le plugin iSPIP pour iPhone est active */
 function iPhoneAlert() {
 	if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))){
