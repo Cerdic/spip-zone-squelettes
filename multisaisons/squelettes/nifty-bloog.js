46b47c5d92cdfc74c3bb0516f937f5a5e8885e80
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-12 23:41:25 +0000

    Bienvenue au bébé.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@25171 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multisaisons/squelettes/nifty-bloog.js
+++ multisaisons/squelettes/nifty-bloog.js
@@ -1,10 +1,12 @@
 window.onload=function(){
 Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.interne,p.interne,span.logorond","transparent");
-Nifty("ul#minipics li,div.grande_boite,div#content,div#footer","transparent");
+Nifty("ul#minipics li,div.grande_boite,div#content,div#footer,div.boite_accueil","transparent");
 Nifty("div.interforum,div.interforumgrand","big,transparent");
 Nifty("div#header","top,big,transparent");
 Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
 Nifty("div#surcontent","bottom,big");
 Nifty("#cel p","bottom,same-height");
 Nifty("#celbas p","bottom");
+Nifty("span.button","small");
+Nifty("div.ui-tabs-panel");
 }
