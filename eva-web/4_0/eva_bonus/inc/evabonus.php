0de761631a2e138216f168341ef125991a861f51
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-01 22:41:59 +0000

    Il faut indiquer à EVA-bonus que les headers de la noisette boussole n'existent plus.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@25674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_bonus/inc/evabonus.php
+++ eva-web/4_0/eva_bonus/inc/evabonus.php
@@ -7,8 +7,7 @@ array('bloc_recherche','sommaire rubrique article breve site auteur','Bloc formu
 array('eva_topten','sommaire rubrique article breve site auteur','Les dix articles les mieux not&eacute;s (plugin notation indispensable)','Olivier Gautier'),
 array('eva_topten_breves','sommaire rubrique article breve site auteur','Les dix br&egrave;ves les mieux not&eacute;es (plugin notation indispensable)','Olivier Gautier'),
 array('eva_topten_forums','sommaire rubrique article breve site auteur','Les dix forums les mieux not&eacute;s (plugin notation indispensable)','Olivier Gautier'),
-array('boussole','sommaire rubrique article breve site auteur entete pied','Un condens&eacute; des sites de r&eacute;f&eacute;rence de la galaxie SPIP (&agrave; accompagner par les headers)','Jacques Jermer, compl&eacute;t&eacute; et modifi&eacute; par Olivier Gautier'),
-array('headers_boussole','headers','Headers pour les noisettes boussole','Jacques Jermer, compl&eacute;t&eacute; et modifi&eacute; par Olivier Gautier'),
+array('boussole','sommaire rubrique article breve site auteur entete pied','Un condens&eacute; des sites de r&eacute;f&eacute;rence de la galaxie SPIP','Jacques Jermer, compl&eacute;t&eacute; et modifi&eacute; par Olivier Gautier'),
 );
 return $retour;
 }
