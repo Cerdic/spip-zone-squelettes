4db850d18a06462fa22bc518979e8e989af2b2b5
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-03 23:55:41 +0000

    Des noisettes météo (utilisation de Rainette) dans eva-bonus : - 4 noisettes disponibles - configuration du code commune et des jours à afficher dans la page privée d'eva-bonus - ajout de la dépendance à eva-habillage dans plugin.xml (necessite) Reste les langues qu'il faudra traiter correctement ultérieurement.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@25722 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_bonus/exec/eva_bonus.php
+++ eva-web/4_0/eva_bonus/exec/eva_bonus.php
@@ -36,7 +36,46 @@ function exec_eva_bonus(){
 	<td style="text-align:center;">'.$tab[2].'</td>
 	<td style="text-align:center;">'.$tab[3].'</td></tr>';
 	}
-	echo '</table>';
+	echo '</table><br style="height:40px;" />';
+	
+	/** Gestion des noisettes meteo **/
+	$req_meteo=sql_select('nom_image','spip_eva_habillage_images',"type='evabonus' AND nom_habillage='Defaut' AND nom_div='code_commune'");
+	$tab_meteo=sql_fetch($req_meteo);
+	$code_commune=$tab_meteo['nom_image'];
+	if ($_POST['code_commune']) {
+		sql_delete('spip_eva_habillage_images',"nom_habillage = 'Defaut' AND type='evabonus' AND nom_div='code_commune'");
+		sql_insertq('spip_eva_habillage_images',array('nom_habillage' => 'Defaut','type' => 'evabonus','nom_div' => 'code_commune','nom_image' => $_POST['code_commune']));
+		$code_commune=$_POST['code_commune'];
+	}
+	$req_meteo=sql_select('nom_image','spip_eva_habillage_images',"type='evabonus' AND nom_habillage='Defaut' AND nom_div='jours_prevision'");
+	$tab_meteo=sql_fetch($req_meteo);
+	$jours_prevision=$tab_meteo['nom_image'];
+	if ($_POST['jours_prevision']) {
+		sql_delete('spip_eva_habillage_images',"nom_habillage = 'Defaut' AND type='evabonus' AND nom_div='jours_prevision'");
+		sql_insertq('spip_eva_habillage_images',array('nom_habillage' => 'Defaut','type' => 'evabonus','nom_div' => 'jours_prevision','nom_image' => $_POST['jours_prevision']));
+		$jours_prevision=$_POST['jours_prevision'];
+	}
+	
+	echo debut_cadre_trait_couleur('../'._DIR_PLUGIN_EVABONUS.'img_pack/rainette.png', true,'','<div style="text-align:center;">Utilisation des noisettes de m&eacute;t&eacute;o</div>');
+	echo "<form method='post' action='".generer_url_ecrire("eva_bonus")."'>Afin d'utiliser les noisettes de m&eacute;t&eacute;o, il vous faut :
+	<ul><li> installer et activer le plugin <a href='http://plugins.spip.net/rainette'>Rainette</a></li>
+	<li> indiquer ici le code commune concern&eacute;.</li></ul>";
+	echo "Pour obtenir le code commune correspondant &agrave; votre commune (la grande ville la plus proche), entrez l'URL suivante dans votre navigateur : http://xoap.weather.com/search/search?where=ma_ville";
+	echo "<br />&nbsp;<br /><center>Code commune : ";
+	echo '&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="code_commune" value="'.$code_commune.'" size="8"><br />&nbsp;<br />';
+	echo 'Concernant la noisette de pr&eacute;vision sur plusieurs jours, combien de jours faut-il afficher ? ';
+	echo "<select name='jours_prevision'>";
+		for ($i=1;$i<=14;$i++) {
+			echo '<option value="'.$i.'" ';
+			if ($i==$jours_prevision) {echo 'selected';}
+			echo '>'.$i.'</option>';
+		}
+	echo '</select>';
+	echo "<br />&nbsp;<br />";
+	echo "<input type='submit' value='Valider' /></center>";
+	echo "</form>";
+	echo fin_cadre_trait_couleur(true);
+	/** Fin meteo **/
 	
 	echo fin_gauche(), fin_page();
 }
--- eva-web/4_0/eva_bonus/inc/evabonus.php
+++ eva-web/4_0/eva_bonus/inc/evabonus.php
@@ -8,6 +8,10 @@ array('eva_topten','sommaire rubrique article breve site auteur','Les dix articl
 array('eva_topten_breves','sommaire rubrique article breve site auteur','Les dix br&egrave;ves les mieux not&eacute;es (plugin notation indispensable)','Olivier Gautier'),
 array('eva_topten_forums','sommaire rubrique article breve site auteur','Les dix forums les mieux not&eacute;s (plugin notation indispensable)','Olivier Gautier'),
 array('boussole','sommaire rubrique article breve site auteur entete pied','Un condens&eacute; des sites de r&eacute;f&eacute;rence de la galaxie SPIP','Jacques Jermer, compl&eacute;t&eacute; et modifi&eacute; par Olivier Gautier'),
+array('meteo_actuelle','sommaire rubrique article breve site auteur','Bloc affichant la m&eacute;t&eacute;o en temps r&eacute;el','Olivier Gautier'),
+array('meteo_infos','sommaire rubrique article breve site auteur','Bloc affichant les informations g&eacute;ographiques de la commune (longitude et latitude)','Olivier Gautier'),
+array('meteo_jour_nuit','sommaire rubrique article breve site auteur','Bloc affichant les pr&eacute;visions m&eacute;t&eacute;o des deux demi-journ&eacute;es du lendemain','Olivier Gautier'),
+array('meteo_previsions','sommaire rubrique article breve site auteur','Bloc affichant les pr&eacute;visions m&eacute;t&eacute;o des jours &agrave; venir','Olivier Gautier'),
 );
 return $retour;
 }
