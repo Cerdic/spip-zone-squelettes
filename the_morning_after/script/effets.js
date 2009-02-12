d665da94a04f4d72f32dd124417b040ebd839dd7
ashaszin@gmail.com <ashaszin@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-12 17:42:15 +0000

    gasp, damned, le script JS... et la zone a du mal...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@26546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ the_morning_after/script/effets.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+
+$("#formulaire_ecrire_auteurnew").hide();
+
+$(".email_auteur").click(function() {
+ 	$("#formulaire_ecrire_auteurnew").slideToggle();
+ });
+ 
+});
+
