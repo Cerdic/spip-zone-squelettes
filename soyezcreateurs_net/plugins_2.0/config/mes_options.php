dfb54a6b93b1740a2e8b660644db6fe1ee9b3059
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-22 17:22:45 +0000

    Pour pouvoir faire une installation qui marche facilement (sous réserve bien sûr d'avoir le rewriterules activées sur le serveur apache, ce qui n'est pas le cas par défaut sur un poste de travail personnel — WAMP ou LAMP). Fichiers vides pour les CSS de surcharge personnelles.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@26857 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ soyezcreateurs_net/plugins_2.0/config/mes_options.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['quota_cache'] = 200;
+$GLOBALS['type_urls'] = 'propres2';
+include_spip('inc/vieilles_defs'); // Nécessaire pour les vieux plugins
+define('_ID_WEBMESTRES', '1:2:3:4'); // Pour que les premiers auteurs soient aussi des Webmestre (accès à la config de SoyezCreateurs)
+?>
