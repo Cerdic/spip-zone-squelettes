8c0842d3ae6edd58fc24974010103821b82e9b3e
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-24 21:30:37 +0000

    On suit un petit changement de nyroceros
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@27507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/fonds/cfg_sarkaspip_plugins_fonctions.php
+++ sarkaspip3/fonds/cfg_sarkaspip_plugins_fonctions.php
@@ -15,7 +15,7 @@ function cfg_sarkaspip_plugins_post_traiter($cfg){
 
 	if ($cfg->val['config_nyroceros']=='sarkaspip') {
 		ecrire_config('nyroceros/', array(	'traiter_toutes_images'=>_SARKASPIP_CONFIG_NYROCEROS_TOUT,
-											'selecteur_image'=>_SARKASPIP_CONFIG_NYROCEROS_IMAGE,
+											'selecteur_commun'=>_SARKASPIP_CONFIG_NYROCEROS_IMAGE,
 											'selecteur_galerie'=>_SARKASPIP_CONFIG_NYROCEROS_GALERIE,
 											'installer_diapo_auto'=>_SARKASPIP_CONFIG_NYROCEROS_DIAPORAMA,
 											'preload'=>_SARKASPIP_CONFIG_NYROCEROS_PRELOAD,
