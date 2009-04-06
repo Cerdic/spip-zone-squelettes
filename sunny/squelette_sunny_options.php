10b6a3cf0270d746dadba39f640985b23067ab95
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-06 11:56:48 +0000

    - passage du squelette Sunny en SPIP 2.0
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@27662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sunny/squelette_sunny_options.php
+++ sunny/squelette_sunny_options.php
@@ -3,6 +3,5 @@
 	$GLOBALS['fin_intertitre'] = "</h2>";
 	
 	# suppression automatique de tous les numéros des titres
-	$GLOBALS['table_des_traitements']['TITRE'][]= 'supprimer_numero(%s)'; 
-	
+	$GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s))'; 
 ?>
