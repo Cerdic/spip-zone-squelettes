e65afd187f9014335279fb6dfdde8a5506a5c145
vxl@noos.fr <vxl@noos.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-02 18:17:58 +0000

    Ajout du squelette "Messages", modification dela page Auteur et e la pagination.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@28352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-projets/lang/local_fr.php
@@ -0,0 +1,15 @@
+<?php
+// Fichier de langue spécifique au squelette
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'accueil_site' => 'Accueil',
+'annuaire' => 'Annuaire',
+'calendrier' => 'Agenda',
+'documents' => 'Documents',
+'messages' => 'Messages'
+);
+
+?>
