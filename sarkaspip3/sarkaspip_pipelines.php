75e7a714a2a1b920175209c742d87c95f82c99f5
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-04 20:42:34 +0000

    Correction du onload, ticket #36 pour toutes les pages du squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@28391 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/sarkaspip_pipelines.php
+++ sarkaspip3/sarkaspip_pipelines.php
@@ -1,8 +1,25 @@
 <?php
-
+// Pipeline insert_head public
 function SarkaSpip_insert_head($flux){
-	$flux .='<script src="'.url_absolue(find_in_path('scripts/menu_deroulant.js')).'" type="text/javascript"></script>';
+	// Recuperation des parametres cfg sur le menu des rubriques
+	$position = lire_config('sarkaspip_menus/position_rubriques');
+	if (!$position) $position = 1;
+	$modele = lire_config('sarkaspip_menus/modele_rubriques');
+	if (!$modele) $modele = 1;
+
+	// Si le menu des rubriques est deroulant dans le bandeau
+	if (($position == 5) && ($modele == 1))
+		$flux .='<script src="'.url_absolue(find_in_path('scripts/menu_deroulant.js')).'" type="text/javascript"></script>';
+
+	// Si le menu des rubriques est deroulant dans la colonne navigation
+	if (($position == 1) && ($modele == 1))
+		$flux .= 
+			'<script type="text/javascript">
+				jQuery(document).ready(function() {
+					setHover()
+				});
+			</script>';
+
 	return $flux;
 }
 ?>
-
