ad3e1bd4712909937198d73164409149f1a0666b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-05 15:38:10 +0000

    Lot de corrections pour le mnu horizontal des rubriques
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@28413 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/scripts/menu_deroulant.js
+++ sarkaspip3/scripts/menu_deroulant.js
@@ -7,7 +7,7 @@
  */
 if (jQuery.browser.msie) {
 $(document).ready(function(){
-	$('#nav li').hover(
+	$('.menu_horiz li').hover(
 		function(){$(this).addClass('sfhover')},
 		function(){$(this).removeClass('sfhover')}
 	);
