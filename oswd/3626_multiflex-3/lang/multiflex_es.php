70321baab54d4fddd9d0b49427d93b699e6a718f
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-11 18:02:50 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29126 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oswd/3626_multiflex-3/lang/multiflex_es.php
@@ -0,0 +1,121 @@
+<?php
+
+$GLOBALS['i18n_multiflex_es'] = array(
+
+// pour entete
+'aller_accueil' => 'Volver a la portada',
+'a_propos' => 'Acerca de',
+'contact' => 'Contacto',
+'contactez_nous' => 'Cont&aacute;ctenos',
+'ok' => 'IR!',
+'qui_sommes_nous' => 'Quienes somos?',
+
+// pour navigation
+'navigation' => 'Navegaci&oacute;n',
+
+// pour centre
+'a_la_une' => 'En portada',
+'autres_sites' => 'Otros sitios',
+'actualites' => 'Novedades',
+'commentaire' => 'comentario',
+'commentaires' => 'comentarios',
+'documents_joints' => 'Documentos adjuntos',
+'dans_rubrique' => '//',
+'dans_groupe' => '//',
+'derniers_sites' => '&Uacute;ltimos sitios',
+'lire_suite' => 'Leer m&aacute;s',
+'liens' => 'V&iacute;nculos',
+'mots_cles' => 'Palabras claves',
+'proposer_site_dans_rubrique' => 'Proponer un sitio para esta secci&oacute;n',
+'sur' => '//',
+'voir_site' => 'Ver el sitio&nbsp;&raquo;',
+
+// pied
+'designer_page' => 'Sitio del dise&ntilde;ador',
+'documentation_multiflex' => 'Documentaci&oacute;n de Multiflex',
+'habillage_de' => 'Un trabajo de ',
+'modifie_par' => 'Modificado por ',
+'modifieur_page' => 'Sitio del modificador',
+'pour' => 'por',
+'site_internet_spip' => 'Sitio de internet de SPIP',
+'squelette_multiflex' => 'Esqueleto Multiflex',
+'tous_droits_reserves' => 'Algunos derechos reservados',
+
+// pour liens CFG
+'multiflex' => 'Multiflex',
+'multiflex_general' => 'General',
+'multiflex_couleurs' => 'Colores',
+'multiflex_navigation' => 'Navegaci&oacute;n',
+'multiflex_layouts' => 'Dise&ntilde;os',
+
+// pour cfg_multiflex
+'afficher_nombre_commentaires' => 'Mostrar el n&uacute;mero de comentarios',
+'apparence_layout' => 'Elegir cuerpo&nbsp;:',
+'apparence_header' => 'Elegir cabecera&nbsp;:',
+'apparence_navigation' => 'Elegir apariencia de la navegaci&oacute;n lateral (si est&aacute; mostrado)',
+
+'blocs' => 'Bloques',
+
+'coefficients_teinte' => 'Coeficiente entre -1 y 1',
+'configuration_multiflex' => 'Configuraci&oacute;n del Esqueleto Multiflex',
+'configuration_principale' => 'Configuraci&oacute;n principal',
+'complement_nom_site' => 'Complemento al nombre del sitio(en peque&ntilde;o)',
+'couleurs' => 'Coulores',
+'couleur_bloc_bleu_titre' => 'Azules, t&iacute;tulo',
+'couleur_bloc_bleu_fond' => 'Azules, relleno',
+'couleur_bloc_orange_titre' => 'Naranjas, t&iacute;tulo',
+'couleur_bloc_orange_fond' => 'Naranjas, relleno',
+'couleur_bloc_vert_titre' => 'Verdes, t&iacute;tulo',
+'couleur_bloc_vert_fond' => 'Verdes, relleno',
+'couleur_actuelle' => 'Color actual',
+'couleur_liens' => 'V&iacute;nculos',
+'couleur_liens_survoles' => 'V&iacute;nculos :hover',
+'couleur_origine' => 'Color original',
+
+'decoration' => 'Decoraci&oacute;n',
+'description_entete' => 'Descripci&oacute;n dentro de la banda de cabecera',
+'descriptifs_optionnels' => 'Descripci&oacute;n opcional',
+'descriptions_site' => 'Descripci&oacute;n del sitio',
+'divers' => 'Varios',
+'dominante' => 'Tonalidad dominante',
+
+'generale' => 'Tonalidad general',
+'grille' => 'Grilla',
+
+'header1' => 'T&iacute;tulo + banda + men&uacute;',
+'header2' => 'T&iacute;tulo + banda',
+'header3' => 'T&iacute;tulo + men&uacute;',
+'header4' => 'Bandeau + men&uacute;',
+'header5' => 'T&iacute;tulo',
+'header6' => 'Banda',
+'header7' => 'Men&uacute;',
+
+
+'layouts' => 'Cabeceras y dise&ntilde;os',
+
+'layout1' => '1 columna',
+'layout2' => '2 columnas',
+'layout3' => '2 columnas inversas',
+'layout4' => '3 columnas',
+'layout5' => '3 columnas inversas',
+
+'non' => 'No',
+'nouvelle_couleur' => 'Nuevo color',
+
+'oui' => 'S&iacute;',
+
+'positionnements_generaux' => 'Posicionamiento general',
+'puces' => 'Vi&ntilde;etas',
+
+'relief' => 'Relieve',
+'relief_fonds' => 'Relieve del tema :',
+
+'sans_puces' => 'Sin vi&ntilde;etas',
+'sous_titre_nom_site' => 'Subt&iacute;tulo del sitio',
+
+'teintes' => 'Tonalidades',
+
+'valeur' => 'Valor'
+);
+
+?>
