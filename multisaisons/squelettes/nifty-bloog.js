b4200da80e9ada68cd68cdd2c0488937632f186b
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-12 18:01:25 +0000

    Mise en avant des auteurs dans les articles. On vire l'iCal qui ne sert à rien. Des logos ronds  Le Tontonlogic gagne pitance sous un autre domaine créatif. Petit soucis sur la page de recherche? trois broutille dans le nifty-bloog.js
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29153 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multisaisons/squelettes/nifty-bloog.js
+++ multisaisons/squelettes/nifty-bloog.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.carte_auteur,div.interne,p.interne,span.logorond","transparent");
+Nifty("div.boite,div.avatar,div.reponse,div.discussion,div.carte_auteur,div.interne,p.interne,li.saisie_document_gis,span.logorond","transparent");
 Nifty("ul#minipics li,div.grande_boite,div#content,div#footer,div.boite_accueil","transparent");
 Nifty("div.interforum,div.interforumgrand","big,transparent");
 Nifty("div#header","top,big,transparent");
