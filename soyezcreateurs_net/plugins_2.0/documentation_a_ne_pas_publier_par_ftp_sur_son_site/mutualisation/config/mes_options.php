e4afdf411c96f1497c89c720f11a6e09cc977867
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-15 14:52:11 +0000

    Intégration du "Branding" dans l'exemple de mutualisation
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29190 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/mutualisation/config/mes_options.php
+++ soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/mutualisation/config/mes_options.php
@@ -9,6 +9,7 @@ $site = str_replace('ww2.', '', $site);
 
 # Bien mettre les parametres de la base sur l'hebergement
 define ('_INSTALL_HOST_DB', 'localhost');
+define ('_INSTALL_HOST_DB_LOCALNAME', 'localhost');
 define ('_INSTALL_USER_DB_ROOT', 'root');
 define ('_INSTALL_PASS_DB_ROOT', '');
 define ('_INSTALL_NAME_DB', 'mutu_'.prefixe_mutualisation($site));
@@ -22,13 +23,15 @@ demarrer_site($site,
 		'creer_base' => true,
 		'creer_user_base' => true,
 		'code' => 'ecureuil',
-		'mail' => 'mutualisation@pyrat.net'
+		'mail' => 'mutualisation@pyrat.net',
+		'branding' => 'Un service propos&eacute; par <a href="http://www.pyrat.net/">Pyrat.net</a>',
+		'branding_logo' => '<a href="http://www.pyrat.net/"><img src="IMG/branding.png" alt="" /></a>'
 		)
 	);
-
 $quota_cache = 200;
 $GLOBALS['type_urls'] = 'propres2';
-
 #Pour compatibilite des vieux plugins avec SPIP (evite d'utiliser le plugin compat193 qui ne fait que ça)
 include_spip('inc/vieilles_defs');
+#Sur un hébergement costaud, fixe la taille maximum des images traitées par GD2 (évite d'avoir à la calculer pour chaque site)
+define('_IMG_GD_MAX_PIXELS',10000000);
 ?>
