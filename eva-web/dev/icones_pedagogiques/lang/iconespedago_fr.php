d0fdabb276de4d2c60a61fedefc6753e8cceefba
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-19 11:40:51 +0000

    On se rapproche du porte-plume
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29281 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/dev/icones_pedagogiques/lang/iconespedago_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//Noms des onglets
+'consignes' => 'Consignes',
+
+//Onglet consignes
+'dire' => 'Dire',
+'lire' => 'Lire',
+'ecrire' => '&Eacute;crire',
+'seul' => 'Seul',
+'plusieurs' => 'A plusieurs',
+);
+?>
