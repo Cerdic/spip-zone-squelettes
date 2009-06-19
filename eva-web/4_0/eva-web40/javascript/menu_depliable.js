39f2ebec4cbb63c1e6019560f2eb95af61ac230a
thomas.delhomenie@gmail.com <thomas.delhomenie@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-19 12:00:10 +0000

    Correction des bugs du menu depliable. L'ajout de la noisette dans les headers n'est désormais plus nécessaire.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29282 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_0/eva-web40/javascript/menu_depliable.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+	//parcours une fois le document à la recherche des élements noeud.
+	var noeud = $("#sommaire li:has(ul)");
+
+	var feuilleracine = $("#sommaire > li:not(:has(ul))");
+	feuilleracine.addClass("feuilleracine");
+
+	var feuille = $("#sommaire ul>li:not(:has(ul))");
+	feuille.addClass("feuille");
+	
+	// ajout de la classe noeud à tous les noeuds pour afficher la flèche
+	noeud.addClass("noeud");
+
+	/*
+	// on masque toutes les sous-rubriques
+	noeud.find(">ul").hide();
+	// et on leur ajoute la classe plier pour avoir la fleche horizontale
+	noeud.find(">span").addClass("plier");
+	alert(noeud.filter(".on").find(">ul").length);
+	// puis on affiche la rubrique affichée
+	noeud.filter(".on").find(">ul").show();
+	// et on enleve la classe plier pour avoir la fleche veticale
+	noeud.filter(".on").find(">span").toggleClass("plier");
+	*/
+	
+	// on masque toutes les sous-rubriques qui ne font pas partie de la rubrique affichée	
+	noeud.filter(":not(.on)").find(">ul").hide();
+	// et on leur ajoute la classe plier pour avoir la flèche horizontale
+	noeud.filter(":not(.on)").addClass("plier");
+	
+	//determine le comportement lors d'un click
+	noeud.click(function (e) {
+		// affiche ou cache la sous liste  sur click de l'élément
+		$(this).toggleClass("plier");
+		$(this).find(">ul").toggle();
+		//interdit la propagation sur les noeuds parents
+		e.stopPropagation();		
+	});
+	//éviter un effet de bord (pliage) lors d'un click sur le lien
+	$("a").click(function (e) {
+		e.stopPropagation();
+	});
+});
