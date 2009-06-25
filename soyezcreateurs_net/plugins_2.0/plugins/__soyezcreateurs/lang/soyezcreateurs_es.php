d41e5b4c417f9078060becc7c5cd00109d6503c3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-25 11:17:50 +0000

    langues (soyezcreateurs)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29399 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.0/plugins/__soyezcreateurs/lang/soyezcreateurs_es.php
+++ soyezcreateurs_net/plugins_2.0/plugins/__soyezcreateurs/lang/soyezcreateurs_es.php
@@ -1,7 +1,14 @@
 <?php
-// This is a SPIP module file  --  Ceci est un fichier module de SPIP
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// R
-'recherche_value' => 'Buscar ?'
+
+	// R
+	'recherche_value' => 'Buscar ?'
 );
+
 ?>
