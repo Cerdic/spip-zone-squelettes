dcadc324fcdced29ff9a1b46930a0c59b1e0b813
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-29 15:57:01 +0000

    page article + on utilise désormais thinckobox et pas nycéros
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29475 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- collection/collection_options.php
+++ collection/collection_options.php
@@ -1,6 +1,5 @@
 <?php
 $GLOBALS['table_des_traitements']['TITRE'][]='typo(supprimer_numero(%s),"TYPO",$connect)';
-$GLOBALS['table_des_traitements']['TITRE']['mots']='typo(supprimer_numero(%s),"TYPO",$connect)';
 $GLOBALS['table_des_traitements']['NOM'][]='typo(supprimer_numero(%s),"TYPO",$connect)';
 $GLOBALS['table_des_traitements']['TYPE']['mots']='typo(supprimer_numero(%s),"TYPO",$connect)';
 
