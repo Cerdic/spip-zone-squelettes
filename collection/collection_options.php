bda7133efa0305c1ff0c465abfc42d77a2e164dd
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-29 12:14:22 +0000

    plugin.xml + modif du afaire  + supprimer numero par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ collection/collection_options.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS['table_des_traitements']['TITRE'][]='typo(supprimer_numero(%s),"TYPO",$connect)';
+$GLOBALS['table_des_traitements']['TITRE']['mots']='typo(supprimer_numero(%s),"TYPO",$connect)';
+$GLOBALS['table_des_traitements']['NOM'][]='typo(supprimer_numero(%s),"TYPO",$connect)';
+$GLOBALS['table_des_traitements']['TYPE']['mots']='typo(supprimer_numero(%s),"TYPO",$connect)';
+
+?>
