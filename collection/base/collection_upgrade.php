c3a45d752d70e9aefbefd1712a16260572b34dca
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-30 12:55:58 +0000

    on utilise upgrade plutôt que install (merci NicolasH et Cerdic)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- collection/base/collection_upgrade.php
+++ collection/base/collection_upgrade.php
@@ -1,20 +1,43 @@
 <?php
+/**
+ * Plugin Acces Restreint 3.0 pour Spip 2.0
+ * Licence GPL (c) 2006-2008 Cedric Morin
+ *
+ */
 
-function collection_install($action){
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-    switch($action){
-        case 'test':
-            return true;
-        case 'install':
-            ecrire_config('collection/menu[]','plan');
-            ecrire_config('collection/sommaire[]','alea9');
-            return true;
-        case 'uninstall':
-            effacer_config('collection/menu[]');
-            effacer_config('collection/sommaire[]');
-            return true;
-    }
+include_spip('inc/meta');
+/**
+ * Fonction d'installation, mise a jour de la base
+ *
+ * @param unknown_type $nom_meta_base_version
+ * @param unknown_type $version_cible
+ */
+function collection_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = 0.0;
 
+	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+		
+		if (version_compare($current_version,'0.1','<=')){
+            ecrire_config('collection/menu',array('plan'));
+            ecrire_config('collection/sommaire',array('alea9'));
+			ecrire_meta($nom_meta_base_version,$current_version="0.1");
+				}
+		ecrire_metas();
+		}
+    
+}
+/**
+ * Fonction de desinstallation
+ *
+ * @param unknown_type $nom_meta_base_version
+ */
+function collection_vider_tables($nom_meta_base_version) {
+    effacer_config('collection/menu');
+    effacer_config('collection/sommaire');
+	effacer_meta($nom_meta_base_version);
 }
 
 ?>
