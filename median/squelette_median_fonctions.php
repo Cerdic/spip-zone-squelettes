a57cec7ad090515601cf0d7ca5cd06df2a97849b
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-01 20:42:17 +0000

    intégration des fonction de gestion des doublons de _renato_ & marcimat
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@29563 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- median/squelette_median_fonctions.php
+++ median/squelette_median_fonctions.php
@@ -30,15 +30,109 @@
 
   // faire un implode sur un array
     function imploser($tab, $sep) {
-        echo '<br>tab= ';
-        var_dump($tab);
-        echo '<br>sep= '.$sep;
-        if ($tab) return implode($sep, $tab);
+        if ($tab AND is_array($tab)) return implode($sep, $tab);
     }
     
   // supprimer le # dans une chaîne (cf couleurs our image_typo
     function suprime_diese($txt) {
         return str_replace('#', '', $txt);
     }
+
+    
+/* balises pour doublons entre #inclure (idee _renato_)
+ *  code de Matthieu Marcillaud (squelette multiflex3)
+ *
+ *  Gestion des doublons entre #inclure
+ * 
+ *  B.html , C.html :
+ *  --------------
+ *  #LOAD_DOUBLONS
+ *  <BOUCLE />
+ *  #SAVE DOUBLONS
+ * 
+ * 
+ *  A.html :
+ *  --------------
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{C}{_unique = #HASH_DOUBLONS})]
+ * 
+ * 
+ * 
+ *  Un peu plus complexe (plusieurs valeurs de doublons)
+ * 
+ *  B.html, C.html :
+ *  ----------------
+ *  [(#SET{doublons, [(#ENV{_doublons}|sinon{#DOUBLON_ALEA})]})]
+ *  #LOAD_DOUBLONS
+ *  <BOUCLE(){doublons = #GET{doublons}} />
+ *  #SAVE DOUBLONS 
+ * 
+ *  A.html :
+ *  ----------------
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS})] // affiche la mÃªme chose car doublon aleatoire
+ * 
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = toto})]
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = toto})] // suite
+ *  [(#INCLURE{B}{_unique = #HASH_DOUBLONS}{_doublons = plouf})] 
+ * 
+ *  [(#INCLURE{C}{_unique = #HASH_DOUBLONS}{_doublons = plouf})]  // suite de plouf
+ * 
+ */
+
+
+/* 
+ * Sauvegarde la table des doublons dans une globale.
+ */
+if (!function_exists('balise_SAVE_DOUBLONS')){
+	function balise_SAVE_DOUBLONS($p) {
+	  $p->code = "vide(\$GLOBALS['recherche_doublons'] = \$doublons)";
+
+	  return $p; 
+	}
+}
+
+/* 
+ * Restaure la globale dans la table des doublons.
+ */
+if (!function_exists('balise_LOAD_DOUBLONS')){
+	function balise_LOAD_DOUBLONS($p) {
+	  if(!isset($GLOBALS['recherche_doublons'])) $GLOBALS['recherche_doublons'] = array();
+	  $p->code = "vide(\$doublons = \$GLOBALS['recherche_doublons'])";
+
+	  return $p; 
+	}
+}
+
+
+/*
+ * Genere un id unique pour le tableau de doublons
+ */ 
+if (!function_exists('balise_HASH_DOUBLONS')){
+	function balise_HASH_DOUBLONS($p) {
+	  #$p->code = "md5(serialize(\$GLOBALS['recherche_doublons'])) . vide(spip_log('#GET_DOUBLONS : ' . serialize(\$GLOBALS['recherche_doublons']) , 'spip_doublons'))";
+	  $p->code = "md5(serialize(\$GLOBALS['recherche_doublons']))";
+
+	  return $p; 
+	}
+}
+
+
+/*
+ * Genere un id unique pour un doublon
+ */ 
+if (!function_exists('balise_DOUBLON_ALEA')){
+	function balise_DOUBLON_ALEA($p) {
+	  $p->code = "calculer_balise_DOUBLON_ALEA()";
+	  return $p; 
+	}
+}
+if (!function_exists('calculer_balise_DOUBLON_ALEA')){
+	function calculer_balise_DOUBLON_ALEA(){
+	  return substr(rand().rand(),0,8);
+	}
+}
+
     
 ?>
