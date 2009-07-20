260c100464ea1b9a99a16e7a75423ae6d527f478
webmaster@art-logic.info <webmaster@art-logic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-20 16:21:44 +0000

    Pour faire la pluie et le beau temps avec rainette. Merci Sarka et Raphael pour l'adaptation.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30014 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multisaisons/squelettes/lang/multi_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP pour le squelette multisaison // Les chaînes de caractères sont regroupées suivant le fichier qui les utilise le plus
+
+	
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	
+	        'meteo' => 'M&eacute;t&eacute;o',
+	        'meteo_titre_conditions' => 'Conditions m&eacute;t&eacute;o &agrave;',
+	        'meteo_titre_previsions' => 'Pr&eacute;visions du',
+	        'meteo_lien_previsions' => 'Pr&eacute;visions',
+	        'meteo_lien_conditions' => 'Conditions',
+	        'meteo_previsions_jour' => 'Pr&eacute;visions du jour',
+	        'meteo_conditions_jour' => 'Conditions du jour',
+        
+
+	
+);
+
+?>
