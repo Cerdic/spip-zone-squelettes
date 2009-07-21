f3d3973a31ade36d82ea2f28345be404de062871
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-21 12:48:22 +0000

    Adaptation du menu Babbibel dans EVA-web (plugin EVA-bonus)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30050 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_0/eva_bonus/inc/evabonus_menu_horizontal_structure.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+include_spip('inc/presentation');
+
+$req_menu_horizontal_actif0=sql_select('nom_image','spip_eva_habillage_images',"type='bloc' AND nom_habillage='Defaut' AND nom_div='menu_depliable_horizontal_articles' AND attach='entete'");
+$req_menu_horizontal_actif1=sql_select('nom_image','spip_eva_habillage_images',"type='bloc' AND nom_habillage='Defaut' AND nom_div='menu_depliable_horizontal' AND attach='entete'");
+$req_menu_horizontal_actif2=sql_select('nom_image','spip_eva_habillage_images',"type='bloc' AND nom_habillage='Defaut' AND nom_div='headers_menu_depliable_horiz' AND attach='headers'");
+$tab_menu_horizontal_actif0=sql_fetch($req_menu_horizontal_actif0);
+$tab_menu_horizontal_actif1=sql_fetch($req_menu_horizontal_actif1);
+$tab_menu_horizontal_actif2=sql_fetch($req_menu_horizontal_actif2);
+$req_menu_horizontal_actif0=$tab_menu_horizontal_actif0['nom_image'];
+$req_menu_horizontal_actif1=$tab_menu_horizontal_actif1['nom_image'];
+$req_menu_horizontal_actif2=$tab_menu_horizontal_actif2['nom_image'];
+if (
+($req_menu_horizontal_actif0=='oui' OR $req_menu_horizontal_actif1=='oui')
+AND $req_menu_horizontal_actif2!='oui') 
+	{
+	echo debut_cadre_enfonce(_DIR_PLUGIN_EVABONUS."img_pack/logo_eva_bonus.png", true, '', _T('evabonus:evabonus_menu_horizontal'));
+	echo _T('evabonus:evabonus_menu_horizontal_non_headers');
+	echo fin_cadre_enfonce(true);
+	}
+elseif (
+($req_menu_horizontal_actif0!='oui' AND $req_menu_horizontal_actif1!='oui')
+AND $req_menu_horizontal_actif2=='oui')
+	{
+	echo debut_cadre_enfonce(_DIR_PLUGIN_EVABONUS."img_pack/logo_eva_bonus.png", true, '', _T('evabonus:evabonus_menu_horizontal'));
+	echo _T('evabonus:evabonus_menu_horizontal_non_entete');
+	echo fin_cadre_enfonce(true);
+	}
+?>
--- eva-web/4_0/eva_bonus/lang/evabonus_fr.php
+++ eva-web/4_0/eva_bonus/lang/evabonus_fr.php
@@ -12,6 +12,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 <p>Notez le nom de la noisette qui vous int&eacute;resse et son domaine d'application avant de l'inscrire dans la page <em>structure</em> du plugin eva-habillage.</p>
 <p>Les noisettes disponibles dans ce plugin sont le fruit d'une mutualisation entre les utilisateurs d'eva-web.<br />N'h&eacute;sitez pas &agrave; proposer vos propres noisettes personnelles !</p>",
 'evahabillage_block_evabonus'=>"Vous pouvez ins&eacute;rer des noisettes suppl&eacute;mentaires fournies par le plugin eva-bonus. En voici la liste (&agrave; ins&eacute;rer dans les parties <em>Ins&eacute;rer un squelette personnel ...</em>) :",
-
+'evabonus_menu_horizontal'=>'<div style="text-align:center;">Menu horizontal du plugin eva-bonus</div>',
+'evabonus_menu_horizontal_non_headers'=>'Pour faire fonctionner le menu horizontal, il faut aussi ins&eacute;rer la noisette <b>headers_menu_depliable_horiz</b> dans les headers',
+'evabonus_menu_horizontal_non_entete'=>'Pour faire fonctionner le menu horizontal, il faut aussi ins&eacute;rer la noisette <b>menu_depliable_horizontal</b> ou <b>menu_depliable_horizontal_articles</b> dans les ent&ecirc;tes',
+'evabonus_menu_horizontal_details'=>'<p style="text-align:center;">Choisissez ici vos pr&eacute;f&eacute;rences concernant l\'aspect graphique de votre menu dynamique horizontal.
+Pour ins&eacute;rer des images de fond, il faut vous rendre dans le module de gestion des images de fond situ&eacute; ci-dessous.</p>',
+'evabonus_menu_horizontal_couleur_fond'=>'Couleur de fond du menu',
+'evabonus_menu_horizontal_couleur_fond_survol'=>'Couleur de fond lors du survol',
+'evabonus_menu_horizontal_couleur_fond_actif'=>'Couleur de fond du secteur actif',
+'evabonus_menu_horizontal_couleur_texte'=>'Couleur du texte du menu',
+'evabonus_menu_horizontal_couleur_texte_survol'=>'Couleur du texte lors du survol',
+'evabonus_menu_horizontal_couleur_texte_actif'=>'Couleur du texte du secteur actif',
+'evabonus_menu_horizontal_couleur_bordure_style'=>'Style de la bordure de s&eacute;paration',
+'evabonus_menu_horizontal_couleur_bordure_largeur'=>'Largeur de la bordure de s&eacute;paration',
+'evabonus_menu_horizontal_couleur_bordure_couleur'=>'Couleur de la bordure de s&eacute;paration',
 );
 ?>
