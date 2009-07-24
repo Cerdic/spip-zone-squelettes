4a591b6aa85355b3d0ff9ca7d84941c7db91ac2d
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-24 09:51:46 +0000

    Grosse progression: - Ajout de la page contact, partenaire - Proposer_site (non disponible) - Une page acceuil dans la config pour expliquer ce que le webmaster dois faire. - Amélioration de toute la css - Plus d'autres chose (ça dates de 5 js)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30156 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- annuaire/lang/annuaire_fr.php
+++ annuaire/lang/annuaire_fr.php
@@ -6,18 +6,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //A
 'acceuil_presentation' =>'Présentation',
-'accueil_intro' =>'Bienvenue sur la page de présentation du plugins "annuaire".',
+'acceuil_intro' =>'Bienvenue sur la page de présentation du plugins "annuaire".',
+'acceuil_option' =>'Certain plugins sont devenu optionnel :',
+'acceuil_option_contact' =>'Le plugins "contact" permets ou non d\'affiché la page de contact',
+'acceuil_option_visiteurs' =>'Le plugins "visiteur connectés" permets d\'afficher dans l\'entête
+							le nombre de visiteur connectés',
 'acceuil_requis' =>'Les requis pour un bon fonctionnement du site',
 'acceuil_requis_1' =>'Pour un bon référencement de votre site, ils vous faut créer :
 					<br />un groupe de mot clef "_META"
 					<br />les mots clefs keywords et description',
+'acceuil_requis_2' =>'Pour afficher des partenaires sur la page "Partenaires" il vous faut
+					créé le groupe "Affichage" avec le mot clef "partenaire" et attibuer le à 
+					chacun des sites voulus.',
 'actuellement' => 'Il y a actuellement ',
 'ajouter_google' => 'Ajouter &agrave; google',
 'annuaire' =>'Annuaire',
+'annuaire_acceuil' =>'Acceuil',
 'annuaire_bandeau' =>'Bandeau',
+'annuaire_contact' =>'Contact',
 'annuaire_colonne' =>'Colonne et pied',
 'annuaire_herbier' =>'Herbier',
 'annuaire_mot' =>'Mot',
+'annuaire_partenaire' =>'Partenaire',
 'annuaire_plan' =>'Plan',
 'annuaire_rubrique' =>'Rubrique',
 'annuaire_site' =>'Site',
@@ -26,8 +36,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //B
 'bandeau' => 'le "bandeau"',
+'bandeau2' => 'du bandeau',
 'boite_acceuil' => 'Acceuil',
-'boite_bandeau' => 'Page bandeau',
+'boite_bandeau' => 'Bandeau',
+'boite_contact' => 'Contact',
 'boite_colonne' => 'Colonne et pied',
 'boite_herbier' => 'Herbier',
 'boite_mot' => 'Page mot',
@@ -38,6 +50,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'boucles' =>'R&eacute;glages des boucles',
 
 //C
+'colonne_pied' => 'de la colonne et du pied',
+'configuration' => 'Configuration',
+'contact' => 'la page "contact"',
 'copyright_site' => '&copy;',
 'css' => 'Les css',
 'css_bandeau' => 'Bandeau',
@@ -54,8 +69,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'css_ho' => 'Couleur de survol',
 
 //D
+'de' => 'de',
 'descriptif_acceuil' => 'Présentation du plugins "annuaire" et de c\'est fonctions.',
 'descriptif_bandeau' => '<h4>R&eacute;glage du bandeau</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
+'descriptif_contact' => '<h4>R&eacute;glage de la page contact</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
 'descriptif_herbier' => '<h4>R&eacute;glage de l\'herbier</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur l\'herbier.
@@ -74,7 +92,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 				<br />Choisissez vos couleurs pour les colonnes et pied de page',
 
 //E
-'epingle_site' => '&eacute;pingl&eacute;s sur l\'',
+'epingle_site' => '&eacute;pingl&eacute;s sur',
 
 //F
 
@@ -95,10 +113,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 //L
 
 //M
+'menu_site' => 'le menu du site',
 'mot' => 'la page "mot"',
 
 //N
-'nav' => 'la colonne "navigation"',
+'nav_bas' => 'la colonne "navigation"partie basse',
+'nav_haut' => 'la colonne "navigation"partie haute',
 'nav_lien' => 'le bloc "liens"',
 'nav_mot' => 'le bloc "mots clef"',
 'nav_rub' => 'le bloc "rubriques"',
@@ -112,30 +132,36 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //P
 'page_herbier' => 'Herbier',
+'page_contact' => 'Contact',
+'page_propo_site' => 'Proposer un site web',
+'partenaire' => 'Partenaires',
 'pas_new' => 'Pas de nouveaut&eacute;s',
 'pas_pagination' => 'Nombre d\'&eacute;l&eacute;ments par page',
-'pas_pop' => 'Pas de populaires, car pas de votes',
+'pas_partenaire' => 'Nous n\'avons pas de partenaires &agrave; ce jour, si vous le souhaitez,
+					rejoingnez-nous.',
+'pas_pop' => 'Pas de populaires, car pas de votes ou pas assez de site',
 'pas_sites' => 'Pas de site rattach&eacute; &agrave; ce mot clef.',
 'pas_syndic' => 'Pas d\'articles syndiqu&eacute; pour ce site.',
 'pas_zard' => 'Pas de sites au hasard, enregistrez-en ...',
-'pied' => 'le pied de page',
 'plan' => 'la page "plan"',
-'plan_site' => 'Plan du site',
 'plus_pop' => 'Les plus populaires',
 'plus_encore' => 'Plus encore !',
 'pop' => 'la boucle "populaire"',
 'popu_site' => 'la popularit&eacute; du site est de ',
 'populaire' => 'Site populaire',
-'propo_site' => 'Proposer un site',
 
 //Q
 
 //R
+'recherche_site' => 'Recherche d\'un site',
+'rechercher' => 'Rechercher',
+'ref_site' => 'R&eacute;f&eacute;rencement de mon site',
 'rss' => 'Nombres d\'articles syndiqu&eacute; par page.',
 'rubrique' => 'la page "rubrique"',
 
 //S
 'site' => 'la page "site"',
+'sites' => 'Les sites',
 'sommaire' => 'la page "sommaire"',
 
 
@@ -144,6 +170,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'tag' => 'la noisette "tags"',
 
 //U
+'utilisation' => 'Utilisation',
+'utilisation_expli' => 'Vous ne verez apparaître cette page (côté public) que si le plugins 
+						"contact avancé" est activé.',
 
 //V
 
