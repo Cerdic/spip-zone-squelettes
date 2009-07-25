044c117dfc17406f9d24bfb8a8db2be645487c0e
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-25 06:02:56 +0000

    'annuaire' tel qu'avant [30187]
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30220 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ annuaire/lang/annuaire_fr.php
@@ -0,0 +1,190 @@
+<?php
+
+// Fichier de langue du plugins annuaire - Pierre KUHN
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+'acceuil_presentation' =>'Présentation',
+'acceuil_intro' =>'Bienvenue sur la page de présentation du plugins "annuaire".',
+'acceuil_option' =>'Certain plugins sont devenu optionnel :',
+'acceuil_option_contact' =>'Le plugins "contact" permets ou non d\'affiché la page de contact',
+'acceuil_option_visiteurs' =>'Le plugins "visiteur connectés" permets d\'afficher dans l\'entête
+							le nombre de visiteur connectés',
+'acceuil_requis' =>'Les requis pour un bon fonctionnement du site',
+'acceuil_requis_1' =>'Pour un bon référencement de votre site, ils vous faut créer :
+					<br />un groupe de mot clef "_META"
+					<br />les mots clefs keywords et description',
+'acceuil_requis_2' =>'Pour afficher des partenaires sur la page "Partenaires" il vous faut
+					créé le groupe "Affichage" avec le mot clef "partenaire" et attibuer le à 
+					chacun des sites voulus.',
+'actuellement' => 'Il y a actuellement ',
+'ajouter_google' => 'Ajouter &agrave; google',
+'annuaire' =>'Annuaire',
+'annuaire_acceuil' =>'Acceuil',
+'annuaire_bandeau' =>'Bandeau',
+'annuaire_contact' =>'Contact',
+'annuaire_colonne' =>'Colonne et pied',
+'annuaire_herbier' =>'Herbier',
+'annuaire_mot' =>'Mot',
+'annuaire_partenaire' =>'Partenaire',
+'annuaire_plan' =>'Plan',
+'annuaire_rubrique' =>'Rubrique',
+'annuaire_site' =>'Site',
+'annuaire_sommaire' =>'Sommaire',
+'annuaire_style' =>'Style',
+
+//B
+'bandeau' => 'le "bandeau"',
+'bandeau2' => 'du bandeau',
+'boite_acceuil' => 'Acceuil',
+'boite_bandeau' => 'Bandeau',
+'boite_contact' => 'Contact',
+'boite_colonne' => 'Colonne et pied',
+'boite_herbier' => 'Herbier',
+'boite_mot' => 'Page mot',
+'boite_plan' => 'Page plan',
+'boite_rubrique' => 'Page rubrique',
+'boite_site' => 'Page sites',
+'boite_sommaire' => 'Page sommaire',
+'boucles' =>'R&eacute;glages des boucles',
+
+//C
+'colonne_pied' => 'de la colonne et du pied',
+'configuration' => 'Configuration',
+'contact' => 'la page "contact"',
+'copyright_site' => '&copy;',
+'css' => 'Les css',
+'css_bandeau' => 'Bandeau',
+'css_bandeau_com' => 'Pour modifier les couleurs du bandeau "ent&ecirc;te"',
+'css_boucles' => 'Boucles',
+'css_couleurs' => 'Cela revient à choisir la couleur d\'arrière plan, la couleur d\'avant plan 
+					utilisée pour le texte et les liens et la couleur de survol des liens.',
+'css_couleurs_' => 'Choisissez vos couleurs pour ',
+'css_couleurs_fond_expli' =>'Cela revient à choisir la couleur d\'arrière plan sur une page.',
+'css_couleurs_fond_page' =>'Choisissez la couleur pour le fond de ',
+'css_bg' => 'Couleur d\'arrière plan',
+'css_fond' => 'Fond de page',
+'css_fg' => 'Couleur d\'avant plan (texte et liens)',
+'css_ho' => 'Couleur de survol',
+
+//D
+'de' => 'de',
+'descriptif_acceuil' => 'Présentation du plugins "annuaire" et de c\'est fonctions.',
+'descriptif_bandeau' => '<h4>R&eacute;glage du bandeau</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
+'descriptif_contact' => '<h4>R&eacute;glage de la page contact</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
+'descriptif_herbier' => '<h4>R&eacute;glage de l\'herbier</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur l\'herbier.
+				<p />C\'est la page ou l\'on affiche l\'actualiter des sites syndiqué',
+'descriptif_mot' => '<h4>R&eacute;glage de la page mot</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur la page mot.',
+'descriptif_plan' => '<h4>R&eacute;glage de la page plan</h4>
+				<br />R&eacute;gl&eacute; vos styles du plan.',
+'descriptif_rubrique' => '<h4>R&eacute;glage de la page rubrique</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur la page rubrique.',
+'descriptif_site' => '<h4>R&eacute;glage de la page site</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles sur la page site.',
+'descriptif_sommaire' => '<h4>R&eacute;glage sur le sommaire</h4>
+				<br />R&eacute;gl&eacute; vos boucles et styles du sommaire.',
+'descriptif_colonne' => '<h4>R&eacute;glage du style</h4>
+				<br />Choisissez vos couleurs pour les colonnes et pied de page',
+
+//E
+'epingle_site' => '&eacute;pingl&eacute;s sur',
+
+//F
+
+//G
+'grp_vide' => 'R&eacute;f&eacute;renc&eacute; des sites avec un mot clef pour ce groupe',
+
+//H
+'hasard' => 'Sites au hasard',
+'herbier' => 'la page "herbier"',
+'hierarchie' => 'la "hierarchie"',
+
+//I
+
+//J
+
+//K
+
+//L
+
+//M
+'menu_site' => 'le menu du site',
+'mot' => 'la page "mot"',
+
+//N
+'nav_bas' => 'la colonne "navigation"partie basse',
+'nav_haut' => 'la colonne "navigation"partie haute',
+'nav_lien' => 'le bloc "liens"',
+'nav_mot' => 'le bloc "mots clef"',
+'nav_rub' => 'le bloc "rubriques"',
+'nav_tag' => 'le bloc "tags"',
+'nbr_articles' => 'Nombre d\'articles :',
+'nbr_sites' => 'Nombre de sites :',
+'new' => 'la boucle "nouveautés"',
+'nouveaute' =>'Site nouveaut&eacute;',
+
+//O
+
+//P
+'page_herbier' => 'Herbier',
+'page_contact' => 'Contact',
+'page_propo_site' => 'Proposer un site web',
+'partenaire' => 'Partenaires',
+'pas_new' => 'Pas de nouveaut&eacute;s',
+'pas_pagination' => 'Nombre d\'&eacute;l&eacute;ments par page',
+'pas_partenaire' => 'Nous n\'avons pas de partenaires &agrave; ce jour, si vous le souhaitez,
+					rejoingnez-nous.',
+'pas_pop' => 'Pas de populaires, car pas de votes ou pas assez de site',
+'pas_sites' => 'Pas de site rattach&eacute; &agrave; ce mot clef.',
+'pas_syndic' => 'Pas d\'articles syndiqu&eacute; pour ce site.',
+'pas_zard' => 'Pas de sites au hasard, enregistrez-en ...',
+'plan' => 'la page "plan"',
+'plus_pop' => 'Les plus populaires',
+'plus_encore' => 'Plus encore !',
+'pop' => 'la boucle "populaire"',
+'popu_site' => 'la popularit&eacute; du site est de ',
+'populaire' => 'Site populaire',
+
+//Q
+
+//R
+'recherche_site' => 'Recherche d\'un site',
+'rechercher' => 'Rechercher',
+'ref_site' => 'R&eacute;f&eacute;rencement de mon site',
+'rss' => 'Nombres d\'articles syndiqu&eacute; par page.',
+'rubrique' => 'la page "rubrique"',
+
+//S
+'site' => 'la page "site"',
+'sites' => 'Les sites',
+'sommaire' => 'la page "sommaire"',
+
+
+//T
+'tags' => 'Tags',
+'tag' => 'la noisette "tags"',
+
+//U
+'utilisation' => 'Utilisation',
+'utilisation_expli' => 'Vous ne verez apparaître cette page (côté public) que si le plugins 
+						"contact avancé" est activé.',
+
+//V
+
+//W
+
+//X
+
+//Y
+
+//Z
+'zard' => 'la boucle "hasard"',
+
+);
+
+?>
