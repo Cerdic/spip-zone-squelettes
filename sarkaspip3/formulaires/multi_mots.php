4241dfc747964fc628dea37febe2cd44c8628a06
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-13 22:14:27 +0000

    La page mot affiche bien les résultats de suite si on vient d'un clic sur un mot-clé. Quelques améliorations mineures en sus
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/formulaires/multi_mots.php
+++ sarkaspip3/formulaires/multi_mots.php
@@ -1,34 +1,30 @@
 <?php
-function formulaires_multi_mots_charger_dist($mot1){
-	// $mot1 vaut l'id du mot selectionne lorsque l'appel s'est fait a partir des noisettes des mots cles de la colonne extra
-	// $mot1 est nul lorsque l'appel se fait directement par le bouton mot-cle du menu inc_nav_raccourci, 
+function formulaires_multi_mots_charger_dist($mot){
+	// $mot vaut l'id du mot selectionne lorsque l'appel s'est fait a partir des noisettes des mots cles de la colonne extra
+	// $mot est nul lorsque l'appel se fait directement par le bouton mot-cle du menu inc_nav_raccourci, 
 
 	$valeurs = array();
-	$valeurs['mot_1'] = $mot1;
+	$valeurs['mot_1'] = $mot;
 	$valeurs['mot_2'] = '';
 	$valeurs['mot_3'] = '';
+	// Si au chargement $mot exite c'est qu'on vient d'un mot-cl direct
+	if ($mot) 
+		$valeurs['message_ok'] = 'oui';
 
 	return $valeurs;
 }
 
-function formulaires_multi_mots_verifier_dist(){
+function formulaires_multi_mots_verifier_dist($mot){
    
 	$valeurs = array();
-	$valeurs['mot_1'] = _request('mot_1');
-	$valeurs['mot_2'] = _request('mot_2');
-	$valeurs['mot_3'] = _request('mot_3');
-	
+	$valeurs['mot_1'] = intval(_request('mot_1'));
+	$valeurs['mot_2'] = intval(_request('mot_2'));
+	$valeurs['mot_3'] = intval(_request('mot_3'));
 	if (array_sum($valeurs) == 0) {
 		return array('message_erreur' =>_T('sarkaspip:choisir_un_critere'));
 	}
 }
-function formulaires_multi_mots_traiter_dist(){
-	
-	$valeurs = array();
-	$valeurs['mot_1'] = _request('mot_1');
-	$valeurs['mot_2'] = _request('mot_2');
-	$valeurs['mot_3'] = _request('mot_3');
-    // pas sur que ce qui precede serve a grand chose
+function formulaires_multi_mots_traiter_dist($mot){
 	
 	return array('message_ok' => 'oui');
 }
