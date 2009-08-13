f920739e65596ac5ffe50fa1a30615e0221dfbf6
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-13 20:31:29 +0000

    La configuration Themes disparait au profit de Maintenance: dans ce formulaire on peut maintenant sauvegarder et restaurer toutes les pages de configurations. Les sauvegardes précédentes sont toujours utilisables mais il faudra les mettre dans cfg/fond/ avant.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30889 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/formulaires/restauration_cfg.php
+++ sarkaspip3/formulaires/restauration_cfg.php
@@ -1,5 +1,5 @@
 <?php
-function formulaires_restauration_cfg_charger_dist($fond, $items_langue=array()){
+function formulaires_restauration_cfg_charger_dist($fonds, $items_langue=array()){
 	$items=array();
 	foreach ($items_langue as $_cle => $_item) {
 		$index = '_' . $_cle;
@@ -13,27 +13,36 @@ function formulaires_restauration_cfg_charger_dist($fond, $items_langue=array())
 								$items);
 
 	$dir = sous_repertoire(_DIR_TMP,"cfg");
-	$saves = preg_files($dir, $fond);
+	$saves = preg_files($dir, implode('|', array_flip($fonds)));
 	$options = '';
+	$groupe = '';
 	foreach ($saves as $_fichier) {
 		$nom = basename($_fichier);
+		$_dir = end(explode('/', dirname($_fichier)));
+		if ($_dir != $groupe) {
+			if ($options) $options .= '</optgroup>';
+			$options .= '<optgroup style="font-weight: strong;" label="'.$fonds[$_dir].'">';
+			$groupe = $_dir;
+		}
 		$options .= '<option value="' . $_fichier . '">' . $nom . '</option>';
 	}
+	if ($options) $options .= '</optgroup>';
 	$args = array_merge($args, array('_fichiers_sauvegardes' => $options));
 
 	return $args;
 }
 
-function formulaires_restauration_cfg_verifier_dist($fond, $items_langue=array()){
+function formulaires_restauration_cfg_verifier_dist($fonds, $items_langue=array()){
 	return array();
 }
 
-function formulaires_restauration_cfg_traiter_dist($fond, $items_langue=array()){
+function formulaires_restauration_cfg_traiter_dist($fonds, $items_langue=array()){
 	$message=array();
 	
-	$fichier = _request('fichier_sauvegarde');
+	$fichier = _request('fichier_a_restaurer');
 	lire_fichier($fichier,$tableau);
 
+	$fond = end(explode('/', dirname($fichier)));
 	$ok = ecrire_config($fond, $tableau);
 	
 	if (!$ok) $message['message_nok'] = _T('sarkaspip:cfg_msg_fichier_restauration_nok');
--- sarkaspip3/formulaires/sauvegarde_cfg.php
+++ sarkaspip3/formulaires/sauvegarde_cfg.php
@@ -1,27 +1,35 @@
 <?php
-function formulaires_sauvegarde_cfg_charger_dist($fond, $items_langue=array()){
+function formulaires_sauvegarde_cfg_charger_dist($fonds, $items_langue=array()){
 	$items=array();
 	foreach ($items_langue as $_cle => $_item) {
 		$index = '_' . $_cle;
 		$items[$index] = $_item;
 	}
 
+	$options = '';
+	foreach ($fonds as $_cle => $_valeur) {
+		$options .= '<option value="' . $_cle . '">' . $_valeur . '</option>';
+	}
+
 	$args = array_merge(array('_titre' => _T('cfg:sauver_meta_titre'),
 								'_legende' => _T('cfg:sauver_meta_legende'),
-								'_description' => _T('cfg:sauver_meta_description')),
+								'_description' => _T('cfg:sauver_meta_description'),
+								'_fonds' => $options),
 								$items);
 
 	return $args;
 }
 
-function formulaires_sauvegarde_cfg_verifier_dist($fond, $items_langue=array()){
+function formulaires_sauvegarde_cfg_verifier_dist($fonds, $items_langue=array()){
 	return array();
 }
 
-function formulaires_sauvegarde_cfg_traiter_dist($fond, $items_langue=array()){
+function formulaires_sauvegarde_cfg_traiter_dist($fonds, $items_langue=array()){
 	$message=array();
 	
+	$fond = _request('fond_a_sauvegarder');
 	$dir = sous_repertoire(_DIR_TMP,"cfg");
+	$dir = sous_repertoire($dir, $fond);
 	$nom = $fond . "_" . date("Ymd_Hi") . ".txt";
 	$f = $dir . $nom;
 	$ok = ecrire_fichier($f, serialize(lire_config($fond)));
