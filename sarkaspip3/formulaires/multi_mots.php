f05234aabc17523535f53c8367554f5d948118f8
sarkarchimed@gmail.com <sarkarchimed@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-13 14:05:25 +0000

    On renomme des fichiers, on en déplace d'autres, on met des icones differents pour chaque titre de recherche
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@30880 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip3/formulaires/multi_mots.php
+++ sarkaspip3/formulaires/multi_mots.php
@@ -1,5 +1,5 @@
 <?php
-function formulaires_multi_criteres_charger_dist($mot1){
+function formulaires_multi_mots_charger_dist($mot1){
 	// $mot1 vaut l'id du mot selectionne lorsque l'appel s'est fait a partir des noisettes des mots cles de la colonne extra
 	// $mot1 est nul lorsque l'appel se fait directement par le bouton mot-cle du menu inc_nav_raccourci, 
 
@@ -11,7 +11,7 @@ function formulaires_multi_criteres_charger_dist($mot1){
 	return $valeurs;
 }
 
-function formulaires_multi_criteres_verifier_dist(){
+function formulaires_multi_mots_verifier_dist(){
    
 	$valeurs = array();
 	$valeurs['mot_1'] = _request('mot_1');
@@ -22,7 +22,7 @@ function formulaires_multi_criteres_verifier_dist(){
 		return array('message_erreur' =>_T('sarkaspip:choisir_un_critere'));
 	}
 }
-function formulaires_multi_criteres_traiter_dist(){
+function formulaires_multi_mots_traiter_dist(){
 	
 	$valeurs = array();
 	$valeurs['mot_1'] = _request('mot_1');
