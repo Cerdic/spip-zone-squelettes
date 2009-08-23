42977af2e25420a0c8cbdea787517092103ba24c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-23 08:46:40 +0000

    langues (eva)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@31072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_0/eva_lang/lang/eva_ca.php
@@ -0,0 +1,169 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces_restreint' => 'Acc&eacute;s restringit',
+	'accueil' => 'Inici',
+	'agenda' => 'Agenda',
+	'aide' => 'Ajuda',
+	'article_complet' => 'Article complet',
+	'article_precedent' => 'Anterior',
+	'article_precedent_premier' => 'Primer',
+	'article_suivant' => 'Seg&uuml;ent',
+	'article_suivant_dernier' => '&Uacute;ltim',
+	'articles' => 'Articles',
+	'aucun_evenement' => 'A l\'agenda d\'aquest mes no hi ha cap esdeveniment previst',
+	'aucun_resultat_pour' => 'Cap resultat per',
+
+	// B
+	'breves' => 'Breus',
+	'breves_rubrique' => 'Breus de la secci&oacute;',
+
+	// C
+	'contact' => 'Contacte',
+	'copyright_eva' => 'i utilitzeu l\'esquelet',
+	'copyright_spip' => 'Aquest lloc est&agrave; gestionat sota',
+
+	// D
+	'de_cet_auteur' => 'd\'aquest autor',
+	'deconnecter' => 'Desconnectar-se',
+	'derniere_mise_a_jour' => '&Uacute;ltima actualitzaci&oacute;',
+	'dernieres_breves' => '&Uacute;ltimes breus',
+	'derniers_articles' => '&Uacute;ltims articles',
+	'derniers_commentaires' => '&Uacute;ltimes comentaris',
+	'derniers_podcasts' => '&Uacute;ltims podcasts ',
+	'derniers_sites' => '&Uacute;ltims llocs',
+	'diaporama' => 'Diaporama ',
+	'dix_meilleurs_articles' => 'Els deu millors articles',
+	'dix_meilleurs_breves' => 'Les deu millors breus ',
+	'dix_meilleurs_commentaires' => 'Els deu millors comentaris',
+	'doc_redacteurs' => 'Documentaci&oacute; en l&iacute;nia per als escriptors de EVA-Web ',
+	'document' => 'Document',
+	'documents_joints' => 'Documents adjunts',
+
+	// E
+	'erreur_404' => 'Error 404',
+	'evenement_aucun' => 'A l\'agenda no hi ha cap esdeveniment proper.',
+	'evenements_a_venir' => 'Propers esdeveniments',
+	'evenements_du' => 'Els esdeveniments del',
+	'evenements_passes' => 'Esdeveniments passats ',
+	'evenements_passes_aucun' => 'A l\'agenda no hi ha cap esdeveniment passat',
+
+	// F
+	'fermer_fenetre' => 'Tancar la finestra',
+	'feuilleter_livre' => 'Examinar el llibre',
+	'form_pet_message_commentaire' => 'Un comentari?',
+
+	// G
+	'galaxie_spip' => 'La gal&agrave;xia SPIP ',
+	'go' => 'anar',
+
+	// I
+	'identifier' => 'Esteu autenticats ',
+	'il_y_a' => 'Hi ha',
+	'il_y_a1' => 'signatura (es) a aquesta petici&oacute;. ',
+	'il_y_a2' => 'Hi ha un total de ',
+	'il_y_a3' => 'article(s).<br /> Aquest bloc en mostra ',
+	'il_y_a4' => 'autor(s).<br /> Aquest bloc en mostra',
+	'il_y_a5' => 'breu(s).<br /> Aquest bloc en mostra',
+
+	// J
+	'j1' => 'dl',
+	'j2' => 'dt',
+	'j3' => 'dc',
+	'j4' => 'dj',
+	'j5' => 'dv',
+	'j6' => 'ds',
+	'j7' => 'dg',
+	'jo1' => 'Dilluns',
+	'jo2' => 'Dimarts',
+	'jo3' => 'Dimecres',
+	'jo4' => 'Dijous',
+	'jo5' => 'Divendres',
+	'jo6' => 'Dissabte',
+	'jo7' => 'Diumenge',
+
+	// L
+	'lancer_diaporama' => 'Llan&ccedil;ar el Diaporama',
+	'lien_externe' => 'Enlla&ccedil; extern al lloc, s\'obre en una nova finestra',
+	'lire_suite' => 'Llegir la continuaci&oacute;',
+
+	// M
+	'm1' => 'gener',
+	'm10' => 'octubre',
+	'm11' => 'novembre',
+	'm12' => 'desembre',
+	'm2' => 'febrer',
+	'm3' => 'mar&ccedil;',
+	'm4' => 'abril',
+	'm5' => 'maig',
+	'm6' => 'juny',
+	'm7' => 'juliol',
+	'm8' => 'agost',
+	'm9' => 'setembre',
+	'meme_rubrique' => 'En aquesta secci&oacute;',
+	'mentions' => 'Mencions',
+	'mentions_legales' => 'Mencions legals',
+	'meteo' => 'Temps',
+	'meteo_info' => 'Informacions geogr&agrave;fiques',
+	'meteo_previsions' => 'Previsi&oacute; del temps',
+	'mis_a_jour' => 'Modificat:',
+	'mot_cle' => 'Paraules clau',
+	'multilinguisme' => 'Mostrar el men&uacute; de llengua d\'EVA-web a les p&agrave;gines p&uacute;bliques?',
+	'multilinguisme_article' => 'Aquest article en:',
+
+	// N
+	'notes' => 'Notes',
+
+	// P
+	'pages' => 'P&agrave;gines',
+	'par' => 'Per:',
+	'partenaires' => 'Socis',
+	'plan_du_site' => 'Mapa',
+	'podcasts' => 'Podcasts',
+	'podcasts_rss' => 'Podcast i RSS',
+	'post_scriptum' => 'Postdata',
+	'pour' => 'per',
+	'publie' => 'Publicat:',
+
+	// R
+	'rechercher' => 'Cercar',
+	'redaction' => 'Redacci&oacute;',
+	'replier' => 'Tornar a plegar',
+	'resultats' => 'Resultats',
+
+	// S
+	'sites' => 'Altres llocs:',
+	'sites_references' => 'Llocs referenciats',
+	'sites_rubrique' => 'Llocs de la secci&oacute;',
+	'sites_syndic' => 'Llocs sindicats de la secci&oacute;',
+	'sous_rubrique' => 'Subsecci&oacute; ',
+	'statut_admin' => 'Estatus: Administrador<br />',
+	'statut_redac' => 'Estatus: Redactor',
+	'statut_visit' => 'Estatus: Visitant',
+	'sur_le_web' => 'A la Web',
+	'sur_un_total_de' => 'd\'un total de ',
+
+	// T
+	'texte_page_404' => '<em>Ho sentim!</em><br />La p&agrave;gina que busqueu no existeix.',
+	'tous_droits' => 'Reservats tots els drets',
+	'tous_les_auteurs' => 'Tots els autors',
+
+	// V
+	'version_eva' => 'EVA-Web 4.0 estable',
+	'visites' => 'Visites',
+	'voir_en_ligne' => 'Veure en l&iacute;nia:',
+	'voir_image' => 'Veure la imatge m&eacute;s gran',
+	'vous_etes_ici' => 'Esteu aqu&iacute;:',
+
+	// Z
+	'zone' => 'zona protegida'
+);
+
+?>
--- eva-web/4_0/eva_lang/lang/eva_en.php
+++ eva-web/4_0/eva_lang/lang/eva_en.php
@@ -18,7 +18,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'article_suivant' => 'Next',
 	'article_suivant_dernier' => 'Lastest article',
 	'articles' => 'Articles',
-	'aucun_evenement' => 'No comming event invisaged this month in the diary',
+	'aucun_evenement' => 'No coming event envisaged this month in the diary',
 	'aucun_resultat_pour' => 'No results for',
 
 	// B
@@ -39,17 +39,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'derniers_commentaires' => 'Latest comments',
 	'derniers_podcasts' => 'Latest podcasts',
 	'derniers_sites' => 'Latest sites',
-	'diaporama' => 'Slide show',
+	'diaporama' => 'Slideshow',
 	'dix_meilleurs_articles' => 'Top ten articles',
 	'dix_meilleurs_breves' => 'Top ten News',
 	'dix_meilleurs_commentaires' => 'Top ten comments',
-	'doc_redacteurs' => 'On line documents for writers using EVA_Red',
+	'doc_redacteurs' => 'Online documents for writers using EVA_Red',
 	'document' => 'Document',
 	'documents_joints' => 'Attachments',
 
 	// E
 	'erreur_404' => 'Error 404',
-	'evenement_aucun' => 'No comming event invisaged this month in the diary',
+	'evenement_aucun' => 'No coming event envisaged this month in the diary',
 	'evenements_a_venir' => 'Events to come',
 	'evenements_du' => 'Events for',
 	'evenements_passes' => 'Past events',
@@ -76,8 +76,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// J
 	'j1' => 'Mon',
 	'j2' => 'Tue',
-	'j3' => 'Wen',
-	'j4' => 'Thur',
+	'j3' => 'Wed',
+	'j4' => 'Thu',
 	'j5' => 'Fri',
 	'j6' => 'Sat',
 	'j7' => 'Sun',
@@ -90,8 +90,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'jo7' => 'Sunday',
 
 	// L
-	'lancer_diaporama' => 'Open Slide show',
-	'lien_externe' => 'External link, open in new window',
+	'lancer_diaporama' => 'Open Slideshow',
+	'lien_externe' => 'External link, opens in a new window',
 	'lire_suite' => 'Read more',
 
 	// M
@@ -108,10 +108,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'm8' => 'August',
 	'm9' => 'September',
 	'meme_rubrique' => 'In this section',
-	'mentions' => 'Mentions',
-	'mentions_legales' => 'Legal mentions',
-	'mis_a_jour' => 'Updating : ',
-	'mot_cle' => 'Key words',
+	'mentions' => 'Notes',
+	'mentions_legales' => 'Legal notes',
+	'meteo' => 'Weather',
+	'meteo_info' => 'Geographical information',
+	'meteo_previsions' => 'Weather forecast',
+	'mis_a_jour' => 'Updated: ',
+	'mot_cle' => 'Keywords',
 	'multilinguisme' => 'Display the menu of language of EVA-Web in the public pages?',
 	'multilinguisme_article' => 'This article in :',
 
