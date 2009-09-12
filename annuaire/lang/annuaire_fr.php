58bec4f0b5716a05ac874ed6149113a89c06ffab
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-12 04:56:53 +0000

    Ajout du formulaire_site dans la page rubrique et du coup supression de la page proposer_site,
    Amélioration de l'affichage des groupe de mots dans la colonne nav, forcer la majuste sur tous les mots clefs
    
    reprise de la langue fr
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@31514 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- annuaire/lang/annuaire_fr.php
+++ annuaire/lang/annuaire_fr.php
@@ -9,8 +9,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'acceuil_intro' =>'Bienvenue sur la page de présentation du plugins "annuaire".',
 'acceuil_option' =>'Certain plugins sont optionnel :',
 'acceuil_option_contact' =>'Le plugins "contact" permets ou non d\'affiché la page de contact',
-'acceuil_option_pub' => 'Si vous souhaitez intégré dans votre site des pub google, il vous 
-						faudra créé le mot clef "pub" dans le groupe "Affichage" et dans une 
+'acceuil_option_pub' => 'Si vous souhaitez intégré dans votre site des pub google, il vous
+						faudra créé le mot clef "pub" dans le groupe "Affichage" et dans une
 						rubrique nommé "squelette"',
 'acceuil_option_visiteurs' =>'Le plugins "visiteur connectés" permets d\'afficher dans l\'entête
 							le nombre de visiteur connectés',
@@ -19,7 +19,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 					<br />un groupe de mot clef "_META"
 					<br />les mots clefs keywords et description',
 'acceuil_requis_2' =>'Pour afficher des partenaires sur la page "Partenaires", il vous faut
-					créé le groupe "Affichage" avec le mot clef "partenaire" et attibuer le à 
+					créé le groupe "Affichage" avec le mot clef "partenaire" et attibuer le à
 					chacun des sites voulus.',
 'actuellement' => 'Il y a actuellement ',
 'affichage' => 'L\'affichage',
@@ -29,11 +29,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'annuaire_bandeau' => 'Bandeau',
 'annuaire_contact' => 'Contact',
 'annuaire_colonne' => 'Colonne et pied',
+'annuaire_coul_mot_clef' => 'Couleur avec les mots',
 'annuaire_herbier' => 'Herbier',
 'annuaire_mot' => 'Mot',
 'annuaire_partenaire' => 'Partenaire',
 'annuaire_plan' => 'Plan',
-'annuaire_propo_site' => 'Proposition de site',
 'annuaire_recherche' => 'Recherche',
 'annuaire_rubrique' => 'Rubrique',
 'annuaire_site' => 'Site',
@@ -42,16 +42,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //B
 'bandeau' => 'le "bandeau"',
-'bandeau2' => 'du bandeau',
 'boite_acceuil' => 'Acceuil',
 'boite_bandeau' => 'Bandeau',
 'boite_contact' => 'Contact',
 'boite_colonne' => 'Colonne et pied',
+'boite_coul_mot' => 'Couleur avec les mots',
 'boite_herbier' => 'Herbier',
-'boite_mot' => 'page mot',
+'boite_mot' => 'Mots',
 'boite_plan' => 'Page plan',
 'boite_partenaire' => 'Page partenaire',
-'boite_proposer_site' => 'Proposer un site',
 'boite_recherche' => 'Page recherche',
 'boite_rubrique' => 'Page rubrique',
 'boite_site' => 'Page sites',
@@ -59,15 +58,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'boucles' =>'R&eacute;glages des boucles',
 
 //C
-'colonne_pied' => 'de la colonne et du pied',
-'configuration' => 'Configuration',
-'contact' => 'la page "contact"',
+'configuration_bandeau' => 'Configuration du bandeau',
+'configuration_colonne_pied' => 'Configuration de la colonne et du pied',
+'configuration_contact' => 'Configuration de la page "contact"',
+'configuration_herbier' => 'Configuration de la page "herbier"',
+'configuration_mot' => 'Configuration de la page "mot"',
+'configuration_partenaire' => 'Configuration de la page "partenaire"',
+'configuration_plan' => 'Configuration de la page "plan"',
+'configuration_recherche' => 'Configuration de la page "recherche"',
+'configuration_rubrique' => 'Configuration de la page "rubrique"',
+'configuration_site' => 'Configuration de la page "site"',
+'configuration_sommaire' => 'Configuration de la page "sommaire"',
 'conception' => 'conception',
 'css' => 'Les css',
 'css_bandeau' => 'Bandeau',
 'css_bandeau_com' => 'Pour modifier les couleurs du bandeau "ent&ecirc;te"',
 'css_boucles' => 'Boucles',
-'css_couleurs' => 'Cela revient à choisir la couleur d\'arrière plan, la couleur d\'avant plan 
+'css_couleurs' => 'Cela revient à choisir la couleur d\'arrière plan, la couleur d\'avant plan
 					utilisée pour le texte et les liens et la couleur de survol des liens.',
 'css_couleurs_' => 'Choisissez vos couleurs pour ',
 'css_couleurs_fond_expli' =>'Cela revient à choisir la couleur d\'arrière plan sur une page.',
@@ -78,12 +85,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'css_ho' => 'Couleur de survol',
 
 //D
-'de' => 'de',
-'descriptif_acceuil' => 'Présentation du plugins "annuaire" et de c\'est fonctions.',
+'descriptif_acceuil' => 'Présentation du plugins "annuaire" et de ses fonctions.',
 'descriptif_bandeau' => '<h4>R&eacute;glage du bandeau</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
 'descriptif_contact' => '<h4>R&eacute;glage de la page contact</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur le bandeau.',
+'descriptif_coul_mot' => '<h4>Choisir une couleur avec des mots clefs</h4>
+				<br />Nous vous laissons choisir une couleur:
+				<br />- en fonction du mot clef que vous avez choisi
+				<br />- la couleur attibuée',
 'descriptif_herbier' => '<h4>R&eacute;glage de l\'herbier</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur l\'herbier.
 				<p />C\'est la page ou l\'on affiche l\'actualiter des sites syndiqué',
@@ -114,12 +124,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //H
 'hasard' => 'Sites au hasard',
-'herbier' => 'la page "herbier"',
 'hierarchie' => 'la "hierarchie"',
 
 //I
-'infos' => 'Afficher une infos sur toutes les rubriques (si vous la souhaitez différentes 
+'infos' => 'Afficher une infos sur toutes les rubriques (si vous la souhaitez différentes
 			utiliser la description.)',
+
 //J
 
 //K
@@ -128,7 +138,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //M
 'menu_site' => 'le menu du site',
-'mot' => 'la page "mot"',
 
 //N
 'nav_bas' => 'la colonne "navigation" partie basse',
@@ -144,11 +153,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'nouveaute' =>'Site nouveaut&eacute;',
 
 //O
+'ok' => 'OK',
 
 //P
 'page_herbier' => 'Herbier',
 'page_contact' => 'Contact',
-'page_propo_site' => 'Proposer un site',
 'page_rank_de' => 'a un page rank de',
 'partenaire' => 'Partenaires',
 'pas_new' => 'Pas de nouveaut&eacute;s',
@@ -158,33 +167,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'pas_pop' => 'Pas de populaires, car pas de votes ou pas assez de site',
 'pas_sites' => 'Pas de site rattach&eacute; &agrave; ce mot clef.',
 'pas_zard' => 'Pas de sites au hasard, enregistrez-en ...',
-'plan' => 'la page "plan"',
 'plus_pop' => 'Les plus populaires',
 'plus_encore' => 'Plus encore !',
 'pop' => 'la boucle "populaire"',
 'popu_site' => 'La popularit&eacute; du site est de ',
 'populaire' => 'Site populaire',
-'pour_propo_site' => 'Afin que tous le monde puisse proposer des sites et suretout le ranger 
-					 o&ugrave; il le souhaite, je vous propose de vous inscrire sur le site 
-					 comme r&eacute;dacteur.',
-'propo_site' => 'la page "proposer un site"',
 
 //Q
 
 //R
 'resultats' => 'Résultats dans les boucles',
-'recherche' => 'la page "recherche"',
 'recherche_site' => 'Recherche d\'un site',
 'rechercher' => 'Rechercher',
 'ref_site' => 'R&eacute;f&eacute;rencement de mon site',
 'rss' => 'Nombres d\'articles syndiqu&eacute; par page.',
-'rubrique' => 'la page "rubrique"',
 
 //S
-'site' => 'la page "site"',
 'site_mois' => 'le site du mois',
 'sites' => 'Les sites',
-'sommaire' => 'la page "sommaire"',
+'supprimer' => 'Supprimer',
 'style' => 'Style :',
 'styliste' => 'Indiquer la personne qui a changer les couleurs : (afficher colonne de droite en
 			 bas uniquement si remplis), possibilitée de mettre du code spip ou html.',
@@ -193,7 +194,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //U
 'utilisation' => 'Utilisation',
-'utilisation_expli' => 'Vous ne verez apparaître cette page (côté public) que si le plugins 
+'utilisation_expli' => 'Vous ne verez apparaître cette page (côté public) que si le plugins
 						"contact avancé" est activé.',
 
 //V
