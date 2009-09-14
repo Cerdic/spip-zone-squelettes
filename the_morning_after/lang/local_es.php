0dafbf9a63308a8f4988913d4c09181e1005e88e
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-14 12:54:17 +0000

    spanish language file
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@31576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ the_morning_after/lang/local_es.php
@@ -0,0 +1,57 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// A
+'about' => 'Acerca de',
+'accueil_site' => 'Inicio',
+'archives' => 'Archivos',
+'article' => 'est&aacute; leyendo...',
+'auteur' => 'Archivos del autor/a',
+'auteur_count' => 'ha escrito',
+
+// B
+'browse' => 'Navegar',
+'breves' => 'Noticias',
+'billet' => 'entrada',
+'billets' => 'entradas',
+
+//C
+'category_count' => 'Esta categor&iacute;a contiene',
+'comment' => 'comentario',
+'comments' => 'comentarios',
+'continue_reading' => 'Seguir leyendo',
+'cryptographic_key' =>'Clave de encriptaci&oacute;n',
+
+
+//D
+'dernier_article' => '&Uacute;ltimo art&iacute;lo',
+'derniers_articles' => '&Uacute;ltimos art&iacute;los',
+'discussion' => 'Comentarios',
+
+//E
+'email_auteur' => 'Enviar email a autor/a',
+
+//F
+'featured_post' => 'Art&iacute;culo destacado',
+'for' => 'por',
+
+//R
+'retour_accueil' => '¿Perdido/a? Vuelva a la p&aacute;gina de inicio',
+'rubriques' => 'Categor&iacute;s',
+'recherche' => 'Buscar',
+'rss_section' => 'Canal RSS para esta secci&oacute;n',
+
+//S
+'subscribe' => 'Subscribirse',
+
+//T
+'tags' => 'Claves',
+
+//W
+'welcome' => 'Bienvenido/a a'
+
+);
+
+?>
