f9242f22b6dd7b99544aa2164adbae40a8d0f22c
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-25 18:28:42 +0000

    On applique le test de présence des tables d'habillage pour le multilinguisme pour le pipeline affiche gauche. Sinon : joli bug...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@31797 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva-web40/evasquelettes_pipeline.php
+++ eva-web/4_0/eva-web40/evasquelettes_pipeline.php
@@ -1,6 +1,8 @@
 <?php
 function evasquelettes_AfficheGauche($flux) {
-	$exec = $flux['args']['exec'];
+$test=sql_showtable('spip_eva_habillage_images',true);
+$exec = $flux['args']['exec'];
+if (($exec == 'config_multilang') AND ($GLOBALS['connect_statut'] == "0minirezo") AND ($test['field'])) {
 	$icone = _DIR_PLUGIN_EVASQUELETTES.'images/eva3_favicon.png';
 	if ($_POST['evamultilinguisme']=='oui') {
 		sql_delete('spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='menu_langue_actif'");
@@ -16,34 +18,33 @@ function evasquelettes_AfficheGauche($flux) {
 	}
 	$test=sql_select('nom_image','spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='menu_langue_actif'");
 	$tab=sql_fetch($test);
-	$go=$tab['nom_image'];	
-	if (($exec == 'config_multilang') AND ($GLOBALS['connect_statut'] == "0minirezo")) {
-		$retour .= debut_cadre_relief($icone,true,'', 'EVA-web : '._T("spip:info_multilinguisme"));
-		$retour .= '<form method="POST" action="'.generer_url_ecrire($exec).'">';
-		$retour .= '<div style="text-align:center;">';
-		$retour .= '<p>';
-		$retour .= _T('local:multilinguisme');
-		$retour .= '</p>';
-		$retour .= '<p>';
-		$retour .= '<input name="evamultilinguisme" value="oui" ';
-		if ($go) {$retour .= 'checked="checked" ';}
-		$retour .= 'type="radio"> ';
-		if ($go) {$retour .= '<b>';}
-		$retour .= _T('ecrire:item_oui');
-		if ($go) {$retour .= '</b>';}
-		$retour .= '&nbsp; <input name="evamultilinguisme" value="non" ';
-		if (!$go) {$retour .= 'checked="checked" ';}
-		$retour .= 'type="radio"> ';
-		if (!$go) {$retour .= '<b>';}
-		$retour .= _T('ecrire:item_non');
-		if (!$go) {$retour .= '</b>';}
-		$retour .= '</p>';
-		$retour .= '<input type="submit" value="'. _T('spip:bouton_valider').'">';
-		$retour .= '</div>';
-		$retour .= '</form>';
-		$retour .= fin_cadre_relief(true);
-		$flux['data'] .= $retour;
-	}
-	return $flux;
+	$go=$tab['nom_image'];
+	$retour .= debut_cadre_relief($icone,true,'', 'EVA-web : '._T("spip:info_multilinguisme"));
+	$retour .= '<form method="POST" action="'.generer_url_ecrire($exec).'">';
+	$retour .= '<div style="text-align:center;">';
+	$retour .= '<p>';
+	$retour .= _T('local:multilinguisme');
+	$retour .= '</p>';
+	$retour .= '<p>';
+	$retour .= '<input name="evamultilinguisme" value="oui" ';
+	if ($go) {$retour .= 'checked="checked" ';}
+	$retour .= 'type="radio"> ';
+	if ($go) {$retour .= '<b>';}
+	$retour .= _T('ecrire:item_oui');
+	if ($go) {$retour .= '</b>';}
+	$retour .= '&nbsp; <input name="evamultilinguisme" value="non" ';
+	if (!$go) {$retour .= 'checked="checked" ';}
+	$retour .= 'type="radio"> ';
+	if (!$go) {$retour .= '<b>';}
+	$retour .= _T('ecrire:item_non');
+	if (!$go) {$retour .= '</b>';}
+	$retour .= '</p>';
+	$retour .= '<input type="submit" value="'. _T('spip:bouton_valider').'">';
+	$retour .= '</div>';
+	$retour .= '</form>';
+	$retour .= fin_cadre_relief(true);
+	$flux['data'] .= $retour;
+}
+return $flux;
 }
 ?>
