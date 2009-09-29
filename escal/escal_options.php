a3097e382a39322950655aee315a3f9084787d81
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-29 14:53:25 +0000

    ajout d'icones pour les liens externes et les mailto modifications des liens type page=machin de spip_out en spip_in on en profite pour styler les liens dans la page annuaire
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@31873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/escal_options.php
+++ escal/escal_options.php
@@ -1,10 +1,16 @@
 <?php
 
-################ CONFIGURATION TITRAGE #######################
+// déclaration du site dans domaine interne 
+// pour changement des class spip_out en spip_in 
+// pour les liens internes de type page=machin
+
+  define('_DOMAINE_INT', $GLOBALS['meta']['adresse_site'] ); 
+
 
 // Suppression globale des nombres devant les titres du type :
 //   1. Mon titre
 
 	$table_des_traitements['TITRE'][]= 'supprimer_numero(typo(%s))';
+
 
 ?>
--- escal/mes_fonctions.php
+++ escal/mes_fonctions.php
@@ -19,4 +19,16 @@ $GLOBALS['diapo_vignette']=60;
 //diaporama : temps de pause en millisecondes :
 $GLOBALS['diapo_temps']=3000;
 
+
+
+
+   // remplace la classe css 'spip_out' par 'spip_in'
+   // pour les liens vers le(s) domaine(s) '_DOMAINE_INT'
+   function inc_lien($lien='', $texte='', $class='', $title='', $hlang='', $rel='', $connect='') {
+     if (preg_match(',('._DOMAINE_INT.'),Ui', $lien)) {
+       $class = 'spip_in';
+     }
+     return inc_lien_dist($lien, $texte, $class, $title, $hlang, $rel, $connect);
+   }
+
 ?>
