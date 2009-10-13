aab81b2f95256a9cb34fac3fec06455f89234889
neofutur777@gmail.com <neofutur777@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-13 18:56:26 +0000

    premiere version sans accents d une traduction en espagnol pour le squelette median
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@32101 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ median/lang/median_es.php
@@ -0,0 +1,96 @@
+<?php
+
+$GLOBALS['i18n_median_fr'] = array(
+
+// A
+'aa_elements' => 'Elementos de los articulos',
+'accueil' => 'Inicio',
+
+// C
+'chapo' => 'chapo',
+'choix_aa' => 'Articulo de presentacion',
+'choix_groupes_mots_techniques' => 'Definicion de los grupos de palabras claves tecnicos',
+'choix_teinte_generale' => 'Las colores del sitio',
+'configuration_median' => 'Configuracion del esqueleto Median',
+'couleur_principale' => 'Color principal',
+'couleur_secondaire' => 'Color secundario',
+'couleur_blocs' => 'Color de los bloques de la pagina de inicio',
+'couleur_boite_docjoints' => 'Color de la cajas de los archivos adjuntados"',
+'couleur_boite_motscles' => 'Color de la cajas de "palabras claves"',
+'couleur_boite_autresarticles' => 'Coulor de la cajas de "otros articulos"',
+'couleur_boite_sousrubriques' => 'Color de las cajas "subsecciones"',
+'couleur_titre_principale' => 'Color de los titulos principales',
+'couleur_titre_secondaire' => 'Color de los titulos secundarios',
+'couleur_titre_tertiaire' => 'Color de los titulos terceros',
+'contact_us' => 'Para contactarnos',
+
+// D
+'defaut_texte_404' => 'esta pagina no existe, o su direcion ha cambiado . . . ahora puedes :
+<ul>
+  <li>usar el menu para explorar las subsecciones</li>
+  <li>seleccionar una palabra clave en el nube de palabras claves abajo</li>
+  <li>usar el buscador</li>
+</ul>
+de todas maneras, van a encontrar lo que buscan !',
+'defaut_meta_keywords' => 'SPIP, esqueleto Median',
+'defaut_texte_titre_nuage' => 'Palabras claves',
+'defaut_texte_creation_compte' => 'El acceso a esta pagina esta reservado para usadores identificados',
+'defaut_texte_acces_restreint' => 'Quieres crear una cuenta?',
+'descriptif_configuration' => 'parametros de configuracion del esqueleto Median',
+
+// E
+'error_install_groupe_technique' => 'error de creacion del grupo de palabras claves tecnicas para el esqueleto Median',
+'error_install_groupe_coordonnees' => 'error de creacion del grupo de palabras claves del grupo de palabras claves de coordenadas para el esqueleto Median',
+'error_creation_mot_cle' => 'error de creacion de la palabra clave',
+'explication_groupes_mots_techniques' => 'las palabras claves de los grupos seleccionados no seran mostrados en las paginas publicas',
+'explication_palette' => 'para elegir una color: primero haz clic en el circulo, y depues en el cuadrado',
+
+// I
+'id' => 'id grupo',
+
+// L
+'lire_suite' => 'Leer mas',
+'logo'=> 'logo',
+
+// M
+'meta_keywords' => 'El contenido de la baliza "meta keywords" ( una quinzena de palabras separadas por comas)',
+'mots_cles_coordonnees' => 'las palabras claves para las coordenadas de contacto',
+'mots_cles_techniques_median' => 'las palabras claves tecnicas para el esqueleto Median',
+
+// N
+'nb_blocs_sommaire' => 'Numero de bloques de articulos',
+
+// P
+'page_sommaire' => 'Pagina de inicio',
+
+// R
+'reglages' => 'Arreglos varios',
+
+// S
+//'saisir_id_groupes_mots' => 'entrez les ID des groupes de mots s&eacute;par&eacute;s par un |',
+'saisir_couleur' => 'Elegir una color haciendo clic en la zona de entrada',
+'sous-titre'=> 'subtitulo',
+
+// T
+'texte' => 'texto',
+'texte_404' => 'Texto de la pagina error 404',
+'textes_caches' => 'Textos que no seran mostrados directamente',
+'textes_interface' => 'Textos configurables',
+'texte_acces_restreint' => 'Texto de ayuda condo es necesario identificarse',
+'texte_creation_compte' => 'Titulo del formulario de creacion de cuenta',
+'texte_titre_nuage' => 'Titulo del nube de palabras claves ( lista de palabras claves ) para las paginas mapa del sition y 404',
+'articles_rubrique' => 'En esta seccion',
+'titre' => 'Titulo',
+'titre_median' => 'Esqueleto Median',
+'top_logo_couleur' => 'Color',
+'top_logo_largeur' => 'Anchura maxima (en pixeles, si el tiyulo es mas largo, habra un salto a la linea)',
+'top_logo_police' => 'fuente (nombre des archivo ttf colocado en la carpeta /squelettes/polices)',
+'top_logo_taille' => 'Tamano de la fuente (en pixeles)',
+'top_logo_url' => 'El logo arriba de la pagina esta un vinculo hasta la pagina de inicio, pero en este campo puedes elegir otra URL',
+
+// V
+'vitesse_diaporama' => 'Duracion entre 2 photos del diaporama de la pagina de inicio (en milisegundos)'
+
+);
+
+?>
