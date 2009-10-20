88400081c9c5f64b469c82c38b0c8c71c005ee8f
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-20 15:44:08 +0000

    On enlève les réglages pour le plugin diapo qui n'ont rien à faire dans Escal.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@32236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/escal_fonctions.php
+++ escal/escal_fonctions.php
@@ -1,26 +1,5 @@
 <?php
 
-// paramètres pour le plugin diapo
-
-//nombre de vignettes par page
-$GLOBALS['diapo_vignettes']=15;
-
-//largeur de la grande image :
-$GLOBALS['diapo_grand']=400;
-
-//largeur maxi de la photo :
-$GLOBALS['diapo_petit']=300;
-//hauteur maxi de la photo :
-$GLOBALS['diapo_petit_h']=300;
-
-//largeur et hauteur maxi des vignettes :
-$GLOBALS['diapo_vignette']=60;
-
-//diaporama : temps de pause en millisecondes :
-$GLOBALS['diapo_temps']=3000;
-
-
-
 
    // pour gérer les classes des différents liens dans les articles
    // Un grand merci à l'auteur : bobof
