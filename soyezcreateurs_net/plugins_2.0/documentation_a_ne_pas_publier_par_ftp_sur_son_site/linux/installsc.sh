83fe26224ef45d333125b6bbd54701cbad15b08d
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-30 00:30:13 +0000

    Finalement, l'édition plein écran, bof bof : ça casse la possibilité de sélectionner un mot entier par double clic, et ça perd la barre typo :(
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@32489 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -62,8 +62,6 @@ svn checkout svn://zone.spip.org/spip-zone/_plugins_/gestion_documents
 sleep 15;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact
 sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/edition_plein_ecran
-sleep 15;
 svn checkout svn://zone.spip.org/spip-zone/_plugins_/protection_formulaires
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/woopra
 #svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation
