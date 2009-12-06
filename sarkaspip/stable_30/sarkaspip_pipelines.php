c45f62f170d1ffe01b28b3402282208c4ff6ac76
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-06 22:07:07 +0000

    - Integration de la gestion des thèmes avec Zen Garden: maintenant Sarka-SPIP peut développer sa bibliothèque de thèmes... go go go - On retrouve l'onglet cfg disparu depuis si longtemps !
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@33559 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/stable_30/sarkaspip_pipelines.php
+++ sarkaspip/stable_30/sarkaspip_pipelines.php
@@ -52,4 +52,45 @@ function SarkaSpip_mes_fichiers_a_sauver($flux){
 	spip_log($flux);
 	return $flux;
 }
+
+// Pipeline "zengarden_activer_theme" permettant d'effectuer les traitements d'activation d'un theme lies ˆ Sarka-SPIP
+function SarkaSpip_zengarden_activer_theme($flux){
+	$dir_cfg = $flux['args']['dir'] . '/cfg';
+	$cfg = preg_files($dir_cfg);
+
+	// On sauvegarde des valeurs actuelles pour un eventuel rappel
+	$dir_cfg = sous_repertoire(_DIR_TMP,"cfg");
+	$dir_cfg = sous_repertoire(_DIR_TMP,"themes");
+	$fonds = array();
+	foreach ($cfg as $_fichier_cfg) {
+		$fonds[] = basename($_fichier_cfg, '.txt');
+	}
+	include_spip('inc/sarkaspip_filtres');
+	$ok = sauvegarder_fonds($fonds, $dir_cfg, 'theme');
+
+	// On ecrit la nouvelle configuration nŽcessaire au theme choisi
+	$flux['data'] = true;
+	foreach ($cfg as $_fichier_cfg) {
+		lire_fichier($_fichier_cfg, $tableau);
+		$fond = basename($_fichier_cfg, '.txt');
+		ecrire_config($fond, $tableau);
+	}
+	return $flux;
+}
+
+// Pipeline "zengarden_effacer_theme" permettant d'effectuer les traitements d'effacement d'un theme lies ˆ Sarka-SPIP
+function SarkaSpip_zengarden_effacer_theme($flux){
+	$dir_cfg = $flux['args']['dir'] . '/cfg';
+	$cfg = preg_files($dir_cfg);
+	$fichiers = array();
+	$flux['data'] = true;
+	foreach ($cfg as $_fichier_cfg) {
+		$fond = basename($_fichier_cfg, '.txt');
+		effacer_config($fond);
+		$fichiers[] = _DIR_TMP . 'themes/cfg/' . $fond . '.txt';
+	}
+	include_spip('inc/sarkaspip_filtres');
+	$ok = restaurer_fonds($fichiers);
+	return $flux;
+}
 ?>
