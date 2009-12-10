46b7baa84ac7f28e3c8a074e3463f68748c39123
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-10 23:14:15 +0000

    Plus besoin de toussa quand on utilise la trousse à Têtue (cf.:  http://romy.tetue.net/tetue_trousse )
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@33656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cheznous/cheznous_fonctions.php
+++ cheznous/cheznous_fonctions.php
@@ -1,11 +1,5 @@
 <?php
 
-$GLOBALS['toujours_paragrapher'] = true;
-// $GLOBALS['debut_intertitre'] = "\n<h2 class=\"spip\">\n";
-// $GLOBALS['fin_intertitre'] = "</h2>\n";
-// $GLOBALS['puce'] = '- ';
-$GLOBALS['table_des_traitements']['TITRE'][]= 'supprimer_numero(typo(%s))';
-
 // Gravatar
 function gravatar_url($email = '')
 {
