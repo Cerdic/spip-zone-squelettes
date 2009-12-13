fee41f1c004ef723b85e4d54810baef5079d5218
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-13 08:53:02 +0000

    Un oubli de langue et correction css
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@33707 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipcollab/lang/local_en.php
+++ spipcollab/lang/local_en.php
@@ -5,13 +5,23 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+//C
 'commentaires' => 'Comments',
 'connecter_comme' => 'Logged in as',
 'courriel' => 'Email',
+
+//D
 'design' => 'Desygn by',
+
+//I
 'icons' => 'Icons by',
 'identifiant' => 'Logout',
 'identifiant_compte' => 'Logout of this account',
+
+//L
+'laissez_commentaire' => 'Leave a comment',
+
+//S
 'surnom' => 'Username',
 
 );
--- spipcollab/lang/local_fr.php
+++ spipcollab/lang/local_fr.php
@@ -5,26 +5,52 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+//A
 'agenda' => 'Agenda',
 'annuaire' => 'Annuaire',
 'aucun_document' => 'Aucun document',
+
+//C
 'categories' => 'Cat&ecute;gories',
 'commentaires' => 'commentaires',
 'connecter_comme' => 'Connecté comme',
 'courriel' => 'Courriel',
+
+
+//D
 'dans' => 'dans',
 'derniers_visiteurs' => 'Derniers visiteurs',
 'design' => 'Charte graphique par',
 'documents' => 'Documents',
+
+//F
 'flux_forum_article' => 'Flux rss du forum de cet article',
+'forum_voir_avant' => 'Voir le forum avant',
+
+//G
 'groupes_travail' => 'Vos groupes de travail',
+
+//H
 'haut_page' => 'Haut de la page',
+
+//I
 'icons' => 'Images par',
 'identifiant' => 'Identifiant',
 'identifiant_compte' => 'Identifiant de ce compte',
+
+//L
+'laissez_commentaire' => 'Laissez un commentaire',
+
+//M
 'messages' => 'Messages',
+
+//N
 'nouveau_message' => 'Nouveau message',
+
+//P
 'publier' => 'Publi&eacute; le',
+
+//S
 'surnom' => 'Surnom',
 
 );
