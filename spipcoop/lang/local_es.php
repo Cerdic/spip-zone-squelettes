b489bee831d2c231535f1297f7cecef116e4499d
joseluis@digital77.com <joseluis@digital77.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-19 21:55:51 +0000

    traduit a l'espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@33846 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipcoop/lang/local_es.php
@@ -0,0 +1,62 @@
+<?php
+// Fichier de langue spécifique au squelette
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+'accueil_site' => 'Portada',
+'agenda' => 'Agenda',
+'ajouter_commentaire' => 'Comentar',
+'annuaire' => 'Participantes',
+'articles_auteur' => 'Mensajes de este autor o autora',
+'aucun_document' => 'No hay documentos',
+'aucun_commentaire' => 'No hay comentarios',
+
+//C
+'categories' => 'Categor&iacute;as',
+'commentaires' => 'Comentarios',
+'connecter_comme' => 'Connectada como',
+'courriel' => 'Courriel',
+
+
+//D
+'dans' => 'en',
+'derniers_articles' => '&Uacute;ltimos mensajes',
+'derniers_visiteurs' => '&Uacute;ltimas visitas',
+'design' => 'Dise&ntilde;o de',
+'documents' => 'Documentos',
+
+//F
+'flux_forum_article' => 'Seguir por RSS estos comentarios',
+
+//G
+'groupes_travail' => 'Tus grupos',
+
+//H
+'haut_page' => 'Subir',
+
+//I
+'icons' => 'Im&aacute;genes de',
+'identifiant' => 'Identificante',
+'identifiant_compte' => 'Identificante de esta cuenta',
+
+//L
+'laissez_commentaire' => 'Comentar',
+
+//M
+'messages' => 'Mensajes',
+
+//N
+'nouveau_message' => 'Nuevo mensaje',
+
+//P
+'publier' => 'Publicado el',
+
+//S
+'surnom' => 'Nombre',
+
+);
+
+?>
