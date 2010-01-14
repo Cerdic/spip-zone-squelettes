b2be5020c6279e38c68de7e2c6f2efd606fe4872
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-14 17:02:56 +0000

    On améliore un peu avant de passer a zpip, sur la page sommaire on afficher toutes les réponses des forum désorméet d'autre baviole corriger.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@34446 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipcoop/lang/local_fr.php
+++ spipcoop/lang/local_fr.php
@@ -7,18 +7,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //A
 'abonnement' => 'Abonnement',
-'accueil' => 'Accueil',
+'accueil_site' => 'Accueil',
 'adresse_mail' => 'Adresse mail',
 'agenda' => 'Agenda',
 'ajouter_commentaire' => 'Ajouter un commentaire',
 'annuaire' => 'Annuaire',
+'antispam_champ_vide' => 'Antispam champ vide',
 'aucun_document' => 'Aucun document',
-'aucun_commentaire' => 'Aucun commentaire',
+
+//B
+'bouton_ajouter_document' => 'Ajouter un document',
+'bouton_radio_modere_priori' => 'Mod&ecute;r&ecute; &agrave; priori',
+'bouton_telecharger' => 'T&ecute;l&ecute;charger',
+'bouton_valider' => 'Valider',
 
 //C
 'categories' => 'Cat&ecute;gories',
 'commentaires' => 'Commentaires',
-'connecter_comme' => 'Connecté comme',
+'connecter_comme' => 'Connect&ecute; comme',
 
 //D
 'dans' => 'dans',
