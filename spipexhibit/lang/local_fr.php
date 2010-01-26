08fc376e29a14610ace96f316aa8711021592396
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-26 20:30:44 +0000

    Squelette Spipexhibit http://www.spip-contrib.net/ecrire/?exec=articles&id_article=3381
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@34741 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipexhibit/lang/local_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+'built_with' => 'Propuls&eacute; par',
+
+// E
+'en_construction' => 'Le site est en construction ...'
+
+
+
+
+);
+?>
