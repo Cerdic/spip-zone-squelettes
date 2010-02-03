90674479cf15b025d1c8f713b320962ede11b673
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-03 23:49:26 +0000

    Mise à jour pour tenir compte du déplacement des ETv3 + optimisation de la lisibilité du code (merci xdjuj)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@34901 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_2.0/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -1,77 +1,48 @@
 #! /bin/sh
 #Installation de SPIP
 svn checkout svn://trac.rezo.net/spip/branches/spip-2.0 ./
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_squelettes_/gribouille
-sleep 15;
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/gribouille
 #Si besoin de la mutualisation, decomenter la ligne suivante
-#svn checkout svn://zone.spip.org/spip-zone/_plugins_/mutualisation
-#sleep 15;
+#sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/mutualisation
 
 #Installation des plugins
 mkdir plugins
 cd plugins
-svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.0/plugins/__soyezcreateurs
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/acces_restreint
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/acronymes
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/agenda/2_0_0 ./agenda/
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/autorite
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/boutonstexte
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/cfg
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/saisies
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/2_0 ./clevermail/
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/crayons
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume/extensions/enluminures_typographiques_v3
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/fonctions_images
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/forms/forms_et_tables_2_0 ./forms/
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/Lecteur_multimedia
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/sedna
-sleep 15;
-#svn checkout svn://zone.spip.org/spip-zone/_plugins_/thickbox2
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/nyroceros
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-2
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/accessibilite
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/fulltext
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/savecfg
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/gestion_documents
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact
-sleep 15;
-svn checkout svn://zone.spip.org/spip-zone/_plugins_/protection_formulaires
-#svn checkout svn://zone.spip.org/spip-zone/_plugins_/woopra
-#svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation
-sleep 15;
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.0/plugins/__soyezcreateurs
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acces_restreint
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acronymes
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/agenda/2_0_0 ./agenda/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/autorite
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/boutonstexte
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/cfg
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/saisies
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/2_0 ./clevermail/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/crayons
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/enluminures_typographiques_v3
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fonctions_images
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/forms/forms_et_tables_2_0 ./forms/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/Lecteur_multimedia
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/sedna
+#sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/thickbox2
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nyroceros
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-2
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/accessibilite
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fulltext
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/savecfg
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gestion_documents
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/protection_formulaires
+#sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/woopra
+#sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation
 
 cd ..
 
 #Installation du squelette SoyezCreateur : il faut le faire dans un sous dossier
 #car sinon, conflit avec le .svn de SPIP
-svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.0/ ./__sc/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.0/ ./__sc/
 #Deplacement du squelette au bon endroit
 mv ./__sc/htaccess.txt ./.htaccess
 
@@ -82,7 +53,4 @@ rm -r -f ./__sc
 mkdir squelettes
 
 #mettre les droits idoines pour les dossiers
-chmod 755 config
-chmod 755 IMG
-chmod 755 local
-chmod 755 tmp
+chmod 755 config/ IMG/ local/ tmp/
