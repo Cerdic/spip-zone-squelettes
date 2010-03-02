00ae6eba752ce7eacfefbb1b4e618180a8eeeda8
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-02 22:12:59 +0000

    dismoi
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@35736 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dismoi/lang/local_fr.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'archives' => 'Archives',
+
+	// B
+
+	// C
+	'conception_graphique_par' => 'Habillage visuel (c)',
+	'commentaire' => 'commentaire',
+	'commentaires' => 'commentaires',
+
+	// D
+	
+	//E
+	
+	//F
+	'forum_page_url' => 'Si vous avez un site, indiquez le ici',
+	'forum_lien_hyper' => '<b>site</b> (optionnel)',
+
+
+	// I
+
+	// L
+	'lire_la_suite' => 'Lire la suite',
+	'lire_la_suite_de' => ' de ',
+
+	// M
+	'message' => 'Message',
+	'messages_forum' => 'Messages',
+
+	// P
+	'personaliser_nav' => 'Personnaliser ce menu',
+
+	// R
+
+	// S
+	'sous_licence' => 'sous Licence',
+
+	// T
+
+	// V
+
+	// Z
+);
+
+?>
