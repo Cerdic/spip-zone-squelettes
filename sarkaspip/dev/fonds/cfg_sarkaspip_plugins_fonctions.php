3fe14c8d53c507c40aa0da850e87b9b4b2435445
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-12 22:11:49 +0000

    Report de 36167
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@36168 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/dev/fonds/cfg_sarkaspip_plugins_fonctions.php
+++ sarkaspip/dev/fonds/cfg_sarkaspip_plugins_fonctions.php
@@ -1,26 +1,14 @@
 <?php
 function cfg_sarkaspip_plugins_post_traiter($cfg){
 	if ($cfg->val['config_boutonstexte']=='sarkaspip') {
-		ecrire_config('boutonstexte/', array(	'txtOnly'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_TXTONLY,
-												'txtBackSpip'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_TXTBACKSPIP,
-												'txtSizeUp'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_TXTSIZEUP,
-												'txtSizeDown'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_TXTSIZEDOWN,
-												'selector'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_SELECTOR,
-												'jsFile'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_JSFILE,
-												'cssFile'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_CSSFILE,
-												'imgPath'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_IMGPATH));
+		ecrire_config('boutonstexte/', array(	'selector'=>_SARKASPIP_CONFIG_BOUTONSTEXTE_SELECTOR));
 	}
 	else
 		effacer_config('boutonstexte/');
 
 	if ($cfg->val['config_fancybox']=='sarkaspip') {
-		ecrire_config('fancybox/', array(	'padding'=>_SARKASPIP_CONFIG_FANCYBOX_MARGE,
-											'selecteur_commun'=>_SARKASPIP_CONFIG_FANCYBOX_IMAGE,
-											'selecteur_galerie'=>_SARKASPIP_CONFIG_FANCYBOX_GALERIE,
-											'imagescale'=>_SARKASPIP_CONFIG_FANCYBOX_RETAILLE,
-											'overlayshow'=>_SARKASPIP_CONFIG_FANCYBOX_ARRIERE_PLAN,
-											'overlayopacity'=>_SARKASPIP_CONFIG_FANCYBOX_OPACITE,
-											'hideoncontentclick'=>_SARKASPIP_CONFIG_FANCYBOX_FERME_CLIC));
+		ecrire_config('fancybox/', array(	'selecteur_commun'=>_SARKASPIP_CONFIG_FANCYBOX_IMAGE,
+											'selecteur_galerie'=>_SARKASPIP_CONFIG_FANCYBOX_GALERIE));
 	}
 	else
 		effacer_config('fancybox/');
@@ -29,15 +17,14 @@ function cfg_sarkaspip_plugins_post_traiter($cfg){
 		ecrire_config('nyroceros/', array(	'traiter_toutes_images'=>_SARKASPIP_CONFIG_NYROCEROS_TOUT,
 											'selecteur_commun'=>_SARKASPIP_CONFIG_NYROCEROS_IMAGE,
 											'selecteur_galerie'=>_SARKASPIP_CONFIG_NYROCEROS_GALERIE,
-											'installer_diapo_auto'=>_SARKASPIP_CONFIG_NYROCEROS_DIAPORAMA,
-											'preload'=>_SARKASPIP_CONFIG_NYROCEROS_PRELOAD,
-											'bgcolor'=>_SARKASPIP_CONFIG_NYROCEROS_BGFOND));
+											'installer_diapo_auto'=>_SARKASPIP_CONFIG_NYROCEROS_DIAPORAMA));
 	}
 	else
 		effacer_config('nyroceros/');
 
 	if ($cfg->val['config_socialtags']=='sarkaspip') {
-		ecrire_config('socialtags/', array(	'jsselector'=>_SARKASPIP_CONFIG_SOCIALTAGS_SELECTOR));
+		ecrire_config('socialtags/', array(	'jsselector'=>_SARKASPIP_CONFIG_SOCIALTAGS_SELECTOR,
+											'tags' => explode(':', _SARKASPIP_CONFIG_SOCIALTAGS_TAGS)));
 	}
 	else
 		effacer_config('socialtags/');
