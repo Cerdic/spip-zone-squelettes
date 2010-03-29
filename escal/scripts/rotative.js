29e8bcf6caa45dc34c116e5943875c559c29a37c
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-29 19:56:41 +0000

    ajout d'une noisette inc-photos.html
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@36749 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/scripts/rotative.js
+++ escal/scripts/rotative.js
@@ -27,3 +27,32 @@ var headline_count;
    $("div.headline:eq(" + current_headline + ")").show().animate({top: 5},"slow");  
    old_headline = current_headline;
  }
+
+
+
+ var headline2_count;
+ var headline2_interval;
+ var old_headline2 = 0;
+ var current_headline2 = 0;
+ 
+ $(document).ready(function(){
+   headline2_count = $("div.headline2").size();
+   $("div.headline2:eq("+current_headline2+")").css('top','5px');
+ 
+   headline2_interval = setInterval(headline2_rotate,5000); //temps en millisecondes
+   $('#scrollup2').hover(function() {
+     clearInterval(headline2_interval);
+   }, function() {
+     headline2_interval = setInterval(headline2_rotate,5000); //temps en millisecondes
+     headline2_rotate();
+   });
+ });
+ 
+ function headline2_rotate() {
+   current_headline2 = (old_headline2 + 1) % headline2_count; 
+   $("div.headline2:eq(" + old_headline2 + ")").animate({top: -205},"slow", function() {
+     $(this).css('top','210px');
+   });
+   $("div.headline2:eq(" + current_headline2 + ")").show().animate({top: 5},"slow");  
+   old_headline2 = current_headline2;
+ }
