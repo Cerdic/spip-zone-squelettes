98b374ec3225bf8b5930ee8b5398391c0c1ad292
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-02 19:17:23 +0000

    Suite à discussion sur http://thread.gmane.org/gmane.comp.web.spip.devel/57112, introduction d'une balise #ARTICLE_URL qui permet que les articles de redirection présent dans la navigation du site pointent effectivement sur l'URL de la redirection, et non sur l'article faisant la redirection.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@36866 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/soyezcreateurs_fonctions.php
+++ soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/soyezcreateurs_fonctions.php
@@ -766,4 +766,36 @@ function direction_css_intelligente ($css, $voulue='') {
 	return $f;
 }
 
+function balise_ARTICLE_URL_dist($p) {
+	if (!is_array($p->param))
+		$p->param=array();
+
+	// Produire le premier argument {article_url}
+	$texte = new Texte;
+	$texte->type='texte';
+	$texte->texte='article_url';
+	$param = array(0=>NULL, 1=>array(0=>$texte));
+	array_unshift($p->param, $param);
+
+	// Transformer les filtres en arguments
+	for ($i=1; $i<count($p->param); $i++) {
+		if ($p->param[$i][0]) {
+			if (!strstr($p->param[$i][0], '='))
+				break;# on a rencontre un vrai filtre, c'est fini
+			$texte = new Texte;
+			$texte->type='texte';
+			$texte->texte=$p->param[$i][0];
+			$param = array(0=>$texte);
+			$p->param[$i][1] = $param;
+			$p->param[$i][0] = NULL;
+		}
+	}
+
+	// Appeler la balise #MODELE{article_url}{arguments}
+	if (!function_exists($f = 'balise_modele'))
+		$f = 'balise_modele_dist';
+	return $f($p);
+}
+
+
 ?>
