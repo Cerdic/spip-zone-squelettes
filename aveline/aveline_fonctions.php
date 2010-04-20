1dd98cb8c42db9c3c55db74931f13ee98add31e3
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-20 16:55:59 +0000

    Ajout du critère archives
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@37466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aveline/aveline_fonctions.php
+++ aveline/aveline_fonctions.php
@@ -68,6 +68,17 @@ function balise_COMPTEUR_FORUM_dist($p) {
    return $p;
 } 
 
+// Critère archives pour afficher uniquement les objets d'une date donnée, par exemple en passant à l'URL ?archives=2010-02
+// Repris du plugin minical
+function critere_archives($idb, &$boucles, $crit, $var_date = 'archives') {
+	$boucle = &$boucles[$idb];
+	$champ_date = "'" . $boucle->id_table ."." .$GLOBALS['table_date'][$boucle->type_requete] . "'";
+	$boucle->where[] = array(
+		'REGEXP',
+		$champ_date, 
+		"sql_quote(('^' . interdire_scripts(entites_html(\$Pile[0]['".$var_date."']))))"
+	);
+}
 
 
 ?>
