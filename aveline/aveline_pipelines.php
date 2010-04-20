c4778d8561a8f1a38d06a02a4a729df41480f5c1
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-20 09:08:36 +0000

    supprimer les erreurs quand $fond est un tableau
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@37440 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aveline/aveline_pipelines.php
+++ aveline/aveline_pipelines.php
@@ -12,7 +12,10 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function aveline_recuperer_fond($flux){
 	include_spip('inc/noizetier');
 	$fond = $flux['args']['fond'];
-	$bloc = substr($fond,0,strpos($fond,'/'));
+	if(!is_array($fond))
+		$bloc = substr($fond,0,strpos($fond,'/'));
+	else
+		$bloc = '';
 	// Si on est sur un bloc contenu, navigation ou extra, on ajoute les noisettes de la page par defaut
 	if (in_array($bloc,array('contenu','navigation','extra'))) {
 		$contexte = $flux['data']['contexte'];
