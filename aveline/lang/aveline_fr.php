385329d9adfdbab6311f381a1b4cf7a9a8d89272
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-21 10:03:11 +0000

    Séparation des chaines de langue dans deux fichiers séparés.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@37485 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aveline/lang/aveline_fr.php
+++ aveline/lang/aveline_fr.php
@@ -5,43 +5,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// Chaines affichées sur la partie publique
-	'rubrique_deux_points' => 'Rubrique&nbsp;:',
-	'categorie_deux_points' => 'Cat&eacute;gorie&nbsp;:',
-	'mots_cles_deux_points' => 'Mots-Cl&eacute;s&nbsp;:',
-	'tags_deux_points' => 'Tags&nbsp;:',
-	'theme_deux_points' => 'Th&egrave;me&nbsp;:',
-	'sujet_deux_points' => 'Sujet&nbsp;:',
-	'themes_deux_points' => 'Th&egrave;mes&nbsp;:',
-	'categories_deux_points' => 'Cat&eacute;gories&nbsp;:',
-	'sujets_deux_points' => 'Sujets&nbsp;:',
-	'tags' => 'Tags',
-	'categories' => 'Cat&eacute;gories',
-	'themes' => 'Th&egrave;mes',
-	'sujets' => 'Sujets',
-	'dans' => 'Dans',
-	'mis_a_jour_le' => 'Mis &agrave; jour le',
-	'par_titre' => 'Par titre',
-	'visiteurs_quotidiens' => 'visiteur(s) quotidien(s)',
-	'articles_les_plus_commentes' => 'Articles les plus comment&eacute;s',
-	'les_plus_commentes' => 'Les plus comment&eacute;s',
-	'les_derniers_publies' => 'Les derniers publi&eacute;s',
-	'articles_les_plus_recents' => 'Articles les plus r&eacute;cents',
-	'les_plus_recents' => 'Les plus r&eacute;cents',
-	'articles_les_plus_anciens' => 'Articles les plus anciens',
-	'les_plus_anciens' => 'Les plus anciens',
-	'les_plus_populaires' => 'Les plus populaires',
-	'articles_les_plus_visites' => 'Articles les plus visit&eacute;s',
-	'les_plus_visites' => 'Les plus visit&eacute;s',
-	'articles_les_mieux_notes' => 'Articles les mieux not&eacute;s',
-	'les_mieux_notes' => 'Les mieux not&eacute;s',
-	'modifies_recemment' => 'Modifi&eacute;s r&eacute;cemment',
-	'notez_cet_article' => 'Notez cet article',
-	'archives_deux_points' => 'Archives&nbsp;:',
-	'annee_deux_points' => 'Ann&eacute;e&nbsp;:',
-	'mois_deux_points' => 'Mois&nbsp;:',
-	'voir_tous' => 'Voir tous',
-
 	// Description des pages
 	
 	'description_pagedefaut' => 'Les blocs de cette page seront ajout&eacute;s sur toutes les pages du site.',
