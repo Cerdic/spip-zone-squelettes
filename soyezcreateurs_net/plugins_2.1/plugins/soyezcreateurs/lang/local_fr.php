4fa2c13ebf3e2e17c55ddffd46db0a03fce7f8fb
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-14 13:26:14 +0000

    Documenter de façon plus précise le champ Descriptif : il sert de bulle d'aide, de chapeau si ce dernier vide et de meta description
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@38178 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/lang/local_fr.php
+++ soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/lang/local_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'admin_modifier_breve' => 'G&eacute;rer cette br&egrave;ve',
 'admin_modifier_mot' => 'G&eacute;rer ce mot-cl&eacute;',
 'admin_modifier_rubrique' => 'G&eacute;rer cette rubrique',
-'texte_descriptif_rapide' => 'Bulle d\'aide <i>(et chapeau si le chapeau vide)</i>',
+'texte_descriptif_rapide' => 'Bulle d\'aide <i>(et chapeau si le chapeau vide)</i> + Meta Description <i>(pour les moteurs de recherche)</i>',
 
 // Modification du texte du formulaire de forum
 'forum_votre_email' => 'Votre adresse email (<strong>ne sera pas rendue publique sur le site</strong>, n&eacute;cessaire pour pouvoir vous r&eacute;pondre personnellement) :'
