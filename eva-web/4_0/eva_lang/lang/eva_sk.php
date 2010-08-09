135b18edd2802263cab2b5122f600f3863f5031c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:21:28 +0000

    langues (eva)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@39744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_0/eva_lang/lang/eva_sk.php
@@ -0,0 +1,216 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces_restreint' => '<NEW>Obmedzen&yacute; pr&iacute;stup',
+	'accueil' => '<NEW>Domovsk&aacute; str&aacute;nka',
+	'agenda' => '<NEW>Di&aacute;r',
+	'aide' => '<NEW>N&aacute;poveda',
+	'article_complet' => '<NEW>Cel&eacute; &#269;l&aacute;nky',
+	'article_precedent' => '<NEW>Predch&aacute;dzaj&uacute;ci',
+	'article_precedent_premier' => '<NEW>Prv&yacute; &#269;l&aacute;nok',
+	'article_suivant' => '<NEW>&#270;al&#353;&iacute;',
+	'article_suivant_dernier' => '<NEW>Najnov&#353;&iacute; &#269;l&aacute;nok',
+	'articles' => '<NEW>&#268;l&aacute;nky',
+	'aucun_evenement' => '<NEW>&#381;iadna pl&aacute;novan&aacute; udalos&#357; nie je zap&iacute;san&aacute; v di&aacute;ri na tento mesiac',
+	'aucun_resultat_pour' => '<NEW>&#381;iadne v&yacute;sledky pre',
+
+	// B
+	'breves' => '<NEW>Novinky',
+	'breves_rubrique' => '<NEW>Novinky v tejto rubrike',
+
+	// C
+	'contact' => '<NEW>Kontakt',
+	'copyright_eva' => 'and use template', # MODIF
+	'copyright_spip' => 'This site is managed by', # MODIF
+
+	// D
+	'de_cet_auteur' => 'By this author', # MODIF
+	'deconnecter' => '<NEW>Odhl&aacute;si&#357; sa',
+	'derniere_mise_a_jour' => '<NEW>Najnov&#353;ie aktualiz&aacute;cie',
+	'dernieres_breves' => '<NEW>Najnov&#353;ie novinky',
+	'derniers_articles' => '<NEW>Najnov&#353;ie &#269;l&aacute;nky',
+	'derniers_commentaires' => '<NEW>Najnov&#353;ie koment&aacute;re',
+	'derniers_podcasts' => '<NEW>Najnov&#353;ie podcasty',
+	'derniers_sites' => '<NEW>Najnov&#353;ie str&aacute;nky',
+	'diaporama' => 'Slideshow', # MODIF
+	'dix_meilleurs_articles' => '<NEW>Najlep&#353;&iacute;ch 10 &#269;l&aacute;nkov',
+	'dix_meilleurs_breves' => '<NEW>Najlep&#353;&iacute;ch 10 noviniek',
+	'dix_meilleurs_commentaires' => '<NEW>Najlep&#353;&iacute;ch 10 koment&aacute;rov',
+	'doc_redacteurs' => 'Online documents for writers using EVA_Red', # MODIF
+	'document' => '<NEW>Dokument',
+	'documents_joints' => '<NEW>Pr&iacute;lohy',
+
+	// E
+	'erreur_404' => '<NEW>Chyba 404',
+	'evenement_aucun' => '<NEW>&#381;iadna udalos&#357; nie je v di&aacute;ri napl&aacute;novan&aacute; na tento mesiac',
+	'evenements_a_venir' => '<NEW>Nasleduj&uacute;ce udalosti',
+	'evenements_du' => '<NEW>Udalosti na',
+	'evenements_passes' => '<NEW>Udalosti v minulosti',
+	'evenements_passes_aucun' => '<NEW>&#381;iadne udalosti v minulosti nie s&uacute; v di&aacute;ri',
+
+	// F
+	'fermer_fenetre' => '<NEW>Zatvori&#357; okno',
+	'feuilleter_livre' => '<NEW>Prelistova&#357; knihu',
+	'form_pet_message_commentaire' => '<NEW>Jeden koment&aacute;r?',
+
+	// G
+	'galaxie_spip' => '<NEW>Galaxia SPIP',
+	'go' => '<NEW>Prejs&#357; na',
+
+	// I
+	'icone_eva' => '<NEW>Icone EVA-web 4', # MODIF
+	'identifier' => '<NEW>Ste pripojen&yacute;',
+	'il_y_a' => 'There is', # MODIF
+	'il_y_a1' => '<NEW>Podpis(y) pre t&uacute;to pet&iacute;ciu',
+	'il_y_a2' => 'There is in total', # MODIF
+	'il_y_a3' => 'Article(s).</br> This block in poster', # MODIF
+	'il_y_a4' => 'Author(s).</br> This block in poster', # MODIF
+	'il_y_a5' => 'News.</br>This block in poster', # MODIF
+	'inscription' => '<NEW>Inscription', # MODIF
+
+	// J
+	'j1' => '<NEW>Pon',
+	'j2' => '<NEW>Uto',
+	'j3' => '<NEW>Str',
+	'j4' => '<NEW>&#352;tv',
+	'j5' => '<NEW>Pia',
+	'j6' => '<NEW>Sob',
+	'j7' => '<NEW>Ned',
+	'jo1' => '<NEW>Pondelok',
+	'jo2' => '<NEW>Utorok',
+	'jo3' => '<NEW>Streda',
+	'jo4' => '<NEW>&#352;tvrtok',
+	'jo5' => '<NEW>Piatok',
+	'jo6' => '<NEW>Sobota',
+	'jo7' => '<NEW>Nede&#318;a',
+
+	// L
+	'lancer_diaporama' => 'Open Slideshow', # MODIF
+	'lien_externe' => '<NEW>Extern&yacute; odkaz, otvor&iacute; sa v novom okne',
+	'lire_suite' => '<NEW>&#268;&iacute;ta&#357; &#271;alej',
+
+	// M
+	'm1' => '<NEW>Janu&aacute;r',
+	'm10' => '<NEW>Okt&oacute;ber',
+	'm11' => 'November', # MODIF
+	'm12' => 'December', # MODIF
+	'm2' => '<NEW>Febru&aacute;r',
+	'm3' => '<NEW>Marec',
+	'm4' => '<NEW>Apr&iacute;l',
+	'm5' => '<NEW>M&aacute;j',
+	'm6' => '<NEW>J&uacute;n',
+	'm7' => '<NEW>J&uacute;l',
+	'm8' => 'August', # MODIF
+	'm9' => 'September', # MODIF
+	'meme_rubrique' => '<NEW>V tejto rubrike',
+	'mentions' => '<NEW>Pozn&aacute;mky',
+	'mentions_adresse' => '<NEW>Adresse :', # MODIF
+	'mentions_directeur_publication' => '<NEW>Directeur de la publication :', # MODIF
+	'mentions_droit_auteur_texte' => '<NEW><p>Ce site rel&egrave;ve de la l&eacute;gislation fran&ccedil;aise et internationale sur le droit d\'auteur et la propri&eacute;t&eacute; intellectuelle.</p>
+	<p>Tous les droits de reproduction sont r&eacute;serv&eacute;s.</p>', # MODIF
+	'mentions_droit_auteur_titre' => '<NEW>Droits d\'auteurs :', # MODIF
+	'mentions_legales' => 'Legal notes', # MODIF
+	'mentions_liens_hypertexte_texte' => '<NEW><p>Ce site contient des liens hypertextes permettant l\'acc&egrave;s &agrave; des sites qui ne sont pas &eacute;dit&eacute;s par le responsable de ce site.</p>
+	<p>En cons&eacute;quence le directeur de publication ne saurait &ecirc;tre tenu pour responsable du contenu des sites auxquels l\'internaute aurait ainsi acc&egrave;s.</p>
+	<p>Il est formellement interdit de collecter et d\'utiliser les informations disponibles sur le site &agrave; des fins commerciales.</p>
+	<p>Cette interdiction s\'&eacute;tend notamment, sans que cette liste ne soit limitative, &agrave; tout &eacute;l&eacute;ment r&eacute;dactionnel figurant sur le site, &agrave; la pr&eacute;sentation des &eacute;crans, aux logiciels n&eacute;cessaires &agrave; l\'exploitation, aux logos, images, photos, graphiques, de quelque nature qu\'ils soient.</p>', # MODIF
+	'mentions_liens_hypertexte_titre' => '<NEW>Liens hypertextes :', # MODIF
+	'mentions_liens_texte' => '<NEW><p>A l\'exception de sites diffusant des informations et/ou contenus ayant un caract&egrave;re ill&eacute;gal et/ou &agrave; caract&egrave;re politique, religieux, pornographique, x&eacute;nophobe, vous pouvez cr&eacute;er un lien hypertexte vers notre Site sur votre site.</p>
+	<p>La mise en place de lien hypertexte n\'autorise en aucune fa&ccedil;on la reproduction d\'&eacute;l&eacute;ments du Site ou la pr&eacute;sentation sur des sites tiers d\'&eacute;l&eacute;ments du Site sous forme de Frame ou syst&egrave;me apparent&eacute;.</p>
+	<p>Enfin, la mise en place de lien hypertexte n\'autorise en aucune fa&ccedil;on de proposer l\'envoi d\'un message pr&eacute;-r&eacute;dig&eacute; &agrave; une adresse mail li&eacute;e au Site ou la mise en place d\'un syst&egrave;me permettant l\'envoi massif de messages quelle qu\'en soit la nature.</p>
+	<p>Tous les droits de reproduction sont r&eacute;serv&eacute;s.</p>', # MODIF
+	'mentions_liens_titre' => '<NEW>Liens hypertextes vers ce site :', # MODIF
+	'mentions_logo_cddp74' => '<NEW>Site officiel du CDDP74', # MODIF
+	'mentions_logo_citic' => '<NEW>Centre de l\'Informatique et des TIC de Haute-Savoie (ex CRI74)', # MODIF
+	'mentions_logo_edres' => '<NEW>&Eacute;ducation R&eacute;seau Haute-Savoie, projet d&eacute;partemental', # MODIF
+	'mentions_logo_eva' => '<NEW>Site officiel du projet eva-web', # MODIF
+	'mentions_logo_spip' => '<NEW>Site officiel de SPIP', # MODIF
+	'mentions_logo_spipedu' => '<NEW>Spip-edu, site de la communaut&eacute; &eacute;ducative', # MODIF
+	'mentions_lois' => '<NEW>En vertu de la loi pour la confiance dans l\'&eacute;conomie num&eacute;rique (LCEN) du 21 juin 2004, voici les coordonn&eacute;es de l\'&eacute;diteur et du prestataire qui accueille le site :', # MODIF
+	'mentions_prestataire' => '<NEW>Prestataire assurant le stockage direct et permanent :', # MODIF
+	'mentions_qualite' => '<NEW>Qualit&eacute; :', # MODIF
+	'mentions_qui_texte' => '<NEW><p>A l\'origine <a href="http://eva-web.edres74.net/eva/">EVA</a>
+	correspond &agrave; un projet de portail d\'Intranet d\'&Eacute;tablissement, install&eacute; sur 
+	les serveurs <a href="http://www.pingoo.org/">PingOO</a>.</p>
+	<p>Cette version, l&eacute;g&egrave;rement adapt&eacute;e, permet &agrave; une &eacute;cole ou &agrave; un &eacute;tablissement de mettre 
+	en place un site web collaboratif, en proposant diff&eacute;rents mod&egrave;les de publication 
+	(article, album photo, diaporama ...) </p>
+	<p>Pour plus de renseignements sur le projet <a href="http://eva-web.edres74.net">EVA-web</a>, consultez le site 
+  	<a href="http://eva-web.edres74.net">http://eva-web.edres74.net</a></p>
+	<p><a href="http://eva-web.edres74.net">EVA-web</a> est un logiciel libre distribu&eacute; sous Licence Publique G&eacute;n&eacute;rale 
+	<a rel="licence" target="_blank" href="http://www.april.org/gnu/gpl_french.html">GNU (GNU General Public License ou GPL)</a>
+	con&ccedil;u pour fonctionner &agrave; partir de l\'application <a href="http://www.spip.net/">SPIP</a>.</p>
+	<div style="text-align:center;"><a rel="licence" target="_blank" href="http://www.april.org/gnu/gpl_french.html"><img alt="Licence GNU GPL" style="border-width:0" width="80" height="100" src="http://www.fsf.org/graphics/philosophical-gnu-sm.jpg"/></a></div>', # MODIF
+	'mentions_qui_titre' => '<NEW>EVA, par qui, pour qui ?', # MODIF
+	'mentions_responsable_edition' => '<NEW>Responsable d\'&eacute;dition :', # MODIF
+	'mentions_site' => '<NEW>Site Internet de : ', # MODIF
+	'mentions_webmestre' => '<NEW>Webmestre :', # MODIF
+	'meteo' => '<NEW>Po&#269;asie',
+	'meteo_info' => '<NEW>Zemepisn&eacute; &uacute;daje',
+	'meteo_previsions' => '<NEW>Predpove&#271; po&#269;asia',
+	'mis_a_jour' => 'Updated: ', # MODIF
+	'mot_cle' => '<NEW>K&#318;&uacute;&#269;ov&eacute; slov&aacute;',
+	'multilinguisme' => 'Display the menu of language of EVA-Web in the public pages?', # MODIF
+	'multilinguisme_article' => '<NEW>Tento &#269;l&aacute;nok je v :',
+
+	// N
+	'notes' => '<NEW>Pozn&aacute;mky',
+
+	// P
+	'page_bas' => '<NEW>Bas de page', # MODIF
+	'page_haut' => '<NEW>Haut de page', # MODIF
+	'pages' => '<NEW>Str&aacute;nky',
+	'par' => 'by :', # MODIF
+	'partenaires' => '<NEW>Partneri',
+	'plan_du_site' => '<NEW>Mapa str&aacute;nky',
+	'podcasts' => '<NEW>Podcasty',
+	'podcasts_rss' => '<NEW>Podcasty a RSS',
+	'post_scriptum' => '<NEW>P.S.',
+	'pour' => '<NEW>pre',
+	'publie' => '<NEW>Publikovan&yacute;',
+
+	// R
+	'rechercher' => '<NEW>Vyh&#318;ada&#357;',
+	'redaction' => '<NEW>Redakcia',
+	'replier' => '<NEW>Zatvori&#357;',
+	'resultats' => '<NEW>V&yacute;sledky',
+
+	// S
+	'site' => '<NEW>site', # MODIF
+	'sites' => '<NEW>In&eacute; str&aacute;nky :',
+	'sites_references' => '<NEW>Odkazy na str&aacute;nky',
+	'sites_rubrique' => '<NEW>Odkazy v tejto rubrike',
+	'sites_syndic' => 'Syndicated sites', # MODIF
+	'sous_rubrique' => '<NEW>Podrubrika',
+	'statut_admin' => '<NEW>Status : Spr&aacute;vca',
+	'statut_redac' => '<NEW>Status : &#381;urnalista',
+	'statut_visit' => '<NEW>Status : N&aacute;v&#353;tevn&iacute;k',
+	'sur_le_web' => '<NEW>Na webe',
+	'sur_un_total_de' => '<NEW>Z celkov&eacute;ho po&#269;tu',
+	'syndic_breves' => '<NEW>Syndiquer les br&egrave;ves du site', # MODIF
+	'syndic_site' => '<NEW>Syndiquer tout le site', # MODIF
+
+	// T
+	'texte_page_404' => '<NEW><em>Prep&aacute;&#269;te .</em></br>Zd&aacute; sa, &#382;e t&aacute;to str&aacute;nka bola odstr&aacute;nen&aacute; z tohto webu.',
+	'tous_droits' => '<NEW>V&#353;etky pr&aacute;va vyhraden&eacute;',
+	'tous_les_auteurs' => '<NEW>V&#353;etci &#382;urnalisti',
+
+	// V
+	'version_eva' => 'EVA-Web 4.1', # MODIF
+	'visites' => '<NEW>N&aacute;v&#353;tevy',
+	'voir_en_ligne' => '<NEW>Zobrazi&#357; na webe',
+	'voir_image' => '<NEW>Zobrazi&#357; obr&aacute;zok v p&ocirc;vodnej ve&#318;kosti',
+	'vous_etes_ici' => '<NEW>Nach&aacute;dzate sa tu',
+
+	// Z
+	'zone' => '<NEW>Chr&aacute;nen&aacute; z&oacute;na'
+);
+
+?>
