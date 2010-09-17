3b47ea22940b83f39ddb72e878f724ae6d495b1d
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-17 17:01:21 +0000

    Il manquait quelques plugins à l'installation automatique de SoyezCréateurs
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@40839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -11,6 +11,7 @@ mkdir plugins_soyezcreateurs
 cd plugins_soyezcreateurs
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acronymes
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/agenda/2_0_0 ./agenda/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/bandeau
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/boutonstexte
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/cfg
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse
@@ -18,12 +19,18 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/crayons
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fonctions_images
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/forms/forms_et_tables_2_0 ./forms/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fulltext
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gestion_documents
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/job_queue
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/mediabox
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/mediatheque
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/noie
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nospam
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notifications
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/enluminures_typographiques_v3
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/partout
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/previsu_redaction
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/protection_formulaires
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/saisies
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/slogan
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/savecfg
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-2
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs
 cd ../..
@@ -34,21 +41,29 @@ cd plugins
 #sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/woopra
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/accessibilite
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acces_restreint
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/authentification/openid
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/autorite
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/2_0 ./clevermail/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/critere_mots
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/eva_freemind/eva_freemind_2_0
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gis
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/googlemap_api
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gravatar
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/insertion_modele
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/Lecteur_multimedia
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/memoization
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/menu_langues_liens
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/noie
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/microblog
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nyroceros
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/rainette
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/sedna
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/thickbox2
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/
-
-
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/socialtags
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip2pdf
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/splickrbox
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/twidget
 cd ..
 
 #Installation du squelette SoyezCreateur : il faut le faire dans un sous dossier
