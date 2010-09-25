dd50e961a722d1218c7453cb1a2e373098273fa6
cyblors@gmail.com <cyblors@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-25 08:05:03 +0000

    Ajout de la localisation anglaise par H! & PAB
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@41116 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oswd/3626_multiflex-3/lang/multiflex_en.php
@@ -0,0 +1,121 @@
+<?php
+
+$GLOBALS['i18n_multiflex_en'] = array(
+
+// pour entete
+'aller_accueil' => 'Go to home page',
+'a_propos' => 'About',
+'contact' => 'Contact',
+'contactez_nous' => 'Contact us',
+'ok' => 'GO!',
+'qui_sommes_nous' => 'Who are we?',
+
+// pour navigation
+'navigation' => 'Navigation',
+
+// pour centre
+'a_la_une' => 'Lead story',
+'autres_sites' => 'Other sites',
+'actualites' => 'news',
+'commentaire' => 'comment',
+'commentaires' => 'comments',
+'documents_joints' => 'Attached documents',
+'dans_rubrique' => '//',
+'dans_groupe' => '//',
+'derniers_sites' => 'Last sites',
+'lire_suite' => 'Read more&nbsp;&raquo;',
+'liens' => 'Hyperlinks',
+'mots_cles' => 'Keywords',
+'proposer_site_dans_rubrique' => 'Suggest a site in this category',
+'sur' => '//',
+'voir_site' => 'See site&nbsp;&raquo;',
+
+// pied
+'designer_page' => 'Designer\'s website',
+'documentation_multiflex' => 'Multiflex documentation',
+'habillage_de' => 'Graphic design from',
+'modifie_par' => 'Modified by',
+'modifieur_page' => 'Modifier\'s website',
+'pour' => 'for',
+'site_internet_spip' => 'SPIP website',
+'squelette_multiflex' => 'Multiflex template',
+'tous_droits_reserves' => 'All rights reserved',
+
+// pour liens CFG
+'multiflex' => 'Multiflex',
+'multiflex_general' => 'Multiflex',
+'multiflex_couleurs' => 'Colors',
+'multiflex_navigation' => 'Navigation',
+'multiflex_layouts' => 'Structure',
+
+// pour cfg_multiflex
+'afficher_nombre_commentaires' => 'Show number of comments',
+'apparence_layout' => 'Body choice:',
+'apparence_header' => 'Header choice:',
+'apparence_navigation' => 'Lateral navigation appearance choice',
+
+'blocs' => 'Blocs',
+
+'coefficients_teinte' => 'Coefficients between -1 and 1',
+'configuration_multiflex' => 'Multiflex template configuration',
+'configuration_principale' => 'Major configuration',
+'complement_nom_site' => 'Website name complement',
+'couleurs' => 'Colors',
+'couleur_bloc_bleu_titre' => 'Blue, heading',
+'couleur_bloc_bleu_fond' => 'Blue, background',
+'couleur_bloc_orange_titre' => 'Orange, heading',
+'couleur_bloc_orange_fond' => 'Orange, background',
+'couleur_bloc_vert_titre' => 'Green, heading',
+'couleur_bloc_vert_fond' => 'Green, background',
+'couleur_actuelle' => 'Current color',
+'couleur_liens' => 'Hyperlink ',
+'couleur_liens_survoles' => 'Overflew hyperlink',
+'couleur_origine' => 'Original color',
+
+'decoration' => 'Decorating',
+'description_entete' => 'Header description',
+'descriptifs_optionnels' => 'Optionnal description',
+'descriptions_site' => 'Website description',
+'divers' => 'Divers',
+'dominante' => 'Teinte dominante',
+
+'generale' => 'General (**) color',
+'grille' => 'Grid',
+
+'header1' => 'Heading + headpiece + menu',
+'header2' => 'Heading + headpiece',
+'header3' => 'Heading + menu',
+'header4' => 'Headpiece + menu',
+'header5' => 'Heading',
+'header6' => 'Headpiece',
+'header7' => 'Menu',
+
+
+'layouts' => 'Headers and Layouts',
+
+'layout1' => '1 column',
+'layout2' => '2 column',
+'layout3' => '2 inverted column',
+'layout4' => '3 column',
+'layout5' => '3 inverted column',
+
+'non' => 'No',
+'nouvelle_couleur' => 'New color',
+
+'oui' => 'Oui',
+
+'positionnements_generaux' => 'Global positioning',
+'puces' => 'Dash',
+
+'relief' => 'Relief',
+'relief_fonds' => 'Theme relief:',
+
+'sans_puces' => 'Without dash',
+'sous_titre_nom_site' => 'Website headings subheading',
+
+'teintes' => 'Colors',
+
+'valeur' => 'Value'
+);
+
+?>
