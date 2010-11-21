9b81db7dbde595e8c95ae37d785e8a5361ce5c04
jml9340@gmail.com <jml9340@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-21 22:49:04 +0000

    affichage image aide dans partie privée : strpos =>strstr
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@42440 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- datice3/balise/daticeaide.php
+++ datice3/balise/daticeaide.php
@@ -2,7 +2,7 @@
 function balise_DATICEAIDE($p) {
 	$num=interprete_argument_balise (1,$p);
   	$add="http://datice.ac-creteil.fr/spip/spip.php?page=aide&id_article=".$num."?keepThis=true&amp;TB_iframe=true&amp;height=500&amp;width=500"  ;
-	if(strpos($_SERVER['REQUEST_URI'],"/ecrire/")){$vers="../";}
+	if(strstr($_SERVER['REQUEST_URI'],"/ecrire/")){$vers="../";}
 	else{$vers="";};
 	$destination=$vers."prive/images/aide.gif";		
 	$texte= "<a href='".$add."' class='thickbox' > <img src='".$destination."'> </a>";
