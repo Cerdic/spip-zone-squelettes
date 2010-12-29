7805781eadf2c5e023d466c2cd22da2554f298e6
jsb92@laposte.net <jsb92@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-29 22:26:37 +0000

    suppression du suivi de version de SPIP.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@43236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scolaspip/scolaspip_pipelines.php
+++ scolaspip/scolaspip_pipelines.php
@@ -32,12 +32,10 @@ function suivi_maj_spip () {
 			$base_revision_spip = $base_scolaspip[0] ;
 			$base_revision_scolaspip = $base_scolaspip[1];
 
-			if ($svn_revision_spip<$base_revision_spip) {$ret.="<div class='cadre cadre-e' style='background-color:red'><div class='cadre_padding'><ul><li><strong>SPIP</strong> n'est pas &agrave; jour</li></ul><center><a href='http://www.spip.net/fr_article1318.html' title='mettre à jour spip'><img src='"._DIR_PLUGIN_SCOLASPIP."/images/majspip.png' alt='mettre à jour SPIP'></a></center></div></div>";}
-				else {$ret.="<div class='cadre cadre-e'><div class='cadre_padding'><ul><li>SPIP &agrave; jour</span></li></ul></div></div>";};
-
+			
 			$ret.="<div class='cadre cadre-e'><div class='cadre_padding'><ul>";
 
-			if ($svn_revision_scolaspip<$base_revision_scolaspip) {$ret.="<li><span style=\"color:green\">Nouvelle version de <strong>SCOLASPIP</strong></span></li></ul><center><a href='http://www.tice.ac-versailles.fr/Squelette-etablissement.html' title='mettre à jour scolaspip'><img src='"._DIR_PLUGIN_SCOLASPIP."/images/majscolaspip.png' alt='mettre à jour ScolaSPIP'></a></center>";} else {$ret.="<li>scolaspip &agrave; jour</li></ul>";};
+			if ($svn_revision_scolaspip<$base_revision_scolaspip) {$ret.="<li><span style=\"color:green\">Nouvelle version de <strong>SCOLASPIP</strong></span></li></ul><center><a href='http://www.tice.ac-versailles.fr/Squelette-etablissement.html' title='mettre &agrave; jour scolaspip'><img src='"._DIR_PLUGIN_SCOLASPIP."/images/majscolaspip.png' alt='mettre &agrave; jour ScolaSpip'></a></center>";} else {$ret.="<li>scolaspip &agrave; jour</li></ul>";};
 			$ret.="</div></div>";
 		}
 
