af37f831079565f5e056ce4aade9e7c24fb98eaf
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-26 19:54:42 +0000

    On modifie le nom des colonnes col-1 et col-2 en extra1 et extra2 Correction du body : le div englobant les colonnes 1 et 2 était unique, on le scinde de façon que chaque colonne soit dans un div particulier.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@44976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- z-dist/z_options.php
+++ z-dist/z_options.php
@@ -1,3 +1,3 @@
 <?php
-$GLOBALS['z_blocs'] = array('content','col-1','col-2','head','head_js','header','footer');
+$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
 ?>
