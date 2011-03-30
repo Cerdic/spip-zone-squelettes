fe66d8f7c733b8753e67ff91799b270fee82c8d8
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-30 20:05:20 +0000

    Des noisettes pour Aveline utilisant AnythingSlider
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@46176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ aveline-extras/aveline-anythingslider/lang/avelineanythingslider_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/aveline/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'description_anythingslider_liste_articles' => 'Affiche une liste d\'articles sous la forme de résumés déroulants.',
+	'label_build_arrows' => 'Afficher les flèches de navigation ?',
+	'label_height' => 'Hauteur de la fenêtre (en pixels) :',
+	'label_options_anythingslider' => 'Options du slider',
+	'label_options_resumes' => 'Options des résumés',
+	'label_resize_contents' => 'Redimensionner les contenus à la fenêtre ?',
+	'label_theme' => 'Thème du slider :',
+	'label_width' => 'Largeur de la fenêtre (en pixels) :',
+	'nom_anythingslider_liste_articles' => 'AnythingSlider avec une liste d\'articles',
+	
+);
+
+?>
