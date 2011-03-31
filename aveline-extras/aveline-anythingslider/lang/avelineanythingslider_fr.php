1696d8cf960b4edd0863ecc39320df78175b5970
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-31 11:01:34 +0000

    Quelques options additionnelles pour paramétrer le slider
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@46203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aveline-extras/aveline-anythingslider/lang/avelineanythingslider_fr.php
+++ aveline-extras/aveline-anythingslider/lang/avelineanythingslider_fr.php
@@ -7,11 +7,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	'description_anythingslider_liste_articles' => 'Affiche une liste d\'articles sous la forme de résumés déroulants.',
 	'label_build_arrows' => 'Afficher les flèches de navigation ?',
+	'label_build_navigation' => 'Afficher les onglets de navigation ?',
+	'label_delay' => 'Durée (en milisecondes) entre deux transistions :',
 	'label_height' => 'Hauteur de la fenêtre (en pixels) :',
 	'label_options_anythingslider' => 'Options du slider',
 	'label_options_resumes' => 'Options des résumés',
 	'label_resize_contents' => 'Redimensionner les contenus à la fenêtre ?',
+	'label_start_stopped' => 'Au chargement de la page, ne pas lancer automatiquement le défilement du slider ?',
 	'label_theme' => 'Thème du slider :',
+	'label_toggle_controls' => 'Afficher les contrôles de navigation seulement au survol ?',
 	'label_width' => 'Largeur de la fenêtre (en pixels) :',
 	'nom_anythingslider_liste_articles' => 'AnythingSlider avec une liste d\'articles',
 	
