9f82daa65503d650557fd1ac4897954141730f5b
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-05 14:46:32 +0000

    Compat Importeur/Exporteur de configuration
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@46372 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-vide/zvide_ieconfig_metas.php
@@ -0,0 +1,10 @@
+<?php
+
+function zvide_ieconfig_metas($table){
+	$table['zvide']['titre'] = _T('zvide:zvide');
+	$table['zvide']['icone'] = 'img/zvide-24.png';
+	$table['zvide']['metas_serialize'] = 'zvide';
+	return $table;
+}
+
+?>
