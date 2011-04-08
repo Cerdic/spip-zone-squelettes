2a9d892a4fe7f5139ac308e29ee6844cb396dad7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-08 22:09:24 +0000

    Dépôt du squelette Sight. Portage d'un thème wordpress en squelette SPIP. Demo : http://sight.wpshower.com/
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@46508 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sight/lang/local_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// H
+'habillage_par' => 'Habillage r&eacute;alis&eacute; par',
+
+// P
+'partage_delicious' => 'Mettre en favori sur Del.icio.us',
+'partage_facebook' => 'Partager sur Facebook',
+'partage_stumbleupon' => 'Partager sur StumbleUpon',
+'partage_twitter' => 'Faites passer le mot sur Twitter',
+'portage_spip_par' => 'Portage pour SPIP par',
+);
+?>
