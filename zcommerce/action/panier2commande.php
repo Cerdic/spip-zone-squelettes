44f52dfdb36abb7091ff45db44f4add0e8f2d5e2
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-24 17:06:24 +0000

    on poursuit la demo basic du squelette de zcommerce, manque le plugin abonnement (en cours de rénovation) puis les notifications. bref, dev de dev
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@49037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zcommerce/action/panier2commande.php
+++ zcommerce/action/panier2commande.php
@@ -5,10 +5,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
  * Remplir la commande avec le panier en cours
- * appel au plugin commandes
+ * cf plugin commandes
  * puis panier2commandes
  */
 function action_panier2commande_dist($arg=null) {
+		
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$args = $securiser_action();
 	
 		include_spip('inc/commandes');
 		creer_commande_encours();
---
+++ zcommerce/lang/zcommerce_fr.php
@@ -0,0 +1,30 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+'continuer'=>'Continuer',
+'aucun_produit'=>'Aucun produit',
+'les_produits'=>'Les produits',
+'ajouter_au_panier'=>'Ajouter au panier',
+'boutique'=>'Boutique',
+'panier'=>'Panier',
+'coordonnees'=>'Coordonn&eacute;es',
+'commande'=>'Commande',
+'paiement'=>'Paiement',
+'modifiez_vos_coordonnees'=>'Modifiez vos coordonn&eacute;es',
+'deja_inscrit'=>'D&eacute;ja inscrit',
+'vous_inscrire'=>'Vous inscrire',
+'vider_le_panier'=>'Vider le panier',
+'votre_commande'=>'Votre commande',
+'votre_commande_sur'=>'Votre commande sur @nom@',
+'une_commande_sur'=>'Une commande sur @nom@',
+'merci_de_votre_commande'=>'Nous vous remercions de votre commande @ref@',
+'vos_commandes_payees'=>'Vos commandes pay&eacute;es',
+'total'=>'Total',
+'acces_facture'=>'Facture',
+'paiement'=>'Paiement',
+'paiement_fictif'=>'Paiement fictif',
+'passer_la_commande'=>'Passer la commande',
+'aucune_commande_valide'=>'Il n\'y a (actuellement) aucune commande.',
+	
+);
+?>
