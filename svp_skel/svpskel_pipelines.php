08530488650391974f560bf86904a7a3619a3ae5
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-27 22:05:36 +0000

    Correction du nom de la fonction pipeline maintenant préfixée par svpskel. Transfert de l'image utilisée dans les css de SVP vers ce plugin et rangement dans css/img/
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@49114 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_skel/svpskel_pipelines.php
+++ svp_skel/svpskel_pipelines.php
@@ -7,7 +7,7 @@
  * @param object $flux
  * @return $flux
  */
-function svp_insert_head_css($flux) {
+function svpskel_insert_head_css($flux) {
 	if ($f = find_in_path('css/svp_habillage.css')) {
 		$flux .= '<link rel="stylesheet" href="'.direction_css($f).'" type="text/css" media="all" />';
 	}
