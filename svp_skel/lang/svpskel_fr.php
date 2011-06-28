46fc1713768f6a3b4154b91c5bb7e584db903c67
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-28 21:22:14 +0000

    Début de nettoyage et répartition des chaines de langue. Attention, le module n'a pas encore été corrigé dans SVP Squelettes
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@49148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/svpskel_fr.php
@@ -0,0 +1,62 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: svpskel
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bulle_aller_categorie' => 'Se rendre sur la page des plugins de cette catégorie',
+	'bulle_aller_demonstration' => 'Se rendre sur la page de démonstration',
+	'bulle_aller_depots' => 'Se rendre sur la page d\'accueil des dépôts',
+	'bulle_aller_developpement' => 'Se rendre sur la zone de développement',
+	'bulle_aller_plugins' => 'Se rendre sur la page d\'accueil des plugins',
+	'bulle_aller_prefixes' => 'Se rendre sur la page des préfixes',
+	'bulle_aller_sources' => 'Consulter les sources du paquet',
+	'bulle_aller_statistiques' => 'Se rendre sur la page des statistiques',
+	'bulle_aller_telechargements' => 'Se rendre sur la page des téléchargements',
+	'bulle_filtrer_par_categorie' => 'Filtrer sur la catégorie',
+	'bulle_filtrer_par_depot' => 'Filtrer sur le dépôt',
+	'bulle_filtrer_par_spip' => 'Filtrer sur la branche SPIP',
+	'bulle_supprimer_filtre' => 'Supprimer le filtre',
+
+// C
+
+
+// I
+	'info_aucun_depot_disponible' => 'Aucun dépôt disponible.',
+	'info_aucun_module_utilise' => 'aucun module de langue utilisé',
+	'info_aucun_paquet_disponible' => 'Aucun paquet disponible.',
+	'info_aucun_plugin_disponible' => 'Aucun plugin disponible.',
+	'info_aucune_compatibilite_spip' => 'Non communiquée',
+	'info_aucune_contribution_disponible' => 'Aucune autre contribution disponible en dehors des plugins',
+	'info_stats_plugins' => 'Le tableau ci-dessous affiche le nombre de plugins hébergés pour chacune des catégories disponibles.',
+	'intertitre_depot_autres_contributions' => 'Liste des autres contributions',
+	'intertitre_depot_plugins' => 'Liste des plugins hébergés',
+	'intertitre_paquets_contribution' => 'Les autres contributions',
+	'intertitre_paquets_plugin' => 'Les plugins',
+	'intertitre_stats_categories' => 'Compteurs par catégorie',
+	'intertitre_stats_globales' => 'Compteurs globaux',
+	'info_actualisation_depot_cron' => 'Les plugins des dépôts sont actualisés automatiquement toutes les @periode@ heure(s).',
+	'info_stats_categories' => '@nb_categories@ catégorie(s) disponible(s) sur 13 possibles.',
+
+// L
+
+
+// M
+
+
+// O
+
+
+// R
+
+
+// T
+
+
+);
+?>
