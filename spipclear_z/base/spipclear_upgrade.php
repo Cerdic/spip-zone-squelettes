04db3e5d7f38b277289220ba7ca4bfb18d92a8a4
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-15 19:45:34 +0000

    - phpdocs - à la première phase d'install ecrire 0.0 dans le meta et non ma version cible
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@49665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipclear_z/base/spipclear_upgrade.php
+++ spipclear_z/base/spipclear_upgrade.php
@@ -5,6 +5,13 @@ include_spip('inc/meta');
 include_spip('inc/cextras_gerer');
 include_spip('spipclear_pipelines');
 
+/**
+ * Fonction d'upgrade/maj
+ * On crée une configuration par défaut
+ *
+ * @param string $nom_meta_base_version
+ * @param string $version_cible
+ */
 function spipclear_upgrade($nom_meta_base_version,$version_cible){
 	$current_version = "0.0";
 	if (	(!isset($GLOBALS['meta'][$nom_meta_base_version]))
@@ -15,26 +22,31 @@ function spipclear_upgrade($nom_meta_base_version,$version_cible){
 		if (version_compare($current_version,'0.0','=')){
 			$config_defaut = _DIR_PLUGIN_SPIPCLEAR . 'config_noizetier/spipclear.yaml';
 			$yaml = spipclear_get_config_noizetier($config_defaut);
-			if( noizetier_importer_configuration('remplacer', 'non', $yaml)) 
-				ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-			else
+			if( noizetier_importer_configuration('remplacer', 'non', $yaml)) {
+				ecrire_meta($nom_meta_base_version,'0.0','non');
+			} else {
 				echo _T('spipclear:msg_erreur_v00')."<br/>";
+			}
 		}
 		if (version_compare($current_version,'0.2','<=')){	
 			$champs = spipclear_declarer_champs_extras();
-			if(installer_champs_extras($champs, $nom_meta_base_version, $version_cible))
-				ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-			else
+			// C'est le plugin Champs Extras qui ecrit le meta
+			if(! installer_champs_extras($champs, $nom_meta_base_version, $version_cible)) {
 				echo _T('spipclear:msg_erreur_v02')."<br/>";
+			}
 		}
 	}
 }
 
+/**
+ * Fonction de desinstallation
+ *
+ * @param float $nom_meta_base_version
+ */
 function spipclear_vider_tables($nom_meta_base_version) {
 	$champs = spipclear_declarer_champs_extras();
+	// C'est le plugin Champs Extras qui supprime le meta
 	desinstaller_champs_extras($champs, $nom_meta_base_version);
-
-	effacer_meta($nom_meta_base_version);
 }
 
 
