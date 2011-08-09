82c37b53665425051bfee7347d8f3c35a1edcb94
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-09 10:19:45 +0000

    Déclaration des pages publiques depot et plugin pour afficher le bouton voir_en_ligne dans les pages privées de ces mêmes objet. Suite du commit SVP 50391
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@50392 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/svpskel_options.php
@@ -0,0 +1,7 @@
+<?php
+
+// Liste des pages publiques d'objet supportees par le squelette (depot, plugin).
+// Permet d'afficher le bouton voir en ligne dans la page d'edition de l'objet
+define('_SVP_PAGES_OBJET_PUBLIQUES', 'depot:plugin');
+
+?>
