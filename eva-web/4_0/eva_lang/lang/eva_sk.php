4f67821caf473bf8e2743dd99bf680f8eeba95a1
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:45:33 +0000

    langues (eva)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@51507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_lang/lang/eva_sk.php
+++ eva-web/4_0/eva_lang/lang/eva_sk.php
@@ -8,209 +8,209 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'acces_restreint' => '<NEW>Obmedzený prístup',
-	'accueil' => '<NEW>Domovská stránka',
-	'agenda' => '<NEW>Diár',
-	'aide' => '<NEW>Nápoveda',
-	'article_complet' => '<NEW>Celé články',
-	'article_precedent' => '<NEW>Predchádzajúci',
-	'article_precedent_premier' => '<NEW>Prvý článok',
-	'article_suivant' => '<NEW>Ďalší',
-	'article_suivant_dernier' => '<NEW>Najnovší článok',
-	'articles' => '<NEW>Články',
-	'aucun_evenement' => '<NEW>Žiadna plánovaná udalosť nie je zapísaná v diári na tento mesiac',
-	'aucun_resultat_pour' => '<NEW>Žiadne výsledky pre',
+	'acces_restreint' => 'Obmedzený prístup',
+	'accueil' => 'Úvodná stránka',
+	'agenda' => 'Diár',
+	'aide' => 'Pomocník',
+	'article_complet' => 'Celý článok',
+	'article_precedent' => 'Predchádzajúci',
+	'article_precedent_premier' => 'Prvý článok',
+	'article_suivant' => 'Ďalší',
+	'article_suivant_dernier' => 'Posledný',
+	'articles' => 'Články',
+	'aucun_evenement' => 'V diári nie je na tento mesiac zapísaná žiadna udalosť',
+	'aucun_resultat_pour' => 'Žiadne výsledky pre',
 
 	// B
-	'breves' => '<NEW>Novinky',
-	'breves_rubrique' => '<NEW>Novinky v tejto rubrike',
+	'breves' => 'Novinky',
+	'breves_rubrique' => 'Novinky v tejto rubrike',
 
 	// C
-	'contact' => '<NEW>Kontakt',
-	'copyright_eva' => 'and use template', # MODIF
+	'contact' => 'Kontakt',
+	'copyright_eva' => 'a používať šablónu',
 	'copyright_spip' => 'This site is managed by',
 
 	// D
-	'de_cet_auteur' => 'By this author', # MODIF
-	'deconnecter' => '<NEW>Odhlásiť sa',
-	'derniere_mise_a_jour' => '<NEW>Najnovšie aktualizácie',
-	'dernieres_breves' => '<NEW>Najnovšie novinky',
-	'derniers_articles' => '<NEW>Najnovšie články',
-	'derniers_commentaires' => '<NEW>Najnovšie komentáre',
-	'derniers_podcasts' => '<NEW>Najnovšie podcasty',
-	'derniers_sites' => '<NEW>Najnovšie stránky',
-	'diaporama' => 'Slideshow', # MODIF
-	'dix_meilleurs_articles' => '<NEW>Najlepších 10 článkov',
-	'dix_meilleurs_breves' => '<NEW>Najlepších 10 noviniek',
-	'dix_meilleurs_commentaires' => '<NEW>Najlepších 10 komentárov',
+	'de_cet_auteur' => 'Od tohto autora',
+	'deconnecter' => 'Odhlásiť sa',
+	'derniere_mise_a_jour' => 'Najnovšie aktualizácie',
+	'dernieres_breves' => 'Najnovšie novinky',
+	'derniers_articles' => 'Najnovšie články',
+	'derniers_commentaires' => 'Najnovšie komentáre',
+	'derniers_podcasts' => 'Najnovšie podcasty',
+	'derniers_sites' => 'Najnovšie stránky',
+	'diaporama' => 'Prezentácia',
+	'dix_meilleurs_articles' => 'Najlepších 10 článkov',
+	'dix_meilleurs_breves' => '10 najlepších noviniek',
+	'dix_meilleurs_commentaires' => '10 najlepších komentárov',
 	'doc_redacteurs' => 'Online documents for writers using EVA_Red',
-	'document' => '<NEW>Dokument',
-	'documents_joints' => '<NEW>Prílohy',
+	'document' => 'Dokument',
+	'documents_joints' => 'Prílohy',
 
 	// E
-	'erreur_404' => '<NEW>Chyba 404',
-	'evenement_aucun' => '<NEW>Žiadna udalosť nie je v diári naplánovaná na tento mesiac',
-	'evenements_a_venir' => '<NEW>Nasledujúce udalosti',
-	'evenements_du' => '<NEW>Udalosti na',
-	'evenements_passes' => '<NEW>Udalosti v minulosti',
-	'evenements_passes_aucun' => '<NEW>Žiadne udalosti v minulosti nie sú v diári',
+	'erreur_404' => 'Chyba 404',
+	'evenement_aucun' => 'Na tento mesiac nie je v diári naplánovaná žiadna udalosť.',
+	'evenements_a_venir' => 'Nasledujúce udalosti',
+	'evenements_du' => 'Udalosti do',
+	'evenements_passes' => 'Udalosti v minulosti',
+	'evenements_passes_aucun' => 'V diári nie sú zapísané žiadne udalosti z minulosti.',
 
 	// F
-	'fermer_fenetre' => '<NEW>Zatvoriť okno',
-	'feuilleter_livre' => '<NEW>Prelistovať knihu',
-	'form_pet_message_commentaire' => '<NEW>Jeden komentár?',
+	'fermer_fenetre' => 'Zatvoriť okno',
+	'feuilleter_livre' => 'Prelistovať knihu',
+	'form_pet_message_commentaire' => 'Komentár?',
 
 	// G
-	'galaxie_spip' => '<NEW>Galaxia SPIP',
-	'go' => '<NEW>Prejsť na',
+	'galaxie_spip' => 'Galaxia SPIPu',
+	'go' => 'Prejsť na',
 
 	// I
-	'icone_eva' => '<NEW>Icone EVA-web 4', # MODIF
-	'identifier' => '<NEW>Ste pripojený',
-	'il_y_a' => 'There is', # MODIF
-	'il_y_a1' => '<NEW>Podpis(y) pre túto petíciu',
-	'il_y_a2' => 'There is in total', # MODIF
-	'il_y_a3' => 'Article(s).</br> This block in poster', # MODIF
-	'il_y_a4' => 'Author(s).</br> This block in poster', # MODIF
+	'icone_eva' => 'Ikona EVA-web 4',
+	'identifier' => 'Ste prihlásený',
+	'il_y_a' => 'Je',
+	'il_y_a1' => 'podpis(y/ov) na túto petíciu.',
+	'il_y_a2' => 'Spolu je',
+	'il_y_a3' => 'článok(-nky/-nkov).</br> Tento blok je pútač',
+	'il_y_a4' => 'autor(i/ov).</br> Tento blok v pútači',
 	'il_y_a5' => 'News.</br>This block in poster',
-	'inscription' => '<NEW>Inscription', # MODIF
+	'inscription' => 'Prihlásenie',
 
 	// J
-	'j1' => '<NEW>Pon',
-	'j2' => '<NEW>Uto',
-	'j3' => '<NEW>Str',
-	'j4' => '<NEW>Štv',
-	'j5' => '<NEW>Pia',
-	'j6' => '<NEW>Sob',
-	'j7' => '<NEW>Ned',
-	'jo1' => '<NEW>Pondelok',
-	'jo2' => '<NEW>Utorok',
-	'jo3' => '<NEW>Streda',
-	'jo4' => '<NEW>Štvrtok',
-	'jo5' => '<NEW>Piatok',
-	'jo6' => '<NEW>Sobota',
-	'jo7' => '<NEW>Nedeľa',
+	'j1' => 'pon',
+	'j2' => 'uto',
+	'j3' => 'str',
+	'j4' => 'štv',
+	'j5' => 'pia',
+	'j6' => 'sob',
+	'j7' => 'ned',
+	'jo1' => 'Pondelok',
+	'jo2' => 'Utorok',
+	'jo3' => 'Streda',
+	'jo4' => 'Štvrtok',
+	'jo5' => 'Piatok',
+	'jo6' => 'Sobota',
+	'jo7' => 'Nedeľa',
 
 	// L
-	'lancer_diaporama' => 'Open Slideshow', # MODIF
-	'lien_externe' => '<NEW>Externý odkaz, otvorí sa v novom okne',
-	'lire_suite' => '<NEW>Čítať ďalej',
+	'lancer_diaporama' => 'Spustiť prezentáciu',
+	'lien_externe' => 'Externý odkaz na stránku, otvorí sa v novom okne',
+	'lire_suite' => 'Čítať ďalej',
 
 	// M
-	'm1' => '<NEW>Január',
-	'm10' => '<NEW>Október',
-	'm11' => 'November', # MODIF
+	'm1' => 'január',
+	'm10' => 'október',
+	'm11' => 'november',
 	'm12' => 'December',
-	'm2' => '<NEW>Február',
-	'm3' => '<NEW>Marec',
-	'm4' => '<NEW>Apríl',
-	'm5' => '<NEW>Máj',
-	'm6' => '<NEW>Jún',
-	'm7' => '<NEW>Júl',
+	'm2' => 'február',
+	'm3' => 'marec',
+	'm4' => 'apríl',
+	'm5' => 'máj',
+	'm6' => 'jún',
+	'm7' => 'júl',
 	'm8' => 'August',
-	'm9' => 'September', # MODIF
-	'meme_rubrique' => '<NEW>V tejto rubrike',
-	'mentions' => '<NEW>Poznámky',
-	'mentions_adresse' => '<NEW>Adresse :', # MODIF
-	'mentions_directeur_publication' => '<NEW>Directeur de la publication :', # MODIF
-	'mentions_droit_auteur_texte' => '<NEW><p>Ce site relève de la législation française et internationale sur le droit d\'auteur et la propriété intellectuelle.</p>
-	<p>Tous les droits de reproduction sont réservés.</p>',
-	'mentions_droit_auteur_titre' => '<NEW>Droits d\'auteurs :', # MODIF
+	'm9' => 'september',
+	'meme_rubrique' => 'V tejto rubrike',
+	'mentions' => 'Pripomienky',
+	'mentions_adresse' => 'Adresa:',
+	'mentions_directeur_publication' => 'Zodpovedný redaktor:',
+	'mentions_droit_auteur_texte' => '<p>Táto stránka spadá pod ochranu francúzskych a medzinárodných autorských a duševného vlastníctva.</p>
+ <p>Všetky práva vyhradené.</p>',
+	'mentions_droit_auteur_titre' => 'Autorské práva:',
 	'mentions_legales' => 'Legal notes',
-	'mentions_liens_hypertexte_texte' => '<NEW><p>Ce site contient des liens hypertextes permettant l\'accès à des sites qui ne sont pas édités par le responsable de ce site.</p>
-	<p>En conséquence le directeur de publication ne saurait être tenu pour responsable du contenu des sites auxquels l\'internaute aurait ainsi accès.</p>
-	<p>Il est formellement interdit de collecter et d\'utiliser les informations disponibles sur le site à des fins commerciales.</p>
-	<p>Cette interdiction s\'étend notamment, sans que cette liste ne soit limitative, à tout élément rédactionnel figurant sur le site, à la présentation des écrans, aux logiciels nécessaires à l\'exploitation, aux logos, images, photos, graphiques, de quelque nature qu\'ils soient.</p>',
-	'mentions_liens_hypertexte_titre' => '<NEW>Liens hypertextes :', # MODIF
-	'mentions_liens_texte' => '<NEW><p>A l\'exception de sites diffusant des informations et/ou contenus ayant un caractère illégal et/ou à caractère politique, religieux, pornographique, xénophobe, vous pouvez créer un lien hypertexte vers notre Site sur votre site.</p>
-	<p>La mise en place de lien hypertexte n\'autorise en aucune façon la reproduction d\'éléments du Site ou la présentation sur des sites tiers d\'éléments du Site sous forme de Frame ou système apparenté.</p>
-	<p>Enfin, la mise en place de lien hypertexte n\'autorise en aucune façon de proposer l\'envoi d\'un message pré-rédigé à une adresse mail liée au Site ou la mise en place d\'un système permettant l\'envoi massif de messages quelle qu\'en soit la nature.</p>
-	<p>Tous les droits de reproduction sont réservés.</p>',
-	'mentions_liens_titre' => '<NEW>Liens hypertextes vers ce site :', # MODIF
-	'mentions_logo_cddp74' => '<NEW>Site officiel du CDDP74', # MODIF
-	'mentions_logo_citic' => '<NEW>Centre de l\'Informatique et des TIC de Haute-Savoie (ex CRI74)', # MODIF
-	'mentions_logo_edres' => '<NEW>Éducation Réseau Haute-Savoie, projet départemental',
-	'mentions_logo_eva' => '<NEW>Site officiel du projet eva-web', # MODIF
-	'mentions_logo_spip' => '<NEW>Site officiel de SPIP', # MODIF
-	'mentions_logo_spipedu' => '<NEW>Spip-edu, site de la communauté éducative',
-	'mentions_lois' => '<NEW>En vertu de la loi pour la confiance dans l\'économie numérique (LCEN) du 21 juin 2004, voici les coordonnées de l\'éditeur et du prestataire qui accueille le site :',
-	'mentions_prestataire' => '<NEW>Prestataire assurant le stockage direct et permanent :', # MODIF
-	'mentions_qualite' => '<NEW>Qualité :',
-	'mentions_qui_texte' => '<NEW><p>A l\'origine <a href="http://eva-web.edres74.net/eva/">EVA</a>
-	correspond à un projet de portail d\'Intranet d\'Établissement, installé sur 
-	les serveurs <a href="http://www.pingoo.org/">PingOO</a>.</p>
-	<p>Cette version, légèrement adaptée, permet à une école ou à un établissement de mettre 
-	en place un site web collaboratif, en proposant différents modèles de publication 
-	(article, album photo, diaporama ...) </p>
-	<p>Pour plus de renseignements sur le projet <a href="http://eva-web.edres74.net">EVA-web</a>, consultez le site 
-  	<a href="http://eva-web.edres74.net">http://eva-web.edres74.net</a></p>
-	<p><a href="http://eva-web.edres74.net">EVA-web</a> est un logiciel libre distribué sous Licence Publique Générale 
-	<a rel="licence" target="_blank" href="http://www.april.org/gnu/gpl_french.html">GNU (GNU General Public License ou GPL)</a>
-	conçu pour fonctionner à partir de l\'application <a href="http://www.spip.net/">SPIP</a>.</p>
-	<div style="text-align:center;"><a rel="licence" target="_blank" href="http://www.april.org/gnu/gpl_french.html"><img alt="Licence GNU GPL" style="border-width:0" width="80" height="100" src="http://www.fsf.org/graphics/philosophical-gnu-sm.jpg"/></a></div>',
-	'mentions_qui_titre' => '<NEW>EVA, par qui, pour qui ?', # MODIF
-	'mentions_responsable_edition' => '<NEW>Responsable d\'édition :',
-	'mentions_site' => '<NEW>Site Internet de : ', # MODIF
-	'mentions_webmestre' => '<NEW>Webmestre :', # MODIF
-	'meteo' => '<NEW>Počasie',
-	'meteo_info' => '<NEW>Zemepisné údaje',
-	'meteo_previsions' => '<NEW>Predpoveď počasia',
+	'mentions_liens_hypertexte_texte' => '<p>Na tejto stránke sú hypertextové odkazy, ktoré neupravuje zodpovedný redaktor tejto stránky.</p>
+ <p>Nemôže byť preto zodpovedný za obsah stránok, ku ktorým sa používatelia môžu dostať.</p>
+ <p>Zbierať a používať údaje zo stránky na komerčné účely je zakázané.</p>
+ <p>Tento zákaz sa vzťahuje na (a nie je tým obmedzený) na všetky redakčné prvky na stránke, prezentačné obrazovky, softvér potrebný na prevádzku, logá, obrázky, fotky, grafiku každého druhu.</p>',
+	'mentions_liens_hypertexte_titre' => 'Hypertextové odkazy:',
+	'mentions_liens_texte' => '<p>Ak nemáte stránku, ktorá poskytuje osobné údaje, a/lebo je ilegálna, a/lebo politická, náboženská, pornografická, xenofóbna, môžete na svojej stránke vytvoriť odkaz na našu stránku.</p>
+ <p>Pridanie odkazu vás v žiadnom prípade neoprávňuje na reprodukciu prvkov stránky ani na prezentáciu prvkov stránok tretích strán v podobe rámu alebo podobnej forme.</p>
+ <p>Napokon, vytvorenie hypertextového odkazu v žiadnom prípade neoprávňuje ani na posielanie správ na predvolenú adresu prepojenú so stránkou a neoprávňuje ani na vytvorenie systému na posielanie hromadných správ bez ohľadu na ich obsah.</p>
+ <p>Všetky práva vyhradené.</p>',
+	'mentions_liens_titre' => 'Hypertextové odkazy na túto stránku:',
+	'mentions_logo_cddp74' => 'Oficiálna stránka CDDP74',
+	'mentions_logo_citic' => 'Centrum pre informatiku a IKT Haute-Savoie (napr. CRI74)',
+	'mentions_logo_edres' => 'Vzdelávacia sieť Haute-Savoie, projekt départementu',
+	'mentions_logo_eva' => 'Oficiálna stránka projektu eva-web',
+	'mentions_logo_spip' => 'Oficiálna stránka SPIPu',
+	'mentions_logo_spipedu' => 'Spip-edu, stránky škôl',
+	'mentions_lois' => 'Podľa zákona o dôvere v digitálnej ekonomike (LCEN) z 21. júna 2004, kontakt na vydavateľa a poskytovateľa, ktorý je hostiteľom webu:',
+	'mentions_prestataire' => 'Služba, ktorá poskytuje priame a trvalé uloženie dát:',
+	'mentions_qualite' => 'Kvalita:',
+	'mentions_qui_texte' => '<p>Pôvodne <a href="http://eva-web.edres74.net/eva/">EVA</a>
+ zodpovedá projektu intranetového portálu inštitúcie nainštalovanom 
+ na serveroch <a href="http://www.pingoo.org/">PingOO.</a></p>
+ <p>Táto mierne upravená verzia  umožňuje škole alebo inštitúcii vytvoriť si
+ internetovú stránku na vzájomnú spoluprácu, pričom sú v ponuke rôzne modely publikovania 
+ (článok, fotoalbum, prezentácia a pod.) </p>
+ <p>Ak chcete zistiť viac informácií o projekte <a href="http://eva-web.edres74.net">EVA-web,</a> navštívte stránku 
+   <a href="http://eva-web.edres74.net">http://eva-web.edres74.net</a></p>
+ <p><a href="http://eva-web.edres74.net">EVA-web</a> je slobodný softvér, ktorý sa distribuuje pod licenciou GNU General Public License 
+ <a rel="licence" target="_blank" href="http://sk.wikipedia.org/wiki/GNU_General_Public_License">GNU (GNU General Public Licencia GPL)</a>
+ určená na používanie <a href="http://www.spip.net/">SPIPu.</a></p>
+ <div style="text-align:center;"><a rel="licence" target="_blank" href="http://sk.wikipedia.org/wiki/GNU_General_Public_License"><img alt="Licencia GNU GPL" style="border-width:0" width="80" height="100" src="http://www.fsf.org/graphics/philosophical-gnu-sm.jpg"/></a></div>',
+	'mentions_qui_titre' => 'EVA, od koho, pre koho?',
+	'mentions_responsable_edition' => 'Zodpovedný za publikovanie:',
+	'mentions_site' => 'Internetová stránka: ',
+	'mentions_webmestre' => 'Webmaster:',
+	'meteo' => 'Počasie',
+	'meteo_info' => 'Zemepisné údaje',
+	'meteo_previsions' => 'Predpoveď počasia',
 	'mis_a_jour' => 'Updated: ',
-	'mot_cle' => '<NEW>Kľúčové slová',
-	'multilinguisme' => 'Display the menu of language of EVA-Web in the public pages?', # MODIF
-	'multilinguisme_article' => '<NEW>Tento článok je v :',
+	'mot_cle' => 'Kľúčové slová',
+	'multilinguisme' => 'Zobraziť jazykové menu EVA-Webu na verejne prístupných stránkach?',
+	'multilinguisme_article' => 'Tento článok je v:',
 
 	// N
-	'notes' => '<NEW>Poznámky',
+	'notes' => 'Poznámky',
 
 	// P
-	'page_bas' => '<NEW>Bas de page', # MODIF
-	'page_haut' => '<NEW>Haut de page', # MODIF
-	'pages' => '<NEW>Stránky',
-	'par' => 'by :', # MODIF
-	'partenaires' => '<NEW>Partneri',
-	'plan_du_site' => '<NEW>Mapa stránky',
-	'podcasts' => '<NEW>Podcasty',
-	'podcasts_rss' => '<NEW>Podcasty a RSS',
-	'post_scriptum' => '<NEW>P.S.',
-	'pour' => '<NEW>pre',
-	'publie' => '<NEW>Publikovaný',
+	'page_bas' => 'Päta stránky',
+	'page_haut' => 'Hlavička stránky',
+	'pages' => 'Stránky',
+	'par' => 'od:',
+	'partenaires' => 'Partneri',
+	'plan_du_site' => 'Mapa stránky',
+	'podcasts' => 'Podcasty',
+	'podcasts_rss' => 'Podcasty a RSS',
+	'post_scriptum' => 'Post scriptum',
+	'pour' => 'pre',
+	'publie' => 'Publikovaný:',
 
 	// R
-	'rechercher' => '<NEW>Vyhľadať',
-	'redaction' => '<NEW>Redakcia',
-	'replier' => '<NEW>Zatvoriť',
-	'resultats' => '<NEW>Výsledky',
+	'rechercher' => 'Vyhľadať',
+	'redaction' => 'Redakcia',
+	'replier' => 'Zatvoriť',
+	'resultats' => 'Výsledky',
 
 	// S
-	'site' => '<NEW>site', # MODIF
-	'sites' => '<NEW>Iné stránky :',
-	'sites_references' => '<NEW>Odkazy na stránky',
-	'sites_rubrique' => '<NEW>Odkazy v tejto rubrike',
+	'site' => 'stránka',
+	'sites' => 'Iné stránky:',
+	'sites_references' => 'Odkazy na stránky',
+	'sites_rubrique' => 'Odkazy v tejto rubrike',
 	'sites_syndic' => 'Syndicated sites',
-	'sous_rubrique' => '<NEW>Podrubrika',
-	'statut_admin' => '<NEW>Status : Správca',
-	'statut_redac' => '<NEW>Status : Žurnalista',
-	'statut_visit' => '<NEW>Status : Návštevník',
-	'sur_le_web' => '<NEW>Na webe',
-	'sur_un_total_de' => '<NEW>Z celkového počtu',
-	'syndic_breves' => '<NEW>Syndiquer les brèves du site',
-	'syndic_site' => '<NEW>Syndiquer tout le site', # MODIF
+	'sous_rubrique' => 'Podrubrika',
+	'statut_admin' => 'Funkcia: administrátor<br />',
+	'statut_redac' => 'Funkcia: redaktor',
+	'statut_visit' => 'Funkcia: návštevník',
+	'sur_le_web' => 'Na webe',
+	'sur_un_total_de' => 'Z celkového počtu',
+	'syndic_breves' => 'Syndikovať novinky na stránke',
+	'syndic_site' => 'Syndikovať celú stránku',
 
 	// T
-	'texte_page_404' => '<NEW><em>Prepáčte .</em></br>Zdá sa, že táto stránka bola odstránená z tohto webu.',
-	'tous_droits' => '<NEW>Všetky práva vyhradené',
-	'tous_les_auteurs' => '<NEW>Všetci žurnalisti',
+	'texte_page_404' => '<em>Je nám ľúto.</em></br>Požadovaná stránka neexistuje.',
+	'tous_droits' => 'Všetky práva vyhradené',
+	'tous_les_auteurs' => 'Všetci autori',
 
 	// V
-	'version_eva' => 'EVA-Web 4.1', # MODIF
-	'visites' => '<NEW>Návštevy',
-	'voir_en_ligne' => '<NEW>Zobraziť na webe',
-	'voir_image' => '<NEW>Zobraziť obrázok v pôvodnej veľkosti',
-	'vous_etes_ici' => '<NEW>Nachádzate sa tu',
+	'version_eva' => 'EVA-Web 4.1',
+	'visites' => 'Návštevy',
+	'voir_en_ligne' => 'Zobraziť online',
+	'voir_image' => 'Zobraziť obrázok vo zväčšenej veľkosti',
+	'vous_etes_ici' => 'Nachádzate sa tu:',
 
 	// Z
-	'zone' => '<NEW>Chránená zóna'
+	'zone' => 'chránená zóna'
 );
 
 ?>
