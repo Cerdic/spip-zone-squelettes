ceaf8cf9563f36ca692d80cd77572414c496a14b
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-22 13:27:51 +0000

    Chaines de lang pour l'état de la commande + fichier lang UTF-8
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@51695 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zcommerce/lang/zcommerce_fr.php
+++ zcommerce/lang/zcommerce_fr.php
@@ -7,11 +7,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'ajouter_au_panier'=>'Ajouter au panier',
 'boutique'=>'Boutique',
 'panier'=>'Panier',
-'coordonnees'=>'Coordonn&eacute;es',
+'coordonnees'=>'Coordonnées',
 'commande'=>'Commande',
 'paiement'=>'Paiement',
-'modifiez_vos_coordonnees'=>'Modifiez vos coordonn&eacute;es',
-'deja_inscrit'=>'D&eacute;ja inscrit',
+'modifiez_vos_coordonnees'=>'Modifiez vos coordonnées',
+'deja_inscrit'=>'Déja inscrit',
 'vous_inscrire'=>'Vous inscrire',
 'vider_le_panier'=>'Vider le panier',
 'votre_commande'=>'Votre commande',
@@ -19,14 +19,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'une_commande_sur'=>'Une commande sur @nom@',
 'merci_de_votre_commande'=>'Nous vous remercions de votre commande @ref@',
 'commande_ref'=>'Commande @ref@',
-'vos_commandes_payees'=>'Vos commandes pay&eacute;es',
+'vos_commandes_payees'=>'Vos commandes payées',
 'total'=>'Total',
 'acces_facture'=>'Facture',
 'paiement'=>'Paiement',
 'paiement_fictif'=>'Paiement fictif',
 'passer_la_commande'=>'Passer la commande',
 'aucune_commande_valide'=>'Il n\'y a (actuellement) aucune commande.',
-'statut_encours'=>'en cours',	
+'statut_encours'=>'en cours',
+'commande_ref_statut'=>'Votre commande @ref@ est @statut@</h1>
+',
 
 );
 ?>
