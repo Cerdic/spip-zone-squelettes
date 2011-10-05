2bd435c47a58b322d536cd5b91bf5571cb17886b
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-05 12:40:56 +0000

    Mise en ordre: branches pour les archives 1.9, trunk pour le squel spip2+ (étape 5 et fin)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@52500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ MiniGriSpip/spip19/MGS_1.4/local_it.php
@@ -0,0 +1,90 @@
+<?php
+
+// MiniGriSpip 1.4.2 - 2006 (c) FredoMkb
+// This is a SPIP language file  --  Questo è un file di lingua di  SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'mgs_accueil' => 'Home page',
+'mgs_aucun_article' => 'Al momento non c\'&egrave; nessun articolo pubblicato...',
+'mgs_aucune_rubrique' => 'La rubrica richiesta &egrave; introvabile...',
+'mgs_aucun_article_auteur' => 'Al momento, nessun articolo pubblicato...',
+'mgs_article' => 'Articolo',
+'mgs_articles' => 'Articoli',
+'mgs_articles_trouves' => 'Articoli trovati: ',
+'mgs_articles_tri_titre' => '(ordinati per titolo)',
+'mgs_articles_tri_date' => '(ordinati per data)',
+'mgs_articles_tri_point' => '(ordinati per pertinenza)',
+'mgs_articles_tri_popu' => '(ordinati per popolarit&agrave;)',
+'mgs_articles_publies' => 'Articoli pubblicati: ',
+'mgs_articles_associes' => 'Articoli collegati: ',
+'mgs_auteur' => 'Autore',
+'mgs_auteurs' => 'Autori: ',
+'mgs_auteurs_enrg' => 'Autori registrati: ',
+'mgs_auteurs_tri_nom' => '(ordinati per nome)',
+'mgs_articles_syndic' => 'Articoli in syndication ',
+
+// B
+'mgs_bienvenue' => 'Benvenuti',
+'mgs_breves_trouvees' => 'Brevi trovate: ',
+'mgs_breves_tri_date' => '(ordinate per data)',
+'mgs_breves_tri_point' => '(ordinate per pertinenza)',
+'mgs_breves_publiees' => 'Brevi pubblicate: ',
+'mgs_breve' => 'Breve',
+'mgs_breves' => 'Brevi',
+
+// C
+'mgs_contact' => 'Contatti',
+
+// D
+'mgs_documents' => 'Documenti allegati',
+
+// E
+'mgs_email' => 'mioindirizzo@mail.it',
+
+// F
+'mgs_forums' => 'Forum',
+'mgs_forums_trouves' => 'Forum trovati: ',
+'mgs_forums_tri_point' => '(ordinati par pertinenza)',
+
+//H
+'mgs_haut' => 'In alto',
+
+// L
+'mgs_lire_suite' => 'Leggi il seguito...',
+
+// M
+'mgs_mot' => 'Parola chiave',
+'mgs_mots' => 'Parole chiave',
+'mgs_messages_trouves' => 'Messaggi trovati: ',
+'mgs_messages_tri_point' => '(ordinati par pertinenza)',
+'mgs_messages_publies' => 'Messaggi pubblicati: ',
+'mgs_messages_tri_date' => '(ordinati per data)',
+'mgs_mot_cle' => 'Parola chiave',
+'mgs_mots_cles' => 'Parole chiave: ',
+'mgs_mots_tri_nom' => '(ordinati per nome)',
+'mgs_mot_groupe' => 'Gruppo : ',
+
+
+
+
+// N
+'mgs_notes' => 'Note:',
+
+// P
+'mgs_plan' => 'Mappa',
+
+// R
+'mgs_redaction' => 'Redazione',
+'mgs_recherche' => 'Cerca',
+// S
+'mgs_sites' => 'Siti',
+'mgs_sites_ref' => 'Siti citati: ',
+'mgs_sites_tri_nom' => '(ordinati per nome)',
+'mgs_site_syndic' => 'Syndication : ',
+'mgs_site_syndic_non' => 'questo sito non &egrave; ancora in syndication...',
+
+);
+
+?>
