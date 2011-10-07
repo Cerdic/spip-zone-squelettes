d387679e0dd84adca44d88991c3ee1042c57fd27
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-07 07:34:24 +0000

    paquet, accessoire et minuscules
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@52780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ MiniGriSpip/trunk/lang/paquet-minigrispip_fr.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'minigrispip_nom' => "MiniGriSpip",
+	'minigrispip_slogan' => "Un squelette simple pour débuter avec SPIP",
+	'minigrispip_description' => "MiniGriSpip est un squelette simple et sobre fait pour les d&#233;butants. Une version 1 du squelette (pour spip 1.9) a &#233;t&#233; document&#233;e sur le site : [->http://fredomkb.free.fr/spip/]. ATTENTION, la version 2 est une adaptation de {{MiniGriSpip 1.2}} &#224; SPIP 2. Les fonctionnalit&#233;s des versions 1.4 et 1.6 ne sont PAS reprises dans la pr&#233;sente version.",
+);
+
+?>
