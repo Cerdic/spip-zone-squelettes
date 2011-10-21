eeb7162241ad4fd2253c40449bfc1c0ad96dac36
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-21 16:45:26 +0000

    Procédure d'installation à jour.
    Tout ces changements d'adresse SVN avec les trunk, c'est un vrai jeu de piste...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@53704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -9,25 +9,29 @@ svn checkout svn://trac.rezo.net/spip/branches/spip-2.1 ./
 cd extensions
 mkdir plugins_soyezcreateurs
 cd plugins_soyezcreateurs
+sleep 15 && svn checkout http://svn.github.com/Cerdic/video_accessible.git ./video_accessible/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/accessibilite
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acronymes
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/agenda/2_0_0 ./agenda/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/bandeau
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/boutonstexte
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/changement_langue
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/cfg
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/changement_langue
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/crayons
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fonctions_images
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/forms/forms_et_tables_2_0 ./forms/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/forum
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fulltext
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/job_queue
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/Lecteur_multimedia
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/mediabox
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/mediatheque
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/noie
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nospam
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notifications
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/changement_langue
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/enluminures_typographiques_v3
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/partout
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/previsu_redaction
@@ -35,24 +39,22 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/protection_form
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/saisies
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/savecfg
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-2
-sleep 15 && svn checkout http://svn.github.com/Cerdic/video_accessible.git ./video_accessible/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs
 cd ../..
 
 #Installation des plugins
 mkdir plugins
 cd plugins
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acces_restreint
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/authentification/openid
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/acces_restreint/branches/v3 ./access_restreint/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/afficher_objets
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/article_pdf
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/autorite
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/2_0 ./clevermail/
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/branches/2.0.0 ./clevermail/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/ ./comments/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact/branches/v0_7 ./contact/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/critere_mots
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/eva_freemind/eva_freemind_2_0
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gis
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/googlemap_api
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gis/branches/v2 ./gis/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gravatar
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/insertion_modele
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/jquery_ui
@@ -61,9 +63,10 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/memoization
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/menu_langues_liens
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/microblog
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nuage
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nuage/trunk ./nuage/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/rainette
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/sedna
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/skeleditor/skeleditor-2 ./skeleditor/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/socialtags
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip2pdf
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/splickrbox
