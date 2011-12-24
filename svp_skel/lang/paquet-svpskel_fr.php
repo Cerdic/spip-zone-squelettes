31e3ed9aa746386aed7659249f4d4a2fb04a3860
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-24 17:15:44 +0000

    Passage en paquet.xml et incrément de version.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@55994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/paquet-svpskel_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Module: paquet-svpskel
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'svpskel_description' => 'Ce plugin est un module optionnel de SVP. 
+_ Il fournit un jeu de squelettes Z simple mais complet pour afficher l\'ensemble des objets SVP.
+Son but est avant tout de permettre une vérification aisée de toutes les informations collectées par SVP sur les objets dépôt, plugin et paquet.',
+	'svpskel_slogan' => 'Jeu de squelettes Z pour SVP',
+);
+?>
