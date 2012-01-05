7790e57752d11b7c3091f0246b147608009cc47c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:51 +0000

    langues (spipclear)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@56639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipclear_z/lang/spipclear_de.php
+++ spipclear_z/lang/spipclear_de.php
@@ -1,106 +1,166 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
-// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-//A
-'ajouter_commentaire' => 'Kommentar hinzug&uuml;gen',
-'archive' => 'Archiv', // Non utilise
-'archives' => 'Archive',
-'attime' => '&agrave;',
-'aucune_archive' => 'Kein Archiv', // Non utilise
-'aucune_categorie' => 'Keine Kategorie', // Non utilise
-'aucun_commentaire' => 'Kein Kommentar', // Non utilise
-'aucune_langue' => 'Kene Sprache', // Non utilise
-'aucun_lien' => 'Kein Link', // Non utilise
-'aucun_trackback' => 'Kein Trackback', // Non utilise
+	// A
+	'accueil' => 'Accueil', # NEW
+	'ajouter_commentaire' => 'Kommentar hinzugügen',
+	'archive' => 'Archiv',
+	'archives' => 'Archive',
+	'archives_precedents' => 'Précédents', # NEW
+	'archives_suivants' => 'Suivants', # NEW
+	'attime' => 'à',
+	'aucun_commentaire' => 'Kein Kommentar',
+	'aucun_lien' => 'Kein Link',
+	'aucun_trackback' => 'Kein Trackback',
+	'aucune_archive' => 'Kein Archiv',
+	'aucune_categorie' => 'Keine Kategorie',
+	'aucune_langue' => 'Kene Sprache',
+	'auteur_inconnu' => 'Anonyme', # NEW
 
-//B
-'boss' => 'Administrator', // Oublie
+	// B
+	'boss' => 'Administrator',
 
-//C
-'calendrier' => 'Kalender', // Non utilise
-'categorie' => 'Kategorie', // Non utilise
-'categories' => 'Kategorien',
-'commentaire' => 'Kommentar',
-'commentaires' => 'Kommentare',
-'commentaires_pour' => 'Kommentare zu',
-'commentaires_fermes' => 'Die Kommetare zu diesem Eintrag sind beendet',
-'credits' => '<a href="http://www.spip.net/de">Angetrieben mit SPIP</a>,
+	// C
+	'calendrier' => 'Kalender',
+	'categorie' => 'Kategorie',
+	'categories' => 'Kategorien',
+	'ce_bestof_label' => 'Billet sélectionné', # NEW
+	'commentaire' => 'Kommentar',
+	'commentaires' => 'Kommentare',
+	'commentaires_fermes' => 'Die Kommetare zu diesem Eintrag sind beendet',
+	'commentaires_pour' => 'Kommentare zu',
+	'contacts' => 'Contacts', # NEW
+	'credits' => '<a href="http://www.spip.net/de">Angetrieben mit SPIP</a>,
 <a href="http://www.spip-contrib.net/SpipClear">beschleunigt mit Spip.Clear</a>',
 
-//D
-'date_jour_abbr_1' => 'So', // Non utilise
-'date_jour_abbr_2' => 'Mo', // Non utilise
-'date_jour_abbr_3' => 'Di', // Non utilise
-'date_jour_abbr_4' => 'Mi', // Non utilise
-'date_jour_abbr_5' => 'Do', // Non utilise
-'date_jour_abbr_6' => 'Fr', // Non utilise
-'date_jour_abbr_7' => 'Sa', // Non utilise
-'de' => 'de', // Non utilise
+	// D
+	'date_jour_abbr_1' => 'So',
+	'date_jour_abbr_2' => 'Mo',
+	'date_jour_abbr_3' => 'Di',
+	'date_jour_abbr_4' => 'Mi',
+	'date_jour_abbr_5' => 'Do',
+	'date_jour_abbr_6' => 'Fr',
+	'date_jour_abbr_7' => 'Sa',
+	'de' => 'de',
+	'derniere_maj' => 'Dernière mise à jour', # NEW
+	'derniers_billets' => 'Derniers billets', # NEW
+	'derniers_commentaires' => 'Derniers commentaires', # NEW
+	'description_page-sommaire' => 'Les blocs de cette page seront ajoutés sur toutes les pages du site.', # NEW
+	'document_non_trouve' => 'Document non trouvé', # NEW
+	'document_non_trouve_explication' => 'Le document que vous cherchez n\'existe pas.', # NEW
 
-//E
+	// F
+	'fil_atom' => 'fil atom',
+	'fil_atom_commentaires' => 'Kommentar ATOM-Stream',
+	'fil_rss' => 'RSS Stream',
+	'fil_rss_commentaires' => 'Kommentar RSS-Stream',
 
-//F
-'fil_rss' => 'RSS Stream',
-'fil_rss_commentaires' => 'Kommentar RSS-Stream',
-'fil_atom' => 'fil atom',
-'fil_atom_commentaires' => 'Kommentar ATOM-Stream',
+	// G
+	'go_main' => 'Zum Inhalt',
+	'go_search' => 'Zur Suche',
+	'go_sidebar' => 'Zum Menü',
 
-//G
-'go_main' => 'Zum Inhalt',
-'go_sidebar' => 'Zum Men&uuml;',
-'go_search' => 'Zur Suche',
+	// H
+	'hebergement' => 'Webhosting von',
 
-//H
-'hebergement' => 'Webhosting von',
+	// I
+	'info_choix_theme' => 'Wählen sie das Layout-Thema für ihr Blog',
 
-//I
-'info_navigation' => 'W&auml;en sie die Position der Navigation', // CFG
-'info_choix_theme' => 'W&auml;hlen sie das Layout-Thema f&uuml;r ihr Blog', // CFG
+	// L
+	'label_limite' => '', # NEW
+	'label_nom_theme' => 'Geben sie den Namen des persönlichen Themas ein (entspricht dem Namen xxxx des Unterverzeichnisses squelettes/themes/xxxx/',
+	'label_theme_defaut' => 'Standardthema (Typ DotClear)',
+	'label_theme_perso' => 'Persönliches Thema (weiter unten definieren)',
+	'langue' => 'Sprache',
+	'langues' => 'Sprachen',
+	'legend_layout' => 'Präsentation',
+	'legend_theme' => 'Thema',
+	'lien' => 'Link',
+	'lien_permanent' => 'Lien permanent', # NEW
+	'liens' => 'Links',
+	'lire_la_suite' => 'Lire la suite', # NEW
+	'look_navigation' => 'Personnalisation du bloc de navigation', # NEW
+	'look_sommaire' => 'Personnalisation de la page d\'accueil', # NEW
 
-//L
-'label_navigation_droite' => 'Navigation rechts (Standard)', // CFG
-'label_navigation_gauche' => 'Navigation links', // CFG
-'label_theme_defaut' => 'Standardthema (Typ DotClear)', // CFG
-'label_theme_perso' => 'Pers&ouml;nliches Thema (weiter unten definieren)', // CFG
-'label_nom_theme' => 'Geben sie den Namen des pers&ouml;nlichen Themas ein (entspricht dem Namen xxxx des Unterverzeichnisses squelettes/themes/xxxx/', // CFG
-'langue' => 'Sprache', // Non utilise
-'langues' => 'Sprachen',
-'legend_layout' => 'Pr&auml;sentation', // CFG
-'legend_theme' => 'Thema', // CFG
-'lien' => 'Link', // Non utilise
-'liens' => 'Links',
+	// M
+	'msg_erreur_v00' => 'Echec ', # NEW
+	'msg_erreur_v02' => 'Echec à la création du champ extra « Billet Sélectionné »', # NEW
 
-//O
-'oksearch' => 'OK',
+	// N
+	'navigation_desactiver_calendrier' => 'Ne pas afficher le calendrier dans la navigation', # NEW
+	'noisette_desc_bestof' => 'Affiche une liste cliquable des billets sélectionnés', # NEW
+	'noisette_desc_calendrier' => 'Affiche un calendrier mensuel', # NEW
+	'noisette_desc_categories' => 'Liste des catégories', # NEW
+	'noisette_desc_contact' => 'Affiche un lien vers la page contact (plugin Contact)', # NEW
+	'noisette_desc_langs' => 'Permet de changer la langue d\'affichage du blog', # NEW
+	'noisette_desc_lastcomments' => 'Liste des n derniers commentaires', # NEW
+	'noisette_desc_lastposts' => 'Liste des n derniers billets sous forme de résumé', # NEW
+	'noisette_desc_links' => 'Affiche la liste de l\'ensemble des sites', # NEW
+	'noisette_desc_navigation' => 'Affiche les liens de navigation (accueil, archives)', # NEW
+	'noisette_desc_subscribe' => 'Affiche les boutons rss et atom pour les billets et les comentaires', # NEW
+	'noisette_titre_bestof' => 'Billets sélectionnés', # NEW
+	'noisette_titre_calendrier' => 'Calendrier', # NEW
+	'noisette_titre_categories' => 'Catégories', # NEW
+	'noisette_titre_contact' => 'Contacts', # NEW
+	'noisette_titre_langs' => 'Langues du blog', # NEW
+	'noisette_titre_lastcomments' => 'Derniers commentaires', # NEW
+	'noisette_titre_lastposts' => 'Derniers billets', # NEW
+	'noisette_titre_links' => 'Liste de sites', # NEW
+	'noisette_titre_navigation' => 'Liens de navigation', # NEW
+	'noisette_titre_search' => 'Moteur de recherche', # NEW
+	'noisette_titre_subscribe' => 'Liens d\'abonnement', # NEW
+	'nom_page-sommaire' => 'Page par défaut de spipclear', # NEW
 
-//P
-'permalink_pour' => 'Permalink zu',
+	// O
+	'oksearch' => 'OK',
 
-//R
-'resultat_recherche' => 'Ergebnis ihrer Suche',
-'rss_pour' => 'RSS-Stream der Kommentare von',
+	// P
+	'par' => 'par', # NEW
+	'permalink_pour' => 'Permalink zu',
+	'publie_le' => 'Publié le', # NEW
 
-//S
-'selection' => 'merken',
-'site_comment' => 'Site',
-'syndication' => 'Syndikation',
+	// R
+	'recherche' => 'Recherche', # NEW
+	'resultat_recherche' => 'Ergebnis ihrer Suche',
+	'rss_commentaire_article' => 'Fil des commentaires de ce billet', # NEW
+	'rss_pour' => 'RSS-Stream der Kommentare von',
 
-//T
-'texte_erreur1' => 'Suche nach',
-'texte_erreur2' => 'hat kein Ergebnis.',
-'titre_erreur' => 'Fehler:',
-'trackback' => 'Trackback', // Non utilise
-'trackbacks' => 'Trackbacks', // Non utilise
-'trackbacks_pour_faire' => 'Trackback zu diesem Eintrag anlegen', // Non utilise
-'trackbacks_fermes' => 'Die Trackbacks zu diesem Eintrag sind geschlossen.', // Non utilise
+	// S
+	'sabonner' => 'S\'abonner', # NEW
+	'secteur_spipclear_aucun' => 'Aucun', # NEW
+	'secteurs_spipclear' => 'Secteurs SpipClear', # NEW
+	'selection' => 'merken',
+	'selectionner_theme' => 'Cliquez pour sélectionner ce thême', # NEW
+	'site_comment' => 'Site',
+	'sommaire_afficher_tags' => 'Ne pas afficher les tags dans les listes d\'articles (page d\'accueil, résultats de recherche, etc).', # NEW
+	'sommaire_articles_entiers' => 'Afficher les articles en entier (par défaut, seule une introduction est affichée)', # NEW
+	'sommaire_spipclear' => 'Appliquer SpipClear sur la page d\'accueil du site (ne fonctionne que pour un secteur).', # NEW
+	'syndication' => 'Syndikation',
 
-//U
-'un_seul_mot' => 'ein einziges Wort', // Oubli
+	// T
+	'tag' => 'Tag', # NEW
+	'tags' => 'Tags', # NEW
+	'texte_erreur1' => 'Suche nach',
+	'texte_erreur2' => 'hat kein Ergebnis.',
+	'themes_installes' => '<strong>Les thèmes installés sur ce spip</strong> (cliquez sur un l\'icone d\'un thème pour le sélectionner)', # NEW
+	'titre_erreur' => 'Fehler:',
+	'trackback' => 'Trackback',
+	'trackbacks' => 'Trackbacks',
+	'trackbacks_fermes' => 'Die Trackbacks zu diesem Eintrag sind geschlossen.',
+	'trackbacks_pour_faire' => 'Trackback zu diesem Eintrag anlegen',
 
+	// U
+	'un_seul_mot' => 'ein einziges Wort',
+
+	// V
+	'voir_capture' => 'Afficher la capture d\'écran', # NEW
+	'votre_commentaire' => 'Votre commentaire' # NEW
 );
 
 ?>
--- spipclear_z/lang/spipclear_fr.php
+++ spipclear_z/lang/spipclear_fr.php
@@ -1,168 +1,164 @@
 <?php
-
-// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/spipclear_z/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-//A
-'accueil' => 'Accueil',
-'ajouter_commentaire' => 'Ajouter un commentaire',
-'archive' => 'Archive', // Non utilise
-'archives' => 'Archives',
-'archives_precedents' => 'Pr&eacute;c&eacute;dents',
-'archives_suivants' => 'Suivants',
-'attime' => '&agrave;',
-'aucune_archive' => 'Aucune archive', // Non utilise
-'aucune_categorie' => 'Aucune cat&eacute;gorie', // Non utilise
-'aucun_commentaire' => 'Aucun commentaire', // Non utilise
-'aucune_langue' => 'Aucune langue', // Non utilise
-'aucun_lien' => 'Auncun lien', // Non utilise
-'aucun_trackback' => 'Aucun trackback', // Non utilise
-'auteur_inconnu' => 'Anonyme',
+	// A
+	'accueil' => 'Accueil',
+	'ajouter_commentaire' => 'Ajouter un commentaire',
+	'archive' => 'Archive',
+	'archives' => 'Archives',
+	'archives_precedents' => 'Précédents',
+	'archives_suivants' => 'Suivants',
+	'attime' => 'à',
+	'aucun_commentaire' => 'Aucun commentaire',
+	'aucun_lien' => 'Auncun lien',
+	'aucun_trackback' => 'Aucun trackback',
+	'aucune_archive' => 'Aucune archive',
+	'aucune_categorie' => 'Aucune catégorie',
+	'aucune_langue' => 'Aucune langue',
+	'auteur_inconnu' => 'Anonyme',
 
-//B
-'boss' => 'Administrateur', // Oubli
+	// B
+	'boss' => 'Administrateur',
 
-//C
-'calendrier' => 'Calendrier', // Non utilise
-'categorie' => 'Cat&eacute;gorie', // Non utilise
-'categories' => 'Cat&eacute;gories',
-'ce_bestof_label' => "Billet s&eacute;lectionn&eacute;",
-'commentaire' => 'commentaire',
-'commentaires' => 'commentaires',
-'commentaires_pour' => 'commentaires pour',
-'commentaires_fermes' => 'Les commentaires pour ce billet sont ferm&eacute;s',
-'contacts' => 'Contacts',
-'credits' => '<a href="http://www.spip.net/fr">motoris&eacute; par SPIP</a>,
-<a href="http://www.spip-contrib.net/SpipClear">propuls&eacute; par Spip.Clear</a>',
+	// C
+	'calendrier' => 'Calendrier',
+	'categorie' => 'Catégorie',
+	'categories' => 'Catégories',
+	'ce_bestof_label' => 'Billet sélectionné',
+	'commentaire' => 'commentaire',
+	'commentaires' => 'commentaires',
+	'commentaires_fermes' => 'Les commentaires pour ce billet sont fermés',
+	'commentaires_pour' => 'commentaires pour',
+	'contacts' => 'Contacts',
+	'credits' => '<a href="http://www.spip.net/fr">motorisé par SPIP</a>,
+<a href="http://www.spip-contrib.net/SpipClear">propulsé par Spip.Clear</a>',
 
-//D
-'date_jour_abbr_1' => 'dim', // Non utilise
-'date_jour_abbr_2' => 'lun', // Non utilise
-'date_jour_abbr_3' => 'mar', // Non utilise
-'date_jour_abbr_4' => 'mer', // Non utilise
-'date_jour_abbr_5' => 'jeu', // Non utilise
-'date_jour_abbr_6' => 'ven', // Non utilise
-'date_jour_abbr_7' => 'sam', // Non utilise
-'de' => 'de', // Non utilise
-'derniers_billets' => 'Derniers billets',
-'derniers_commentaires' => 'Derniers commentaires',
-'derniere_maj' => 'Derni&egrave;re mise &agrave; jour', // Non utilise
-'description_page-sommaire' => 'Les blocs de cette page seront ajout&eacute;s sur toutes les pages du site.',
-'document_non_trouve' => 'Document non trouv&eacute;',
-'document_non_trouve_explication' => 'Le document que vous cherchez n\'existe pas.',
+	// D
+	'date_jour_abbr_1' => 'dim',
+	'date_jour_abbr_2' => 'lun',
+	'date_jour_abbr_3' => 'mar',
+	'date_jour_abbr_4' => 'mer',
+	'date_jour_abbr_5' => 'jeu',
+	'date_jour_abbr_6' => 'ven',
+	'date_jour_abbr_7' => 'sam',
+	'de' => 'de',
+	'derniere_maj' => 'Dernière mise à jour',
+	'derniers_billets' => 'Derniers billets',
+	'derniers_commentaires' => 'Derniers commentaires',
+	'description_page-sommaire' => 'Les blocs de cette page seront ajoutés sur toutes les pages du site.',
+	'document_non_trouve' => 'Document non trouvé',
+	'document_non_trouve_explication' => 'Le document que vous cherchez n\'existe pas.',
 
-//E
+	// F
+	'fil_atom' => 'Fil Atom des billets',
+	'fil_atom_commentaires' => 'Fil Atom des commentaires',
+	'fil_rss' => 'Fil RSS des billets',
+	'fil_rss_commentaires' => 'Fil RSS des commentaires',
 
-//F
-'fil_rss' => 'Fil RSS des billets',
-'fil_rss_commentaires' => 'Fil RSS des commentaires',
-'fil_atom' => 'Fil Atom des billets',
-'fil_atom_commentaires' => 'Fil Atom des commentaires',
+	// G
+	'go_main' => 'Aller au contenu',
+	'go_search' => 'Aller à la recherche',
+	'go_sidebar' => 'Aller au menu',
 
-//G
-'go_main' => 'Aller au contenu',
-'go_sidebar' => 'Aller au menu',
-'go_search' => 'Aller &agrave; la recherche',
+	// H
+	'hebergement' => 'hébergement par',
 
-//H
-'hebergement' => 'h&eacute;bergement par',
+	// I
+	'info_choix_theme' => 'Choisissez le thème que vous souhaitez utiliser pour votre blog',
 
-//I
-'info_choix_theme' => 'Choisissez le th&egrave;me que vous souhaitez utiliser pour votre blog', // CFG
+	// L
+	'label_limite' => '',
+	'label_nom_theme' => 'Saisissez le nom du thème perso (correspond &agrave xxxx du sous-répertoire squelettes/themes/xxxx/)',
+	'label_theme_defaut' => 'Thème par défaut (type DotClear)',
+	'label_theme_perso' => 'Thème perso (à définir ci-dessous)',
+	'langue' => 'Langue',
+	'langues' => 'Langues',
+	'legend_layout' => 'Présentation',
+	'legend_theme' => 'Thème',
+	'lien' => 'Lien',
+	'lien_permanent' => 'Lien permanent',
+	'liens' => 'Liens',
+	'lire_la_suite' => 'Lire la suite',
+	'look_navigation' => 'Personnalisation du bloc de navigation',
+	'look_sommaire' => 'Personnalisation de la page d\'accueil',
 
-//L
-'label_limite' => '',
-'label_nom_theme' => 'Saisissez le nom du th&egrave;me perso (correspond &agrave xxxx du sous-r&eacute;pertoire squelettes/themes/xxxx/)', // CFG
-'label_theme_defaut' => 'Th&egrave;me par d&eacute;faut (type DotClear)', // CFG
-'label_theme_perso' => 'Th&egrave;me perso (&agrave; d&eacute;finir ci-dessous)', // CFG
-'langue' => 'Langue', // Non utilise
-'langues' => 'Langues',
-'legend_layout' => 'Pr&eacute;sentation', // CFG
-'legend_theme' => 'Th&egrave;me', // CFG
-'lien' => 'Lien', // Non utilise
-'liens' => 'Liens',
-'lien_permanent' => 'Lien permanent',
-'lire_la_suite' => 'Lire la suite',
-'look_sommaire' => 'Personnalisation de la page d\'accueil',
-'look_navigation' => 'Personnalisation du bloc de navigation',
+	// M
+	'msg_erreur_v00' => 'Echec ',
+	'msg_erreur_v02' => 'Echec à la création du champ extra « Billet Sélectionné »',
 
-//M
-'msg_erreur_v00' => 'Echec � l\'importation de la configuration du noiZetier',
-'msg_erreur_v02' => 'Echec &agrave; la cr&eacute;ation du champ extra &laquo;&nbsp;Billet S&eacute;lectionn&eacute;&nbsp;&raquo;',
+	// N
+	'navigation_desactiver_calendrier' => 'Ne pas afficher le calendrier dans la navigation',
+	'noisette_desc_bestof' => 'Affiche une liste cliquable des billets sélectionnés',
+	'noisette_desc_calendrier' => 'Affiche un calendrier mensuel',
+	'noisette_desc_categories' => 'Liste des catégories',
+	'noisette_desc_contact' => 'Affiche un lien vers la page contact (plugin Contact)',
+	'noisette_desc_langs' => 'Permet de changer la langue d\'affichage du blog',
+	'noisette_desc_lastcomments' => 'Liste des n derniers commentaires',
+	'noisette_desc_lastposts' => 'Liste des n derniers billets sous forme de résumé',
+	'noisette_desc_links' => 'Affiche la liste de l\'ensemble des sites',
+	'noisette_desc_navigation' => 'Affiche les liens de navigation (accueil, archives)',
+	'noisette_desc_subscribe' => 'Affiche les boutons rss et atom pour les billets et les comentaires',
+	'noisette_titre_bestof' => 'Billets sélectionnés',
+	'noisette_titre_calendrier' => 'Calendrier',
+	'noisette_titre_categories' => 'Catégories',
+	'noisette_titre_contact' => 'Contacts',
+	'noisette_titre_langs' => 'Langues du blog',
+	'noisette_titre_lastcomments' => 'Derniers commentaires',
+	'noisette_titre_lastposts' => 'Derniers billets',
+	'noisette_titre_links' => 'Liste de sites',
+	'noisette_titre_navigation' => 'Liens de navigation',
+	'noisette_titre_search' => 'Moteur de recherche',
+	'noisette_titre_subscribe' => 'Liens d\'abonnement',
+	'nom_page-sommaire' => 'Page par défaut de spipclear',
 
-//N
-'navigation_desactiver_calendrier' => 'Ne pas afficher le calendrier dans la navigation',
-'noisette_desc_bestof' => 'Affiche une liste cliquable des billets s&eacute;lectionn&eacute;s',
-'noisette_desc_calendrier' => 'Affiche un calendrier mensuel',
-'noisette_desc_categories' => 'Liste des cat&eacute;gories',
-'noisette_desc_contact' => 'Affiche un lien vers la page contact (plugin Contact)',
-'noisette_desc_langs' => 'Permet de changer la langue d\'affichage du blog',
-'noisette_desc_lastcomments' => 'Liste des n derniers commentaires',
-'noisette_desc_lastposts' => 'Liste des n derniers billets sous forme de r&eacute;sum&eacute;',
-'noisette_desc_links' => 'Affiche la liste de l\'ensemble des sites',
-'noisette_desc_navigation' => 'Affiche les liens de navigation (accueil, archives)',
-'noisette_desc_subscribe' => 'Affiche les boutons rss et atom pour les billets et les comentaires',
-'noisette_titre_bestof' => 'Billets s&eacute;lectionn&eacute;s',
-'noisette_titre_calendrier' => 'Calendrier',
-'noisette_titre_categories' => 'Cat&eacute;gories',
-'noisette_titre_contact' => 'Contacts',
-'noisette_titre_langs' => 'Langues du blog',
-'noisette_titre_lastcomments' => 'Derniers commentaires',
-'noisette_titre_lastposts' => 'Derniers billets',
-'noisette_titre_links' => 'Liste de sites',
-'noisette_titre_navigation' => 'Liens de navigation',
-'noisette_titre_search' => 'Moteur de recherche',
-'noisette_titre_subscribe' => 'Liens d\'abonnement',
-'nom_page-sommaire' => 'Page par d&eacute;faut de spipclear',
+	// O
+	'oksearch' => 'ok',
 
+	// P
+	'par' => 'par',
+	'permalink_pour' => 'Lien permanent vers',
+	'publie_le' => 'Publié le',
 
-//O
-'oksearch' => 'ok',
+	// R
+	'recherche' => 'Recherche',
+	'resultat_recherche' => 'Résultats pour votre recherche de',
+	'rss_commentaire_article' => 'Fil des commentaires de ce billet',
+	'rss_pour' => 'fil RSS des commentaires de',
 
-//P
-'par' => 'par',
-'permalink_pour' => 'Lien permanent vers',
-'publie_le' => 'Publi&eacute; le',
+	// S
+	'sabonner' => 'S\'abonner',
+	'secteur_spipclear_aucun' => 'Aucun',
+	'secteurs_spipclear' => 'Secteurs SpipClear',
+	'selection' => 'À retenir',
+	'selectionner_theme' => 'Cliquez pour sélectionner ce thême',
+	'site_comment' => 'site',
+	'sommaire_afficher_tags' => 'Ne pas afficher les tags dans les listes d\'articles (page d\'accueil, résultats de recherche, etc).',
+	'sommaire_articles_entiers' => 'Afficher les articles en entier (par défaut, seule une introduction est affichée)',
+	'sommaire_spipclear' => 'Appliquer SpipClear sur la page d\'accueil du site (ne fonctionne que pour un secteur).',
+	'syndication' => 'Syndication',
 
-//R
-'recherche' => 'Recherche',
-'resultat_recherche' => 'R&eacute;sultats pour votre recherche de',
-'rss_pour' => 'fil RSS des commentaires de',
-'rss_commentaire_article' => 'Fil des commentaires de ce billet',
+	// T
+	'tag' => 'Tag',
+	'tags' => 'Tags',
+	'texte_erreur1' => 'La recherche de',
+	'texte_erreur2' => 'ne donne aucun résultat.',
+	'themes_installes' => '<strong>Les thèmes installés sur ce spip</strong> (cliquez sur un l\'icone d\'un thème pour le sélectionner)',
+	'titre_erreur' => 'Erreur :',
+	'trackback' => 'trackback',
+	'trackbacks' => 'trackbacks',
+	'trackbacks_fermes' => 'Les trackbacks pour ce billet sont fermés.',
+	'trackbacks_pour_faire' => 'Pour faire un trackback sur ce billet',
 
-//S
-'sabonner' => 'S\'abonner',
-'secteurs_spipclear' => 'Secteurs SpipClear',
-'secteur_spipclear_aucun' => 'Aucun',
-'selection' => '&Agrave; retenir',
-'site_comment' => 'site',
-'syndication' => 'Syndication',
-'selectionner_theme' => 'Cliquez pour s&eacute;lectionner ce th&ecirc;me',
-'sommaire_afficher_tags' => 'Ne pas afficher les tags dans les listes d\'articles (page d\'accueil, r&eacute;sultats de recherche, etc).',
-'sommaire_articles_entiers' => 'Afficher les articles en entier (par d&eacute;faut, seule une introduction est affich&eacute;e)',
-'sommaire_spipclear' => 'Appliquer SpipClear sur la page d\'accueil du site (ne fonctionne que pour un secteur).',
+	// U
+	'un_seul_mot' => 'un seul mot',
 
-//T
-'tag' => 'Tag',
-'tags' => 'Tags',
-'texte_erreur1' => 'La recherche de',
-'texte_erreur2' => 'ne donne aucun r&eacute;sultat.',
-'titre_erreur' => 'Erreur :',
-'trackback' => 'trackback', // Non utilise
-'trackbacks' => 'trackbacks', // Non utilise
-'trackbacks_pour_faire' => 'Pour faire un trackback sur ce billet', // Non utilise
-'trackbacks_fermes' => 'Les trackbacks pour ce billet sont ferm&eacute;s.', // Non utilise
-'themes_installes'=> '<strong>Les th&egrave;mes install&eacute;s sur ce spip</strong> (cliquez sur un l\'icone d\'un th&egrave;me pour le s&eacute;lectionner)',
-
-//U
-'un_seul_mot' => 'un seul mot', // Oubli
-
-// V
-'voir_capture'=> 'Afficher la capture d\'&eacute;cran',
-'votre_commentaire'=> 'Votre commentaire'
-
+	// V
+	'voir_capture' => 'Afficher la capture d\'écran',
+	'votre_commentaire' => 'Votre commentaire'
 );
 
 ?>
