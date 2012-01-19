d9d3b5033ac398f7872a45a600c45de503214494
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-19 15:20:34 +0000

    Oups des choses ont été commitées par inadvertance
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@57335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipclear_z/branches/v3.2/base/spipclear_upgrade.php
+++ spipclear_z/branches/v3.2/base/spipclear_upgrade.php
@@ -3,29 +3,39 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/meta');
 include_spip('inc/cextras_gerer');
+include_spip('spipclear_pipelines');
 
 /**
  * Fonction d'upgrade/maj
- * On cree une configuration par defaut
+ * On cr�e une configuration par d�faut
  *
  * @param string $nom_meta_base_version
  * @param string $version_cible
  */
 function spipclear_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = "0.0";
+	if (	(!isset($GLOBALS['meta'][$nom_meta_base_version]))
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 		include_spip('spipclear_pipelines');
-		include_spip('noisetier_fonctions');
+		include_spip('inc/noizetier');
 
-		$config_defaut = _DIR_PLUGIN_SPIPCLEAR . 'config_noizetier/spipclear.yaml';
-		
-		$maj['create'] = array(
-			array('spipclear_get_config_noizetier',$config_defaut),
-			array('spipclear_declarer_champs_extras','')
-		);
-		$maj['0.2'] = array(
-			array('spipclear_declarer_champs_extras','')
-		);
-		include_spip('base/upgrade');
-		maj_plugin($nom_meta_base_version, $version_cible, $maj);
+		if (version_compare($current_version,'0.0','=')){
+			$config_defaut = _DIR_PLUGIN_SPIPCLEAR . 'config_noizetier/spipclear.yaml';
+			$yaml = spipclear_get_config_noizetier($config_defaut);
+			if( noizetier_importer_configuration('remplacer', 'non', $yaml)) {
+				ecrire_meta($nom_meta_base_version,'0.0','non');
+			} else {
+				echo _T('spipclear:msg_erreur_v00')."<br/>";
+			}
+		}
+		if (version_compare($current_version,'0.2','<=')){	
+			$champs = spipclear_declarer_champs_extras();
+			// C'est le plugin Champs Extras qui ecrit le meta
+			if(! installer_champs_extras($champs, $nom_meta_base_version, $version_cible)) {
+				echo _T('spipclear:msg_erreur_v02')."<br/>";
+			}
+		}
+	}
 }
 
 /**
