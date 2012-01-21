c701c122ccb7f73b8826a4290a2ca4b3825f9abb
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-21 16:10:16 +0000

    Indiquer le changement de version et passage en développement
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@57459 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ aveline/trunk/lang/paquet-aveline_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-aveline
+// Langue: fr
+// Date: 21-01-2012 16:07:45
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'aveline_description' => 'Collection de noisettes hautement paramétrables et compatibles avec les thèmes Z.',
+	'aveline_slogan' => 'Collection de noisettes compatibles Z',
+);
+?>
