97e656de88ee01804bc311bb43d4aa3cfd1fcb55
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 16:22:19 +0000

    langues (spipclear)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@57672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipclear_z/trunk/lang/spipclear_sk.php
@@ -0,0 +1,166 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil' => 'Úvodná stránka',
+	'ajouter_commentaire' => 'Pridať komentár',
+	'archive' => 'Archív',
+	'archives' => 'Archívy',
+	'archives_precedents' => 'Predchádzajúce',
+	'archives_suivants' => 'Ďalšie',
+	'attime' => 'o',
+	'aucun_commentaire' => 'Žiaden komentár',
+	'aucun_lien' => 'Žiaden odkaz',
+	'aucun_trackback' => 'Aucun trackback', # NEW
+	'aucune_archive' => 'Žiaden archív',
+	'aucune_categorie' => 'Žiadna kategória',
+	'aucune_langue' => 'Žiaden jazyk',
+	'auteur_inconnu' => 'Anonym',
+
+	// B
+	'boss' => 'Administrátor',
+
+	// C
+	'calendrier' => 'Kalendár',
+	'categorie' => 'Kategória',
+	'categories' => 'Kategórie',
+	'ce_bestof_label' => 'Lístok vybratý',
+	'commentaire' => 'komentár',
+	'commentaires' => 'komentárov',
+	'commentaires_fermes' => 'Komentáre pre tento lístok sú uzavreté',
+	'commentaires_pour' => 'komentáre na',
+	'contacts' => 'Kontakty',
+	'credits' => '<a href="http://www.spip.net/fr">motorisé par SPIP</a>,
+<a href="http://www.spip-contrib.net/SpipClear">propulsé par Spip.Clear</a>', # NEW
+
+	// D
+	'date_jour_abbr_1' => 'ned',
+	'date_jour_abbr_2' => 'pon',
+	'date_jour_abbr_3' => 'uto',
+	'date_jour_abbr_4' => 'str',
+	'date_jour_abbr_5' => 'štv',
+	'date_jour_abbr_6' => 'pia',
+	'date_jour_abbr_7' => 'sob',
+	'de' => 'de', # NEW
+	'derniere_maj' => 'Posledná aktualizácia',
+	'derniers_billets' => 'Posledné lístky',
+	'derniers_commentaires' => 'Najnovšie komentáre',
+	'description_page-sommaire' => 'Bloky tejto stránky budú pridaný na všetky stránky tohto webu.',
+	'document_non_trouve' => 'Dokument sa nenašiel',
+	'document_non_trouve_explication' => 'Dokument, ktorý hľadáte, neexistuje.',
+
+	// F
+	'fil_atom' => 'Fil Atom des billets', # NEW
+	'fil_atom_commentaires' => 'Fil Atom des commentaires', # NEW
+	'fil_rss' => 'Fil RSS des billets', # NEW
+	'fil_rss_commentaires' => 'Fil RSS des commentaires', # NEW
+
+	// G
+	'go_main' => 'Aller au contenu', # NEW
+	'go_search' => 'Aller à la recherche', # NEW
+	'go_sidebar' => 'Aller au menu', # NEW
+
+	// H
+	'hebergement' => 'hébergement par', # NEW
+
+	// I
+	'info_choix_theme' => 'Choisissez le thème que vous souhaitez utiliser pour votre blog', # NEW
+
+	// L
+	'label_limite' => '', # NEW
+	'label_nom_theme' => 'Saisissez le nom du thème perso (correspond &agrave xxxx du sous-répertoire squelettes/themes/xxxx/)', # NEW
+	'label_theme_defaut' => 'Predvolený farebný motív (typ DotClear – bez bodiek)',
+	'label_theme_perso' => 'Thème perso (à définir ci-dessous)', # NEW
+	'langue' => 'Jazyk',
+	'langues' => 'Jazyky',
+	'legend_layout' => 'Vzhľad',
+	'legend_theme' => 'Farebný motív',
+	'lien' => 'Odkaz',
+	'lien_permanent' => 'Trvalý odkaz',
+	'liens' => 'Odkazy',
+	'lire_la_suite' => 'Čítať ďalej',
+	'look_navigation' => 'Prispôsobenie bloku s navigáciou',
+	'look_sommaire' => 'Prispôsobenie úvodnej stránky',
+
+	// M
+	'msg_erreur_v00' => 'Echec ', # NEW
+	'msg_erreur_v02' => 'Echec à la création du champ extra « Billet Sélectionné »', # NEW
+
+	// N
+	'navigation_desactiver_calendrier' => 'Ne pas afficher le calendrier dans la navigation', # NEW
+	'noisette_desc_bestof' => 'Affiche une liste cliquable des billets sélectionnés', # NEW
+	'noisette_desc_calendrier' => 'Affiche un calendrier mensuel', # NEW
+	'noisette_desc_categories' => 'Zoznam kategórií',
+	'noisette_desc_contact' => 'Affiche un lien vers la page contact (plugin Contact)', # NEW
+	'noisette_desc_langs' => 'Umožní zmeniť jazyk blogu',
+	'noisette_desc_lastcomments' => 'Zoznam n najnovších komentárov',
+	'noisette_desc_lastposts' => 'Zoznam posledných n lístkov vo forme zhrnutia',
+	'noisette_desc_links' => 'Affiche la liste de l\'ensemble des sites', # NEW
+	'noisette_desc_navigation' => 'Zobrazí odkazy navigácie (úvodná stránka, archívy)',
+	'noisette_desc_subscribe' => 'Affiche les boutons rss et atom pour les billets et les comentaires', # NEW
+	'noisette_titre_bestof' => 'Vybrané lístky',
+	'noisette_titre_calendrier' => 'Kalendár',
+	'noisette_titre_categories' => 'Kategórie',
+	'noisette_titre_contact' => 'Kontakty',
+	'noisette_titre_langs' => 'Jazyky blogu',
+	'noisette_titre_lastcomments' => 'Najnovšie komentáre',
+	'noisette_titre_lastposts' => 'Posledné lístky',
+	'noisette_titre_links' => 'Zoznam stránok',
+	'noisette_titre_navigation' => 'Odkazy navigácie',
+	'noisette_titre_search' => 'Vyhľadávač',
+	'noisette_titre_subscribe' => 'Liens d\'abonnement', # NEW
+	'nom_page-sommaire' => 'Page par défaut de spipclear', # NEW
+
+	// O
+	'oksearch' => 'ok',
+
+	// P
+	'par' => 'podľa',
+	'permalink_pour' => 'Trvalý odkaz na',
+	'publie_le' => 'Publikovaný',
+
+	// R
+	'recherche' => 'Vyhľadávanie',
+	'resultat_recherche' => 'Résultats pour votre recherche de', # NEW
+	'rss_commentaire_article' => 'Fil des commentaires de ce billet', # NEW
+	'rss_pour' => 'fil RSS des commentaires de', # NEW
+
+	// S
+	'sabonner' => 'S\'abonner', # NEW
+	'secteur_spipclear_aucun' => 'Aucun', # NEW
+	'secteurs_spipclear' => 'Secteurs SpipClear', # NEW
+	'selection' => 'À retenir', # NEW
+	'selectionner_theme' => 'Cliquez pour sélectionner ce thême', # NEW
+	'site_comment' => 'stránka',
+	'sommaire_afficher_tags' => 'Ne pas afficher les tags dans les listes d\'articles (page d\'accueil, résultats de recherche, etc).', # NEW
+	'sommaire_articles_entiers' => 'Afficher les articles en entier (par défaut, seule une introduction est affichée)', # NEW
+	'sommaire_spipclear' => 'Appliquer SpipClear sur la page d\'accueil du site (ne fonctionne que pour un secteur).', # NEW
+	'syndication' => 'Syndikácia',
+
+	// T
+	'tag' => 'Tag',
+	'tags' => 'Tagy',
+	'texte_erreur1' => 'La recherche de', # NEW
+	'texte_erreur2' => 'ne donne aucun résultat.', # NEW
+	'themes_installes' => '<strong>Les thèmes installés sur ce spip</strong> (cliquez sur un l\'icone d\'un thème pour le sélectionner)', # NEW
+	'titre_erreur' => 'Chyba:',
+	'trackback' => 'trackback', # NEW
+	'trackbacks' => 'trackbacks', # NEW
+	'trackbacks_fermes' => 'Les trackbacks pour ce billet sont fermés.', # NEW
+	'trackbacks_pour_faire' => 'Pour faire un trackback sur ce billet', # NEW
+
+	// U
+	'un_seul_mot' => 'un seul mot', # NEW
+
+	// V
+	'voir_capture' => 'Afficher la capture d\'écran', # NEW
+	'votre_commentaire' => 'Votre commentaire' # NEW
+);
+
+?>
