a05327cbddabde5273b03bdea0443280ce664c31
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-30 18:24:38 +0000

    Pas de texte rajouté après les entrées des sigles : pose problème sur |couper et |attribut_html
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@57850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/soyezcreateurs_fonctions.php
+++ soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/soyezcreateurs_fonctions.php
@@ -919,12 +919,14 @@ function dictionnaires_remplacer_defaut($mot, $definition){
  * (car seuls les gens avec des yeux valides et un pointeur de souris ont accès à l'information)
  */
 function dictionnaires_remplacer_abbr($mot, $definition){
-	return '<abbr title="'.couper(trim(attribut_html(supprimer_tags(typo($definition['texte'])))),80).'">'.$mot.'</abbr>'
+	return '<abbr title="'.couper(trim(attribut_html(supprimer_tags(typo($definition['texte'])))),80).'">'.$mot.'</abbr>';
+	/* Pas de texte rajouté après : pose problème sur |couper et |attribut_html
 		.'<sup><a href="'
 		.($definition['url']?$definition['url']:generer_url_public('sigles', 'id_dictionnaire='.$definition['id_dictionnaire'].'&type=abbr#sigle'.$definition['id_definition']))
 		.'" title="'._T('definition:titre_definition').': '.couper(trim(attribut_html(supprimer_tags(typo($definition['texte'])))),80).'">'
 		.'?'
 		.'</a></sup>';
+	*/
 }
 
 ?>
