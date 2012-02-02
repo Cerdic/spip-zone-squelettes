5412a314c37a0cbd6647ce1732d4030d1eb831ec
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-02 17:22:35 +0000

    pas de préfixe différent - un paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@57968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ MiniGriSpip/mgs_1-6/lang/paquet-minigrispip_fr.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'minigrispip_nom' => "MiniGriSpip",
+	'minigrispip_slogan' => "Un squelette simple pour débuter avec SPIP",
+	'minigrispip_description' => "	MiniGriSpip est un squelette simple et sobre fait par un d&#233;butant pour les d&#233;butants, bas&#233; sur le squelette Minimal-SPIP, mais avec quelques modifications pour une adaptation &#224; l'&#233;volution de SPIP.",
+);
+
+?>
