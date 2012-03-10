bf2af248b94d8288ad0e7ac80ce93752bfa25c0b
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-10 09:30:11 +0000

    utilisation de la balise #PLUGIN et suppression de la balise et de la fonction #VERSION_SQUELETTE devenues inutiles
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@59182 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/branche_V3/escal_fonctions.php
+++ escal/branche_V3/escal_fonctions.php
@@ -1,33 +1,5 @@
 <?php
-// =======================================================================================================================================
-// Balise : #VERSION_SQUELETTE
-// =======================================================================================================================================
-// Auteur: SpipFactory
-// Fonction : affiche la version utilisee du squelette Escal variable globale $version_squelette
-// =======================================================================================================================================
-//
-function balise_VERSION_SQUELETTE($p) {
-	$p->code = 'calcul_version_squelette()';
-	$p->interdire_scripts = false;
-	return $p;
-}
 
-function calcul_version_squelette() {
-
-	$version = NULL;
-
-	if (lire_fichier(_DIR_PLUGIN_ESCAL.'/paquet.xml', $contenu)
-	&& preg_match('/<version>(.*?)<\/version>/', $contenu, $match))
-		$version .= trim($match[1]);
-
-	$revision = version_svn_courante(_DIR_PLUGIN_ESCAL);
-	if ($revision > 0)
-		$version .= ' ['.strval($revision).']';
-	else if ($revision < 0)
-		$version .= ' ['.strval(abs($revision)).'&nbsp;<strong>svn</strong>]';
-
-	return $version;
-}
 
 // =======================================================================================================================================
    // pour gerer les classes des differents liens dans les articles
