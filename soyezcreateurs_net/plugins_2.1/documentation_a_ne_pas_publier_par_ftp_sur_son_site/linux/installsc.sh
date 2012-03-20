9ef2f179af9059052c14353c8f1f589cd7307333
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-20 15:42:01 +0000

    Pays, Coordonnées et Contacts et organisations dans l'installateur
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@59580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
+++ soyezcreateurs_net/plugins_2.1/documentation_a_ne_pas_publier_par_ftp_sur_son_site/linux/installsc.sh
@@ -9,9 +9,7 @@ svn checkout svn://trac.rezo.net/spip/branches/spip-2.1 ./
 cd extensions
 mkdir plugins_soyezcreateurs
 cd plugins_soyezcreateurs
-sleep 15 && svn checkout http://svn.github.com/Cerdic/video_accessible.git ./video_accessible/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/accessibilite
-sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/dictionnaires/branches/v0 ./dictionnaires/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/agenda/2_0_0 ./agenda/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/bandeau
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/boutonstexte/branches/v1 ./boutonstexte/
@@ -19,6 +17,7 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/cfg
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/changement_langue
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/crayons
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/dictionnaires/branches/v0 ./dictionnaires/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/facteur/branches/v1 ./facteur/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/fonctions_images
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/forms/forms_et_tables_2_0 ./forms/
@@ -40,6 +39,7 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/saisies
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/savecfg
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-2
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs
+sleep 15 && svn checkout http://svn.github.com/Cerdic/video_accessible.git ./video_accessible/
 cd ../..
 
 #Installation des plugins
@@ -52,6 +52,8 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/autorite
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/clevermail/branches/2.0.0 ./clevermail/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/ ./comments/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contact/branches/v0_7 ./contact/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/contacts_et_organisations/branches/v1 ./contacts_et_organisations/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/coordonnees/branches/v1.4 ./coordonnees/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/critere_mots/branches/1.2.3 ./critere_mots
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/eva_freemind/eva_freemind_2_0
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/formidable/branches/v0
@@ -59,13 +61,14 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gis/branches/v2
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/gravatar
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/inserer_modeles/branches/v_0 ./insertion_modele
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/jquery_ui
-sleep 15 && svn checkout _plugins_/player/branches/lm_v1 ./lecteur_multimedia/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/player/branches/lm_v1 ./lecteur_multimedia/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/memoization
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/menu_langues_liens
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/microblog
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/notation/branches/v_0_9 ./notation
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/nuage/trunk ./nuage/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/odt2spip/version_0.1_stable ./odt2spip/
+sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/pays/branches/v1.0 ./pays/
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/rainette
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/sedna
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/skeleditor/skeleditor-2 ./skeleditor/
@@ -75,7 +78,6 @@ sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/splickrbox
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/twidget
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/verifier
 sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/yaml
-#sleep 15 && svn checkout svn://zone.spip.org/spip-zone/_plugins_/woopra
 cd ..
 
 #Installation du squelette SoyezCreateur : il faut le faire dans un sous dossier
