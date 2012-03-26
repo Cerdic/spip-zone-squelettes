fb69b4a3dbedc4ba8eb67dc0825bd2cf8a369a24
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-26 18:59:54 +0000

    Mise à jour des squelettes EVA-web pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@59766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_web42/formulaires/evasquelettes_multilangue.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_evasquelettes_multilangue_charger_dist(){
+	foreach(array(
+		"evamultilinguisme"
+		) as $m)
+		$valeurs[$m] = 'non';
+		$test=sql_showtable('spip_eva_habillage_images',true);
+		if ($test['field']) {
+			$test=sql_select('nom_image','spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='menu_langue_actif'");
+			$tab=sql_fetch($test);
+			$go=$tab['nom_image'];
+			if ($go) $valeurs[$m] = 'oui';
+		}
+	return $valeurs;
+}
+
+
+function formulaires_evasquelettes_multilangue_traiter_dist(){
+	$res = array('editable'=>true);
+	foreach(array(
+		"evamultilinguisme"
+		) as $m)
+		if (!is_null($v=_request($m))) {
+			if ($v=='oui') {
+				sql_delete('spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='menu_langue_actif'");
+				sql_insertq('spip_eva_habillage_images',array(
+				'type' => 'multilinguisme',
+				'nom_habillage' => 'Defaut',
+				'nom_div' => 'menu_langue_actif',
+				'nom_image' => 'oui'));
+			}
+			elseif ($v=='non') sql_delete('spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='menu_langue_actif'");
+		}
+
+	$res['message_ok'] = _T('config_info_enregistree');
+	return $res;
+}
+
