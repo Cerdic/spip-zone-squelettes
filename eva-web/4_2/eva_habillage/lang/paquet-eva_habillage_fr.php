5351eae801687a3d454b6496785e20fb8ddc9ef2
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-27 14:26:55 +0000

    Mise à jour minimale pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@59815 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_habillage/lang/paquet-eva_habillage_fr.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'eva_habillage_slogan' => 'G&#233;rer les habillages d&#8217;EVA',
+'eva_habillage_description' => 'Gestion automatique des habillages d&#8217;EVA-web 4.2 par un administrateur du site.',
+);
+?>
