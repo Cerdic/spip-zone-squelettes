5823847b0d3659ddf42204934a47b4c1df57b4f4
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-27 14:31:59 +0000

    Mise à jour minimale pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@59817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_bonus/lang/evabonus_config_fr.php
@@ -0,0 +1,7 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'bouton_evabonus'=>'Noisettes bonus pour EVA-web',
+);
+?>
---
+++ eva-web/4_2/eva_bonus/lang/paquet-evabonus_fr.php
@@ -0,0 +1,9 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'evabonus_slogan'=>'Des ajouts bonus pour EVA-web',
+'evabonus_description'=>'Noisettes suppl&#233;mentaires pour les squelettes EVA-web,...',
+
+);
+?>
