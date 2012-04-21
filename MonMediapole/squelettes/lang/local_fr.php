2bf8d4c28b33835b323205787e80c6054d4b29e5
jsb92@laposte.net <jsb92@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-21 17:27:38 +0000

    Les squelettes de mp-boulogne.ac-versailles.fr adaptés pour SPIP 3.0.0rc
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@60530 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ MonMediapole/squelettes/lang/local_fr.php
@@ -0,0 +1,74 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nomm� admin_lang genere le NOW()
+// langue / language = fr
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// A
+'about' => '&Agrave;&nbsp;propos',
+'accueil_site' => 'Accueil',
+'archives' => 'Archives',
+'article' => 'Vous lisez...',
+'auteur' => 'archives de l&#39;auteur',
+'auteur_count' => 'a &eacute;crit',
+'annuaires' => 'M&eacute;langeur de flux',
+'agenda' => 'Agenda',
+'agenda_site' => 'Agenda du site',
+'actu' => 'br&egrave;ve',
+'actus' => 'br&egrave;ves',
+
+// B
+'browse' => 'Th&eacute;matiques',
+'breves' => 'Br&egrave;ves',
+'billet' => 'billet',
+'billets' => 'billets',
+
+//C
+'category_count' => 'Cette cat&eacute;gorie contient',
+'comment' => 'commentaire',
+'comments' => 'commentaires',
+'continue_reading' => 'Lire la suite',
+'cryptographic_key' =>'Cl&eacute; cryptographique',
+
+
+//D
+'dernier_article' => 'Dernier billet',
+'dernieres_breves' => 'Derni&egrave;res br&egrave;ves',
+'derniers_articles' => 'Billets r&eacute;cents',
+'discussion' => 'Commentaires',
+'derniers_evens' => 'Prochains &eacute;v&eacute;nements',
+
+//E
+'email_auteur' => '&Eacute;crire &agrave; l&#39;auteur',
+
+//F
+'featured_post' => 'S&eacute;lection',
+'for' => 'pour',
+
+//
+'les_evens' => 'Les &eacute;v&eacute;nements',
+
+//R
+'retour_accueil' => 'Perdu ? Retour &agrave; la page d&#39;accueil',
+'rubriques' => 'Cat&eacute;gories',
+'recherche' => 'Recherche',
+'rss_section' => 'Fil RSS pour cette rubrique',
+
+//S
+'subscribe' => 'Abonnement',
+'syndiquer_site' => 'Tout le site',
+
+//T
+'tags' => '&Eacute;tiquettes',
+
+//V
+'dernieres_videos' => 'S&eacute;lection vid&eacute;o',
+
+//W
+'welcome' => 'Bienvenue sur'
+
+);
+
+?>
