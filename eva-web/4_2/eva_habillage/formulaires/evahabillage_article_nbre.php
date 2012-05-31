3deb403888e198e19db9bb71abf4bf3e8a6638a6
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-31 18:58:08 +0000

    Gestion du nombre d'éléments affichés dans les blocs des pages des articles : réécriture pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@61920 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_habillage/formulaires/evahabillage_article_nbre.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_evahabillage_article_nbre_charger_dist(){
+	//Rien à retourner ici : tout est dans le formulaire html et en php
+	$valeurs=array();
+	return $valeurs;
+}
+
+
+function formulaires_evahabillage_article_nbre_traiter_dist(){
+	$res = array('editable'=>true);
+	$res['message_ok'] = 'Aucune modification n\'a &eacute;t&eacute; enregistr&eacute;e';
+	if (_request('submit_nbre_article')) {
+		include_spip("inc/eva_habillage_definition_themes");
+		$les_blocs_array = EVA_mes_nbres();
+		$val_nbre=$les_blocs_array['EVA_nbre_article'];
+		foreach($val_nbre as $val) {
+			if (_request($val)) {
+				sql_delete('spip_eva_habillage_images',"nom_habillage='Defaut' AND nom_div='".$val."'");
+				sql_insertq('spip_eva_habillage_images',array('type' => 'nbre','nom_habillage' => 'Defaut','nom_div' => $val,'nom_image' => _request($val)));
+			}
+		}
+		$res['message_ok'] = 'Modification enregistr&eacute;e';
+	}
+	return $res;
+}
+
