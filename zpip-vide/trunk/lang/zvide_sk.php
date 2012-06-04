2cab3e03fd3e47a0234225b697483c49c0f3a16d
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-04 01:39:00 +0000

    langues (zvide)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@62073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-vide/trunk/lang/zvide_sk.php
@@ -0,0 +1,93 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/zvide?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_zvide' => 'Hlavička a päta stránky',
+
+	// D
+	'description_bloc_post_contenu' => 'Les noisettes de ce bloc seront insérées sur toutes les pages après le bloc <i>Contenu</i>.', # NEW
+	'description_bloc_post_extra' => 'Les noisettes de ce bloc seront insérées sur toutes les pages après le bloc <i>Extra</i>.', # NEW
+	'description_bloc_post_navigation' => 'Les noisettes de ce bloc seront insérées sur toutes les pages après le bloc <i>Navigation</i>.', # NEW
+	'description_bloc_pre_contenu' => 'Les noisettes de ce bloc seront insérées sur toutes les pages avant le bloc <i>Contenu</i>.', # NEW
+	'description_bloc_pre_extra' => 'Les noisettes de ce bloc seront insérées sur toutes les pages avant le bloc <i>Extra</i>.', # NEW
+	'description_bloc_pre_navigation' => 'Les noisettes de ce bloc seront insérées sur toutes les pages avant le bloc <i>Navigation</i>.', # NEW
+	'description_page-401' => 'Cette page est affichée lorsqu\'un visiteur demande à voir une page pour laquelle il n\'est pas autorisé.', # NEW
+	'description_page-404' => 'Cette page est affichée lorsqu\'un visiteur demande à voir une page qui n\'existe pas ou plus.', # NEW
+	'description_page-agenda' => 'Page destinée à présenter les évènements / l\'agenda de votre site.', # NEW
+	'description_page-forum' => 'Cette page est appelée lorsqu\'un visiteur souhaiter poster un message dans un forum.', # NEW
+	'description_page-login' => 'Cette page est nécessaire pour se connecter à l\'espace privé. Par sécurité, si la noisette <i>Formulaire d\'identification</i> spécifique à cette page n\'est pas insérée dans le bloc <i>Contenu</i>, elle y sera insérée d\'office.', # NEW
+	'description_page-plan' => 'Cette page est appelée pour afficher le plan du site.', # NEW
+	'description_page-recherche' => 'Cette page est affichée lorsqu\'une recherche est effectuée sur le site.', # NEW
+	'description_page-spip_pass' => 'Cette page est affichée lorsqu\'un visiteur a oublié son mot de passe et souhaite en changer.', # NEW
+	'description_page_article' => 'Predvolená stránka pre články.',
+	'description_page_auteur' => 'Predvolená stránka pre autorov.',
+	'description_page_breve' => 'Predvolená stránka pre novinky.',
+	'description_page_evenement' => 'Predvolená stránka pre udalosti.',
+	'description_page_groupe_mots' => 'Nepovinná stránka pre skupiny kľúčových slov.',
+	'description_page_mot' => 'Predvolená stránka pre kľúčové slová.',
+	'description_page_rubrique' => 'Predvolená stránka pre rubriky.',
+	'description_page_site' => 'Predvolená stránka pre webové stránky, na ktoré odkazujete.',
+	'description_pagedefaut' => 'Les blocs de cette page seront ajoutés sur toutes les pages du site.', # NEW
+
+	// E
+	'explication_liens_add' => 'Vous pouvez saisir ici un ou plusieurs liens additionnels à mettre dans le pieds de page. Si vous ajoutez plusieurs liens, pensez à les séparer avec un |. Vous pouvez utiliser les raccourcis SPIP. Par exemple : <code>[Contact->12] | [Mentions légales->art13]</code>', # NEW
+	'explication_masquer_connexion' => 'Schovať odkazy umožňujúce prihlásiť sa/odhlásiť sa?',
+	'explication_masquer_logo' => 'Schovať logo stránky?',
+	'explication_masquer_plan' => 'Schovať odkaz na mapu stránky?',
+	'explication_masquer_rss' => 'Schovať odkaz na kanál RSS?',
+	'explication_masquer_slogan' => 'Schovať slogan stránky?',
+	'explication_menu_lang' => 'Cette option n\'affecte que les sites multilingues.<br />L\'option <em>Défaut</em> reproduit le fonction de Zpip-dist : un formulaire de choix de langue est affiché sur toutes les pages. Lorsqu\'une langue est sélectionnée par l\'utilisateur, la page est rechargée en lui passant un paramètre <code>lang</code>. Ce fonctionnement est adapté aux sites utilisant les blocs multilingues (<code><multi></code>) dans les objets éditoriaux et ayant définit la variable globale <code>forcer_lang</code> à <code>true</code>.<br />L\'option <em>Page d\'accueil seulement</em> affichera le formulaire de sélection de langue uniquement sur la page d\'accueil.<br />L\'option <em>Retour à la page d\'accueil</em> affichera le formulaire sur toutes les pages, mais le choix d\'une langue entraînera le retour à la page d\'accueil dans la langue choisie.<br />Enfin, l\'option <em>Liens de traduction</em> correspond aux sites utilisant des liens de traduction entre articles. Le formulaire de choix de la langue ne sera affiché que sur les pages ne correspondant pas à un objet éditorial (accueil, plan du site, etc.). Sur les pages des articles, le formulaire sera affiché si des traductions sont disponibles et pointera sur ces traductions. Le fonctionnement sera équivalent sur les pages des rubriques si le plugin <em>trad_rub</em> est installé.', # NEW
+
+	// L
+	'label_choix_menu_lang_defaut' => 'Défaut', # NEW
+	'label_choix_menu_lang_liens_trad' => 'Odkazy na preklad',
+	'label_choix_menu_lang_masquer' => 'Schovať všetky stránky',
+	'label_choix_menu_lang_retour_sommaire' => 'Vrátiť sa na úvodnú stránku',
+	'label_choix_menu_lang_sommaire' => 'Iba úvodná stránka',
+	'label_liens_add' => 'Liens additionnels', # NEW
+	'label_masquer_connexion' => 'Odkaz na prihlásenie sa',
+	'label_masquer_logo' => 'Logo stránky',
+	'label_masquer_plan' => 'Mapa stránky',
+	'label_masquer_rss' => 'Kanál RSS',
+	'label_masquer_slogan' => 'Slogan stránky',
+	'label_menu_lang' => 'Jazykové menu',
+	'label_options_en_tete' => 'Možnosti hlavičky stránky',
+	'label_options_pied' => 'Možnosti päty stránky',
+
+	// N
+	'nom_bloc_post_contenu' => 'Post-Contenu', # NEW
+	'nom_bloc_post_extra' => 'Post-Extra', # NEW
+	'nom_bloc_post_navigation' => 'Post-Navigation', # NEW
+	'nom_bloc_pre_contenu' => 'Pré-Contenu', # NEW
+	'nom_bloc_pre_extra' => 'Pré-Extra', # NEW
+	'nom_bloc_pre_navigation' => 'Pré-Navigation', # NEW
+	'nom_page-401' => 'Chyba 401',
+	'nom_page-404' => 'Chyba 404',
+	'nom_page-agenda' => 'Kalendár udalostí',
+	'nom_page-forum' => 'Diskusné fórum',
+	'nom_page-login' => 'Prihlásiť sa',
+	'nom_page-plan' => 'Mapa stránky',
+	'nom_page-recherche' => 'Vyhľadať na stránke',
+	'nom_page-sommaire' => 'Úvodná stránka webu',
+	'nom_page-spip_pass' => 'Zabudnuté heslo',
+	'nom_page_article' => 'Článok',
+	'nom_page_auteur' => 'Autor',
+	'nom_page_breve' => 'Novinka',
+	'nom_page_evenement' => 'Udalosť',
+	'nom_page_groupe_mots' => 'Skupina kľúčových slov',
+	'nom_page_mot' => 'Kľúčové slovo',
+	'nom_page_rubrique' => 'Rubrika',
+	'nom_page_site' => 'Stránka, na ktorú odkazujete',
+	'nom_pagedefaut' => 'Predvolená stránka',
+
+	// Z
+	'zvide' => 'Zpip-vide'
+);
+
+?>
