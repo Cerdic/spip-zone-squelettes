2c8cb26215781823c73d8ffa32f3da5586e9de6f
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-05 07:15:57 +0000

    Lien, slogan et descriptif pour le plugin Suppression d'un fichier devenu inutile
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@62145 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_calendrier/lang/paquet-eva_calendrier_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'eva_calendrier_slogan' => 'Affichage d&#8217;une rubrique sous forme de calendrier',
+'eva_calendrier_description' => 'Affichage d&#8217;une rubrique sous forme de calendrier mensuel avec syst&#232;me de navigation vers les mois pr&#233;c&#233;dents et suivants.
+La rubrique s&#8217;affiche sous forme de calendrier d&#233;s lors qu&#8217;on lui joint le mot cl&#233; {{calendrier}}.
+Les articles sont inscrits dans le calendrier en leurs attribuant une date de r&#233;daction ant&#233;rieure correspondant &#224; la date de l&#8217;&#233;v&#232;nement concern&#233;.',
+);
+?>
