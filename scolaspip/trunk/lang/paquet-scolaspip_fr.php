88ae20311ab9b5c55250ffb67eb42ea69e71e7be
johan.pustoch@crdp.ac-versailles.fr <johan.pustoch@crdp.ac-versailles.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-16 08:55:25 +0000

    description du plugin plus précise + changement du chemin du logo en espérant que plugins.net prenne en compte le nouveau logo
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@62583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scolaspip/trunk/lang/paquet-scolaspip_fr.php
+++ scolaspip/trunk/lang/paquet-scolaspip_fr.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'scolaspip_slogan' => 'Un squelette personnalisable pour établissements scolaires',
-'scolaspip_description' => 'Un squelette personnalisable pour les sites d\'écoles, collèges, lycées',
+'scolaspip_description' => 'ScolaSPIP est destiné aux webmestres débutants et permet facilement de personnaliser un site SPIP : à partir du menu « Squelettes » une interface permet de choisir ses couleurs, options d’affichage de la page d’accueil et des contenus. Des mots-clés permettent ensuite de personnaliser les menus et contenus.',
 
 );
 
