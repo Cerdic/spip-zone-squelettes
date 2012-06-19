f0ba80d933e5b46a4af0f6ad26ae8178dbbdcc8d
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-19 06:24:02 +0000

    Pour Escal 3 : choix du pas de pagination pour les articles d'une rubrique + chtites corrections
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@62691 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/branche_V3/lang/local_fr.php
+++ escal/branche_V3/lang/local_fr.php
@@ -6,6 +6,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'form_pet_envoi_mail_confirmation' => 'Un courrier &eacute;lectronique de confirmation vient d\'&ecirc;tre envoy&eacute; au webmaster du site afin de valider votre inscription. ',
-'acces_direct' => 'Test',
+
 );
 ?>
