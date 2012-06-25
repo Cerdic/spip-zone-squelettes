4bbd96954d5c07afd70b73c5c698db1af6cf4612
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-25 14:27:36 +0000

    Mise à jour d'EVA-livre pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@62980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_livre_2/lang/paquet-livre_2_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'livre_2_slogan' => 'Publication d&#8217;une s&#233;rie d&#8217;articles sous forme de livre',
+'livre_2_description' => '{{Int&#233;ret de ce plugin}}
+	
+- Publier les articles d&#8217;une rubrique sous forme de livre
+- Permettre le passage d&#8217;un article à un autre par pagination
+
+{{Mise en place de ce plugin}}
+
+- Activer le plugin en cochant la case ci-contre, puis valider
+- Suivre la proc&#233;dure de mise en place du livre d&#233;crite dans l&#8217;aide d&#8217;Eva',
+    
+);
+
+?>
