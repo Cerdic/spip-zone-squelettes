ef3da2b1e487b4671e90875c0790a3d0ff0fbbcc
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-23 22:15:02 +0000

    correction du paquet.xml : pas de numéro dans le prefix, attribut documentation correct, versionnage x.y.z + chaines de langues slogan
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@63666 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ typomaniac/lang/typomaniac-paquet_fr.php
@@ -0,0 +1,11 @@
+<?php
+/** Fichier de langue de SPIP **/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'slogan'=>'Un squelette minimal, responsive et typographique'
+	
+);
+
+?>
+
