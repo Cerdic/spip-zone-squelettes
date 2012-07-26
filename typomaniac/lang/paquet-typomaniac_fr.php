c67dda9ce2beeac0b401ffa9fb5787b4b1d8e218
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 13:50:55 +0000

    bonne chaine de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@64025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- typomaniac/lang/paquet-typomaniac_fr.php
+++ typomaniac/lang/paquet-typomaniac_fr.php
@@ -3,7 +3,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'slogan'=>'Un squelette minimal, responsive et typographique'
+	'typomaniac_slogan'=>'Un squelette minimal, responsive et typographique'
 	
 );
 
