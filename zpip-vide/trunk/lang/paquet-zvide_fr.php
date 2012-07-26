0bb16a8aaab22333cd76f37f3eba1443f6a2f514
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 10:58:16 +0000

    langues (paquet-zvide)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@64018 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-vide/trunk/lang/paquet-zvide_fr.php
+++ zpip-vide/trunk/lang/paquet-zvide_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-zvide
-// Langue: fr
-// Date: 21-01-2012 16:48:54
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/zpip-vide/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// Z
+	// Z
 	'zvide_description' => 'Un squelette Z dont l\'ensemble des contenus sont gérés sous forme de noisettes configurables avec le noiZetier. Ce squelette s\'utilise donc en conjonction avec le noiZetier et une collection de noisettes.',
-	'zvide_slogan' => 'Squelette Z vide de contenu à remplir avec des noisettes',
+	'zvide_slogan' => 'Squelette Z vide de contenu à remplir avec des noisettes'
 );
+
 ?>
