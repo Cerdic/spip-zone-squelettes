4cfc1ca14f955fab4f1c92e4b75ba41c88916f09
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-28 13:40:53 +0000

    Renommer les blocs extra1/ extra2/ en aside/ et extra/
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66334 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-dist/branches/z-bootstrap-dev/z_options.php
+++ zpip-dist/branches/z-bootstrap-dev/z_options.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
+$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer');
 define('_ZENGARDEN_FILTRE_THEMES','zboot');
 
 ?>
