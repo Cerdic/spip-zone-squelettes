88f96e63472a3d502889084be0eaa16c92c69da6
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-30 15:34:38 +0000

    Propager la correction du pipeline de Zen garden (report de 66367)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/branches/v_32/sarkaspip_pipelines.php
+++ sarkaspip/branches/v_32/sarkaspip_pipelines.php
@@ -53,44 +53,48 @@ function SarkaSpip_mes_fichiers_a_sauver($flux){
 	return $flux;
 }
 
-// Pipeline "zengarden_activer_theme" permettant d'effectuer les traitements d'activation d'un theme lies a Sarka-SPIP
+// Pipeline "zengarden_activer_theme" permettant d'effectuer les traitements d'activation d'un theme lies � Sarka-SPIP
 function SarkaSpip_zengarden_activer_theme($flux){
-	$dir_cfg = $flux['args']['dir'] . '/cfg';
+	$dir = $flux['args']['dir'];
+	$action = $flux['args']['action'];
+
+	$dir_cfg = $dir . '/cfg';
 	$cfg = preg_files($dir_cfg);
 
-	// On sauvegarde des valeurs actuelles pour un eventuel rappel
-	$dir_cfg = sous_repertoire(_DIR_TMP,"cfg");
-	$dir_cfg = sous_repertoire(_DIR_TMP,"themes");
-	$fonds = array();
-	foreach ($cfg as $_fichier_cfg) {
-		$fonds[] = basename($_fichier_cfg, '.txt');
-	}
-	include_spip('inc/sarkaspip_filtres');
-	$ok = sauvegarder_fonds($fonds, $dir_cfg, 'theme');
+	if ($action == 'activer') {
+		// On sauvegarde des valeurs actuelles pour un eventuel rappel
+		$dir_cfg = sous_repertoire(_DIR_TMP,"cfg");
+		$dir_cfg = sous_repertoire(_DIR_TMP,"themes");
+		$fonds = array();
+		foreach ($cfg as $_fichier_cfg) {
+			$fonds[] = basename($_fichier_cfg, '.txt');
+		}
+		include_spip('inc/sarkaspip_filtres');
+		$ok = sauvegarder_fonds($fonds, $dir_cfg, 'theme');
 
-	// On ecrit la nouvelle configuration n�cessaire au theme choisi
-	$flux['data'] = true;
-	foreach ($cfg as $_fichier_cfg) {
-		lire_fichier($_fichier_cfg, $tableau);
-		$fond = basename($_fichier_cfg, '.txt');
-		ecrire_config($fond, $tableau);
+		// On ecrit la nouvelle configuration n�cessaire au theme choisi
+		$flux['data'] = true;
+		foreach ($cfg as $_fichier_cfg) {
+			lire_fichier($_fichier_cfg, $tableau);
+			$fond = basename($_fichier_cfg, '.txt');
+			ecrire_config($fond, $tableau);
+		}
 	}
-	return $flux;
-}
+	else if ($action == 'effacer') {
+		$fichiers = array();
+		$flux['data'] = true;
+		foreach ($cfg as $_fichier_cfg) {
+			$fond = basename($_fichier_cfg, '.txt');
+			effacer_config($fond);
+			$fichiers[] = _DIR_TMP . 'themes/cfg/' . $fond . '.txt';
+		}
+		include_spip('inc/sarkaspip_filtres');
+		$ok = restaurer_fonds($fichiers);
+	}
+	else {
+		// action apercevoir : on ne fait rien, on sait pas faire..
+	}
 
-// Pipeline "zengarden_effacer_theme" permettant d'effectuer les traitements d'effacement d'un theme lies a Sarka-SPIP
-function SarkaSpip_zengarden_effacer_theme($flux){
-	$dir_cfg = $flux['args']['dir'] . '/cfg';
-	$cfg = preg_files($dir_cfg);
-	$fichiers = array();
-	$flux['data'] = true;
-	foreach ($cfg as $_fichier_cfg) {
-		$fond = basename($_fichier_cfg, '.txt');
-		effacer_config($fond);
-		$fichiers[] = _DIR_TMP . 'themes/cfg/' . $fond . '.txt';
-	}
-	include_spip('inc/sarkaspip_filtres');
-	$ok = restaurer_fonds($fichiers);
 	return $flux;
 }
 ?>
