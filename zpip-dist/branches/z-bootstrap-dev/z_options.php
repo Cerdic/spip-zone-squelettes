7d5743b7d0295797f2d4b04efe5b278763a1787d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-09 11:18:14 +0000

    Remise a plat des listes d'objet : structure commune .liste[.long][.short] > .liste-items[.long][.short] > .item le contenu de chaque .item est fourni par inclure/resume/xxx.html (article, rubrique, breve... a completer) qui a une structure commune (héritée de hNews) dans un <article>
    Les classes modificatrices .long ou .short permettent d'avoir une liste plus detaillee ou plus courte, mais ce n'est qu'une option de presentation, le html etant identique.
    Le seul parametre pris en compte par resume/xxx est "coupe" qui permet de changer la longueur de coupe
    
    Un repertoire de demo (activé pour les seuls webmestres) avec une page de presentation des listes (toutes variantes)
    .secondary sur le contenu secondaire du layout (les colonnes)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66607 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-dist/branches/z-bootstrap-dev/z_options.php
+++ zpip-dist/branches/z-bootstrap-dev/z_options.php
@@ -3,4 +3,9 @@ $GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header',
 define('_ZENGARDEN_FILTRE_THEMES','zboot');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
+if (isset($GLOBALS['visiteur_session']['statut'])
+  AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
+  AND $GLOBALS['visiteur_session']['webmestre']=='oui')
+	_chemin(_DIR_PLUGIN_ZBOOT."demo/");
+
 ?>
