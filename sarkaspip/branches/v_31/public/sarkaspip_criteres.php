2f93d46cd9e389dca6128923aa0d0b62e82aa19a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-12 17:24:16 +0000

    Passage en LF Unix plutot que Windows qui semble poser des soucis sur certains serveurs. Incrément de version pour marquer le coup.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/branches/v_31/public/sarkaspip_criteres.php
+++ sarkaspip/branches/v_31/public/sarkaspip_criteres.php
@@ -3,7 +3,7 @@
 // Critere : tout_voir
 // =======================================================================================================================================
 // Auteur: SarkASmeL
-// Fonction : permet d'éviter une erreur quand le plugin ACCES RESTREINT n'est pas actif. Le critère ne fait rien
+// Fonction : permet d'√©viter une erreur quand le plugin ACCES RESTREINT n'est pas actif. Le crit√®re ne fait rien
 // =======================================================================================================================================
 //
 if (!defined('_DIR_PLUGIN_ACCESRESTREINT')) {
