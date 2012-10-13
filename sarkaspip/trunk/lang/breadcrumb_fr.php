0870f3f222d44e55ed69a745ac577f45c99b0e46
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-13 15:04:06 +0000

    breadcrumb par defaut pour les pages, et deplacement de classes sur le body
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66775 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sarkaspip/trunk/lang/breadcrumb_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: sarkaspip
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'abonnement_newsletter' => 'Abonnement',
+	'agenda' => 'Agenda',
+	'galerie' => 'Galerie',
+	'mot' => 'Mots-clés',
+	'herbier' => 'Sites favoris',
+	'site' => 'Sur le web',
+	'404' => 'Page introuvable',
+	'plan' => 'Plan du site',
+	'recherche' => 'Recherche',
+);
+?>
