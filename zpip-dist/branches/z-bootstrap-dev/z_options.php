f8dd592ecf6cbb53436f7651efeb8a3a00fcd4a0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-15 20:09:48 +0000

    on extrait le fil d'ariane en tant que bloc indépendant qui peut donc être plus facilement positionné dans la page
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66824 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-dist/branches/z-bootstrap-dev/z_options.php
+++ zpip-dist/branches/z-bootstrap-dev/z_options.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer');
+$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
 define('_ZENGARDEN_FILTRE_THEMES','zboot');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
