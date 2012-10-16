0cf7631b2a288200cbfb4ce4d353721e4f8de9e0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-16 14:21:09 +0000

    Un blog et puis c'est tout
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@66845 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-dist/branches/z-blog-bootstrap-dev/zblog_options.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+define('_ZENGARDEN_FILTRE_THEMES','zboot');
+define('_ALBUMS_INSERT_HEAD_CSS',false);
+
+
+?>
