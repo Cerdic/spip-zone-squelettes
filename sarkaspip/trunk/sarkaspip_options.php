4d24cbe6ccbbbc17e2c2eff6a5523dc7294b547f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-30 16:54:39 +0000

    desactiver l'insertion de albums.css et fil d'ariane sur les albums
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@67315 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/trunk/sarkaspip_options.php
+++ sarkaspip/trunk/sarkaspip_options.php
@@ -7,6 +7,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+define('_ALBUMS_INSERT_HEAD_CSS',false);
 
 // Liste des rubriques specialisees standard du squelette
 // Pour ajouter des rubriques perso, definir de la meme facon les constantes _PERSO_XXX
