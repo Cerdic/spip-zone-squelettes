78b01d51ba69ecfb355f7cd966930eed8788795c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-06 17:26:44 +0000

    Boutons et action d'ajout de page (rubrique) et chapitre (article) et de depubli/suppression d'un chapitre mise en forme placeholder sur les champs vide quand on est admin
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@67525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-dist/branches/z-doc/zdoc_options.php
+++ zpip-dist/branches/z-doc/zdoc_options.php
@@ -1,11 +1,16 @@
 <?php
 $GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer');
+
+if ($GLOBALS['visiteur_session']['statut']=='0minirezo')
+	$GLOBALS['marqueur'].=":minirezo";
+
 define('_ZENGARDEN_FILTRE_THEMES','zboot');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
 function title2anchor($titre,$id_article=""){
 	include_spip("inc/charsets");
 	$titre = translitteration($titre);
+	$titre = supprimer_numero($titre);
 	$titre = preg_replace(",\W,","",$titre);
 	$titre = strtolower($titre);
 	if (preg_match(',^\d,',$titre))
@@ -13,4 +18,21 @@ function title2anchor($titre,$id_article=""){
 	return $titre;
 }
 
+function urls_generer_url_article_dist($id, $args, $ancre){
+
+	$row = sql_fetsel('id_rubrique,titre','spip_articles','id_article='.intval($id));
+	return generer_url_entite($row['id_rubrique'],'rubrique','',title2anchor($row['titre'],$id),true);
+
+}
+
+function placeholder($texte,$p=true){
+	if (!$texte
+		AND !strlen($texte)
+		AND $GLOBALS['visiteur_session']['statut']=='0minirezo'){
+		$texte = "<i class='mute' title='Inserer un texte'>¤</i>";
+		if ($p)
+			$texte = "<p class='placeholder muted'>$texte</p>";
+	}
+	return $texte;
+}
 ?>
