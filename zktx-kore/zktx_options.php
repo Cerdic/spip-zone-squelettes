6a73d89b2a6c6d5b290567070c838149c80dc036
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-08 16:35:18 +0000

    (work in progress...)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@67608 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zktx-kore/zktx_options.php
+++ zktx-kore/zktx_options.php
@@ -1,8 +1,5 @@
 <?php
-// une url, c'est en minuscule et ce, quelque soit son format !
-	define('_url_minuscules',true);
-	// et ici je forcerais bien la nature du separateur ("-") si je savais comment faire ...
-	
+
 	// Forcer les url absolues a peu pres partout 
 	// certainement optimisable (pas sur que ca releve de typo)
 	/*
@@ -35,36 +32,14 @@
 		'head',
 		'head_js',
 		'header',
-		'nav', // rajout
+		'nav',
 		'footer',
 		'foot_js',
 		'social' //,
-		// 'p_cartouche',
-		// 'p_contenu',
-		// 'p_extra'
 	);
-
-// Chuck Norris ne fait jamais d'erreur !
-// Quand bien même, s'il en fait une, 
-// il faudrait être suicidaire pour la lui signaler !
-	@ini_set('display_errors', 'off');
 	
-// Ce qui se passe à Vegas reste à Vegas !!!
-	$GLOBALS['spip_header_silencieux'] = true; 
-
-// CK : Des Options ... de série !
-	$GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['NOM'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
-	$GLOBALS['toujours_paragrapher']=true;
-	$GLOBALS['derniere_modif_invalide']=true;
-	if (!defined('_DUREE_CACHE_DEFAUT')) define('_DUREE_CACHE_DEFAUT',86400);
-	if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
-	$GLOBALS['quota_cache']=100;
-	if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
-	if (!defined('_TRANCHES')) define('_TRANCHES',10);
-	
-// BTE
-	// Niveaux de titre
+// Semantique on a dit !
+	// Titre (BTE PP)
 	$GLOBALS['debut_intertitre'] = '<h2 class="h2">';
 	$GLOBALS['fin_intertitre'] = '</h2>';
 	$GLOBALS['debut_intertitre_2'] = '<h3 class="h3">';
@@ -75,6 +50,9 @@
 	$GLOBALS['fin_intertitre_4'] = '</h5>';
 	$GLOBALS['debut_intertitre_5'] = '<h6 class="h6">';
 	$GLOBALS['fin_intertitre_5'] = '</h6>';
-	// My CSS is better than yours, bitch !
 	$GLOBALS['BarreTypoEnrichie_Preserve_Header'] = true; 
+	// (CK)
+	$GLOBALS['table_des_traitements']['TITRE'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+	$GLOBALS['table_des_traitements']['NOM'][]= 'typo(supprimer_numero(%s), "TYPO", $connect)';
+	$GLOBALS['toujours_paragrapher']=true;
 ?>
