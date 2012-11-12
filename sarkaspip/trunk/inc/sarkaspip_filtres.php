85f02f5f8937cc544b672226e49c2358cfabd2ef
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-12 16:36:40 +0000

    tous les filtres agenda sont du code mort + deplacer les filtres plus utilisés dans un vieilles_def qu'il faut inclure au besoin.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@67683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/trunk/inc/sarkaspip_filtres.php
+++ sarkaspip/trunk/inc/sarkaspip_filtres.php
@@ -157,87 +157,6 @@ function typo_couleur($texte) {
 }
 
 // =======================================================================================================================================
-// Filtre : premier_jour_annee
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : Retourne la date du premier jour de l'annee passe en argument
-// =======================================================================================================================================
-//
-function premier_jour_annee($annee) {
-	if (!$annee) return;
-	
-	$jour = date("Y-m-d H:i:s", mktime(0,0,0,1,1,$annee));
-	return $jour;
-}
-// FIN du Filtre : premier_jour_annee
-
-// =======================================================================================================================================
-// Filtre : dernier_jour_annee
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : Retourne la date du dernier jour de l'annee passe en argument
-// =======================================================================================================================================
-//
-function dernier_jour_annee($annee) {
-	if (!$annee) return;
-	
-	$jour = date("Y-m-d H:i:s", mktime(23,59,59,12,31,$annee));
-	return $jour;
-}
-// FIN du Filtre : dernier_jour_annee
-
-// =======================================================================================================================================
-// Filtre : debut_journee
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : Retourne la date-heure de debut de la journee passee en argument
-// =======================================================================================================================================
-//
-function debut_journee($date) {
-	if (!$date) return;
-	
-	$jour = date('d', strtotime($date));
-	$mois = date('m', strtotime($date));
-	$annee = date('Y', strtotime($date));
-	$jour = date("Y-m-d H:i:s", mktime(0,0,0,$mois,$jour,$annee));
-	return $jour;
-}
-// FIN du Filtre : debut_journee
-
-// =======================================================================================================================================
-// Filtre : fin_mois_precedent
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : Calcul la date au format demande correspondant au dernier jour du mois precedent celui du timestamp en argument
-// =======================================================================================================================================
-//
-function fin_mois_precedent($timestamp, $format) {
-	if (!$timestamp) return;
-
-	$date = mktime(0, 0, 0, date('m', $timestamp), 0, date('Y', $timestamp));
-	return date($format, $date);
-}
-// FIN du Filtre : premier_jour_mois
-
-// =======================================================================================================================================
-// Filtre : fin_journee
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : Retourne la date-heure de fin de la journee passee en argument
-// =======================================================================================================================================
-//
-function fin_journee($date) {
-	if (!$date) return;
-	
-	$jour = date('d', strtotime($date));
-	$mois = date('m', strtotime($date));
-	$annee = date('Y', strtotime($date));
-	$jour = date("Y-m-d H:i:s", mktime(23,59,59,$mois,$jour,$annee));
-	return $jour;
-}
-// FIN du Filtre : fin_journee
-
-// =======================================================================================================================================
 // Filtre : libelle_statut
 // =======================================================================================================================================
 // Auteur: Smellup
@@ -310,16 +229,6 @@ function afficher_config($meta) {
 	return $texte;
 }
 // FIN du Filtre : afficher_config
-
-
-// =======================================================================================================================================
-// Filtres : module AGENDA
-// =======================================================================================================================================
-// Auteur: Smellup
-// Fonction : regroupe les filtres permettant les affichages de l'agenda
-// =======================================================================================================================================
-//
-include_spip('inc/sarkaspip_filtres_agenda');
 
 
 ?>
