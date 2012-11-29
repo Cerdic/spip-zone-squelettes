b1c589fe7fa0c566f61126a484d93cdb500e4ef4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-29 15:58:27 +0000

    paquet.xml, prefixe, icone
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@68019 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-dist/trunk/spipr_dist_options.php
+++ spipr-dist/trunk/spipr_dist_options.php
@@ -1,11 +1,11 @@
 <?php
 $GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
-define('_ZENGARDEN_FILTRE_THEMES','zboot');
+define('_ZENGARDEN_FILTRE_THEMES','spipr');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
 if (isset($GLOBALS['visiteur_session']['statut'])
   AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
   AND $GLOBALS['visiteur_session']['webmestre']=='oui')
-	_chemin(_DIR_PLUGIN_ZBOOT."demo/");
+	_chemin(_DIR_PLUGIN_SPIPR_DIST."demo/");
 
 ?>
