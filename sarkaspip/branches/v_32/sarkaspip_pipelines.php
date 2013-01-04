9de4328067f7cfe5071e0932b43b9c163d874e00
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-04 16:22:24 +0000

    Avec tous les fichiers c'est mieux !
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@68713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/branches/v_32/sarkaspip_pipelines.php
+++ sarkaspip/branches/v_32/sarkaspip_pipelines.php
@@ -1,6 +1,6 @@
 <?php
 // Pipeline insert_head public
-function SarkaSpip_insert_head($flux) {
+function sarkaspip_insert_head($flux) {
 
 	include_spip('inc/config');
 	
@@ -40,7 +40,7 @@ function SarkaSpip_insert_head($flux) {
 }
 
 // Pipeline "mes_fichiers_a_sauver" permettant de rajouter des fichiers � sauvegarder dans le plugin Mes Fichiers 2
-function SarkaSpip_mes_fichiers_a_sauver($flux){
+function sarkaspip_mes_fichiers_a_sauver($flux){
 	$tmp_fonds = defined('_DIR_TMP') ? _DIR_TMP.'fonds/': _DIR_RACINE.'tmp/fonds/';
 	$tmp_styles = defined('_DIR_TMP') ? _DIR_TMP.'cfg/': _DIR_RACINE.'tmp/cfg/';
 
@@ -51,13 +51,13 @@ function SarkaSpip_mes_fichiers_a_sauver($flux){
 	if (@is_dir($tmp_styles))
 		$flux[] = $tmp_styles;
 
-	spip_log('*** SarkaSpip_mes_fichiers_a_sauver ***');
+	spip_log('*** sarkaspip_mes_fichiers_a_sauver ***');
 	spip_log($flux);
 	return $flux;
 }
 
 // Pipeline "zengarden_activer_theme" permettant d'effectuer les traitements d'activation d'un theme lies � Sarka-SPIP
-function SarkaSpip_zengarden_activer_theme($flux){
+function sarkaspip_zengarden_activer_theme($flux){
 	$dir = $flux['args']['dir'];
 	$action = $flux['args']['action'];
 
@@ -102,4 +102,77 @@ function SarkaSpip_zengarden_activer_theme($flux){
 
 	return $flux;
 }
+
+// Pipelines specifiques a Sarka-SPIP
+// -- Fonction d'insertion en debut de colonne extra
+function sarkaspip_colonne_extra_debut($flux){
+	if (defined('_PERSO_COLONNE_EXTRA_DEBUT'))
+		return afficher_noisettes(_PERSO_COLONNE_EXTRA_DEBUT, $flux, true);
+}
+// -- Fonction d'insertion en fin de colonne extra
+function sarkaspip_colonne_extra_fin($flux){
+	if (defined('_PERSO_COLONNE_EXTRA_FIN'))
+		return afficher_noisettes(_PERSO_COLONNE_EXTRA_FIN, $flux, true);
+}
+// -- Fonction d'insertion en debut de colonne navigation
+function sarkaspip_colonne_navigation_debut($flux){
+	if (defined('_PERSO_COLONNE_NAVIGATION_DEBUT'))
+		return afficher_noisettes(_PERSO_COLONNE_NAVIGATION_DEBUT, $flux, true);
+}
+// -- Fonction d'insertion en fin de colonne navigation
+function sarkaspip_colonne_navigation_fin($flux){
+	if (defined('_PERSO_COLONNE_NAVIGATION_FIN'))
+		return afficher_noisettes(_PERSO_COLONNE_NAVIGATION_FIN, $flux, true);
+}
+// -- Fonction d'insertion en fin de menu des pages speciales
+function sarkaspip_menu_pages_speciales_fin($flux){
+	if (defined('_PERSO_MENU_PAGES_SPECIALES_FIN'))
+		return afficher_noisettes(_PERSO_MENU_PAGES_SPECIALES_FIN, $flux, false);
+}
+// -- Fonction d'insertion en debut de bandeau haut
+function sarkaspip_bandeau_haut_debut($flux){
+	if (defined('_PERSO_BANDEAU_HAUT_DEBUT'))
+		return afficher_noisettes(_PERSO_BANDEAU_HAUT_DEBUT, $flux, false);
+}
+// -- Fonction d'insertion en fin de bandeau haut
+function sarkaspip_bandeau_haut_fin($flux){
+	if (defined('_PERSO_BANDEAU_HAUT_FIN'))
+		return afficher_noisettes(_PERSO_BANDEAU_HAUT_FIN, $flux, false);
+}
+// -- Fonction d'insertion en debut de bandeau bas
+function sarkaspip_bandeau_bas_debut($flux){
+	if (defined('_PERSO_BANDEAU_BAS_DEBUT'))
+		return afficher_noisettes(_PERSO_BANDEAU_BAS_DEBUT, $flux, false);
+}
+// -- Fonction d'insertion en fin de bandeau bas
+function sarkaspip_bandeau_bas_fin($flux){
+	if (defined('_PERSO_BANDEAU_BAS_FIN'))
+		return afficher_noisettes(_PERSO_BANDEAU_BAS_FIN, $flux, false);
+}
+// -- Fonction d'insertion en debut de pied
+function sarkaspip_pied_debut($flux){
+	if (defined('_PERSO_PIED_DEBUT'))
+		return afficher_noisettes(_PERSO_PIED_DEBUT, $flux, false);
+}
+// -- Fonction d'insertion en fin de pied
+function sarkaspip_pied_fin($flux){
+	if (defined('_PERSO_PIED_FIN'))
+		return afficher_noisettes(_PERSO_PIED_FIN, $flux, false);
+}
+
+// -- Fonction d'affichage des noisettes
+function afficher_noisettes($define, $flux, $ajax=true){
+	$noisettes = explode(':', $define);
+	foreach ($noisettes as $_fond) {
+		if (find_in_path($_fond.'.html')) {
+			$contexte = $ajax ? array_merge($flux['args'], array('ajax' => true)) : $flux['args'];
+			$html = recuperer_fond($_fond, $contexte);
+			$flux['data'] .= $html;
+		}
+		else
+			$flux['data'] .= '<div class="noisette avertissement" style="margin-top: 0; font-size: 0.95em">' . _T('sarkaspip:msg_fichier_introuvable', array('fichier' => $_fond . '.html')) . '</div>';
+	}
+	return $flux;
+}
+
 ?>
