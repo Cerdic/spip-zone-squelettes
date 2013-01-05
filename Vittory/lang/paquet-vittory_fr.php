00de9e41fffdf071a91fdf9d5977f2e97105f759
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-05 11:28:46 +0000

    Petit oubli du commit 68718
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@68719 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Vittory/lang/paquet-vittory_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-vittory
+// Langue: fr
+// Date: 05-01-2013 12:03:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// V
+	'vittory_description' => 'Ce plugin active ou désactive le squelette Vittori\'s.',
+	'vittory_slogan' => 'Squelette Vittori\'s pour SPIP.',
+);
+?>
