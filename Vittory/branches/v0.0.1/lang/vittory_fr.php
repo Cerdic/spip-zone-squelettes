ba9a3b805805493f85d7f194a75af0819eb63314
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-06 18:15:07 +0000

    On continue la branche
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@68762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Vittory/branches/v0.0.1/lang/vittory_fr.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	// B
+	// C
+	'cfg_alpha' => 'Transparence',
+	'cfg_boite_info' => '<h1>Vittory\'s</h1>Configuration<br /><br />[Documentation->http://www.spip-contrib.net/]',
+	'cfg_color_back' => 'Couleur de fond',
+	'cfg_color_grid' => 'Couleur de la grille',
+	'cfg_color_title' => 'Couleur du titre',
+	'cfg_color_text' => 'Couleur du texte',
+	'cfg_color_link' => 'Couleur des liens',
+	'cfg_color_shadow' => 'Couleur de l\'ombre',
+	'cfg_default_background' => 'Fond d\'écran par défaut',
+	'cfg_hover' => ' au survol',
+	'cfg_height' => 'Hauteur',
+	'cfg_maps_address' => 'Adresse',
+	'cfg_maps_use' => 'Utiliser Google Maps',
+	'cfg_maps_lat' => 'Latitude',
+	'cfg_maps_lon' => 'Longitude',
+	'cfg_maps_menu_name' => 'Google Maps',
+	'cfg_maps_tilt' => 'Orienter la vue de 45&deg;',
+	'cfg_maps_heading' => 'Orientation cardinale',
+	'cfg_maps_zoom' => 'Zoom par défaut',
+	'cfg_opacity' => 'Opacitée',
+	'cfg_thumbswidth' => 'Largeur des miniatures',
+	'cfg_thumbsheight' => 'Hauteur des miniatures',
+	'cfg_text_1' => 'Titre de la page',
+	'cfg_text_2' => 'Sous-titre',
+	'cfg_titre_accueil' => 'Vittory\'s',
+	'cfg_titre_boite' => 'Configuration de Vittory\'s',
+	'cfg_width' => 'Largeur',
+	// D
+	// E
+	// F
+	// G
+	'general' => 'Apparence générale',
+	// H
+	// I 
+	// J
+	// K
+	// L
+	// M
+	// N
+	'non' => 'non',
+	// O
+	'oui' => 'oui',
+	// P
+	'portfolio' => 'Slider le portfolio',
+	// Q
+	// R
+	// S
+	// T
+	// U
+	// V
+	// W
+	// X
+	// Y
+	// Z
+);
+
+?>
