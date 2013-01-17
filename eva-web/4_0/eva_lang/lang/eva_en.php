88d7d56c5ccdd2270fc3312c449519a61e3dbc5f
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-17 02:41:45 +0000

    langues (eva)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@69040 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_lang/lang/eva_en.php
+++ eva-web/4_0/eva_lang/lang/eva_en.php
@@ -203,7 +203,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'tous_les_auteurs' => 'All writers',
 
 	// V
-	'version_eva' => 'EVA-Web 4.2', # MODIF
+	'version_eva' => 'EVA-Web 4.2',
 	'visites' => 'Visits',
 	'voir_en_ligne' => 'See on the web',
 	'voir_image' => 'See the picture with original size',
