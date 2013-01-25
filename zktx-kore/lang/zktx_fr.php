92578fdd98c60211f696e7a26ecd76036904b569
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-25 15:39:06 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@69386 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zktx-kore/lang/zktx_fr.php
+++ zktx-kore/lang/zktx_fr.php
@@ -7,7 +7,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'pass_erreur_404' => 'Le document que vous recherchez n\'existe plus ou a &eacute;t&eacute; d&eacute;plac&eacute;.',
+	'pass_erreur_404' => '<b>Malheureusement, il semble que le document que vous recherchiez ne soit pas disponible. Soit il n\'existe plus, soit il a &eacute;t&eacute; d&eacute;plac&eacute; ou renomm&eacute;.</b>',
 	'pass_erreur_401' => 'Une authentification est n&eacute;cessaire pour acc&eacute;der &agrave; la page.',
 	'pass_erreur_503' => 'Service temporairement indisponible ou en maintenance.'
 );
