44a6068af7a68a7c152223ce4fd46f4c8626e9f9
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-12 09:22:42 +0000

    on adopte un thème bleu (comme le plan de la XFLR7 dans Objectif Lune);
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@69858 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spiplogo/skel/lang/paquet-spiplogo_skel_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+//
+///  Fichier produit par PlugOnet
+// Module: paquet-spipr_dist
+// Langue: fr
+// Date: 29-11-2012 16:35:29
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'spiplogo_skel_description' => 'Les squelettes du site SPIP logo',
+	'spiplogo_skel_slogan' => 'ça mettra du temps mais on va y arriver !',
+);
+?>
