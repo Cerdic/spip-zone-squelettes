a7ac1b8ea64d2b1b0febdc8bdea84c4f31b20ff0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-06 02:41:39 +0000

    langues (paquet-mediaspip_init)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@70451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip/mediaspip_init/trunk/lang/paquet-mediaspip_init_en.php
+++ mediaspip/mediaspip_init/trunk/lang/paquet-mediaspip_init_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_init
-// Langue: en
-// Date: 09-08-2012 11:36:47
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_init?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_init_description' => 'Initialization of MediaSPIP websites',
 	'mediaspip_init_nom' => 'MediaSPIP Initialization',
-	'mediaspip_init_slogan' => 'Initialization of MediaSPIP websites',
+	'mediaspip_init_slogan' => 'Initialization of MediaSPIP websites'
 );
+
 ?>
--- mediaspip/mediaspip_init/trunk/lang/paquet-mediaspip_init_fr.php
+++ mediaspip/mediaspip_init/trunk/lang/paquet-mediaspip_init_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_init
-// Langue: fr
-// Date: 09-08-2012 11:36:47
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/mediaspip/mediaspip_init/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_init_description' => 'Initialisation des sites MediaSPIP',
 	'mediaspip_init_nom' => 'MediaSPIP Initialisation',
-	'mediaspip_init_slogan' => 'Initialisation des sites MediaSPIP',
+	'mediaspip_init_slogan' => 'Initialisation des sites MediaSPIP'
 );
+
 ?>
