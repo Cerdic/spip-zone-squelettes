7969b745b0ad02dd007db95194a0327f87a82e7d
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 02:43:04 +0000

    langues (mediaspip_init)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@70801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip/mediaspip_init/trunk/lang/mediaspip_init_es.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mediaspip_init?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entree_powered_mediaspip' => 'Desarrollado por <a href="http://www.mediaspip.net">MediaSPIP</a> basado en <a href="http://www.spip.net">SPIP</a>',
+
+	// M
+	'menu_lateral' => 'Menú lateral',
+	'menu_pied' => 'Menú de pie de página',
+	'menu_principal' => 'Menú principal',
+
+	// N
+	'notif_texte_bonjour' => 'Buenos días @nom@',
+	'notif_texte_compte_cree' => 'su cuenta ha sido creada automáticamente en "@nouveau_site@" con el mismo nombre de usuario y correo electrónico que en "@site_maitre@".',
+	'notif_texte_compte_pass' => 'Debe redefinir la contraseña de esta cuenta para que esté operativa, basta con renovarla a través del enlace "Contraseña olvidada" en el formulario de autentificación. ',
+	'notif_titre_recuperation_compte' => '[@nom_site_spip@] Su cuenta ha sido creada',
+
+	// T
+	'titre_rubrique_actus' => 'Noticias',
+	'titre_rubrique_editos' => 'Editoriales',
+	'titre_rubrique_mag' => 'Revista',
+	'titre_rubrique_medias' => 'Medios',
+	'titre_rubrique_sites' => 'Sitios'
+);
+
+?>
