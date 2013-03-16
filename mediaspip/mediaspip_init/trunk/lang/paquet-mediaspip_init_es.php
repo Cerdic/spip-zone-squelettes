d37913d75de261db731ee430405e9e32f93c449f
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 02:43:06 +0000

    langues (paquet-mediaspip_init)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@70802 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip/mediaspip_init/trunk/lang/paquet-mediaspip_init_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_init?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediaspip_init_description' => 'Inicialización de los sitios MediaSPIP',
+	'mediaspip_init_nom' => 'MediaSPIP Inicialización',
+	'mediaspip_init_slogan' => 'Inicialización de los sitios MediaSPIP'
+);
+
+?>
