b8fd2b0f18919551695340d67a98f64971abf88c
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 02:43:01 +0000

    langues (paquet-mediaspip_config)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@70800 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip/mediaspip_config/trunk/lang/paquet-mediaspip_config_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_config?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediaspip_config_description' => 'Esqueleto de configuración para las plataformas MediaSPIP.
+_ Permite tener una parte de configuración pública.',
+	'mediaspip_config_nom' => 'MediaSPIP Configuración',
+	'mediaspip_config_slogan' => 'Configurar sonido MediaSPIP'
+);
+
+?>
