3542390e30e62004019f7d4343805a8e3548efc1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 02:42:59 +0000

    langues (mediaspip_config)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@70799 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip/mediaspip_config/trunk/lang/mediaspip_config_es.php
@@ -0,0 +1,107 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mediaspip_config?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_retablir_notices' => 'Reestablecer todos los avisos',
+	'bouton_supprimer_notice' => 'Dejar de mostrar este aviso',
+	'bouton_verifier_originaux' => 'Revisar los archivos que faltan',
+
+	// C
+	'configuration_auteurs' => 'Usuarios',
+	'configuration_avancee' => 'Configuración avanzada',
+	'configuration_documents' => 'Gestión de archivos',
+	'configuration_interactivite' => 'Interactividad',
+	'configuration_outils' => 'Herramientas',
+	'configuration_plugins' => 'Configuración de otros plugins ',
+	'configuration_spip' => 'Configuración general del sitio',
+	'configuration_squelettes' => 'Esqueletos',
+	'configuration_statistiques' => 'Estadísticas',
+
+	// E
+	'editer_secteur_nouveau' => 'Crear un nuevo sector',
+	'erreur_config_autorisation_identifie' => 'Debe estar identificado para acceder a la administración.',
+	'erreur_config_autorisation_insuffisante' => 'No dispone de un nivel de autorización suficiente para acceder a esta página. ',
+	'erreur_saveauto_mail_fichier_lourd' => 'El archivo de copia de seguridad es demasiado grande para ser enviado por correo electrónico. Puede recuperarlo desde su interfaz de administración en la dirección @url@',
+	'explication_accepter_visiteurs' => 'Si desea abrir la libre inscripción en el sitio a nuevos visitantes, puede activar esta opción aquí:',
+	'explication_form_signature_auteur' => 'Su nombre o su pseudónimo',
+	'explication_plugins_conseilles' => 'Tras un análisis rápido del uso del sitio, los siguientes plugins adicionales pueden serle útiles. ',
+
+	// H
+	'hierarchie_configuration' => 'Configuración',
+
+	// I
+	'icone_admin_iextras' => 'Adjuntar/modificar campos extras',
+	'icone_admin_plugin' => 'Gestión de los plugins',
+	'icone_admin_spipmotion_file' => 'Lista de espera de codificación',
+	'icone_admin_themes' => 'Gestión de los temas',
+	'icone_config_diogene' => 'Configuración de plantillas de formularios',
+	'icone_config_diogene_nouveau' => 'Crear una nueva plantilla de formulario',
+	'icone_config_gestion_mutu' => 'Gestión de casos de puesta en común',
+	'icone_config_gestion_secteurs' => 'Gestión de sectores',
+	'icone_config_menus' => 'Configuración de los menús',
+	'icone_docs_disparus' => 'Documentos desaparecidos',
+	'icone_forums_config' => 'Configuración de los foros',
+	'icone_forums_management' => 'Gestión de los foros',
+	'icone_mediaspip' => 'Gestión del esqueleto',
+	'icone_phpinfo' => 'Configuración de PHP',
+	'icone_supprimer_secteur' => 'Eliminar este sector',
+	'icone_theme' => 'Configuración del tema actual',
+	'icone_visiteurs' => 'Gestión de los usuarios',
+	'icone_votre_site' => 'Información del sitio',
+	'info_plugin_non_disponible' => 'Este plugin no está disponible en su instalación.',
+	'infos_champs_formulaire_forum' => 'Campos disponibles en el formulario',
+	'infos_inscription_visiteurs' => 'Inscripción de los usuarios',
+	'infos_tous_diogenes' => 'Todas las plantillas',
+	'infos_tous_menus' => 'Todos los menús',
+	'infos_tous_secteurs' => 'Todos los sectores',
+	'infos_visiteur_choisir_modifier' => 'Modificar un usuario',
+	'infos_visiteur_creer' => 'Usted añade un nuevo usuario',
+	'infos_visiteur_creer_modifier' => 'Añadir o modificar un usuario',
+	'infos_visiteur_modifier' => 'Usted modifica el usuario «@login@»',
+
+	// L
+	'label_form_bio_auteur' => '¿Quién es esta persona?',
+	'label_form_email_auteur' => 'Su dirección de correo electrónico',
+	'label_form_nom_site_auteur' => 'El nombre de su sitio',
+	'label_form_url_site_auteur' => 'La dirección (URL) de su sitio',
+	'label_site_background' => 'Cargar una nueva imagen de fondo:',
+	'legend_form_identification_auteur' => 'Su información de conexión',
+	'lien_creer_auteur' => 'Crear un nuevo usuario',
+
+	// M
+	'message_acceder_conf_doc2img' => 'Acceder a la configuración de la conversión de los documentos en imagen.',
+	'message_acceder_conf_emballe_medias' => 'Acceder a la configuración de los archivos pudiendo estar en línea',
+	'message_acceder_conf_plugins' => 'Acceder a la interfaz de administración de los plugins.',
+	'message_acceder_conf_spipmotion' => 'Acceder a la configuración del codificador de medios. ',
+	'message_config_lang_ok' => 'La consideración de los idiomas se ha modificado',
+	'message_config_site_ok' => 'La información del sitio ha sido modificada',
+	'message_config_visiteurs_ok' => 'La configuración que concierne a la inscripción de los usuarios se ha modificado',
+	'message_confirmation_supprimer_secteur' => '¿Está seguro de querer eliminar este sector?',
+	'message_doc_disparu_objet' => 'Este documento estaba asociado al objeto tipo "@objet@" número "@id_objet@"',
+	'message_formats_doc2img' => 'Algunos tipos de archivos susceptibles de meterse en línea y compatibles con el conversor de archivo en serie de imágenes (Doc2img) no están presentes en la configuración del conversor:',
+	'message_formats_doc2img_extensions' => '@formats@',
+	'message_formats_emballe_media_plugin' => 'Para el uso de determinados tipos de archivos que pueden ponerse en línea (@formats@), se recomiendan determinados plugins:',
+	'message_formats_spipmotion' => 'Algunos tipos de archivos susceptibles de meterse en línea y compatibles con el codificador de medios (SPIPmotion) no están presentes en la configuración del codificador:',
+	'message_formats_spipmotion_audio' => '@formats@ para los archivos audios;',
+	'message_formats_spipmotion_video' => '@formats@ para los archivos videos;',
+	'message_page_config' => 'Este apartado del sitio le permite configurar su funcionamiento',
+
+	// T
+	'theme_desactiver_previsu' => 'Desactivar la previsualización',
+	'theme_previsu' => 'Usted previsualiza actualmente',
+	'titre_autres_diogenes' => 'Otras plantillas',
+	'titre_autres_menus' => 'Otros menús ',
+	'titre_autres_secteurs' => 'Otros sectores',
+	'titre_configuration' => 'Configuración del sitio',
+	'titre_documents_manquants' => '@nb@ documentos adicionales que faltan',
+	'titre_plugins_conseilles' => 'Plugins adicionalies recomendados',
+	'titre_site_background' => 'Imagen de fondo'
+);
+
+?>
