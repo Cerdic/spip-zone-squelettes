cc729924eb2dd596c9dde9afdb70f55d86dd5395
johan.pustoch@crdp.ac-versailles.fr <johan.pustoch@crdp.ac-versailles.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 10:38:56 +0000

    pour r71450 : mieux avec les nouveaux fichiers...
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@71451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ scolaspip/trunk/lang/paquet-scolaspip_pt_br.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'scolaspip_slogan' => 'Um esqueleto personaliz&aacute;vel para estabelecimentos escolares',
+'scolaspip_description' => 'ScolaSPIP &eacute; destinado a iniciantes e permite facilmente a personaliza&ccedil;&atilde;o de um site SPIP: a partir do menu « Esqueletos » uma interface permite a escolha de cores, op&ccedil;&otilde;es de axibi&ccedil;&atilde;o da p&aacute;gina principal e de seus conte&uacute;dos. Algumas palavras-chave permitem em seguida a personaliza&ccedil;&atilde;o dos menus e dos conte&uacute;dos.',
+
+);
+
+?>
