bacf7b418ba3531b2f1d7984477b3ccff4fad2d8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:38:05 +0000

    langues (paquet-mediaspip_core)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@71939 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip/mediaspip_core/trunk/lang/paquet-mediaspip_core_en.php
+++ mediaspip/mediaspip_core/trunk/lang/paquet-mediaspip_core_en.php
@@ -1,19 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_core
-// Langue: en
-// Date: 12-06-2012 23:09:22
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_core?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_core_description' => 'Main functions and skeletons of MediaSPIP',
-	'mediaspip_core_slogan' => 'Main functions and skeletons of MediaSPIP',
+	'mediaspip_core_slogan' => 'Main functions and skeletons of MediaSPIP'
 );
+
 ?>
--- mediaspip/mediaspip_core/trunk/lang/paquet-mediaspip_core_fr.php
+++ mediaspip/mediaspip_core/trunk/lang/paquet-mediaspip_core_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_core
-// Langue: fr
-// Date: 12-06-2012 23:09:22
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/mediaspip/mediaspip_core/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_core_description' => 'Les fonctions et squelettes principaux de MediaSPIP',
-	'mediaspip_core_slogan' => 'Les fonctions et squelettes principaux de MediaSPIP',
+	'mediaspip_core_slogan' => 'Les fonctions et squelettes principaux de MediaSPIP'
 );
+
 ?>
