371ffb60062ed070d87fcb687a9285fee8676e77
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-12 18:51:34 +0000

    solgan + description (tonton)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@71989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ balloons/lang/paquet-balloons_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'balloons_description' => 'Balloons est un squelette de blog RWD pour SPIP 3 basé sur la dist (les squelettes par défaut de SPIP).',
+	'balloons_slogan' => 'Un blog et des ballons...',
+);
+
+?>
