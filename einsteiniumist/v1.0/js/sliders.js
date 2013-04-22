edef538100de59dc5fb7c85ee5e41a3c14841398
amas@openstudio.fr <amas@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-22 09:11:46 +0000

    Premier commit du squelette Einsteiniumist
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@72226 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ einsteiniumist/v1.0/js/sliders.js
@@ -0,0 +1,45 @@
+function padd_create_slideshow() {
+	jQuery('div#slideshow-box').append('<a class="dir-button dir-button-l" id="jqc-prev" href="#"></a>');
+	jQuery('div#slideshow-box').append('<a class="dir-button dir-button-r" id="jqc-next" href="#"></a>');
+	jQuery('div#slideshow-box').append('<div id="slideshow-controller"><span id="jqc-pages"></span></div>');
+	len = jQuery('div#slideshow-box .item').length;
+	jQuery('div#slideshow-box .dir-button-l').css('z-index', len + 2);
+	jQuery('div#slideshow-box .dir-button-r').css('z-index', len + 3);
+	jQuery('div#slideshow-controller').css('z-index', len + 4);
+	jQuery('div#slideshow-box div.list').cycle({
+		fx                : 'fade',
+		speed             : 1000,
+		timeout           : 3000,
+		cleartypeNoBg     : true,
+		activePagerClass  : 'jqc-active',
+		pager             : '#jqc-pages',
+		prev              : '#jqc-prev',
+		next              : '#jqc-next',
+		pause             : true,
+		pagerAnchorBuilder: function (index,elem) {
+			return '<button class="jqc-button jqc-button-pages" id="jqc-button-' + index + '" value="' + index + '"><span>' + (index+1) + '</span></button>';
+		}
+ 	});
+}
+
+function padd_tabs_init() {
+
+}
+
+jQuery(document).ready(function() {
+	jQuery.noConflict();
+
+	jQuery('#menubar ul').superfish({
+		autoArrows: false,
+		hoverClass: 'hover',
+		speed     : 500,
+		delay     : 0,
+		animation : {
+			opacity: 'show',
+			height : 'show'
+		}
+	});
+
+	padd_create_slideshow();
+
+});
