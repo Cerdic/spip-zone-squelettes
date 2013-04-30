ead1571fe3682afe0807f68ee5583b545d382e06
amas@openstudio.fr <amas@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-30 09:29:07 +0000

    Suppression du dossier v1.0 et mise en place de l'affichage de descriptif des articles au survol des images de la page-sommaire, géré en CSS3.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@72465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ einsteiniumist/lang/einsteiniumist_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entree_adresse_email' => 'Email',
+	
+	// F
+	'form_prop_envoyer' => 'Envoyer',
+
+	// M
+	'info_texte_message' => 'Message',
+
+	// R
+	'rechercher' => 'Rechercher',
+
+	// S
+	'savoir_plus' => 'En savoir plus',
+
+);
+
+?>
