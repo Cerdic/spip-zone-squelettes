b0ac76a6a39242f244878a34ac4292546e86c38c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-15 08:28:22 +0000

    constante pour permettre d'activer l'acces derogatoire aux demos
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@72824 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-dist/trunk/spipr_dist_options.php
+++ spipr-dist/trunk/spipr_dist_options.php
@@ -3,9 +3,14 @@ $GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header',
 define('_ZENGARDEN_FILTRE_THEMES','spipr');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
-if (isset($GLOBALS['visiteur_session']['statut'])
-  AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
-  AND $GLOBALS['visiteur_session']['webmestre']=='oui')
+if (
+	defined('_SPIPR_AUTH_DEMO')?
+		_SPIPR_AUTH_DEMO
+		:
+		(isset($GLOBALS['visiteur_session']['statut'])
+    AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
+    AND $GLOBALS['visiteur_session']['webmestre']=='oui')
+	)
 	_chemin(_DIR_PLUGIN_SPIPR_DIST."demo/");
 
 ?>
