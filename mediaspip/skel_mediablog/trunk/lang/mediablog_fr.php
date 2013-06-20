4406294091defa9f246d3de021e0325d746e102e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-20 14:46:13 +0000

    Modifications diverses
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@73757 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip/skel_mediablog/trunk/lang/mediablog_fr.php
+++ mediaspip/skel_mediablog/trunk/lang/mediablog_fr.php
@@ -3,9 +3,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'info_publie_dans' => 'Posté dans :',
+	
 	// L
+	'lien_permanent' => 'Permalien',
 	'lien_post_precedent' => 'Post précédent',
 	'lien_post_suivant' => 'Post suivant'
+	
 );
 
 ?>
