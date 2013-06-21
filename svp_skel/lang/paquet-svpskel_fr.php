6bbfda787e818f18a8a1f89a2bd6acd3831a5a1b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-21 11:13:03 +0000

    Ajout du nom du squelette dans les traductions et amélioration de la description.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@73780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_skel/lang/paquet-svpskel_fr.php
+++ svp_skel/lang/paquet-svpskel_fr.php
@@ -10,9 +10,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // S
-	'svpskel_description' => 'Ce plugin est un module optionnel de SVP. 
+	'svpskel_nom' => 'SVP Squelettes',
+	'svpskel_description' => 'Ce plugin est un module optionnel de SVP.
 _ Il fournit un jeu de squelettes Z simple mais complet pour afficher l\'ensemble des objets SVP.
-Son but est avant tout de permettre une vérification aisée de toutes les informations collectées par SVP sur les objets dépôt, plugin et paquet.',
+Son but est avant tout de permettre une vérification aisée de toutes les informations collectées par SVP sur les objets dépôt, plugin et paquet mais il peut aussi être utilisé pour créer un annuaire de plugins comme Plugins SPIP.',
 	'svpskel_slogan' => 'Jeu de squelettes Z pour SVP',
 );
 ?>
