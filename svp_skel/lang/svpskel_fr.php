23d578b3410d68d189726088f939333185b81d58
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-22 03:37:57 +0000

    langues (svpskel)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@73791 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_skel/lang/svpskel_fr.php
+++ svp_skel/lang/svpskel_fr.php
@@ -1,14 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Module: svpskel
-// Langue: fr
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/svp_skel/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B
+	// B
 	'bouton_demontrer' => 'La démo',
 	'bouton_developper' => 'La zone de dev',
 	'bouton_documenter' => 'La documentation',
@@ -29,20 +26,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bulle_filtrer_par_spip' => 'Filtrer sur la branche SPIP',
 	'bulle_supprimer_filtre' => 'Supprimer le filtre',
 
-// D
+	// D
 	'derniere_maj' => 'Mis à jour le',
 	'derniere_version' => 'Dernière version',
 
-// I
+	// I
 	'info_actualisation_depot_cron' => 'Les plugins des dépôts sont actualisés automatiquement toutes les @periode@ heure(s).',
 	'info_aucun_depot_disponible' => 'Aucun dépôt disponible.',
 	'info_aucun_module_utilise' => 'Aucun module de langue utilisé',
 	'info_aucun_paquet_disponible' => 'Aucun paquet disponible.',
-    'info_aucun_plugin_disponible' => 'Aucun plugin disponible.',
-    'info_aucun_prefixe_disponible' => 'Aucun préfixe utilisé.',
+	'info_aucun_plugin_disponible' => 'Aucun plugin disponible.',
+	'info_aucun_plugin_recherche' => 'Aucun plugin correspondant à la recherche',
+	'info_aucun_prefixe_disponible' => 'Aucun préfixe utilisé.',
 	'info_aucune_compatibilite_spip' => 'Non communiquée',
 	'info_aucune_contribution_disponible' => 'Aucune autre contribution disponible en dehors des plugins',
-    'info_aucun_plugin_recherche' => 'Aucun plugin correspondant à la recherche',
 	'info_compatible' => 'Compatible avec : ',
 	'info_rechercher_plugin' => 'Rechercher un plugin :',
 	'info_stats_categories' => '@nb_categories@ catégorie(s) disponible(s) sur 13 possibles.',
@@ -54,7 +51,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'intertitre_stats_categories' => 'Compteurs par catégorie',
 	'intertitre_stats_globales' => 'Compteurs globaux',
 
-// L
+	// L
 	'label_archive' => 'Archive',
 	'label_auteur' => 'Auteur(s)',
 	'label_copyright' => 'Copyright',
@@ -72,9 +69,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_traductions' => 'Traductions',
 	'label_utilise_plugins' => 'Compatible avec',
 
-// R
+	// R
 	'resume_table_autres_contributions' => 'Liste des autres contributions',
-// T
+
+	// T
 	'titre_filtre_categorie' => 'Filtrer par catégorie',
 	'titre_filtre_depot' => 'Filtrer par dépôt',
 	'titre_filtre_spip' => 'Filtrer par branche',
@@ -82,12 +80,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_navigation_plugin' => 'Dans la catégorie',
 	'titre_page_depots' => 'Dépôts',
 	'titre_page_plugins' => 'Plugins',
-    'titre_page_prefixes' => 'Préfixes',
-    'titre_page_recherche' => 'Recherche',
+	'titre_page_prefixes' => 'Préfixes',
+	'titre_page_recherche' => 'Recherche',
 	'titre_page_sommaire' => 'Plugins mis à jour récemment',
 	'titre_page_statistiques' => 'Statistiques',
 	'titre_page_telechargements' => 'Téléchargements',
-	'toutes_versions_spip' => 'Toutes les versions',
+	'toutes_versions_spip' => 'Toutes les versions'
 );
 
 ?>
