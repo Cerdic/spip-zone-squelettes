af45d62c1b8d4d8e34bbd2bd06f250ae8660dc90
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-06 09:24:44 +0000

    ON commence à faire du portage pour SPIP 3. On va utiliser zcore à la place de zpip v1
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zcommerce/trunk/lang/paquet-zcommerce_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-zcommerce
+// Langue: fr
+// Date: 06-07-2013 11:14:27
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Z
+	'zcommerce_description' => 'Z-commerce permet d\'indiquer à STEP de charger en une fois des plugins dédiés
+	à la création  d\'une boutique en ligne.
+	Il sert également de démo avec la page publique <code>?page=z-commerce</code> et contient des fichiers d\'exemples basiques. Désactivez ensuite ce plugin si le site est en production.',
+	'zcommerce_slogan' => 'Charger les plugins pour commercer',
+);
+?>
