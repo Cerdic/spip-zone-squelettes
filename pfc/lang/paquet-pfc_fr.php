ef11a016020d71536e87f57e20f78d5201864e62
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-21 17:53:27 +0000

    On améliore la description XML de ce squelette. Ajout du logo utilisé dans l'article de doc. Il reste à passer le texte en items de langue !
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74424 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pfc/lang/paquet-pfc_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pfc_description' => 'Ce squelette a été conçu dans le cadre du BTS SIO (Services Informatiques aux Organisations) et mis à la disposition des étudiants et des professeurs qui souhaitent l\'utiliser ou s\'en inspirer pour constituer leur propre portefeuille de compétences.',
+	'pfc_slogan' => 'Un portfolio numérique pour afficher vos compétences'
+);
+
+?>
