f01cfdd46db16f4d084fa1dea2a94f613a6c3949
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:28 +0000

    langues (paquet-zvide)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-vide/trunk/lang/paquet-zvide_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-zvide?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zvide_description' => 'Un squelette Z dont l\'ensemble des contenus sont gérés sous forme de noisettes configurables avec le noiZetier. Ce squelette s\'utilise donc en conjonction avec le noiZetier et une collection de noisettes.', # NEW
+	'zvide_slogan' => 'Šablóna Z vide, ktorú treba naplniť orieškami'
+);
+
+?>
