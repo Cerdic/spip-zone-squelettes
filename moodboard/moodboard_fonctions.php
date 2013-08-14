1bac390e244d230af176ba29b3942db7a78e1a1d
haplooswebdesign@free.fr <haplooswebdesign@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-14 09:40:58 +0000

    Dépôt du squelette Moodboard
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74746 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ moodboard/moodboard_fonctions.php
@@ -0,0 +1,16 @@
+<?
+    $GLOBALS['spip_pipeline']['insert_head'] .= "|moodboard";
+     
+    function moodboard_insert_head($flux){
+    $js_jquery_wookmark = find_in_path('js/jquery.wookmark.js');
+    $flux .= "<script type='text/javascript' src='$js_jquery_wookmark'></script>\n";
+    return $flux;
+    }
+    $GLOBALS['spip_pipeline']['insert_head_css'] .= "|moodboard";
+     
+    function moodboard_insert_head_css($flux){
+    $css_moodboard = find_in_path('css/moodboard.css');
+    $flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css_moodboard' />\n";
+    return $flux;
+    }
+?>
