6e51d768e362f329574ed1e51def5a50b8114c47
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-19 19:40:15 +0000

    Premier ajout des fichiers du nouveau squelette SPIP RADIUS
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74849 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipradius/trunk/lang/paquet-spipradius_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-spipradius
+// Langue: fr
+// Date: 19-08-2013 21:10:53
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'spipradius_description' => 'SPIP RADIUS est un jeu de squelettes multilingues (français, anglais et espagnol) compatibles avec SPIP 3.
+	Il permet de mettre en place un site de type blog. Le squelette sur deux colonnes est basé sur le layout de la distribution officielle de SPIP.',
+	'spipradius_slogan' => 'Squelette multilingue de type blog',
+);
+?>
