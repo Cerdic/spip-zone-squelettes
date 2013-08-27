580625e0cf7b48fac70c434df71d2dfa1f244e5c
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:35 +0000

    langues (paquet-svpskel)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74996 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/paquet-svpskel_es.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-svpskel?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svpskel_description' => 'Este plugin es un módulo opcional de SVP.
+_ Proporciona un juego de esqueletos Z simple pero completo para mostrar el conjunto de objetos SVP.
+Su objetivo es ante todo permitir una verificación fácil de todas las informaciones recogidas por SVP en los objetos repositorio, plugin y paquete pero puede también ser utilizado para crear un anuario de plugins como Plugins SPIP.',
+	'svpskel_nom' => 'SVP Esqueletos',
+	'svpskel_slogan' => 'Juego de esqueletos Z para SVP'
+);
+
+?>
