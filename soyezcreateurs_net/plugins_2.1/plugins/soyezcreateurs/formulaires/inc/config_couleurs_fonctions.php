39cb4d8c4666733b5eacb26faefd7fb3caa9cdf0
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 20:02:31 +0000

    Ne pas utiliser Palette dans la CFG si la couleur est transparent (librement inspiré de http://zone.spip.org/trac/spip-zone/changeset/74267)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@75011 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ soyezcreateurs_net/plugins_2.1/plugins/soyezcreateurs/formulaires/inc/config_couleurs_fonctions.php
@@ -0,0 +1,13 @@
+<?php
+// Pour gérer la couleur transparente avec palette
+// Repris de http://zone.spip.org/trac/spip-zone/changeset/74267 
+function ajuster_couleur_input($couleur, $type='') {
+	$transparent = ($type == 'background') ? '#ffffff' : '#000000';
+	if (strtolower($couleur) == 'transparent')
+		$couleur_calculee = $transparent;
+	else
+		$couleur_calculee = $couleur;
+
+	return $couleur_calculee;
+}
+?>
