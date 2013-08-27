6d4df396f4e36a4eb384352ded6ec96927843294
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:34 +0000

    langues (svpskel)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@74995 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/svpskel_es.php
@@ -0,0 +1,93 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/svpskel?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_demontrer' => 'La demo',
+	'bouton_developper' => 'La zona de dev',
+	'bouton_documenter' => 'La documentación',
+	'bouton_sourcer' => 'Las fuentes',
+	'bouton_telecharger' => 'El archivo',
+	'bouton_xmler' => 'El archivo XML',
+	'bulle_aller_categorie' => 'Ir a la página de los plugins de esta categoría',
+	'bulle_aller_demonstration' => 'Ir a la página de demostración',
+	'bulle_aller_depots' => 'Ir a la página de inicio de los repositorios',
+	'bulle_aller_developpement' => 'Ir a la zona de desarrollo',
+	'bulle_aller_plugins' => 'Ir a la página de inicio de los plugins',
+	'bulle_aller_prefixes' => 'Ir a la página de los prefijos',
+	'bulle_aller_sources' => 'Consultar las fuentes del paquete',
+	'bulle_aller_statistiques' => 'Ir a la página de las estadísticas',
+	'bulle_aller_telechargements' => 'Ir a la página de las descargas',
+	'bulle_filtrer_par_categorie' => 'Filtrar por la categoría',
+	'bulle_filtrer_par_depot' => 'Filtrar por depositorio',
+	'bulle_filtrer_par_spip' => 'Filtrar por rama SPIP',
+	'bulle_supprimer_filtre' => 'Cancelar el filtro',
+
+	// D
+	'derniere_maj' => 'Actualizar el',
+	'derniere_version' => 'Última versión',
+
+	// I
+	'info_actualisation_depot_cron' => 'Los plugins de los repositorios son actualizados automáticamente todos los  @periode@ hora(s).',
+	'info_aucun_depot_disponible' => 'Ningún repositorio disponible.',
+	'info_aucun_module_utilise' => 'Ningún módulo de idioma utilizado',
+	'info_aucun_paquet_disponible' => 'Ningún paquete disponible.',
+	'info_aucun_plugin_disponible' => 'Ningún plugin disponible.',
+	'info_aucun_plugin_recherche' => 'Ningún plugin corresponde a la búsqueda',
+	'info_aucun_prefixe_disponible' => 'Ningún prefijo utilizado.',
+	'info_aucune_compatibilite_spip' => 'No comunicada',
+	'info_aucune_contribution_disponible' => 'Ninguna otra contribución disponible fuera de los plugins',
+	'info_compatible' => 'Compatible con:',
+	'info_rechercher_plugin' => 'Buscar un plugin:',
+	'info_stats_categories' => '@nb_categories@ categoría(s) disponible(s) en 13 posibles.',
+	'info_stats_plugins' => 'La siguiente tabla muestra el número de plugins alojados por cada una de las categorías disponibles.',
+	'intertitre_depot_autres_contributions' => 'Lista de las otras contribuciones',
+	'intertitre_depot_plugins' => 'Lista de plugins alojados',
+	'intertitre_paquets_contribution' => 'Otras contribuciones',
+	'intertitre_paquets_plugin' => 'Los plugins',
+	'intertitre_stats_categories' => 'Contadores por categoría',
+	'intertitre_stats_globales' => 'Contadores globales',
+
+	// L
+	'label_archive' => 'Archivo',
+	'label_auteur' => 'Autor(es)',
+	'label_copyright' => 'Copyright',
+	'label_credit' => 'Créditos',
+	'label_etat' => 'Estado',
+	'label_gestionnaire' => 'Gestor',
+	'label_hebergement' => 'Alojado por',
+	'label_langue_reference' => 'Idioma de referencia',
+	'label_licence' => 'Licencia',
+	'label_maj' => 'Generada el',
+	'label_module' => 'Módulo de idioma',
+	'label_necessite_librairies' => 'Necesita librerías',
+	'label_necessite_plugins' => 'Necesita plugins',
+	'label_taille' => 'Tamaño',
+	'label_traductions' => 'Traducciones',
+	'label_utilise_plugins' => 'Compatible con',
+
+	// R
+	'resume_table_autres_contributions' => 'Lista de otras contribuciones',
+
+	// T
+	'titre_filtre_categorie' => 'Filtrar por categoría',
+	'titre_filtre_depot' => 'Filtrar por repositorio',
+	'titre_filtre_spip' => 'Filtrar por rama',
+	'titre_navigation_depot' => 'Repositorios',
+	'titre_navigation_plugin' => 'En la categoría',
+	'titre_page_depots' => 'Repositorios',
+	'titre_page_plugins' => 'Plugins',
+	'titre_page_prefixes' => 'Prefijos',
+	'titre_page_recherche' => 'Búsqueda',
+	'titre_page_sommaire' => 'Plugins actualizados recientemente',
+	'titre_page_statistiques' => 'Estadísticas',
+	'titre_page_telechargements' => 'Descargas',
+	'toutes_versions_spip' => 'Todas las versiones'
+);
+
+?>
