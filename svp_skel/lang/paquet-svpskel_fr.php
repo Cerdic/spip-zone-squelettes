98d01753a035b6ff467933009440dafffa696c47
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:06:10 +0000

    langues (paquet-svpskel)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@76393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_skel/lang/paquet-svpskel_fr.php
+++ svp_skel/lang/paquet-svpskel_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'svpskel_description' => 'Ce plugin est un module optionnel de SVP.
-_ Il fournit un jeu de squelettes Z simple mais complet pour afficher l\'ensemble des objets SVP.
+_ Il fournit un jeu de squelettes Z simple mais complet pour afficher l’ensemble des objets SVP.
 Son but est avant tout de permettre une vérification aisée de toutes les informations collectées par SVP sur les objets dépôt, plugin et paquet mais il peut aussi être utilisé pour créer un annuaire de plugins comme Plugins SPIP.',
 	'svpskel_nom' => 'SVP Squelettes',
 	'svpskel_slogan' => 'Jeu de squelettes Z pour SVP'
