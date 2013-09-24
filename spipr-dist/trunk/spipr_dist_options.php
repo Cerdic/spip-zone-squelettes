a9558608b3c79f5eedbe4e5a892d1242e9750a81
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-24 13:31:53 +0000

    permettre à l'utilisateur de personnaliser la liste des blocs (thierry)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@76461 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-dist/trunk/spipr_dist_options.php
+++ spipr-dist/trunk/spipr_dist_options.php
@@ -1,5 +1,7 @@
 <?php
-$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+if (!isset($GLOBALS['z_blocs']))
+	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+
 define('_ZENGARDEN_FILTRE_THEMES','spipr');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
