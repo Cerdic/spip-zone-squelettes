4d77431ea9755dbda846138cf82d82d1ee54a3b4
amdgaye@gmail.com <amdgaye@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 08:06:39 +0000

    Remplacement des fichiers de langue de type public_xx.php par des fichiers de type local_xx.php
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@77292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipradius/trunk/lang/local_fr.php
@@ -0,0 +1,20 @@
+<?php
+//  Fichier langue de SPIP propre au squelette SPIPRADIUS
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'au_hasard' => 'Au hasard',
+	'anonyme' => 'Anonyme',
+	'les_plus_vues' => 'Les plus vues',
+	'les_plus_commentés' => 'Les plus commentés',
+    'mots_clef' => 'Tag',
+	'mots_clefs' => 'Tags',
+	'mots_clefs_meme_groupe' => 'Tags dans le même groupe',
+	'messages_forum' => 'Commentaires',
+	'photos' => 'photos'
+		
+		);
+
+?>
