c7ebb58777b981c889b44d6a9ba94203316e37c7
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-30 17:13:07 +0000

    Changement du nom et du slogan du plugin. Correction des items de langue du paquet.xml inopérants depuis le changement de préfixe.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@78991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/trunk/lang/paquet-sarkaspip_fr.php
+++ sarkaspip/trunk/lang/paquet-sarkaspip_fr.php
@@ -2,20 +2,16 @@
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-///  Fichier produit par PlugOnet
-// Module: paquet-sarkaspip
-// Langue: fr
-// Date: 26-05-2012 16:45:18
-// Items: 2
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // S
-	'sarkaspip_description' => 'Sarka-SPIP est un squelette généraliste qui offre toute la panoplie des fonctions de base permettant de visualiser les objets éditoriaux de SPIP mais aussi quelques extras comme l\'agenda, la galerie, les forums type phpBB, l\'accès restreint (via le plugin), les sites favoris... Il est compatible avec beaucoup de plugins qu\'il intègre souvent nativement.
+	'sarkaspipr_description' => 'Sarka-SPIP est un squelette généraliste qui offre toute la panoplie des fonctions de base permettant de visualiser les objets éditoriaux de SPIP mais aussi quelques extras comme l\'agenda, la galerie, les forums type phpBB, l\'accès restreint (via le plugin), les sites favoris... Il est compatible avec beaucoup de plugins qu\'il intègre souvent nativement.
 
-	Destiné à tout public, il est simple d\'installation et d\'utilisation. Pour les webmestres même débutants, il reste facile à personnaliser au travers de son interface intégrée à l\'espace privé de SPIP (layout, noisettes, couleurs, format des pages...).',
-	'sarkaspip_slogan' => 'Squelette généraliste et hautement configurable',
+	Destiné à tout public, il est simple d\'installation et d\'utilisation. Pour les webmestres même débutants, il reste facile à personnaliser au travers de son interface intégrée à l\'espace privé de SPIP.
+
+	Cette version responsive se démarque des précédentes en adoptant les principes Z et le framework Bootstrap.',
+	'sarkaspipr_slogan' => 'Squelette généraliste, configurable et responsive',
 );
 ?>
