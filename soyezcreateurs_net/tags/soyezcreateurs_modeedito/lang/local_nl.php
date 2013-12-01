543b165c27b961ec3a657eb23c36cf1292589c65
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-01 16:06:11 +0000

    Comme le trunk va pas mal évoluer, poser un tag sur la version actuelle pour permettre une mise à jour vers cette dernière au cas où.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@79025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ soyezcreateurs_net/tags/soyezcreateurs_modeedito/lang/local_nl.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return; 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Modification de l'interface d'admin
+'texte_descriptif_rapide' => 'Bulle d\'aide <i>(et chapeau si le chapeau vide)</i>',
+
+// Modification du texte du formulaire de forum
+'forum_votre_email' => 'Uw adres e-mail (<strong>zal niet openbaar op de website gemaakt worden</strong>, noodzakelijk om u te kunnen persoonlijk antwoorden) :'
+
+
+);
+?>
