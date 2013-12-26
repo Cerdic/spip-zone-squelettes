041d029976379da6f8799c3974b741cad02ef134
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 19:23:20 +0000

    EscalV3 : un cadeau de Noel un peu en retard : passage de la page contact en formulaire CVT grâce à Arnaud avec les chaines de langues qui vont bien (manque encore le japonais) + travail sur la noisette inc-photos.html pour le mélange d'images en portrait et en paysage avec centrage des images
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@79630 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ escal/branche_V3/formulaires/contact.php
@@ -0,0 +1,140 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+
+function formulaires_contact_charger_dist(){
+        $valeurs = array(
+            'nom'=>'',
+            'prenom'=>'',
+            'email'=>'',
+            'champsup1'=>'',
+            'champsup2'=>'',
+            'sujet'=>'',
+            'checkbox'=>'',
+            'Antispam'=>'',
+            'message'=>''
+        );
+        
+        return $valeurs;
+}
+
+function formulaires_contact_verifier_dist(){
+        include_spip('inc/config');
+        include_spip('inc/filtres');
+        
+        $erreurs = array();
+        
+        // verifier que les champs obligatoires sont bien la :
+        foreach(array('nom','prenom','email','champsup1','champsup2','sujet','checkbox') as $obligatoire)
+                if (!_request($obligatoire)) $erreurs[$obligatoire] = _T('info_obligatoire_02'); 
+       
+        foreach(array('message') as $obligatoire)
+                if (!_request($obligatoire)) $erreurs[$obligatoire] = _T('escal:contact_alerte_message');
+                        
+               // Si le champ mail est activé dans la configuration de escal
+        if(lire_config('escal/config/contactmail') == 'oui'){
+            $email = _request('email');
+            // verifier que si un email a été saisi, il est bien valide :
+            if (!_request('email')) {
+                    $erreurs['email'] = _T('info_obligatoire_02');       
+            }
+            // Vérifier que c'est une adresse valide
+            else if(!preg_match("#^[0-9a-z]([-_.]?[0-9a-z])*@[0-9a-z]([-_.]?[0-9a-z])*\\.[a-z]{2,4}$#",$email)){
+                    $erreurs['email'] = _T('escal:contact_alerte_mail');
+            }
+        }
+      
+	if (!_request('sujet') AND lire_config('escal/config/radio') == 'oui' ) 
+        $erreurs['sujet'] = _T('info_obligatoire_02');
+        $test_message = nocode(_request('message'));
+        if($test_message==FALSE){
+        $erreurs['message']=_T('escal:contact_alerte_interdit').$test_message;
+        }
+        if (count($erreurs))
+                $erreurs['message_erreur'] = _T('escal:contact_alerte_entete');
+                
+        return $erreurs;
+}
+
+
+function formulaires_contact_traiter_dist(){
+    
+        include_spip('inc/config');
+        
+        $champs = array(
+            'nom'  => _request('nom'),
+            'prenom' => _request('prenom'),
+            'email' => _request('email'),
+            'message'=> _request('message'),
+            'champsup1'=>_request('champsup1'),
+            'champsup2'=>_request('champsup2'),
+            'sujet'=>_request('sujet'),
+            'checkbox'=>_request('checkbox'),
+            'anstispam'=>_request('Antispam'),
+        );
+        
+        $nom_site = extraire_multi($GLOBALS['meta']["nom_site"]); 
+        $email_webmaster = $GLOBALS['meta']['email_webmaster'];
+        $email_to= lire_config('escal/config/destinataire').",".$email_webmaster;
+        $sujet = _T('escal:page_contact2')." : ".$nom_site;
+        $email_from = $champs['email'];
+
+
+        
+        $message = _T('escal:envoi_mail_nom').clean($champs['nom'])."\n";
+        $message .= _T('escal:envoi_mail_prenom').clean($champs['prenom'])."\n\n\n";
+
+        
+        if (lire_config('escal/config/champsup1') == 'oui') {
+            $message .= lire_config('escal/config/titrechampsup1')." :\n";
+            $message .= clean($champs['champsup1'])."\n\n";
+        }
+        
+        if (lire_config('escal/config/champsup2') == 'oui') {
+            $message.= lire_config('escal/config/titrechampsup2')." :\n";
+            $message.= clean($champs['champsup2'])."\n\n";
+        }
+        
+        if (lire_config('escal/config/radio') == 'oui') {
+            $message.= _T('escal:envoi_mail_motif')."\n";
+            $message.= clean($champs['sujet'])."\n\n";
+        }
+        
+        if (lire_config('escal/config/checkbox') == 'oui') {
+            $message .= lire_config('escal/config/titrecheckbox') ." :\n" ;
+            if (is_array($champs['checkbox'])) {
+              $message .= implode(" - ",$champs['checkbox'])."\n\n";
+            }
+        }
+        
+        $message .= _T('escal:envoi_mail_message')."\n ".clean($champs['message'])."\n\n";
+             
+        if ($champs['antispam']=='' ){
+            $envoyer_mail = charger_fonction('envoyer_mail','inc');
+            $envoyer_mail($email_to,$sujet,$message,$email_from);
+            return array(
+                'message_ok'=>_T('escal:contact_retour_commentaire')."\n"."<strong>". _request('email')."</strong>"
+                );
+        }else{
+            return array('message_erreur'=>'Pas de robots ici !!');
+        }
+
+        
+}
+// Vérification basique d'insertion de code pour la fonction vérifier
+function nocode($text){
+    if(!preg_match("/[]%~#`$&|}{^[><]/",$text))
+        return trim($text);
+    else
+        return FALSE;
+}
+// Nettoyage minimal pour les champs textes input
+function clean($text){
+    $text = htmlentities(trim($text));
+    return $text;
+}
+
+
+?>
