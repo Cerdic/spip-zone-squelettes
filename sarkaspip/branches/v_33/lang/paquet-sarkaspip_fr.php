4511ccce7ba131a0bab2c138e3a228e39d4312e4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-12 03:41:27 +0000

    [Salvatore] [source:_squelettes_/sarkaspip/branches/v_33/lang/ paquet-sarkaspip] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@81833 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/branches/v_33/lang/paquet-sarkaspip_fr.php
+++ sarkaspip/branches/v_33/lang/paquet-sarkaspip_fr.php
@@ -1,22 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-sarkaspip
-// Langue: fr
-// Date: 26-05-2012 16:45:18
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/sarkaspip/branches/v_33/lang
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
-	'sarkaspip_description' => 'Sarka-SPIP est un squelette généraliste qui offre toute la panoplie des fonctions de base permettant de visualiser les objets éditoriaux de SPIP mais aussi quelques extras comme l\'agenda, la galerie, les forums type phpBB, l\'accès restreint (via le plugin), les sites favoris... Il est compatible avec nombre de plugins qu\'il intègre souvent nativement.
+	// S
+	'sarkaspip_description' => 'Sarka-SPIP est un squelette généraliste qui offre toute la panoplie des fonctions de base permettant de visualiser les objets éditoriaux de SPIP mais aussi quelques extras comme l’agenda, la galerie, les forums type phpBB, l’accès restreint (via le plugin), les sites favoris... Il est compatible avec nombre de plugins qu’il intègre souvent nativement.
 	Cette version est basée sur le concept de squelette Z.
 
-	Destiné à tout public, il est simple d\'installation et d\'utilisation. Pour les webmestres même débutants, il reste facile à personnaliser au travers de son interface intégrée à l\'espace privé de SPIP (layout, noisettes, couleurs, format des pages...).',
-	'sarkaspip_slogan' => 'Squelette généraliste et hautement configurable',
+	Destiné à tout public, il est simple d’installation et d’utilisation. Pour les webmestres même débutants, il reste facile à personnaliser au travers de son interface intégrée à l’espace privé de SPIP (layout, noisettes, couleurs, format des pages...).',
+	'sarkaspip_slogan' => 'Squelette généraliste et hautement configurable'
 );
+
 ?>
