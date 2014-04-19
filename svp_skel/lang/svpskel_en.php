98898ea03b6a09454850cc2ebf48eb6e8abf2326
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-19 03:40:54 +0000

    [Salvatore] [source:_squelettes_/svp_skel/lang/ svpskel] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@81986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/svpskel_en.php
@@ -0,0 +1,77 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/svpskel?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_demontrer' => 'Demo',
+	'bouton_developper' => 'Devel',
+	'bouton_documenter' => 'Documentation',
+	'bouton_sourcer' => 'Source code',
+	'bouton_telecharger' => 'Zip file',
+	'bouton_xmler' => 'XML File',
+
+	// D
+	'derniere_maj' => 'Updated on',
+
+	// I
+	'info_aucun_depot_disponible' => 'No repository available.',
+	'info_aucun_module_utilise' => 'No language module used',
+	'info_aucun_paquet_disponible' => 'No package available.',
+	'info_aucun_plugin_disponible' => 'No plugin available.',
+	'info_aucun_plugin_recherche' => 'No plugin is matching the search',
+	'info_aucun_prefixe_disponible' => 'No prefix used.',
+	'info_aucune_compatibilite_spip' => 'Unknown',
+	'info_compatible' => 'Compatible with : ',
+	'info_rechercher_plugin' => 'Search a plugin :',
+	'info_stats_categories' => '@nb_categories@ categories available (up to 13).',
+	'info_stats_plugins' => 'The table below displays statistics by category about hosted plugins.',
+	'intertitre_depot_autres_contributions' => 'Others Contributions',
+	'intertitre_depot_plugins' => 'Hosted Plugins',
+	'intertitre_paquets_contribution' => 'Others Contributions',
+	'intertitre_paquets_plugin' => 'Plugins',
+	'intertitre_stats_categories' => 'Statistics by category',
+	'intertitre_stats_globales' => 'Global statistics',
+
+	// L
+	'label_archive' => 'Zip file',
+	'label_auteur' => 'Author(s)',
+	'label_copyright' => 'Copyright',
+	'label_credit' => 'Credits',
+	'label_etat' => 'Status',
+	'label_gestionnaire' => 'Translator',
+	'label_hebergement' => 'Hosted by',
+	'label_langue_reference' => 'Reference language',
+	'label_licence' => 'License',
+	'label_maj' => 'Built on',
+	'label_module' => 'Language module',
+	'label_necessite_librairies' => 'Required librairies',
+	'label_necessite_plugins' => 'Required plugins',
+	'label_taille' => 'Size',
+	'label_traductions' => 'Translations',
+	'label_utilise_plugins' => 'Compatible with',
+
+	// R
+	'resume_table_autres_contributions' => 'Others Contributions',
+
+	// T
+	'titre_filtre_categorie' => 'Filter by category',
+	'titre_filtre_depot' => 'Filter by repository',
+	'titre_filtre_spip' => 'Filter by branch',
+	'titre_navigation_depot' => 'Repositories',
+	'titre_navigation_plugin' => 'Same category',
+	'titre_page_depots' => 'Repositories',
+	'titre_page_plugins' => 'Plugins',
+	'titre_page_prefixes' => 'Prefixes',
+	'titre_page_recherche' => 'Search',
+	'titre_page_sommaire' => 'Plugins recently updated',
+	'titre_page_statistiques' => 'Statistics',
+	'titre_page_telechargements' => 'Downloads',
+	'toutes_versions_spip' => 'All versions'
+);
+
+?>
