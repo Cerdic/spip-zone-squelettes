774da4ad60df2c7e94a8f980a73b5b26720befc3
kerivo1@wanadoo.fr <kerivo1@wanadoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-24 03:39:59 +0000

    [Salvatore] [source:_squelettes_/sarkaspip/branches/v_33/lang/ paquet-sarkaspip] Export depuis http://trad.spip.net de la langue br
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@82087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sarkaspip/branches/v_33/lang/paquet-sarkaspip_br.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-sarkaspip?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sarkaspip_description' => 'Sarka-SPIP a zo ur skeledenn hollek o kinnig roll an arc’hwelioù diazez en e bezh gouest da ziskouez dafaroù embann SPIP met ivez un nebeut traoù ouzhpenn evel an deiziataer, ar c’hatalog, ar foromoù mod pppBB, ar  moned bevennet (dre ar plugin), al lec’hiennoù pennrollet... Kenglotus eo gant kalz plugined degaset en un doare genidik.
+Ar  stumm-mañ a zo diazezet diwar meizad ar skeledenn Z.
+
+Aes eo da staliañ ha da implij, dre ma ’z eo bet graet evit an holl. 
+Evit ar  vistri-gwiad, hag e vefent o kregiñ gantañ, ez eo aes personelaat anezhañ dre an etrefas a zo e-barzh tachenn brevez SPIP (layout, kraoñ, liv, furmat pajennoù...).',
+	'sarkaspip_slogan' => 'Skeledenn hollek hag aes da gefluniañ'
+);
+
+?>
