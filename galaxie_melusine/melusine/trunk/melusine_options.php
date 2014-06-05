74ed9f213d9799ec296ece7ade4ce18a16765b0c
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-05 09:34:04 +0000

    La zcore-ificataion se poursuit... (ça marche presque)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@82854 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ galaxie_melusine/melusine/trunk/melusine_options.php
@@ -0,0 +1,7 @@
+<?php
+
+// Définition des blocs dispos pour Mélusine:
+
+if (!isset($GLOBALS['z_blocs']))
+	$GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer','nav','breadcrumb');
+?>
