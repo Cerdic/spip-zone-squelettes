78c34eb08bd60119cfa1e83738e75f3ec5729de6
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-05 15:56:42 +0000

    Des zblocs avec des noms
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@82874 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/melusine_options.php
+++ galaxie_melusine/melusine/trunk/melusine_options.php
@@ -1,7 +1,23 @@
 <?php
 
+
+// Des noms intelligibles pour les z_blocs
+	$GLOBALS['noms_z_blocs'] = array(
+		'content' => 'Contenu principal',
+		'aside' => 'Contenu secondaire',
+		'head' => 'En-têtes HTML',
+		'head_js' => 'Scripts javascript',
+		'header' => 'Bandeau',
+		'footer' => 'Pied de page',
+		'nav'	=> 'Onglets',
+		'breadcrumb' => 'Chemins',
+		'extra' => 'Contenu connexe'
+	);
+
 // Définition des blocs dispos pour Mélusine:
 
 if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer','nav','breadcrumb','extra');
+	$GLOBALS['z_blocs'] = array_keys($GLOBALS['noms_z_blocs']);
+
+
 ?>
