98cb79cefc175138e89299b543d2b1b61a58e29e
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-05 09:50:24 +0000

    oubli de déclarer le z_bloc extra
    + ajout des dists oubliés dans le commit précédent
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@82855 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/melusine_options.php
+++ galaxie_melusine/melusine/trunk/melusine_options.php
@@ -3,5 +3,5 @@
 // Définition des blocs dispos pour Mélusine:
 
 if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer','nav','breadcrumb');
+	$GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer','nav','breadcrumb','extra');
 ?>
