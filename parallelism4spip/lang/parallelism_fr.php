06f8220f12e0fa00e3412006086e34fa4a7cc7eb
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-07 13:16:29 +0000

    Pétouilles - plus de crayons - html5 valide
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@82899 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parallelism4spip/lang/parallelism_fr.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'img_ss_titre' => 'Image sans titre',
+'inserer_titre' => 'Ajoutez un titre !',
+'signalement_mail' => 'Je vous invite à visiter la page suivante:'
+);
+?>
