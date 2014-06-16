cbb6e5bce9befb776c9e16f265b4122e9477e812
jml9340@gmail.com <jml9340@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-16 21:13:10 +0000

    choix des du gabarit pour les pages
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@83101 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/formulaires/melusine_position.php
+++ galaxie_melusine/melusine/trunk/formulaires/melusine_position.php
@@ -22,7 +22,7 @@ function formulaires_melusine_position_verifier(){
 	// $style=_request('style');
 	// $gauche=lire_config("melusine_squelettes/g");
 	
-	//$droite=lire_config("melusine_squelettes/d");
+	
 	// if($style=="layout2.css" and melusine_colonne_pasvide($droite) ){$erreurs['style'] ="<span style='color:red'>La colonne droite  doit &ecirc;tre vide</span>"	;};
 	// if($style=="layout3.css" and melusine_colonne_pasvide($gauche) ){$erreurs['style'] ="<span style='color:red'>La colonne gauche doit &ecirc;tre vide</span>"	;};
 	if (count($erreurs))
@@ -34,10 +34,10 @@ function formulaires_melusine_position_verifier(){
 function formulaires_melusine_position_traiter(){
 	$style=_request('style');
 	$gabarit=_request('gabarit');
-	$width=_request('width');
+	
 	$chemin_gabarit="melusine_data/gabarits/".$gabarit;
 	ecrire_config($chemin_gabarit,$style);	
-	ecrire_config("melusine_data/width",$width);
+
 	
 	
 	
