b2e35d6493751da0ce4cd2f4b72a364037dd6486
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-16 08:25:25 +0000

    Sans majuscule en début et changement de nom de globale
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@83075 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/melusine_options.php
+++ galaxie_melusine/melusine/trunk/melusine_options.php
@@ -3,15 +3,15 @@
 
 // Des noms intelligibles pour les z_blocs
 	$GLOBALS['noms_z_blocs'] = array(
-		'content' => 'Contenu principal',
-		'aside' => 'Contenu secondaire',
-		'head' => 'En-têtes HTML',
-		'head_js' => 'Scripts javascript',
-		'header' => 'Bandeau',
-		'footer' => 'Pied de page',
-		'nav'	=> 'Onglets',
-		'breadcrumb' => 'Chemins',
-		'extra' => 'Contenu connexe'
+		'content' => 'contenu principal',
+		'aside' => 'contenu secondaire',
+		'head' => 'nn-têtes HTML',
+		'head_js' => 'scripts javascript',
+		'header' => 'bandeau',
+		'footer' => 'pied de page',
+		'nav'	=> 'onglets',
+		'breadcrumb' => 'chemins',
+		'extra' => 'contenu connexe'
 	);
 
 // Définition des blocs dispos pour Mélusine:
@@ -20,10 +20,10 @@ if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array_keys($GLOBALS['noms_z_blocs']);
 
 // Des noms pour les types de pages gérées
-	$GLOBALS['types_pages_melusine'] = array(
-		"rubrique" => "Page de catégorie de contenus",
-		"sommaire" => "Page d'accueil",
-		"article" => "Page du contenu"
+	$GLOBALS['types_layouts_melusine'] = array(
+		"rubrique" => "page de catégorie de contenus",
+		"sommaire" => "page d'accueil",
+		"article" => "page du contenu"
 	);
 
 ?>
