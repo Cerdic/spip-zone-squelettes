3919033b763959dcbe03bdb3094c36a86d2a22ce
jml9340@gmail.com <jml9340@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-30 12:38:44 +0000

    ajout module fil_twitter
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@83508 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/formulaires/melusine_edition_module.php
+++ galaxie_melusine/melusine/trunk/formulaires/melusine_edition_module.php
@@ -44,13 +44,35 @@ function formulaires_melusine_edition_module_verifier(){
 	return $erreurs;
 }
 
+function trouve_input_file_value($casier,$id_noisette){
+		$valeurs=array();
+	$valeurs['id_noisette'] = $id_noisette;
+	$entrees = sql_fetsel(
+			'parametres',
+			'spip_noisettes',
+			'id_noisette = '.$id_noisette
+		);
+	if(!empty($entrees['parametres'])){
+		$params=unserialize($entrees['parametres']);
+	
+  		foreach($params as $cle => $param){ 
+  			//echo $param;
+  			if($cle==$casier) {return $param;};
+  			
+   		}
+   }
+   //print_r($valeurs);
+
+  return "";
+}
+
 
 function formulaires_melusine_edition_module_traiter(){
 include_spip('action/editer_objet');
-
+	
 	$id_noisette=_request('id_noisette');
 	$param=_request('param');
-	//echo $param;
+	
 	$casiers=array();
 	$params=unserialize($param);
 	//print_r($params);
@@ -66,12 +88,50 @@ include_spip('action/editer_objet');
 	$params=array();
 	foreach($casiers as $casier){
 		$data=_request($casier);
-		if(!isset($data)){$data="none";};
-		$params[$casier]=$data;
 		
+		//if(isset($data)){
+		 // if(strpos($casier, 'imge')==0){
+			if(!empty($_FILES[$casier]['tmp_name']) ){
+				
+				
+				$nom_fichier= $_FILES[$casier]['tmp_name'];
+				$chemin_destination_boutons="IMG/config/boutons";
+				$chemin_destination_config="IMG/config";
+				$nom_destination='IMG/config/boutons/'.$_FILES[$casier]['name'];
+				
+				if(!is_dir("$chemin_destination_boutons")){
+					if(!is_dir($chemin_destination_config)){
+						mkdir($chemin_destination_config,0777);
+					}
+					mkdir($chemin_destination_boutons,0777);
+				};
+				move_uploaded_file($nom_fichier, $nom_destination);
+				$data =$nom_destination;
+				echo "aa".$casier."/".$data."<br>";
+				$params[$casier]=$data;
+			}
+			elseif(isset($data)){
+				echo "bb".$casier."/".$data."<br>";
+				$params[$casier]=$data;
+			}
+			else{$data=trouve_input_file_value($casier,$id_noisette);
+				echo $casier."/".$data."<br>";
+				$params[$casier]=trouve_input_file_value($casier,$id_noisette);
+			}
+				
+			
+
+		//}
 		
 		
 	}
+
+
+		
+		
+		
+		
+
 
 	$set=array('parametres'=>serialize($params));
 	objet_modifier("noisette", $id_noisette, $set);
