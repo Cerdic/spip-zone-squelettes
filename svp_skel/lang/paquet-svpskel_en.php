b9f83328ba286c8f68d27d90f22491d2fd56a767
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-26 04:41:40 +0000

    [Salvatore] [source:_squelettes_/svp_skel/lang/ paquet-svpskel] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@85632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_skel/lang/paquet-svpskel_en.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-svpskel?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svpskel_description' => 'This plugin is an optional module of SVP. 
+_ It provides a set of simple but complete Z templates to display all  objects of SVP. 
+Its purpose is primarily to allow easy verification of all information gathered by the SVP on the objects repository, plugin, and package. But it can also be used to create a directory of plugins like "Plugin SPIP".',
+	'svpskel_nom' => 'SVP templates',
+	'svpskel_slogan' => 'Set of Z templates for SVP'
+);
+
+?>
