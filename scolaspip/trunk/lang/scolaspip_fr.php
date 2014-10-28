a53be28c621cd8b8610f71b0ba3f14a0e50679ca
johan.pustoch@crdp.ac-versailles.fr <johan.pustoch@crdp.ac-versailles.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-28 15:53:00 +0000

    Revue des formulaires de ScolaSPIP pour qu'ils restent bien présentés également avec SPIP 3.1. On revoie la compatibilité : ScolaSPIP n'est pour l'instant pas installable avec SPIP 3.1 car dépend de zpip-dist qui necessite SPIP <=3.0.99 actuellement. Incrément de z dans la version du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@85701 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scolaspip/trunk/lang/scolaspip_fr.php
+++ scolaspip/trunk/lang/scolaspip_fr.php
@@ -95,10 +95,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'compte_twitter'=>'Compte Twitter',
     'compte_twitter_identifiant'=>'Identifiant du compte Twitter',
     'c_scolaspip_info'=>'Bien démarrer avec ScolaSPIP',
-    'c_scolaspip_info_texte'=>'Trois onglets sont proposés ici et vous permettent de personnaliser votre site :
+    'c_scolaspip_info_texte'=>'Trois onglets sont proposés ici et vous permettent de personnaliser votre site&nbsp;:
                             <ul class="spip">
-                            <li>Les blocs affichés sur votre page d\'accueil. Sachez également que d\'autres contenus et liens s\'affichent à partir de mots-clés;</li>
-                            <li>Les couleurs et options de présentation;</li>
+                            <li>Les blocs affichés sur votre page d\'accueil. Sachez également que d\'autres contenus et liens s\'affichent à partir de mots-clés&nbsp;;</li>
+                            <li>Les couleurs et options de présentation&nbsp;;</li>
                             <li>Diverses options (petits plus).</li>
                             </ul>
                             <p>Pensez également à bien configurer toutes les options de SPIP (identité du site, gestions des mots-clés, forums — il est conseillé de les modérer à priori si vous les utilisez — génération des vignettes, etc.).</p>',
