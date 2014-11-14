055a90ebb09ce091eed32c0cab6c8214ce5b1508
jml9340@gmail.com <jml9340@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-14 09:57:24 +0000

    retour à la gestion antérieure des gabarits - pages
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86069 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/melusine_options.php
+++ galaxie_melusine/melusine/trunk/melusine_options.php
@@ -23,31 +23,10 @@ if (!isset($GLOBALS['z_blocs']))
 
 // Des noms pour les types de gabarits gérées
 	$GLOBALS['types_gabarits_melusine'] = array(
-		"categorie" => "Disposition 1 - 3 colonnes - aside/content/extra",
-		"accueil" => "Disposition 2 - 3 colonnes - content/aside/extra",
-		"contenu" => "Disposition 3 - 2 colonnes - aside/content",
-		"bonus" => "Disposition 4 - 2 colonnes - content/extra"
+		"categorie" => "gabarit de catégorie de contenus",
+		"accueil" => "gabarit d'accueil",
+		"contenu" => "gabarit du contenu"
 	);
 
-if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array_keys($GLOBALS['noms_z_blocs']);
 
-// Des noms pour les types de dispositions gérées
-	$GLOBALS['types_dispositions_melusine'] = array(
-		"Dispo 1" => "Disposition 1 - 3 colonnes - aside/content/extra",
-		"Dispo 2" => "Disposition 2 - 3 colonnes - content/aside/extra",
-		"Dispo 3" => "Disposition 3 - 2 colonnes - aside/content",
-		"Dispo 4" => "Disposition 4 - 2 colonnes - content/extra"
-	);
-
-if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array_keys($GLOBALS['noms_z_blocs']);
-
-// Des images pour les types de dispositions gérées
-	$GLOBALS['images_dispositions_melusine'] = array(
-		"Dispo 1" => "/layouts/layout11.gif",
-		"Dispo 2" => "/layouts/layout1.gif",
-		"Dispo 3" => "/layouts/layout2.gif",
-		"Dispo 4" => "/layouts/layout3.gif"
-	);
 ?>
