69b68e527a83d722c3ffffa69062171fe04ed295
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-20 16:56:51 +0000

    Compta 3.1, retitrage pour regrouper dans l'interface des plugins, slogan
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86192 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ eva-web/4_2/eva_mini_calendrier/lang/paquet-mini_calendrier_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'mini_calendrier_slogan' => 'Un mini-calendrier mensuel pour EVA-web 4.2',
+    'mini_calendrier_description' => '',
+    
+);
+
+?>
