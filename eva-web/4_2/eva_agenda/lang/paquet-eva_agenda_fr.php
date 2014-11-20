19dbaa56f943a06496fbf4786d16ce4990b97b19
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-20 17:04:51 +0000

    Compat 3.1, retitrage pour regrouper dans l'interface des plugins, slogan
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86204 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_2/eva_agenda/lang/paquet-eva_agenda_fr.php
+++ eva-web/4_2/eva_agenda/lang/paquet-eva_agenda_fr.php
@@ -2,7 +2,7 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'eva_agenda_slogan' => 'Affichage d&#8217;une rubrique sous forme d&#8217;agenda.',
+'eva_agenda_slogan' => 'Une rubrique sous forme d&#8217;agenda pour EVA-web 4.2.',
 'eva_agenda_description' => 'La rubrique s&#8217;affiche sous forme d&#8217;agenda lorsqu&#8217;on lui joint le mot cl&#233; {{agenda}}.
 Les articles sont inscrits dans l&#8217;agenda en leurs attribuant une date de r&#233;daction ant&#233;rieure correspondant &#224; la date de l&#8217;&#233;v&#232;nement concern&#233;.',
 );
