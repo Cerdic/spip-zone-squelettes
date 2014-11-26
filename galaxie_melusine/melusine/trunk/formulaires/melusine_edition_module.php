ca9d0fb560108b8fff49fb4ccfb1c22f4738340d
noemie-edith-so.bernard@ac-creteil.fr <noemie-edith-so.bernard@ac-creteil.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-26 11:54:54 +0000

    Disparition : "configuration du squelette"
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86290 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/formulaires/melusine_edition_module.php
+++ galaxie_melusine/melusine/trunk/formulaires/melusine_edition_module.php
@@ -1,32 +1,39 @@
 <?php 
 include_spip('inc/config');
 
-function formulaires_melusine_edition_module_charger($id_noisette){
+function formulaires_melusine_edition_module_charger($id_noisette)
+{
 	$valeurs=array();
 	$valeurs['id_noisette'] = $id_noisette;
-	$entrees = sql_fetsel(
-			'parametres',
-			'spip_noisettes',
-			'id_noisette = '.$id_noisette
-		);
-	if(!empty($entrees['parametres'])){
-		$params=unserialize($entrees['parametres']);
+	$entrees = sql_fetsel
+	(
+		'parametres',
+		'spip_noisettes',
+		'id_noisette = '.$id_noisette
+	);
 	
-  		foreach($params as $cle => $param){
-  			$valeurs[$cle]=$param;
-   		}
-   }
+	if(!empty($entrees['parametres']))
+	{
+		$params=unserialize($entrees['parametres']);
+		foreach($params as $cle => $param)
+			{
+  				$valeurs[$cle]=$param;
+   			}
+  	 }
+   
    //print_r($valeurs);
-
-  return $valeurs;
+	
+	return $valeurs;
 }
 
-function formulaires_melusine_edition_module_verifier(){
+function formulaires_melusine_edition_module_verifier()
+{
 	$erreurs = array();
 	
 	// verifier que les champs obligatoires sont bien la :
 	//foreach(array('pos1','pos2','pos3','pos4','pos5') as $obligatoire)
-	//	if (_request($style)) $erreurs[$obligatoire] = 'Ce champ est obligatoire';
+	//	if (_request($style)) $erreurs[$obligatoire] = 
+	// 'Ce champ est obligatoire';
 	
 	// verifier que si un email a été saisi, il est bien valide :
 	//include_spip('inc/filtres');
@@ -37,38 +44,48 @@ function formulaires_melusine_edition_module_verifier(){
 	// $gauche=lire_config("melusine_squelettes/g");
 	
 	//$droite=lire_config("melusine_squelettes/d");
-	// if($style=="layout2.css" and melusine_colonne_pasvide($droite) ){$erreurs['style'] ="<span style='color:red'>La colonne droite  doit &ecirc;tre vide</span>"	;};
-	// if($style=="layout3.css" and melusine_colonne_pasvide($gauche) ){$erreurs['style'] ="<span style='color:red'>La colonne gauche doit &ecirc;tre vide</span>"	;};
+	// if($style=="layout2.css" and melusine_colonne_pasvide($droite) )
+	//{$erreurs['style'] ="<span style='color:red'>La colonne droite  
+	// doit &ecirc;tre vide</span>"	;};
+	// if($style=="layout3.css" and melusine_colonne_pasvide($gauche) )
+	// {$erreurs['style'] ="<span style='color:red'>
+	// La colonne gauche doit &ecirc;tre vide</span>"	;};
 	if (count($erreurs))
 		$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
 	return $erreurs;
 }
 
-function trouve_input_file_value($casier,$id_noisette){
-		$valeurs=array();
+function trouve_input_file_value($casier,$id_noisette)
+{
+	$valeurs=array();
 	$valeurs['id_noisette'] = $id_noisette;
-	$entrees = sql_fetsel(
-			'parametres',
-			'spip_noisettes',
-			'id_noisette = '.$id_noisette
-		);
-	if(!empty($entrees['parametres'])){
+	$entrees = sql_fetsel
+	(
+		'parametres',
+		'spip_noisettes',
+		'id_noisette = '.$id_noisette
+	);
+	if(!empty($entrees['parametres']))
+	{
 		$params=unserialize($entrees['parametres']);
 	
-  		foreach($params as $cle => $param){ 
+  		foreach($params as $cle => $param)
+  		{ 
   			//echo $param;
-  			if($cle==$casier) {return $param;};
-  			
+  			if($cle==$casier) 
+  				{return $param;};
    		}
    }
+
    //print_r($valeurs);
 
-  return "";
+  	return "";
 }
 
 
-function formulaires_melusine_edition_module_traiter(){
-include_spip('action/editer_objet');
+function formulaires_melusine_edition_module_traiter()
+{
+	include_spip('action/editer_objet');
 	
 	$id_noisette=_request('id_noisette');
 	$param=_request('param');
@@ -78,69 +95,58 @@ include_spip('action/editer_objet');
 	$params=unserialize($param);
 	//print_r($params);
 
-	foreach($params as $p){
-		
-			// echo "<pre>";
-			// print_r($options);
-			// echo "</pre>";
+	foreach($params as $p)
+	{
+		// echo "<pre>";
+		// print_r($options);
+		// echo "</pre>";
 		$casiers[]=$p['options']['nom'];
-		
 	}
 	
 	$params=array();
-	foreach($casiers as $casier){
+	foreach($casiers as $casier)
+	{
 		$data=_request($casier);
 		
 		//if(isset($data)){
-		 // if(strpos($casier, 'imge')==0){
-			if(!empty($_FILES[$casier]['tmp_name']) ){
+		// if(strpos($casier, 'imge')==0){
+		if(!empty($_FILES[$casier]['tmp_name']) )
+		{
+			$nom_fichier= $_FILES[$casier]['tmp_name'];
+			$chemin_destination_boutons="IMG/config/boutons";
+			$chemin_destination_config="IMG/config";
+			$nom_destination='IMG/config/boutons/'.$_FILES[$casier]['name'];
 				
-				
-				$nom_fichier= $_FILES[$casier]['tmp_name'];
-				$chemin_destination_boutons="IMG/config/boutons";
-				$chemin_destination_config="IMG/config";
-				$nom_destination='IMG/config/boutons/'.$_FILES[$casier]['name'];
-				
-				if(!is_dir("$chemin_destination_boutons")){
-					if(!is_dir($chemin_destination_config)){
-						mkdir($chemin_destination_config,0777);
-					}
-					mkdir($chemin_destination_boutons,0777);
-				};
-				move_uploaded_file($nom_fichier, $nom_destination);
-				$data =$nom_destination;
-				echo "aa".$casier."/".$data."<br>";
-				$params[$casier]=$data;
-			}
-			elseif(isset($data)){
-				//echo "bb".$casier."/".$data."<br>";
-				$params[$casier]=$data;
-			}
-			else{$data=trouve_input_file_value($casier,$id_noisette);
-				//echo $casier."/".$data."<br>";
-				$params[$casier]=trouve_input_file_value($casier,$id_noisette);
-			}
-				
+			if(!is_dir("$chemin_destination_boutons"))
+			{
+				if(!is_dir($chemin_destination_config))
+				{
+					mkdir($chemin_destination_config,0777);
+				}
+				mkdir($chemin_destination_boutons,0777);
+			};
 			
-
-		//}
-		
-		
+			move_uploaded_file($nom_fichier, $nom_destination);
+			$data =$nom_destination;
+			echo "aa".$casier."/".$data."<br>";
+			$params[$casier]=$data;
+		}
+		elseif(isset($data))
+		{
+			//echo "bb".$casier."/".$data."<br>";
+			$params[$casier]=$data;
+		}
+		else
+		{
+			$data=trouve_input_file_value($casier,$id_noisette);
+			//echo $casier."/".$data."<br>";
+			$params[$casier]=trouve_input_file_value($casier,$id_noisette);
+		}	
 	}
-
-
-		
-		
-		
-		
-
 
 	$set=array('parametres'=>serialize($params));
 	objet_modifier("noisette", $id_noisette, $set);
-	
 	return array('message_ok'=>'Saisie enregistr&eacute;e', 'id_noisette'=>$id);
 }
-
-
 
 ?>
