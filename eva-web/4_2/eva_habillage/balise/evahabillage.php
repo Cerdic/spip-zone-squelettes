e68fb4a7b6ea442925f29b000f8e58b0853a8de1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-26 16:57:09 +0000

    indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_2/eva_habillage/balise/evahabillage.php
+++ eva-web/4_2/eva_habillage/balise/evahabillage.php
@@ -1,95 +1,95 @@
 <?php
-function balise_EVAHABILLAGE($p) {
-$test = _DIR_PLUGIN_EVASQUELETTES;
-if (defined("_DIR_PLUGIN_EVASQUELETTES")) {$chemin_squelettes =$test;} else {$chemin_squelettes = 'squelettes/';}
-    $resultat1 = sql_select('habillage','spip_eva_habillage',"sauvegarde = 'Defaut'");
-    $resultat1_tableau = sql_fetch($resultat1);
-    $mon_habillage = $resultat1_tableau['habillage'];
-    if (($mon_habillage=='0') OR ($mon_habillage=='')) {
-        $envoi='<link rel="stylesheet" type="text/css" href="'.$chemin_squelettes.'eva_style.css" media="screen" />';
-    }
-    else {
-        $envoi='<link rel="stylesheet" type="text/css" href="'.$chemin_squelettes.'eva_styles/'.$mon_habillage.'" media="screen" />';
-    }
-    $envoi .="\n";
-    $envoi .='<style type="text/css">';
-    $envoi .="\n";
-    include_spip('inc/eva_habillage_definition_themes');
-    $eva_habillage_themes = eva_habillage_definition_themes ();
-    foreach($eva_habillage_themes as $eva_themes => $eva_proprietes) {
-        $res_verif_themes_defini = sql_select($eva_themes,'spip_eva_habillage_themes',"nom = 'Defaut'");
-        $res_verif_themes_tableau = sql_fetch($res_verif_themes_defini);
-        $habillage_temp = $res_verif_themes_tableau[$eva_themes];
-        if ($habillage_temp) {
-            foreach ($eva_proprietes as $eva_prop => $eva_select) {
-                $envoi .= implode(' , ',$eva_select);
-                $envoi .="{".$eva_prop.":".$habillage_temp.";}\n";
-            }
-        }
-    }
-    $image_def = EVA_div_images();
-    foreach ($image_def as $image_cle => $image_val) {
-        $verif_image_def = sql_select('type,nom_image,pos_x,pos_y,repetition,attach','spip_eva_habillage_images',"nom_habillage = 'Defaut' AND nom_div = '".$image_cle."'");
-        $tab_image_def = sql_fetch($verif_image_def);
-        if ($tab_image_def['type']!='') {
-            if (strpos($image_cle,'image_')!==FALSE) {
-                $envoi .= implode(' , ',$image_val);
-		if (file_exists(_DIR_IMG."eva_habillage/".$tab_image_def['nom_image'])) {
-		$envoi .=" {background-image : url("._DIR_IMG."eva_habillage/".$tab_image_def['nom_image'].");\n";}
-		elseif (file_exists(_DIR_PLUGIN_EVA_HABILLAGE."mon_image/".$tab_image_def['nom_image'])) {
-				$envoi .=" {background-image : url("._DIR_PLUGIN_EVA_HABILLAGE."mon_image/".$tab_image_def['nom_image'].");\n";}
-                $envoi .= "background-position : ".$tab_image_def['pos_x']." ".$tab_image_def['pos_y'].";\n";
-                $envoi .= "background-repeat : ".$tab_image_def['repetition'].";\n";
-                $envoi .="background-attachment : ".$tab_image_def['attach'].";}\n";
-            }
-            elseif (strpos($image_cle,'liste_')!==FALSE) {
-                $envoi .= implode(', ',$image_val);
-		if (file_exists(_DIR_IMG."eva_habillage/".$tab_image_def['nom_image'])) {
-                $envoi .=" {list-style-image : url("._DIR_IMG."eva_habillage/".$tab_image_def['nom_image'].");\n";}
-		elseif(file_exists(_DIR_PLUGIN_EVA_HABILLAGE."mon_image/".$tab_image_def['nom_image'])) {
-				$envoi .=" {list-style-image : url("._DIR_PLUGIN_EVA_HABILLAGE."mon_image/".$tab_image_def['nom_image'].");\n";
+function balise_EVAHABILLAGE($p){
+	$test = _DIR_PLUGIN_EVASQUELETTES;
+	if (defined("_DIR_PLUGIN_EVASQUELETTES")){
+		$chemin_squelettes = $test;
+	} else {
+		$chemin_squelettes = 'squelettes/';
+	}
+	$resultat1 = sql_select('habillage', 'spip_eva_habillage', "sauvegarde = 'Defaut'");
+	$resultat1_tableau = sql_fetch($resultat1);
+	$mon_habillage = $resultat1_tableau['habillage'];
+	if (($mon_habillage=='0') OR ($mon_habillage=='')){
+		$envoi = '<link rel="stylesheet" type="text/css" href="' . $chemin_squelettes . 'eva_style.css" media="screen" />';
+	} else {
+		$envoi = '<link rel="stylesheet" type="text/css" href="' . $chemin_squelettes . 'eva_styles/' . $mon_habillage . '" media="screen" />';
+	}
+	$envoi .= "\n";
+	$envoi .= '<style type="text/css">';
+	$envoi .= "\n";
+	include_spip('inc/eva_habillage_definition_themes');
+	$eva_habillage_themes = eva_habillage_definition_themes();
+	foreach ($eva_habillage_themes as $eva_themes => $eva_proprietes){
+		$res_verif_themes_defini = sql_select($eva_themes, 'spip_eva_habillage_themes', "nom = 'Defaut'");
+		$res_verif_themes_tableau = sql_fetch($res_verif_themes_defini);
+		$habillage_temp = $res_verif_themes_tableau[$eva_themes];
+		if ($habillage_temp){
+			foreach ($eva_proprietes as $eva_prop => $eva_select){
+				$envoi .= implode(' , ', $eva_select);
+				$envoi .= "{" . $eva_prop . ":" . $habillage_temp . ";}\n";
+			}
+		}
+	}
+	$image_def = EVA_div_images();
+	foreach ($image_def as $image_cle => $image_val){
+		$verif_image_def = sql_select('type,nom_image,pos_x,pos_y,repetition,attach', 'spip_eva_habillage_images', "nom_habillage = 'Defaut' AND nom_div = '" . $image_cle . "'");
+		$tab_image_def = sql_fetch($verif_image_def);
+		if ($tab_image_def['type']!=''){
+			if (strpos($image_cle, 'image_')!==FALSE){
+				$envoi .= implode(' , ', $image_val);
+				if (file_exists(_DIR_IMG . "eva_habillage/" . $tab_image_def['nom_image'])){
+					$envoi .= " {background-image : url(" . _DIR_IMG . "eva_habillage/" . $tab_image_def['nom_image'] . ");\n";
+				} elseif (file_exists(_DIR_PLUGIN_EVA_HABILLAGE . "mon_image/" . $tab_image_def['nom_image'])) {
+					$envoi .= " {background-image : url(" . _DIR_PLUGIN_EVA_HABILLAGE . "mon_image/" . $tab_image_def['nom_image'] . ");\n";
 				}
-                $envoi .= "list-style-position : ".$tab_image_def['pos_x'].";}\n";
-            }
-        }
-    }
+				$envoi .= "background-position : " . $tab_image_def['pos_x'] . " " . $tab_image_def['pos_y'] . ";\n";
+				$envoi .= "background-repeat : " . $tab_image_def['repetition'] . ";\n";
+				$envoi .= "background-attachment : " . $tab_image_def['attach'] . ";}\n";
+			} elseif (strpos($image_cle, 'liste_')!==FALSE) {
+				$envoi .= implode(', ', $image_val);
+				if (file_exists(_DIR_IMG . "eva_habillage/" . $tab_image_def['nom_image'])){
+					$envoi .= " {list-style-image : url(" . _DIR_IMG . "eva_habillage/" . $tab_image_def['nom_image'] . ");\n";
+				} elseif (file_exists(_DIR_PLUGIN_EVA_HABILLAGE . "mon_image/" . $tab_image_def['nom_image'])) {
+					$envoi .= " {list-style-image : url(" . _DIR_PLUGIN_EVA_HABILLAGE . "mon_image/" . $tab_image_def['nom_image'] . ");\n";
+				}
+				$envoi .= "list-style-position : " . $tab_image_def['pos_x'] . ";}\n";
+			}
+		}
+	}
 
 //Menu depliable
-$tab_evabonus_menu=EVA_menu_dynamique_horizontal();
-foreach ($tab_evabonus_menu as $cle_evabonus_menu=>$val_evabonus_menu)
-{
-	$req_menu_depliable=sql_select('nom_image','spip_eva_habillage_images',"type='menu_depliable_horizontal' AND nom_habillage='Defaut' AND nom_div='".$cle_evabonus_menu."'");
-	$tab_menu_depliable=sql_fetch($req_menu_depliable);
-	$code_menu_depliable=$tab_menu_depliable['nom_image'];
-	if ($code_menu_depliable)
-	{
-		$envoi .="$val_evabonus_menu".$code_menu_depliable.";}\n";
+	$tab_evabonus_menu = EVA_menu_dynamique_horizontal();
+	foreach ($tab_evabonus_menu as $cle_evabonus_menu => $val_evabonus_menu){
+		$req_menu_depliable = sql_select('nom_image', 'spip_eva_habillage_images', "type='menu_depliable_horizontal' AND nom_habillage='Defaut' AND nom_div='" . $cle_evabonus_menu . "'");
+		$tab_menu_depliable = sql_fetch($req_menu_depliable);
+		$code_menu_depliable = $tab_menu_depliable['nom_image'];
+		if ($code_menu_depliable){
+			$envoi .= "$val_evabonus_menu" . $code_menu_depliable . ";}\n";
+		}
 	}
-} 
 //Fin Menu depliable
 
 //Menu de langue
-$tab_evabonus_menu=EVA_menu_langue();
-foreach ($tab_evabonus_menu as $cle_evabonus_menu=>$val_evabonus_menu)
-{
-	$req_menu_depliable=sql_select('nom_image','spip_eva_habillage_images',"type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='".$cle_evabonus_menu."'");
-	$tab_menu_depliable=sql_fetch($req_menu_depliable);
-	$code_menu_depliable=$tab_menu_depliable['nom_image'];
-	if ($code_menu_depliable)
-	{
-		$envoi .="$val_evabonus_menu".$code_menu_depliable.";}\n";
+	$tab_evabonus_menu = EVA_menu_langue();
+	foreach ($tab_evabonus_menu as $cle_evabonus_menu => $val_evabonus_menu){
+		$req_menu_depliable = sql_select('nom_image', 'spip_eva_habillage_images', "type='multilinguisme' AND nom_habillage='Defaut' AND nom_div='" . $cle_evabonus_menu . "'");
+		$tab_menu_depliable = sql_fetch($req_menu_depliable);
+		$code_menu_depliable = $tab_menu_depliable['nom_image'];
+		if ($code_menu_depliable){
+			$envoi .= "$val_evabonus_menu" . $code_menu_depliable . ";}\n";
+		}
 	}
-} 
 //Fin Menu de langue
 
-    $mes_CSS = sql_select('nom_div','spip_eva_habillage_images',"nom_habillage = 'Defaut' AND type = 'perso'");
-    while ($CSS = sql_fetch($mes_CSS)) {
-	$CSS_remp=str_replace('/rep_eva_habillage/',_DIR_PLUGIN_EVA_HABILLAGE,$CSS['nom_div']);
-	$envoi .=$CSS_remp."\n";}
-    $envoi .= '</style>';
-    $p->code = "'".$envoi."'";
-    $p->interdire_scripts = false;
-    return $p;
+	$mes_CSS = sql_select('nom_div', 'spip_eva_habillage_images', "nom_habillage = 'Defaut' AND type = 'perso'");
+	while ($CSS = sql_fetch($mes_CSS)){
+		$CSS_remp = str_replace('/rep_eva_habillage/', _DIR_PLUGIN_EVA_HABILLAGE, $CSS['nom_div']);
+		$envoi .= $CSS_remp . "\n";
+	}
+	$envoi .= '</style>';
+	$p->code = "'" . $envoi . "'";
+	$p->interdire_scripts = false;
+	return $p;
 }
 
 ?>
