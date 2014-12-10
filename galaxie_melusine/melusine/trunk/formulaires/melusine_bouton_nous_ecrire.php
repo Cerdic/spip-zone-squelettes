1c4f23a316ef45ce7a3fbf7854dd7702246cc803
noemie-edith-so.bernard@ac-creteil.fr <noemie-edith-so.bernard@ac-creteil.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-10 15:40:45 +0000

    Module "Nous écrire" : possibilité de changer le texte à afficher.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@86564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galaxie_melusine/melusine/trunk/formulaires/melusine_bouton_nous_ecrire.php
+++ galaxie_melusine/melusine/trunk/formulaires/melusine_bouton_nous_ecrire.php
@@ -19,7 +19,7 @@ function formulaires_melusine_bouton_nous_ecrire_traiter()
 		$id_noisette=_request('id_noisette');
 		/* On récupère l'auteur choisi dans la liste déroulante 
 		"personneajoindre" */
-		$casiers=array('style','personneajoindre');
+		$casiers=array('style','personneajoindre','textebouton');
 
 		$params=array();
 		foreach($casiers as $casier)
