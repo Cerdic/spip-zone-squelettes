b1bae9a6512098d0d90c787e4b4848adb8a6ddf8
noemie-edith-so.bernard@ac-creteil.fr <noemie-edith-so.bernard@ac-creteil.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-05 15:11:29 +0000

    Module "Plan du site" : YAML en formulaire pour plus de choix dans l'apparence
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@87371 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ galaxie_melusine/melusine/trunk/formulaires/melusine_bouton_plan_du_site.php
@@ -0,0 +1,55 @@
+<?php 
+include_spip('inc/config');
+
+function formulaires_melusine_bouton_plan_du_site_charger($id_noisette)
+{
+	$valeurs=array();
+	$valeurs['id_noisette'] = $id_noisette;
+  	return $valeurs;
+}
+
+function formulaires_melusine_bouton_plan_du_site_verifier()
+{
+}
+
+
+function formulaires_melusine_bouton_plan_du_site_traiter()
+{
+	include_spip('action/editer_objet');
+	$id_noisette=_request('id_noisette');
+
+	if (isset($_POST['ok'])) 
+	{	/* On récupère les valeurs du formulaire */
+		$casiers=array('textebouton','style','centrer');
+	}
+	elseif (isset($_POST['reset'])) 
+	{	/* On initialise les valeurs du formulaire */
+		$casiers=array('Plan du site','bloc_visible','gauche');
+	}
+
+	$params=array();
+	foreach($casiers as $casier)
+	{
+		$data=_request($casier);
+		if(!isset($data))
+			{$data="none";};
+		$params[$casier]=$data;
+		$set=array('parametres'=>serialize($params));
+	}
+	objet_modifier("noisette", $id_noisette, $set);
+
+/* Si le bouton Enregistrer a été sélectionné sans texte à afficher */	
+	if (isset($_POST['ok']) 
+			&& $params['textebouton']=='') 
+		{	
+			return array('message_erreur'=>'Il faut un texte pour le bouton'
+			,'id_noisette'=>$id);
+		}
+/* Si le bouton Enregistrer a été sélectionné sans oubli de champs */
+	elseif (isset($_POST['ok'])) 
+		{	
+			return array('message_ok'=>'Saisie enregistr&eacute;e'
+			,'id_noisette'=>$id);
+		}
+}
+?>
