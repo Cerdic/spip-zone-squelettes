d09ddfd38bdc179cbc9085f272dc299d0f14ec94
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-06 15:40:39 +0000

    traduction allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@88484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipr-blog/trunk/lang/public_de.php
@@ -0,0 +1,17 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: public
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'articles_rubrique' => 'In dieser Kategorie',
+	'articles_auteur' => 'Beiträge dieses Autors',
+	'articles' => 'Beiträge',
+	'categories' => 'Kategorien',
+	'derniers_articles' => 'Neue Beiträge',
+	);
+
+?>
