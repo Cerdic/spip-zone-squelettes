4a24ec322d2d0b1178d95f3f1230c4dbfba4f5b0
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-06 15:36:41 +0000

    internationalisation : categories
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@88482 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-blog/trunk/lang/public_fr.php
+++ spipr-blog/trunk/lang/public_fr.php
@@ -7,11 +7,11 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'derniers_articles' => 'Derniers billets',
 	'articles_rubrique' => 'Dans cette catégorie',
 	'articles_auteur' => 'Billets de cet auteur',
 	'articles' => 'Billets',
-);
+	'categories' => 'Catégories',
+	'derniers_articles' => 'Derniers billets',
+	);
 
 ?>
