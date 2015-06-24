8411055d01db0c48784b8ccc4764aa3df96633f6
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-24 13:49:32 +0000

    suite formulaire de choix du logo pour simulation : css + lang
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@90484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spiplogo/skel/lang/spiplogo_skel_fr.php
+++ spiplogo/skel/lang/spiplogo_skel_fr.php
@@ -4,5 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// L
+	'label_choixlogo' => 'Choisir un logo puis valider pour le voir en place sur quelques sites de la galaxie.',
+
 );
-?>
