e6cdd3f00b58dad04e72dd89fed5f1a02252fd54
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-24 22:06:22 +0000

    Bazar est un petit squelette qui permet facilement d'étaler tout son bazar sur la page d'accueil : http://contrib.spip.net/ecrire/?exec=article&id_article=4692
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@90497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bazar/trunk/lang/bazar_fr.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_titre' => 'Configurer le Bazar',
+	'configurer_accueil' => 'Configurer la page d\'accueil',
+	
+	// E
+	'erreur_config_creer_preview' => 'Attention : la génération de miniatures des images est actuellement inactive, veuillez  l\'activer dans les <a href=".?exec=configurer_avancees">fonctions avancées</a> de la configuration du site !',
+	'erreur_config_image_process' => 'Attention : La méthode de fabrication des vignettes n\'a pas été choisie, veuillez  en sélectionner une dans les <a href=".?exec=configurer_avancees">fonctions avancées</a> de la configuration du site !',
+// 	'explication_panel1' => 'Choisir la rubrique dont sera tirer le premier article (par ordre de numéro du titre, sinon par ordre chronologique inversé) pour être afficher dans le premier panneau de la page d\'accueil.',
+	'explication_logobkg' => 'Sinon le logo sera sous le titre.',
+	'explication_couper' => 'En nombre de caractères. <a href="http://www.spip.net/fr_article3965.html">Par défaut 600 caractères</a>.',
+	'explication_nomsrows' => 'Renseignez ici le nom de chaque ligne (séparé par une virgule, sans accents et sans espace, pas de doublon) que vous voulez ajouter sur la page d\'accueil. Vous pouvez en créer autant que nécessaire.
+	Exemple : "presentation,actualites,articles" ou plus simplement : "ligne1,ligne2,ligne3".',
+	'explication_doc' => 'Utilisez le premier document associé à l\'article de format jpg, gif ou png, par ordre chronologique ou par numéro du titre :
+	<br />- soit pour l\'afficher sous le titre, comme un logo.
+	<br />- soit pour l\'utiliser en arrière-plan.',
+	
+	// L
+// 	'label_panel1' => 'Afficher un texte dans le premier bloc',
+	'label_doc' => 'Utilisez une image',
+	'label_doc_logo' => 'sous le titre, comme un logo.',
+	'label_doc_bkg' => 'en arrière-plan.',
+	'label_nb_d_article' => 'Nombre d\'article sur la ligne',
+	'label_rubrique' => 'Choisir la rubrique',
+	'label_logo_arriere_plan' => 'Le logo en arrière-plan',
+	'label_afficher_l_introduction' => 'Afficher l\'introduction',
+	'label_longueur_de_l_introduction' => 'Définir la longueur de l\'introduction',
+	'label_nomsrows' => 'Noms des lignes',
+	
+	// T
+	'titre_menu' => 'le Bazar',
+);
