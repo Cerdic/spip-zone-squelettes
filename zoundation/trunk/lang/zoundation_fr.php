2e9f89ad145789160afd2c121af73e99b0f45514
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-16 19:26:39 +0000

    Compatible avec le plugin Album
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@91891 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zoundation/trunk/lang/zoundation_fr.php
+++ zoundation/trunk/lang/zoundation_fr.php
@@ -5,8 +5,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'zoundation_titre' => 'Zoundation',
-	'titre_page_configurer_zoundation' => 'Configuration du squelette Zoundation',
+    'zoundation_titre' => 'Zoundation',
+    'titre_page_configurer_zoundation' => 'Configuration du squelette Zoundation',
     'rubrique_sommaire' => 'id_ubrique du sommaire',
-    'explication_rubrique_sommaire' => "Limite l'affichage des articles sur la page d'accueil à une ou plusieurs rubriques (séparer par une virgule). <br />Si aucune rubrique n'est choisie, l'ensemble des rubriques sera utilisé.<br /> Exemple: 1,12,36"
+    'explication_rubrique_sommaire' => "Limite l'affichage des articles sur la page d'accueil à une ou plusieurs rubriques (séparer par une virgule). <br />Si aucune rubrique n'est choisie, l'ensemble des rubriques sera utilisé.<br /> Exemple: 1,12,36",
+    'dernier_album' => 'Dernier album'
 );
