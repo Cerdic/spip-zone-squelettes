c424a6dbda7dfa7b51d95b4b3001a5968f0bfc7c
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-22 16:06:03 +0000

    menu smart + broutilles
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@92010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bazar/trunk/js/script.js
+++ bazar/trunk/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	// placer le html du bouton du menu sous 768px
+	$('.nav').prepend('<button type="button" class="btn-nav" aria-label="afficher/masquer le menu de navigation"><span class="ham"></span><span class="ham"></span>MENU</button>');
 	// afficher/masquer le menu
 	$('.btn-nav').bind('click', function(){
 		$(this).siblings('ul').toggleClass('ouvert');
