c8c41872cffa24f5bec82ae1361250687ddd3c7a
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-21 17:17:13 +0000

    Adapter la pagination foundation à foundation 6
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@93867 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zoundation/trunk/lang/zoundation_fr.php
+++ zoundation/trunk/lang/zoundation_fr.php
@@ -10,5 +10,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'rubrique_sommaire' => 'id_rubrique du sommaire',
     'explication_rubrique_sommaire' => "Limite l'affichage des articles sur la page d'accueil à une ou plusieurs rubriques (séparer par une virgule). <br />Si aucune rubrique n'est choisie, l'ensemble des rubriques sera utilisé.<br /> Exemple: 1,12,36",
     'dernier_album' => 'Dernier album',
-    'vous_etes_ici' => 'Vous êtes ici'
+    'vous_etes_ici' => 'Vous êtes ici',
+    'vous_etes_page' => 'Vous êtes à la page'
 );
