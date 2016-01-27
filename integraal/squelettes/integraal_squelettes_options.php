f593592a5389ebbeb54804b9e64c05414ab054bb
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-27 16:28:10 +0000

    règles d'écriture
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@94779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- integraal/squelettes/integraal_squelettes_options.php
+++ integraal/squelettes/integraal_squelettes_options.php
@@ -1,13 +1,24 @@
 <?php
 
 // Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 // Activer HTML5 depuis le squelette
 $GLOBALS['meta']['version_html_max'] = 'html5';
 
-if (!isset($GLOBALS['z_blocs'])){
-	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+if (!isset($GLOBALS['z_blocs'])) {
+	$GLOBALS['z_blocs'] = array(
+		'content',
+		'aside',
+		'extra',
+		'head',
+		'head_js',
+		'header',
+		'footer',
+		'breadcrumb',
+	);
 }
 
 define('_ZENGARDEN_FILTRE_THEMES','integraal');
