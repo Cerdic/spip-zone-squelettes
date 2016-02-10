134d6426f29189a4fe93fa06837ce4bfbeba88b5
johan.pustoch@crdp.ac-versailles.fr <johan.pustoch@crdp.ac-versailles.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-10 09:18:29 +0000

    Correction des bornes de compatibilité (Franck_)
    Phillipe R. :
    le formulaire de recherche est remonté en première ligne avant les rubriques
    suppression des doublons d’articles de la page d’accueil
    l'ordre d'affichage dans la barre de menu est maintenant rubriques-articles-sites
    les sous-titre des articles sont affichés sous les titres
    le bouton « liens » permet d’accéder à tous les liens lorsque le menu déroulant est désactivé.
    le chapo des rubriques n’est plus écrit en gras
    les rubriques actives du menu déroulant sont maintenant de la couleur des rubriques survolées
    les HR sont un peu plus visibles.
    les lignes verticales des tableaux ont été ajoutées
    la première ligne des tableaux (en gras) est maintenant de la couleur du bandeau de navigation.
    
    Egalement : logo scolaspip en transparent, correction du oembed de la scolawebtv
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@95067 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scolaspip/trunk/scolaspip_options.php
+++ scolaspip/trunk/scolaspip_options.php
@@ -1,5 +1,6 @@
 <?php
 $GLOBALS['oembed_providers'] = array(
     'http://webtv.ac-versailles.fr/*' => 'http://webtv.ac-versailles.fr/oembed.api',
-    'http://scolawebtv.crdp-versailles.fr/*' => 'http://scolawebtv.crdp-versailles.fr/oembed.api',
+    'http://scolawebtv.crdp-versailles.fr/*' => 'http://scolawebtv.crdp-versailles.fr/oembed.api/',
+    'http://scolawebtv.ac-versailles.fr/*' => 'http://scolawebtv.ac-versailles.fr/oembed.api/',
 );
