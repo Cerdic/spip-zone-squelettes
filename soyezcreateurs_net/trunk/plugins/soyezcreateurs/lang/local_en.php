4bb6e3229bcca0e4edbf88c1d22b1a6e0439a762
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-24 08:11:41 +0000

    Intégré ce qui était traduit dans http://trad.spip.net/tradlang_module/soyezcreateurs
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@95421 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_en.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_en.php
@@ -6,7 +6,4 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // Modification de l'interface d'admin
 'texte_descriptif_rapide' => 'Tooltip',
 
-// Modification du texte du formulaire de forum
-'forum_votre_email' => 'Your email address (<strong>This will not be made public on the site,</strong>, but is necessary in order to send replies to you.) :'
-
 );
--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_it.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_it.php
@@ -6,7 +6,4 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // Modification de l'interface d'admin
 'texte_descriptif_rapide' => 'Bulle d\'aide <i>(et chapeau si le chapeau vide)</i>',
 
-// Modification du texte du formulaire de forum
-'forum_votre_email' => 'Votre adresse email (<strong>ne sera pas rendue publique sur le site</strong>, n&eacute;cessaire pour pouvoir vous r&eacute;pondre personnellement) :'
-
 );
