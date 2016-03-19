5732e6ee325ab15c5af7b78e6ea9a3f01dd5e4fa
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-19 23:17:21 +0000

    Indentation, tab à la place d'espace+ avec accord de l'auteur je passe les necessite à la première borne dispo qui soit sous la forme x.y.z avec borne de compatibilité "clean" z+1 car il y a pas mal de fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@96089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_2/eva_calendrier/lang/paquet-eva_calendrier_fr.php
+++ eva-web/4_2/eva_calendrier/lang/paquet-eva_calendrier_fr.php
@@ -2,9 +2,9 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'eva_calendrier_slogan' => 'Une rubrique sous forme de calendrier pour EVA-Web 4.2',
-'eva_calendrier_description' => 'Affichage d&#8217;une rubrique sous forme de calendrier mensuel avec syst&#232;me de navigation vers les mois pr&#233;c&#233;dents et suivants.
-La rubrique s&#8217;affiche sous forme de calendrier d&#233;s lors qu&#8217;on lui joint le mot cl&#233; {{calendrier}}.
-Les articles sont inscrits dans le calendrier en leurs attribuant une date de r&#233;daction ant&#233;rieure correspondant &#224; la date de l&#8217;&#233;v&#232;nement concern&#233;.',
+	'eva_calendrier_slogan' => 'Une rubrique sous forme de calendrier pour EVA-Web 4.2',
+	'eva_calendrier_description' => 'Affichage d&#8217;une rubrique sous forme de calendrier mensuel avec syst&#232;me de navigation vers les mois pr&#233;c&#233;dents et suivants.
+	La rubrique s&#8217;affiche sous forme de calendrier d&#233;s lors qu&#8217;on lui joint le mot cl&#233; {{calendrier}}.
+	Les articles sont inscrits dans le calendrier en leurs attribuant une date de r&#233;daction ant&#233;rieure correspondant &#224; la date de l&#8217;&#233;v&#232;nement concern&#233;.',
 );
 ?>
