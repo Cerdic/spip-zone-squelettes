d5b49f16502ed910899ab2f7e968d9796174d03e
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-26 22:26:19 +0000

    Je passe avec accord de l'auteur le necessite en 4.2.1 car il s'agit de la premiere version en x.y.z et avec des bornes de compatibilité spip bien défini - J'en profite pour mettre des tabs à la place d'espaces pour le mettre un peu plus conforme avec ​http://www.spip.net/fr_article3497.html#regles_codage - Indentation, z+1 car pleins de fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@96214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_2/eva_livre_2/lang/paquet-livre_2_fr.php
+++ eva-web/4_2/eva_livre_2/lang/paquet-livre_2_fr.php
@@ -4,17 +4,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'livre_2_slogan' => 'Une s&#233;rie d&#8217;articles sous forme de livre pour EVA-web 4.2',
-'livre_2_description' => '{{Int&#233;ret de ce plugin}}
-	
-- Publier les articles d&#8217;une rubrique sous forme de livre
-- Permettre le passage d&#8217;un article à un autre par pagination
+	'livre_2_slogan' => 'Une s&#233;rie d&#8217;articles sous forme de livre pour EVA-web 4.2',
+	'livre_2_description' => '{{Int&#233;ret de ce plugin}}
 
-{{Mise en place de ce plugin}}
+	- Publier les articles d&#8217;une rubrique sous forme de livre
+	- Permettre le passage d&#8217;un article à un autre par pagination
 
-- Activer le plugin en cochant la case ci-contre, puis valider
-- Suivre la proc&#233;dure de mise en place du livre d&#233;crite dans l&#8217;aide d&#8217;Eva',
-    
+	{{Mise en place de ce plugin}}
+
+	- Activer le plugin en cochant la case ci-contre, puis valider
+	- Suivre la proc&#233;dure de mise en place du livre d&#233;crite dans l&#8217;aide d&#8217;Eva',
+
 );
 
 ?>
