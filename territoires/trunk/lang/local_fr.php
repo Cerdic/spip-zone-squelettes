ceb00f39db8a163b490b29b281cb1a7d9cd3eda6
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-02 15:38:09 +0000

    import initial du squelette de territoires; voir www.valleeducousin.fr pour un exemple
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@98639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ territoires/trunk/lang/local_fr.php
@@ -0,0 +1,164 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'about' => 'À propos de',
+	'abonnezvous' => 'S\'abonner à',
+	'accederalafichedudocument' => 'Acc&eacute;der à la fiche du document',
+	'activites' => 'Activit&eacute;s',	
+	'adresses_publiees' => 'adresses publi&eacute;es',
+	'affichettes' => 'Affichettes',
+	'alire' => 'A lire',	
+	'alireaussi' => 'A lire aussi',
+	'ajouteruncredit' => '&rarr; Ajouter un cr&eacute;dit ?',
+	'ajouterundescriptif' => '&rarr; Ajouter un descriptif ?',
+	'ajouterunnumerodetelephone' => '&rarr; Ajouter un num&eacute;ro de t&eacute;l&eacute;phone ?',
+	'ajouteruntitre' => '&rarr; Ajouter un titre ?',
+	'ajouterunsurtitre' => '&rarr; Ajouter un surtitre ?',
+	'ajouterunsoustitre' => '&rarr; Ajouter un soustitre ?',			
+	'articleslies' => 'Articles li&eacute;s',
+	'articlesconnexes' => 'Articles connexes',
+	'articles_publies' => 'articles publi&eacute;s',
+	'auteurs' => 'Auteurs',
+	'auteursconnexes' => 'Auteurs connexes',
+	'autresauteurs' => 'Autres auteurs',
+	
+	// B
+	'breves' => 'Br&egrave;ves',
+	
+	// C
+	'calendrier' => 'Calendrier',
+	'ciels' => 'Ciels',
+	'credits' => 'Cr&eacute;dits',
+	'communes' => 'Communes',
+	'compteur' => 'Compteur',	
+	'connexions' => 'Connexions',
+	'contactez' => 'Contactez',	
+	'contact' => 'Contact',	
+	'contacts' => 'Contacts',	
+	'contexte' => 'Dans le même contexte',
+
+
+	// D
+	'date' => 'Date',	
+	'datededebut' => 'Date de d&eacute;but',
+	'datedefin' => 'Date de fin',	
+	'dimensionsenpx' => 'Dimensions du document en pixels',
+	'documents' => 'Documents',
+	'documents_publies' => 'documents publi&eacute;s',
+	'documentnumero' => 'Document n°',
+	'dautresauteurs' => 'D\'autres auteurs',
+	'datepublication' => 'Date de publication',
+	'derniersarticles' => 'Derniers articles',	
+	'dernieres_publications' => 'Derni&egrave;res publications',
+	'dernieres_images' => 'Derni&egrave;res images',	
+
+	// E
+	'ecrirea' => '&Eacute;crire à',
+	'encours' => 'En cours...',
+	'en_reponse_a' => 'En r&eacute;ponse à',
+	'evenement' => '&Eacute;v&eacute;nement',
+	'evenements' => '&Eacute;v&eacute;nements',
+	'evenementsalaffiche' => '&Eacute;v&eacute;nements à l\'affiche',
+	'evenements_annonces' => '&eacute;v&eacute;nements annoncés',
+	'evenements_annonces_maj' => '&Eacute;v&eacute;nements annoncés',
+	'evenementsdesprochainsjours' => 'Prochains év&eacute;nements à l\'affiche',
+	'evenementspasses' => '&Eacute;v&eacute;nements passés',
+	'evenementsassocies' => '&Eacute;v&eacute;nements associés',
+    'evenementsliesaudocument' => '&Eacute;v&eacute;nements reli&eacute;s &agrave; ce document',
+
+	// F
+	'forum' => 'Forum',
+
+	// H
+	'hasard' => 'Hasard',
+
+	// I
+	'images' => 'Images',	
+	'imagechoisie' => 'Image choisie',
+	'imagesconnexes' => 'Images connexes',
+	'imagesdernieres' => 'Derni&eagrave;res images',
+	'imagesposteesparcetauteur' => 'Images post&eacute;es par cet auteur',
+	
+	// L
+	'lauteur' => 'L\'auteur',
+	'lesauteurs' => 'Les auteurs',
+	'liste' => 'Liste',
+	'lire' => 'Lire',
+	'lire_en_ligne' => 'Lire en ligne',
+
+	// M
+	'manchettes' => 'Manchettes',
+	'message_du' => 'Message du',
+	'metadonnees' => 'M&eacute;tadonn&eacute;es',
+	'misajour' => 'Mis &agrave; jour le',
+	'miseajour' => 'Mise &agrave; jour',
+	'motsalaffiche' => 'Mots-cl&eacute;s &agrave; l\'affiche',
+	'motsconnexes' => 'Mots connexes',	
+	'motspasses' => 'Mots-cl&eacute;s passés',
+	'mots_cles' => 'Mots-cl&eacute;s',
+	'mots_cles_dans_le_meme_groupe' => 'Mots-cl&eacute;s dans le même groupe',
+
+	// O
+	'oups' => 'Oups !',
+	
+	// P
+	'par' => 'Par',
+	'pardate' => 'Par date',
+	'pardatededebut' => 'Par date de d&eacute;but',
+	'pardatedefin' => 'Par date de fin',
+	'parhasard' => 'par hasard',
+	'parpoids' => 'par poids',
+	'partitre' => 'Par titre',
+	'poids' => 'Poids',
+	'poidsdufichier' => 'Poids du fichier',
+	'postele' => 'Post&eacute; le',
+	'pournousecrire' => 'Pour nous &eacute;crire',	
+	'poursuivre_cette_discussion' => 'Poursuivre cette discussion',
+	'prochainement' => 'Prochainement',
+	'programme' => 'programm&eacute;',
+	'publicationsdernieres' => 'Les derni&egrave;res publications',
+	'publiele' => 'Publi&eacute; le',	
+	'publiepar' => 'Publi&eacute; par',
+	'publiespar' => 'publi&eacute;s par',		
+	
+	// R
+	'recherche' => 'Recherche',
+	'remonter' => 'Remonter',	
+	'resultats' => 'R&eacute;sultats',
+	'repondre_message' => 'R&eacute;pondre au message',
+	
+	// S
+	'sinformer' => 'S\'informer',
+    'sites_conseilles' => 'Sites conseill&eacute;s',
+	'sortir' => 'Sortir',
+	'sujet_du_message' => 'Sujet du message',
+	'sujets' => 'Sujets',
+	
+	// T
+	'tendanceactuelle' => 'Tendance actuelle : ',
+	'titre' => 'Titre',
+	'tridate' => 'par date de publication',
+	'trihasard' => 'par hasard',
+	'triimage' => 'par images',	
+	'trimiseajour' => 'par date de mise &agrave; jour',
+	'tripoids' => 'par poids d&eacute;croissant',		
+	'triliste' => 'par liste chronologique',
+	'trimot' => 'par mots-cl&eacute;s',	
+	'trivignette' => 'en vignettes',
+	'typemime' => 'Type MIME',
+	
+	// V
+	'vignettes' => 'Vignettes',
+	'voir' => 'Voir',
+	'voir_sur_le_site' => 'Voir sur le site',
+	'votreadresseelectronique' => 'votreadresse@&eacute;lectronique'
+	
+
+);
+
+?>
---
+++ territoires/trunk/mes_fonctions.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * Retirer les elements paragraphes d'un texte
+ * @author Christian Paulus
+ * @version 20120201
+ */
+function deparagrapher ($texte)
+{
+    $texte = trim ($texte);
+    if (substr ($texte, 0, 3) == '<p>')
+    {
+        $texte = substr ($texte, 3);
+        if (substr ($texte, -4) == '</p>')
+        {
+            $texte = substr ($texte, 0, strlen ($texte) - 4);
+        }
+    }
+    return ($texte);
+}
+
+
+// #lesmots
+// les mots d'un objet
+// http://www.spip.net/fr_article902.html
+// http://www.spip.net/fr_article911.html
+// http://doc.spip.org/@balise_lesmots_dist
+function balise_lesmots_dist ($p) {
+	// Cherche le champ 'lesmots' dans la pile
+	$_lesmots = champ_sql('lesmots', $p, false);
+
+	// Si le champ n'existe pas (cas de spip_articles), on applique
+	// le modele lesmots.html en passant id_article dans le contexte;
+	// dans le cas contraire on prend le champ 'lesmots'
+	// (cf extension sites/)
+	if ($_lesmots
+	AND $_lesmots != '@$Pile[0][\'lesmots\']') {
+		$p->code = "safehtml($_lesmots)";
+		// $p->interdire_scripts = true;
+	} else {
+		if(!$p->id_boucle){
+			$connect = '';
+			$objet = 'article';
+			$id_table_objet = 'id_article';
+		}
+		else{
+			$b = $p->nom_boucle ? $p->nom_boucle : $p->id_boucle;
+			$connect = $p->boucles[$b]->sql_serveur;
+			$type_boucle = $p->boucles[$b]->type_requete;
+			$objet = objet_type($type_boucle);
+			$id_table_objet = id_table_objet($type_boucle);
+		}
+		$c = memoriser_contexte_compil($p);
+
+		$p->code = sprintf(CODE_RECUPERER_FOND, "'modeles/lesmots'",
+				   "array('objet'=>'".$objet.
+					   "','id_objet' => ".champ_sql($id_table_objet, $p) .
+					   ",'$id_table_objet' => ".champ_sql($id_table_objet, $p) .
+					   ($objet=='article'?"":",'id_article' => ".champ_sql('id_article', $p)).
+					   ")",
+				   "'trim'=>true, 'compil'=>array($c)",
+				   _q($connect));
+		$p->interdire_scripts = false; // securite apposee par recuperer_fond()
+	}
+	return $p;
+}
+
+// #lescommunes
+// les communes d'un objet
+function balise_lescommunes_dist ($p) {
+	// Cherche le champ 'lescommunes' dans la pile
+	$_lescommunes = champ_sql('lescommunes', $p, false);
+
+	// Si le champ n'existe pas (cas de spip_articles), on applique
+	// le modele lescommunes.html en passant id_article dans le contexte;
+	// dans le cas contraire on prend le champ 'lescommunes'
+	// (cf extension sites/)
+	if ($_lescommunes
+	AND $_lescommunes != '@$Pile[0][\'lescommunes\']') {
+		$p->code = "safehtml($_lescommunes)";
+		// $p->interdire_scripts = true;
+	} else {
+		if(!$p->id_boucle){
+			$connect = '';
+			$objet = 'article';
+			$id_table_objet = 'id_article';
+		}
+		else{
+			$b = $p->nom_boucle ? $p->nom_boucle : $p->id_boucle;
+			$connect = $p->boucles[$b]->sql_serveur;
+			$type_boucle = $p->boucles[$b]->type_requete;
+			$objet = objet_type($type_boucle);
+			$id_table_objet = id_table_objet($type_boucle);
+		}
+		$c = memoriser_contexte_compil($p);
+
+		$p->code = sprintf(CODE_RECUPERER_FOND, "'modeles/lescommunes'",
+				   "array('objet'=>'".$objet.
+					   "','id_objet' => ".champ_sql($id_table_objet, $p) .
+					   ",'$id_table_objet' => ".champ_sql($id_table_objet, $p) .
+					   ($objet=='article'?"":",'id_article' => ".champ_sql('id_article', $p)).
+					   ")",
+				   "'trim'=>true, 'compil'=>array($c)",
+				   _q($connect));
+		$p->interdire_scripts = false; // securite apposee par recuperer_fond()
+	}
+	return $p;
+}
+
+function mini_html($texte) {
+   $texte = preg_replace("/\s{128,}/"," ", $texte);
+   return $texte;
+}
+
+function minisauvage_html($texte) {
+   $texte = preg_replace(",\s+,"," ", $texte);    
+   $texte = preg_replace(",\n[\n\t\s]*,", "\n", $texte);
+   return $texte;
+}
+
+?>
