47a4198ccbd9cc77073c6b82df31ba504e0232b1
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-13 20:59:56 +0000

    Première version non complétement fonctionnelle du theme https://html5up.net/alpha/
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@98867 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ html5up_alpha/lang/alpha_fr.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'bouton_subscribe' => 'Sign Up',
+
+    'explication_config_contenu' => "Le squelette utilise les sous titre pour les articles, n'oubliez pas de les activer dans <a href='?exec=configurer_contenu'>configurer contenu</a>",
+    'explication_doc_home' => "L'id SPIP du document qui sera utilisé pour la grande image du haut.Le thème original utilise une image de taille <b>1800 × 905 pixels</b> ",
+    'explication_doc_home2' => "L'id SPIP du document qui sera utilisé pour l'image <i>featured</i> . Le theme Alpha original utilise une image de taille  <b>1200 × 393 pixels</b>",
+    'explication_instagram' => "Url compte Instagram",
+    'explication_facebook' => "Url compte facebook",
+    'explication_twitter' => "Url compte twitter",
+
+
+    'label_config_contenu' => "Sous-titre",
+    'label_doc_home2' => "Id doc home fearured",
+    'label_doc_home' => "Id doc home",
+    'label_twitter' => 'Twitter',
+    'label_facebook' => 'Facebook',
+    'label_instagram' => 'Instagram',
+
+    'legend1' => "Explications",
+    'legend2' => "Paramétrages",
+
+
+    'titre_calltoaction' => "Newsletter",
+    'texte_calltoaction' => "Pour avoir des nouvelles régulières, laisse ton mail ",
+
+);
+
+
---
+++ html5up_alpha/lang/paquet-alpha_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'alpha_description' => "Ce plugin permet de reprendre le design proposé sur Html5Up .",
+    'alpha_slogan' => "L'intégration dans SPIP du design https://html5up.net/alpha "
+);
