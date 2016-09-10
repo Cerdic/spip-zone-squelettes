bcda4ba578ee5642452fefb30148b9b4df66d0f5
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-10 16:52:45 +0000

    début de l'internationalisation - A personnaliser en trois langues
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@99436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipr-doc/trunk/lang/spipr_doc_de.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par klaus++
+//
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'a_personnaliser' => 'personalisieren',
+
+);
+?>
