48c5cb56f0f022e75768cb7156d4d6ff4b52de9f
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-21 17:21:54 +0000

    Conditionner la partie mailsubscribers du formulaire à la présence du plugins en question
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@99545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zoundation/trunk/formulaires/configurer_zoundation.php
+++ zoundation/trunk/formulaires/configurer_zoundation.php
@@ -5,11 +5,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 function formulaires_configurer_zoundation_saisies_dist() {
-	include_spip('inc/mailsubscribers');
-
-	// Récupérer les listes de newsletter et créer une data compatible
-	$listes = mailsubscribers_listes();
-	$listes = array_column($listes, 'titre', 'id');
 
 	$saisies = array(
 		array(
@@ -19,16 +14,28 @@ function formulaires_configurer_zoundation_saisies_dist() {
 				'label' => _T('zoundation:rubrique_sommaire'),
 				'explication' => _T('zoundation:explication_rubrique_sommaire')
 			)
-		),
-		array(
+		)
+	);
+
+
+	$plugin = charger_filtre('info_plugin');
+
+	if ($plugin('mailsubscribers', 'est_actif')) {
+		include_spip('inc/mailsubscribers');
+
+		// Récupérer les listes de newsletter et créer une data compatible
+		$listes = mailsubscribers_listes();
+		$listes = array_column($listes, 'titre', 'id');
+
+		$saisies[] = array(
 			'saisie' => 'selection',
 			'options' => array(
 				'nom' => 'mailsubscribers_liste',
 				'label' => _T('zoundation:mailsubscribers_liste'),
 				'datas' => $listes
 			)
-		)
-	);
+		);
+	}
 
 	return $saisies;
 }
