926e783e32199b0a310bc0bc766f97494b4232d7
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-06 13:40:45 +0000

    On remplace une fonction php dépréciée par sql_quote et un test sur la présence de guillemets ou apostrophes dans les noms des sauvegardes. Incrémentation de version.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@99828 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_2/eva_habillage/formulaires/evahabillage_sauvegarde.php
+++ eva-web/4_2/eva_habillage/formulaires/evahabillage_sauvegarde.php
@@ -34,37 +34,44 @@ function formulaires_evahabillage_sauvegarde_traiter_dist() {
 	$res['message_ok'] = 'Module inchang&eacute;';
 	if ((_request('sauvegarder')=='Sauvegarder') AND (_request('sauvegarde_habillage')!='Defaut')) {
 		$nom_sauvegarde=_request('sauvegarde_habillage');
-		$nom_habillage_defaut=sql_select("habillage","spip_eva_habillage","sauvegarde = 'Defaut'");
-		$tab_habillage_defaut=sql_fetch($nom_habillage_defaut);
-		sql_delete("spip_eva_habillage","sauvegarde='".mysql_real_escape_string($nom_sauvegarde)."'");
-		sql_insertq("spip_eva_habillage",array('habillage' => $tab_habillage_defaut['habillage'],'sauvegarde' => mysql_real_escape_string($nom_sauvegarde)));
-		sql_delete("spip_eva_habillage_themes","nom='".mysql_real_escape_string($nom_sauvegarde)."'");
-		sql_insertq("spip_eva_habillage_themes",array('nom' => mysql_real_escape_string($nom_sauvegarde)));
-		$result_valeurs=sql_select("*","spip_eva_habillage_themes","nom='Defaut'");
-		$tab_valeurs=sql_fetch($result_valeurs);
-		include_spip("inc/eva_habillage_definition_themes");
-		$def_themes = eva_habillage_definition_themes ();
-		foreach($def_themes as $habillage_cles => $habillage_inutile) {sql_updateq('spip_eva_habillage_themes',array($habillage_cles => $tab_valeurs[$habillage_cles]),"nom='".mysql_real_escape_string($nom_sauvegarde)."'");}
-		sql_delete("spip_eva_habillage_images","nom_habillage='".mysql_real_escape_string($nom_sauvegarde)."'");
-		$result_images_sauve=sql_select('type,nom_div,nom_image,pos_x,pos_y,repetition,attach','spip_eva_habillage_images',"nom_habillage='Defaut'");
-		while ($tab=sql_fetch($result_images_sauve)) {
-			sql_insertq("spip_eva_habillage_images",array('type' => $tab['type'],'nom_habillage' => mysql_real_escape_string($nom_sauvegarde),'nom_div' => $tab['nom_div'], 'nom_image' => $tab['nom_image'], 'pos_x' => $tab['pos_x'],'pos_y' => $tab['pos_y'],'repetition' => $tab['repetition'], 'attach' => $tab['attach']));
+		if (strpos($nom_sauvegarde,"'") OR strpos($nom_sauvegarde,'"')) {
+			$res['message_erreur'] = 'Le nom de sauvegarde ne doit pas comporter d\'apostrophe ou de guillemet. La sauvegarde n\'a pas &eacute;t&eacute; enregistr&eacute;e';
+			$res['message_ok'] = '';
 		}
-		$res['message_ok'] = 'L\'habillage <b>'.mysql_real_escape_string($nom_sauvegarde).'</b> a &eacute;t&eacute; sauvegard&eacute;';
+		else {
+			$nom_habillage_defaut=sql_select("habillage","spip_eva_habillage","sauvegarde = 'Defaut'");
+			$tab_habillage_defaut=sql_fetch($nom_habillage_defaut);
+			sql_delete('spip_eva_habillage','sauvegarde='.sql_quote($nom_sauvegarde));
+			sql_insertq('spip_eva_habillage',array('habillage' => $tab_habillage_defaut['habillage'],'sauvegarde' => $nom_sauvegarde));
+			sql_delete('spip_eva_habillage_themes','nom='.sql_quote($nom_sauvegarde));
+			sql_insertq('spip_eva_habillage_themes',array('nom' => $nom_sauvegarde));
+			$result_valeurs=sql_select("*","spip_eva_habillage_themes","nom='Defaut'");
+			$tab_valeurs=sql_fetch($result_valeurs);
+			include_spip("inc/eva_habillage_definition_themes");
+			$def_themes = eva_habillage_definition_themes ();
+			foreach($def_themes as $habillage_cles => $habillage_inutile) {sql_updateq('spip_eva_habillage_themes',array($habillage_cles => $tab_valeurs[$habillage_cles]),'nom='.sql_quote($nom_sauvegarde));
+			}
+			sql_delete('spip_eva_habillage_images','nom_habillage='.sql_quote($nom_sauvegarde));
+			$result_images_sauve=sql_select('type,nom_div,nom_image,pos_x,pos_y,repetition,attach','spip_eva_habillage_images',"nom_habillage='Defaut'");
+			while ($tab=sql_fetch($result_images_sauve)) {
+				sql_insertq("spip_eva_habillage_images",array('type' => $tab['type'],'nom_habillage' => $nom_sauvegarde,'nom_div' => $tab['nom_div'], 'nom_image' => $tab['nom_image'], 'pos_x' => $tab['pos_x'],'pos_y' => $tab['pos_y'],'repetition' => $tab['repetition'], 'attach' => $tab['attach']));
+			}
+			$res['message_ok'] = 'L\'habillage <b>'.$nom_sauvegarde.'</b> a &eacute;t&eacute; sauvegard&eacute;';
+		}
 	}
 	elseif ((_request('sauvegarder')=='Restaurer') AND (_request('restaurer_habillage')!='Defaut')) {
 		$nom_sauvegarde=_request('restaurer_habillage');
-		$recherche_habillage_restaure = sql_select('habillage','spip_eva_habillage',"sauvegarde='".mysql_escape_string($nom_sauvegarde)."'");
+		$recherche_habillage_restaure = sql_select('habillage','spip_eva_habillage','sauvegarde='.sql_quote($nom_sauvegarde));
 		$tab__habillage_restaure = sql_fetch($recherche_habillage_restaure);
 		sql_updateq('spip_eva_habillage',array('habillage' => $tab__habillage_restaure['habillage']),"sauvegarde='Defaut'");
 
-		sql_delete('spip_eva_habillage_images',"nom_habillage='Defaut'");
-		$recherche_images_restaure=sql_select('type,nom_div,nom_image,pos_x,pos_y,repetition,attach','spip_eva_habillage_images',"nom_habillage='".mysql_escape_string($nom_sauvegarde)."'");
+		sql_delete("spip_eva_habillage_images","nom_habillage='Defaut'");
+		$recherche_images_restaure=sql_select('type,nom_div,nom_image,pos_x,pos_y,repetition,attach','spip_eva_habillage_images','nom_habillage='.sql_quote($nom_sauvegarde));
 		while ($tab=sql_fetch($recherche_images_restaure)) {
 			sql_insertq('spip_eva_habillage_images',array('type' => $tab['type'],'nom_habillage' => 'Defaut','nom_div' => $tab['nom_div'],'nom_image' => $tab['nom_image'],'pos_x' => $tab['pos_x'],'pos_y' => $tab['pos_y'],'repetition' => $tab['repetition'],'attach' => $tab['attach']));
         }
 
-		$result_restaure=sql_select('*','spip_eva_habillage_themes',"nom='".mysql_escape_string($nom_sauvegarde)."'");
+		$result_restaure=sql_select('*','spip_eva_habillage_themes','nom='.sql_quote($nom_sauvegarde));
 		$tab_restaure=sql_fetch($result_restaure);
 		include_spip("inc/eva_habillage_definition_themes");
 		$def_themes = eva_habillage_definition_themes ();
@@ -72,7 +79,7 @@ function formulaires_evahabillage_sauvegarde_traiter_dist() {
 			if (isset($tab_restaure[$habillage_cles])) {sql_updateq('spip_eva_habillage_themes',array($habillage_cles => $tab_restaure[$habillage_cles]),"nom='Defaut'");}
 			else {sql_updateq('spip_eva_habillage_themes',array($habillage_cles => ''),"nom='Defaut'");}
 		}
-		$res['message_ok'] = 'La sauvegarde de l\'habillage <b>'.mysql_escape_string($nom_sauvegarde).'</b> a &eacute;t&eacute; restaur&eacute;e';
+		$res['message_ok'] = 'La sauvegarde de l\'habillage <b>'.$nom_sauvegarde.'</b> a &eacute;t&eacute; restaur&eacute;e';
 		include_spip('inc/eva_habillage_transition_module');
 		eva_habillage_transition_module();
 		include_spip('inc/eva_habillage_parametres_accessibilite');
@@ -80,10 +87,10 @@ function formulaires_evahabillage_sauvegarde_traiter_dist() {
 	}
 	elseif ((_request('sauvegarder')=='Supprimer ?') AND (_request('supprimer_habillage')!='Defaut')) {
 		$nom_sauvegarde=_request('supprimer_habillage');
-		sql_delete("spip_eva_habillage","sauvegarde='".mysql_escape_string($nom_sauvegarde)."'");
-		sql_delete("spip_eva_habillage_themes","nom='".mysql_escape_string($nom_sauvegarde)."'");
-		sql_delete("spip_eva_habillage_images","nom_habillage='".mysql_escape_string($nom_sauvegarde)."'");
-		$res['message_ok'] = 'La sauvegarde de l\'habillage <b>'.mysql_escape_string($nom_sauvegarde).'</b> a &eacute;t&eacute; supprim&eacute;e';
+		sql_delete('spip_eva_habillage','sauvegarde='.sql_quote($nom_sauvegarde));
+		sql_delete('spip_eva_habillage_themes','nom='.sql_quote($nom_sauvegarde));
+		sql_delete('spip_eva_habillage_images','nom_habillage='.sql_quote($nom_sauvegarde));
+		$res['message_ok'] = 'La sauvegarde de l\'habillage <b>'.$nom_sauvegarde.'</b> a &eacute;t&eacute; supprim&eacute;e';
 	}
 	return $res;
 }
