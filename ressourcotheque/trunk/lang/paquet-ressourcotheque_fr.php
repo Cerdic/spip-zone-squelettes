905f795ea6e742e4797b98322a0c35b19ebd31b9
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-06 13:00:46 +0000

    paquet.xml minimum
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@100387 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ressourcotheque/trunk/lang/paquet-ressourcotheque_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-motus
+// Langue: fr
+// Date: 07-12-2011 17:03:35
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'ressourcotheque_description' => 'Ce squelette permet de classer et rechercher des ressources documentaires, en proposant une recherche par facette et de mettre certains documents en accès restreint',
+	'ressourcotheque_slogan' => 'Classer et retrouver des ressources documentaires',
+);
