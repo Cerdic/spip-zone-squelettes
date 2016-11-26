d4b11fb6bfd4fc7a597608e367490c9b97b88eb7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-26 18:39:58 +0000

    message si aucun résultat
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@100706 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ressourcotheque/trunk/lang/ressourcotheque_fr.php
+++ ressourcotheque/trunk/lang/ressourcotheque_fr.php
@@ -4,6 +4,7 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	'aucun_resultat' => 'Aucun résultat','aucun_resultat' => 'Aucun résultat',  
 	'explication_recherche' => 'Commençez par une recherche libre à travers le formulaire. 
 	
 	Vous pourrez ensuite filtrer les résultats par mots-clef.
