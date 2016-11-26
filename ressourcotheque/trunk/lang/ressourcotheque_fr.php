58f70bfe8c72f691d14d9a1f9bb1f061c7e4f778
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-26 18:10:55 +0000

    ne pas proposer de filtre tant qu'aucune recherche n'a été effectuée
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@100700 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ressourcotheque/trunk/lang/ressourcotheque_fr.php
+++ ressourcotheque/trunk/lang/ressourcotheque_fr.php
@@ -4,6 +4,9 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
+	'explication_recherche' => 'Commençez par une recherche libre à travers le formulaire. 
+	
+	Vous pourrez ensuite filtrer les résultats par mots-clef.
+	',
 	'types_ressources' => 'Types de ressources disponibles',
 );
