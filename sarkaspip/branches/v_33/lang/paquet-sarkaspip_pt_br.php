867aee50849d8714532374e22bf56294d1d6d833
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-17 04:33:05 +0000

    [Salvatore] [source:_squelettes_/sarkaspip/branches/v_33/lang/ paquet-sarkaspip] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@101777 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/branches/v_33/lang/paquet-sarkaspip_pt_br.php
+++ sarkaspip/branches/v_33/lang/paquet-sarkaspip_pt_br.php
@@ -12,6 +12,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'sarkaspip_description' => 'Sarka-SPIP é um template generalista que oferece toda a gama de funções básicas que permitem a visualização dos objetos editoriais do SPIP, além de alguns extras como agenda, galeria, fóruns semelhantes ao phpBB, acesso restrito (via plugin), sites favoritos... É compatível com um grande número de plugins que ele integra frequentemente em modo nativo. Esta versão é baseada no conceito do template Z.
 
-Destinado a todos os públicos, sua instalação e utilização são simples. Mesmo para os webmasters novatos, é fácil de personalizar através da sua interface integrada à área restrita do SPIP (layout, noisettes, cores, formatação das páginas...)', # RELIRE
+Destinado a todos os públicos, sua instalação e utilização são simples. Mesmo para os webmasters novatos, é fácil de personalizar através da sua interface integrada à área restrita do SPIP (layout, noisettes, cores, formatação das páginas...)',
 	'sarkaspip_slogan' => 'Template generalista e altamente configurável'
 );
