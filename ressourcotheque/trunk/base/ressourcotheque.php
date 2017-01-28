00bc8da5188181282c7ff152115f2e802a407ca3
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-28 20:55:34 +0000

    pour le cas où on n'a pas l'outil qui permet d'indexer automatiquement les documents -> utiliser un champ extra pour copier-coller le contenu
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@102529 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ressourcotheque/trunk/base/ressourcotheque.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function ressourcotheque_declarer_champs_extras($champs = array()) {
+  $champs['spip_articles']['contenu_documents'] = array(
+      'saisie' => 'textarea',//Type du champ (voir plugin Saisies)
+      'options' => array(
+            'nom' => 'contenu_documents', 
+						'label' => _T('ressourcotheque:contenu_documents'),
+						'conteneur_class'=>'pleine_largeur',
+						'explication' => _T('ressourcotheque:contenu_documents_explication'), 	
+            'sql' => "LONGTEXT NOT NULL DEFAULT ''",
+						'defaut' => '',// Valeur par défaut
+						'rechercher' => true
+      ),
+  );
+  return $champs;	
+}
--- ressourcotheque/trunk/lang/ressourcotheque_fr.php
+++ ressourcotheque/trunk/lang/ressourcotheque_fr.php
@@ -5,6 +5,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_resultat' => 'Aucun résultat','aucun_resultat' => 'Aucun résultat',  
+	'contenu_documents'=>'Contenu des documents liés', 
+	'contenu_documents_explication' => 'Mettez ici le contenu des documents liés si votre serveur ne dispose pas de la fonction d\'indexation des documents.
+	Ce contenu apparaîtra dans les recherches, mais pas sur la fiche.', 
 	'explication_recherche' => 'Commençez par une recherche libre à travers le formulaire. 
 	
 	Vous pourrez ensuite filtrer les résultats par mots-clef.
