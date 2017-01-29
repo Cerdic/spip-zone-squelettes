8a4923f783deab4b319c795a64c640bebbf503c7
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-29 21:31:32 +0000

    Pour être installable, ce plugin a aussi besoin du dépôt : http://files.spip.org/externals/archives_externals.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@102564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs_installateur/lang/paquet-soyezcreateurs_installateur_fr.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs_installateur/lang/paquet-soyezcreateurs_installateur_fr.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'soyezcreateurs_installateur_description' => 'Une fois que ce plugin a téléchargé tous les plugins utilisables avec SoyezCréateurs, il ne vous reste plus qu\'à désactiver les plugins qui ne vous serons pas utiles.',
+	'soyezcreateurs_installateur_description' => 'Une fois que ce plugin a téléchargé tous les plugins utilisables avec SoyezCréateurs, il ne vous reste plus qu\'à désactiver les plugins qui ne vous serons pas utiles.<br />Il vous faudra aussi configurer le dépôt http://files.spip.org/externals/archives_externals.xml pour installer tous les plugins',
 	'soyezcreateurs_installateur_slogan' => 'Plugin utilitaire pour télécharger les plugins facultatifs de SoyezCréateurs',
 );
 ?>
