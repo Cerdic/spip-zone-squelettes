d0796d8fd9d3ca2b92ea2caf94ed889215204e75
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-29 04:33:26 +0000

    [Salvatore] [source:_squelettes_/ressourcotheque/trunk/lang/ ressourcotheque] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@102548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ressourcotheque/trunk/lang/ressourcotheque_fr.php
+++ ressourcotheque/trunk/lang/ressourcotheque_fr.php
@@ -1,17 +1,29 @@
 <?php
-
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/ressourcotheque/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'aucun_resultat' => 'Aucun résultat','aucun_resultat' => 'Aucun résultat',  
-	'contenu_documents'=>'Contenu des documents liés', 
-	'contenu_documents_explication' => 'Mettez ici le contenu des documents liés si votre serveur ne dispose pas de la fonction d\'indexation des documents.
-	Ce contenu apparaîtra dans les recherches, mais pas sur la fiche.', 
+
+	// A
+	'aucun_resultat' => 'Aucun résultat',
+
+	// C
+	'contenu_documents' => 'Contenu des documents liés',
+	'contenu_documents_explication' => 'Mettez ici le contenu des documents liés si votre serveur ne dispose pas de la fonction d’indexation des documents.
+	Ce contenu apparaîtra dans les recherches, mais pas sur la fiche.',
+
+	// E
 	'explication_recherche' => 'Commençez par une recherche libre à travers le formulaire. 
 	
 	Vous pourrez ensuite filtrer les résultats par mots-clef.
 	',
+
+	// T
 	'types_ressources' => 'Types de ressources disponibles',
-	'zip' => 'Zip de l\'ensemble des documents joints'
+
+	// Z
+	'zip' => 'Zip de l’ensemble des documents joints'
 );
