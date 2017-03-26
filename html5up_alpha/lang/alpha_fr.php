7ed4e9ac324f64a1be6ae76b16a2d95a6470ee12
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-26 09:23:11 +0000

    Le titre sur la home peut être viré par configuration ( si l'image de home comprends le titre ) ... Il faudrait que le titre soit là par defaut
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@103545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_alpha/lang/alpha_fr.php
+++ html5up_alpha/lang/alpha_fr.php
@@ -7,14 +7,17 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'texte_config_titre_sommaire' => "Le titre du site apparait sur la page d'accueil ? ",
+
     'bouton_subscribe' => 'Sign Up',
 
+	'explication_config_titre_sommaire' => "Parfois sur la home page le titre du site fait un peu tâche sur l'image",  
     'explication_config_contenu' => "Le squelette utilise les sous titre pour les articles, n'oubliez pas de les activer dans <a href='?exec=configurer_contenu'>configurer contenu</a>",
     'explication_doc_home' => "L'id SPIP du document qui sera utilisé pour la grande image du haut.Le thème original utilise une image de taille <b>1800 × 905 pixels</b> ",
     'explication_doc_home2' => "L'id SPIP du document qui sera utilisé pour l'image <i>featured</i> . Le theme Alpha original utilise une image de taille  <b>1200 × 393 pixels</b>",
-    'explication_instagram' => "Url compte Instagram",
-    'explication_facebook' => "Url compte facebook",
-    'explication_twitter' => "Url compte twitter",
+    'explication_instagram' => "Url complète compte Instagram",
+    'explication_facebook' => "Url complète compte facebook",
+    'explication_twitter' => "Url complète compte twitter",
 
 
     'label_config_contenu' => "Sous-titre",
@@ -23,9 +26,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'label_twitter' => 'Twitter',
     'label_facebook' => 'Facebook',
     'label_instagram' => 'Instagram',
+	'label_config_titre_sommaire' => 'Titre', 
 
     'legend1' => "Explications",
-    'legend2' => "Paramétrages",
+    'legend_home' => "Paramétrages Sommaire ",
+    'legend_footer' => "Paramétrages Pied de page ",
 
 
     'titre_calltoaction' => "Newsletter",
