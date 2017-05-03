e4221061e8e1294cd2232ab5b942d9d714a14b11
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-03 14:16:59 +0000

    Chaine de langue Logo pour le plugin QQ calages
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@104145 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_alpha/lang/alpha_fr.php
+++ html5up_alpha/lang/alpha_fr.php
@@ -7,11 +7,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'texte_config_titre_sommaire' => "Le titre du site apparait sur la page d'accueil ? ",
+	// A
+	'accueil' => 'Accueil',
 
-    'bouton_subscribe' => 'Sign Up',
 
-	'explication_config_titre_sommaire' => "Parfois sur la home page le titre du site fait un peu tâche sur l'image",  
+	// B
+	'bouton_subscribe' => 'S\'abonner',
+
+	// C
+	'contact' => 'Contact',
+
+
+	// E
+	'explication_config_titre_sommaire' => "Parfois sur la home page le titre du site fait un peu tâche sur l'image",
     'explication_config_contenu' => "Le squelette utilise les sous titre pour les articles, n'oubliez pas de les activer dans <a href='?exec=configurer_contenu'>configurer contenu</a>",
     'explication_doc_home' => "L'id SPIP du document qui sera utilisé pour la grande image du haut.Le thème original utilise une image de taille <b>1800 × 905 pixels</b> ",
     'explication_doc_home2' => "L'id SPIP du document qui sera utilisé pour l'image <i>featured</i> . Le theme Alpha original utilise une image de taille  <b>1200 × 393 pixels</b>",
@@ -20,7 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'explication_facebook' => "Url complète compte facebook",
     'explication_twitter' => "Url complète compte twitter",
 
-
+	 // L
     'label_config_contenu' => "Sous-titre",
     'label_doc_home2' => "Id doc home fearured",
     'label_articles_home' => "Id article(s)",
@@ -28,15 +36,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'label_twitter' => 'Twitter',
     'label_facebook' => 'Facebook',
     'label_instagram' => 'Instagram',
-	'label_config_titre_sommaire' => 'Titre', 
-
+	'label_config_titre_sommaire' => 'Titre',
     'legend1' => "Explications",
     'legend_home' => "Paramétrages Sommaire ",
     'legend_footer' => "Paramétrages Pied de page ",
 
+	// R
+	'read_more' => 'Lire la suite',
 
-    'titre_calltoaction' => "Newsletter",
-    'texte_calltoaction' => "Pour avoir des nouvelles régulières, laisse ton mail ",
+	// T
+	'texte_config_titre_sommaire' => "Le titre du site apparait sur la page d'accueil ? ",
+	'titre_calltoaction' => "Newsletter",
+	'texte_calltoaction' => "Pour avoir des nouvelles régulières, laisse ton mail ",
 
 );
 
--- html5up_alpha/lang/paquet-alpha_fr.php
+++ html5up_alpha/lang/paquet-alpha_fr.php
@@ -7,5 +7,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
     'alpha_description' => "Ce plugin permet de reprendre le design proposé sur Html5Up .",
-    'alpha_slogan' => "L'intégration dans SPIP du design https://html5up.net/alpha "
+    'alpha_slogan' => "Thème Alpha https://html5up.net/alpha"
 );
