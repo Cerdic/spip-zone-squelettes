54b1d1ba94828bd4aaead6eedbebbabfb5ae182d
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-03 18:15:48 +0000

    orth
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@104150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_alpha/lang/alpha_fr.php
+++ html5up_alpha/lang/alpha_fr.php
@@ -24,11 +24,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_doc_home' => "L'id SPIP du document qui sera utilisé pour la grande image du haut.Le thème original utilise une image de taille <b>1800 × 905 pixels</b> ",
 	'explication_doc_home2' => "L'id SPIP du document qui sera utilisé pour l'image <i>featured</i> . Le theme Alpha original utilise une image de taille  <b>1200 × 393 pixels</b>",
 	'explication_articles_home' => "Le(s) numéro(s) d'article(s) SPIP ou de page(s) unique séparés par une virgule. Pour les mettre dans l'ordre voulu ajouter un 1. 2. dans le titre de l'article ( les articles sont triés par num titre) " ,
-	'explication_instagram' => "URL complète compte Instagram",
-	'explication_facebook' => "URL complète compte facebook",
-	'explication_twitter' => "URL complète compte twitter",
-	'explication_soundcloud' => "URL complète compte soundcloud",
-	'explication_vimeo' => "URL complète compte vimeo",
+	'explication_instagram' => "URL complète du compte Instagram",
+	'explication_facebook' => "URL complète du compte facebook",
+	'explication_twitter' => "URL complète du compte twitter",
+	'explication_soundcloud' => "URL complète du compte soundcloud",
+	'explication_vimeo' => "URL complète du compte vimeo",
 
 	 // L
 	'label_config_contenu' => "Sous-titre",
