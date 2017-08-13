056da12680b867122ac17e7d2197626372c28d5b
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-13 00:12:35 +0000

    L’obstination va t-elle payer ? ces barre qui vont à droite a gauche sont ennuyeuses ! Ne pas confondre la transmission alt=1 qui indique le sens, avec le fait l’alterner 1/2 le sens.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@105745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_solid_state/html5up_solid_state_fonctions.php
+++ html5up_solid_state/html5up_solid_state_fonctions.php
@@ -12,3 +12,24 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
+
+/**
+ * Retourne des classes css à écrire.
+ *
+ * @param int $position
+ * @param null|bool $alt
+ * @param null|bool $spot
+ */
+function html5up_sections_css($position = 1, $alt = null, $spot = null) {
+	$classes = 'style' . $position;
+	if ($spot) {
+		$classes .= ' spotlight';
+	}
+	if (is_null($alt)) {
+		$alt = ($position + 1) % 2;
+	}
+	if ($alt) {
+		$classes .= ' alt';
+	}
+	return $classes;
+}
