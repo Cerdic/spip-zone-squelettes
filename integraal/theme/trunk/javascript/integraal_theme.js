2bad531cfa3b8740eafdf1c046d6abc441b40378
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-22 09:52:23 +0000

    Afficher la grille typo avec un JS tout siple plutôt qu'avec des CSS compliquées
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@107696 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- integraal/theme/trunk/javascript/integraal_theme.js
+++ integraal/theme/trunk/javascript/integraal_theme.js
@@ -10,5 +10,15 @@
 					return false;
 				})
 		);
+		
+		// Afficher la grille typo si besoin pendant le dev
+		function debug_grille(){
+			var link = document.createElement("link");
+			link.setAttribute("rel", "stylesheet");
+			link.setAttribute("href", "http://basehold.it/"+parseInt(window.getComputedStyle(document.body).getPropertyValue("line-height"),10));
+			document.head.appendChild(link);
+		}
+		//debug_grille();
+		
 	});
 })(jQuery);
