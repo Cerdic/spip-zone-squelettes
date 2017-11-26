46896055ef3832e22877aa4017245a3fe7b85079
ako@mail.com <ako@mail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-26 04:36:39 +0000

    [Salvatore] [source:_squelettes_/zpip-vide/trunk/lang/ paquet-zvide] Export depuis http://trad.spip.net de la langue hac
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@107749 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-vide/trunk/lang/paquet-zvide_hac.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-zvide?lang_cible=hac
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zvide_description' => 'چېۋەکېش پېسەو دلېکاری گونجیا بۍ . ئی بەشە ۋەرو ئانەیە چنی پەیوەندکاری و کۆکریا و دلیکارەکا بەکارئامان  Z بەشو'
+);
