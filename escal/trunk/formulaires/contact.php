20b3b9192717195f82c88748734329391446b3ae
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-17 20:37:40 +0000

    On copie _squelettes_/escal/branches/escal_v3 to _squelettes_/escal/trunk.
    pour garder l'historique v3 > v4
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@108087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ escal/trunk/formulaires/contact.php
@@ -0,0 +1,179 @@
+<?php
+
+// Un énorme merci à Arnaud Bérard pour son aide
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_contact_charger_dist(){
+        $valeurs = array(
+            'nom'=>'',
+            'prenom'=>'',
+            'email'=>'',
+            'champsup1'=>'',
+            'champsup2'=>'',
+            'sujet'=>'',
+            'checkbox'=>'',
+            'Antispam'=>'',
+            'message'=>''
+        );
+
+        return $valeurs;
+}
+
+function formulaires_contact_verifier_dist(){
+        include_spip('inc/config');
+        include_spip('inc/filtres');
+
+        $erreurs = array();
+
+        // verifier que les champs nom et prenom sont bien la :
+        foreach(array('nom','prenom') as $obligatoire)
+                if (!_request($obligatoire)) $erreurs[$obligatoire] = _T('info_obligatoire_02'); 
+
+
+
+        // Si le champ mail est active dans la configuration de escal
+        if(lire_config('escal/config/contactmail') == 'oui'){
+            $email = _request('email');
+            // verifier si un email a ete saisi
+            if (!_request('email')) {
+                    $erreurs['email'] = _T('info_obligatoire_02');       
+            }
+            // Verifier que c'est une adresse valide
+            else if(!preg_match("#^[0-9A-Za-z]([-_.]?[0-9A-Za-z])*@[0-9A-Za-z]([-_.]?[0-9A-Za-z])*\\.[A-Za-z]{2,4}$#",$email)){
+                    $erreurs['email'] = _T('escal:contact_alerte_mail');
+            }
+        }
+
+        // Si les autres champs sont actives dans la configuration de escal
+          if (!_request('champsup1') AND lire_config('escal/config/champsup1') == 'oui' AND lire_config('escal/config/champsup1oblig') == 'oui' ) 
+              $erreurs['champsup1'] = _T('info_obligatoire_02');              
+          if (!_request('champsup2') AND lire_config('escal/config/champsup2') == 'oui' AND lire_config('escal/config/champsup2oblig') == 'oui' ) 
+              $erreurs['champsup2'] = _T('info_obligatoire_02'); 
+          if (!_request('sujet') AND lire_config('escal/config/radio') == 'oui' AND lire_config('escal/config/radiooblig') == 'oui' ) 
+              $erreurs['sujet'] = _T('info_obligatoire_02');
+          if (!_request('checkbox') AND lire_config('escal/config/checkbox') == 'oui' AND lire_config('escal/config/checkboxoblig') == 'oui' ) 
+              $erreurs['checkbox'] = _T('info_obligatoire_02');        
+
+
+        // verification du message        
+        $test_message = nocode(_request('message'));
+            // verifier si un message a ete saisi
+            if (!_request('message')) {
+                    $erreurs['message'] = _T('escal:contact_alerte_message');       
+            }
+            // Verifier que c'est un message valide
+            else if($test_message==FALSE){
+                    $erreurs['message']=_T('escal:contact_alerte_interdit').$test_message;
+            }                    
+
+        // message general si oubli ou erreur
+        if (count($erreurs))
+                $erreurs['message_erreur'] = _T('escal:contact_alerte_entete');
+
+        return $erreurs;
+}
+
+function formulaires_contact_traiter_dist(){
+
+        include_spip('inc/config');
+
+        $champs = array(
+            'nom'  => _request('nom'),
+            'prenom' => _request('prenom'),
+            'email' => _request('email'),
+            'message'=> _request('message'),
+            'champsup1'=>_request('champsup1'),
+            'champsup2'=>_request('champsup2'),
+            'sujet'=>_request('sujet'),
+            'checkbox'=>_request('checkbox'),
+            'anstispam'=>_request('Antispam'),
+        );
+
+        $nom_site = extraire_multi($GLOBALS['meta']["nom_site"]); 
+        $email_webmaster = $GLOBALS['meta']['email_webmaster'];
+        $email_to= lire_config('escal/config/destinataire').",".$email_webmaster;
+        $sujet = _T('escal:page_contact2')." ".$nom_site;
+        $email_from = $champs['email'];
+
+
+        $message =  "<strong>"._T('escal:envoi_mail_nom')."</strong>".$champs['nom']."\n\n";
+        $message .= "<strong>"._T('escal:envoi_mail_prenom')."</strong>".$champs['prenom']."\n\n";
+
+
+        if (lire_config('escal/config/champsup1') == 'oui') {
+            $message .= "<strong>"._T_ou_typo(lire_config('escal/config/titrechampsup1'))."&nbsp"."</strong>";
+            $message .= $champs['champsup1']."\n\n";
+        }
+
+        if (lire_config('escal/config/champsup2') == 'oui') {
+            $message.= "<strong>"._T_ou_typo(lire_config('escal/config/titrechampsup2'))."&nbsp"."</strong>";
+            $message.= $champs['champsup2']."\n\n";
+        }
+
+        if (lire_config('escal/config/radio') == 'oui') {
+            $message.= "<strong>"._T('escal:envoi_mail_motif')."</strong>";
+            $message.= $champs['sujet']."\n\n";
+        }
+
+        if (lire_config('escal/config/checkbox') == 'oui') {
+            $message .= "<strong>"._T_ou_typo(lire_config('escal/config/titrecheckbox'))."</strong>" ;
+            if (is_array($champs['checkbox'])) {
+              $message .= implode(" - ",$champs['checkbox'])."\n\n";
+            }
+        }
+
+        $message .= "<strong>"._T('escal:envoi_mail_message')."</strong>"."\n ".$champs['message'];
+
+      $message_html='<html> 
+            <head> 
+              <title>'.$sujet.'</title> 
+            </head> 
+            <body>
+              <div style="width:500px; margin:auto">
+                <div style="padding:10px; font-size:20px; font-weight:bold; background-color:#82ADE2">
+                  '.$sujet.'
+                </div>
+                <div style="padding:10px; background-color:#DAE6F6">
+                  '.nl2br($message).'
+                </div>
+              </div>
+            </body></html>'; 
+
+    $envoyer_mail = charger_fonction('envoyer_mail','inc');
+
+    $corps= array(
+        'from'=>$email_from,
+        'texte'=>supprimer_tags($message),
+        'html'=> $message_html,
+    	   'headers'=> array(
+             "X-Originating-IP: ".$GLOBALS['ip']
+         ),
+    );
+
+        if ($champs['antispam']=='' ){
+            if($envoyer_mail($email_to,$sujet,$corps)){
+                return array(
+                    'editable' => false,
+                    'message_ok'=>_T('escal:contact_retour_commentaire')."\n"."<strong>". _request('email')."</strong>"
+                    );
+            }else{
+                return array(
+                'message_erreur'=>_T('pass_erreur_probleme_technique')
+                );
+            }
+        }else{
+            return array('message_erreur'=>'Pas de robots ici !!');
+        }
+
+
+}
+// Verification basique d'insertion de code pour la fonction verifier
+function nocode($text){
+    if(!preg_match("/[]%~#`$&|}{^[><]/",$text))
+        return trim($text);
+    else
+        return FALSE;
+}
+
+?>
---
+++ escal/trunk/lang/paquet-escal_al.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// E
+'escal_description' => 'Të përshtatshme për versionin 3 të SPIP, ajo ofron:
+-* një konfiguracion shtyjnë në zonë private
+-* një plan urbanistik configurable në 2 ose 3 kolona duke zgjedhur një fletë stil 
+-* menaxhimin e shumëgjuhësinë
+-* style thjeshtë phpBB
+-* një përzgjedhje të gjerë të arrave për të mbajtur apo jo, disa prej të identifikimit të tepërta, menu horizontale dhe / ose vertikale.
+-* në vend të kësaj, ngjyra dhe përmbajtje të lehtë për të ndryshuar blloqe anësore
+-* një menu horizontale dhe / ose vertikale (zgjedhja 2) të bjerë me zhvillimin e temës aktuale
+-* redirection automatik të shkrimit, nëse ai është i vetëm në kolonën e tij
+-* navigacion nga fjalë kyçe
+-* një kalendar dhe / ose një listë e ngjarjeve
+-* një shfaqje e artikujt e fundit apo artikujve në të njëjtin seksion
+-* ën-kategori ekran dhe sende në çdo faqe kategori
+-* a artikuj ekranit Forumet
+-* Autorët e një të kontaktit në qoftë se ata të listuara email e tyre
+-* një kontakt të zhvilluar
+-* një sitemap
+-* një fotografi backend për faqen syndication
+-* une feuille de style sp&eacute;ciale pour l&#39;impression des articles
+-* një zonë lidhje në faqen kryesore (2 zgjedhje viewing)
+-* vendosur një XHTML valide 1.0 strikte',
+'escal_slogan' => 'Skelet 2 ose 3 kolona përbërë nga shumë lajthi',
+
+);
+?>
