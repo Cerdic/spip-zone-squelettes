c8a794667d0cc9ef3bed45587191c088c6e86ec5
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-24 02:32:13 +0000

    boulot du jour. Menu corrections, ménage et améliorations diverses
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@108176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zktx-kore/trunk/zktx_options.php
+++ zktx-kore/trunk/zktx_options.php
@@ -1,30 +1,5 @@
 <?php
-
-	// Forcer les url absolues a peu pres partout 
-	// certainement optimisable (pas sur que ca releve de typo)
-	/*
-	if (!defined('_SET_HTML_BASE')) define('_SET_HTML_BASE',true);
- 	$GLOBALS['table_des_traitements']['URL_ARTICLE'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_AUTEUR'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_RUBRIQUE'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_MOT'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_SITE'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_BREVE'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['URL_PAGE'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['CHEMIN'][]= 'typo(url_absolue(%s), "TYPO", $connect)';
-	$GLOBALS['table_des_traitements']['CSS'][]= 'typo(url_absolue(%s), "TYPO", $connect)';  
-	*/
-	// Et ca, forcement ca marche pas ...
-	//$GLOBALS['table_des_traitements']['TEXTE'][]= 'typo(abs_url(%s), "TYPO", $connect)';
-	//$GLOBALS['table_des_traitements']['CHAPO'][]= 'typo(abs_url(%s), "TYPO", $connect)';
-	//$GLOBALS['table_des_traitements']['DESCRIPTIF'][]= 'typo(abs_url(%s), "TYPO", $connect)';
-
-// connection noiZetier
-  define('_NOIZETIER_REPERTOIRE_PAGES','content/');
-	define('_NOIZETIER_LISTER_PAGES_SANS_XML',true);
-
-
-// Pousse toi d'là que j'mette mes nouveaux blocs Z !	
+// blocs Z !	
 	$GLOBALS['z_blocs'] = array(
 		'content',
 		'extra1',
@@ -38,8 +13,7 @@
 		'social' //,
 	);
 	
-// Semantique on a dit !
-	// Titre (BTE PP)
+// Titre (BTE PP)
 	$GLOBALS['debut_intertitre'] = '<h2 class="h2">';
 	$GLOBALS['fin_intertitre'] = '</h2>';
 	$GLOBALS['debut_intertitre_2'] = '<h3 class="h3">';
@@ -51,4 +25,3 @@
 	$GLOBALS['debut_intertitre_5'] = '<h6 class="h6">';
 	$GLOBALS['fin_intertitre_5'] = '</h6>';
 	$GLOBALS['BarreTypoEnrichie_Preserve_Header'] = true; 
-?>
