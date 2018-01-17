db0947d1695c0d2d31734feb004f80e835854418
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-17 17:29:14 +0000

    Compatible 3.2 d'après Perline sur contrib. On suit la norme qui veut qu'on ait un paquet.xml + un plugin.xml (même si plugin.xml suffirait).
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@108546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ leadescent/lang/paquet-squelette_leadescent_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-squelette_leadescent
+// Langue: fr
+// Date: 17-01-2018 18:26:38
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'squelette_leadescent_description' => 'Squelette avec slider, fonctionnant avec le thème du même nom.',
+	'squelette_leadescent_slogan' => 'Sites de type blog',
+);
+?>
