a545b58143641bfa7d053995fc1dbec8f85055b2
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-10 22:01:46 +0000

    Compatibilité SPIP 3.2. Up de y
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@109865 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- moodboard/lang/paquet-moodboard_en.php
+++ moodboard/lang/paquet-moodboard_en.php
@@ -1,9 +1,12 @@
 <?php
-	if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-	$GLOBALS[$GLOBALS['idx_lang']] = array(
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'moodboard_nom' => "Moodboard",
 	'moodboard_slogan' => "Pin your articles!",
 	'moodboard_description' => "Moodboard is a set of skeletons for SPIP 3.
                                 It allows you to create a moodboard design for your spip website.",
-	);
+);
 ?>
--- moodboard/moodboard_fonctions.php
+++ moodboard/moodboard_fonctions.php
@@ -1,20 +1,19 @@
 <?php
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-$GLOBALS['spip_pipeline']['insert_head'] .= "|moodboard";
-
-function moodboard_insert_head($flux){
+function moodboard_insert_head($flux) {
 	$js_jquery_wookmark = find_in_path('js/jquery.wookmark.js');
 	$flux .= "<script type='text/javascript' src='$js_jquery_wookmark'></script>\n";
+
 	return $flux;
 }
 
-$GLOBALS['spip_pipeline']['insert_head_css'] .= "|moodboard";
-
-function moodboard_insert_head_css($flux){
+function moodboard_insert_head_css($flux) {
 	$css_moodboard = find_in_path('css/moodboard.css');
 	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css_moodboard' />\n";
+
 	return $flux;
 }
-?>
