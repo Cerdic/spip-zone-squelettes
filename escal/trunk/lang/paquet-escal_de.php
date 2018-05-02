3fed915a850661c831b53d3965517b260e5b5b29
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-02 19:49:31 +0000

    Escal V4 : on prépare les fichiers pour spip-trad
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@110067 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/trunk/lang/paquet-escal_de.php
+++ escal/trunk/lang/paquet-escal_de.php
@@ -5,28 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // E
-'escal_description' => 'Adapt&eacute; pour la version 3 de SPIP, il propose :
--* une configuration pouss&eacute;e dans l\'espace priv&eacute;
--* une mise en page param&eacute;trable en 2 ou 3 colonnes par le choix d&#39;une feuille de style
--* une gestion du multilinguisme
--* un forum simple style phpBB
--* un large choix de noisettes &agrave; garder ou pas, certaines &eacute;tant redondantes : identification, menu horizontal et/ou vertical.
--* place, couleur et contenu des blocs lat&eacute;raux faciles &agrave; changer
--* un menu horizontal et/ou vertical (2 au choix) d&eacute;roulants avec mise en valeur de la rubrique courante
--* une redirection automatique vers l&#39;article s&#39;il est seul dans sa rubrique
--* une navigation par mots-cl&eacute;s
--* un calendrier et/ou une liste d&#39;&eacute;v&egrave;nements
--* un affichage des derniers articles ou des articles dans la m&ecirc;me rubrique
--* un affichage des sous-rubriques et des articles dans chaque page rubrique
--* un affichage des forums des articles
--* un formulaire de contact des auteurs s&#39;ils ont indiqu&eacute; leur e-mail
--* une page contact &eacute;labor&eacute;e
--* un plan du site
--* un fichier backend pour la syndication du site
--* une feuille de style sp&eacute;ciale pour l&#39;impression des articles
--* une zone de connexion en page d&#39;accueil (2 choix possibles d&#39;affichage)
--* un ensemble valide XHTML Strict 1.0',
-'escal_slogan' => 'Squelette 2 ou 3 colonnes compos&eacute; de nombreuses noisettes',
+
 
 );
 ?>
