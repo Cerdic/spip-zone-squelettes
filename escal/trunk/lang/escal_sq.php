cc7d76ee234b96602246c2f9ace93db9f3fbac8e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-04 03:37:22 +0000

    [Salvatore] [source:_squelettes_/escal/trunk/lang/ escal] Export depuis http://trad.spip.net
    La langue 'de' devrait Ãªtre supprimÃ©e car trop peu traduite (20.25 %)
    La langue 'eo' devrait Ãªtre supprimÃ©e car trop peu traduite (18.11 %)
    La langue 'es' devrait Ãªtre supprimÃ©e car trop peu traduite (20.38 %)
    La langue 'ja' devrait Ãªtre supprimÃ©e car trop peu traduite (20.38 %)
    La langue 'lv' devrait Ãªtre supprimÃ©e car trop peu traduite (20.00 %)
    La langue 'nl' devrait Ãªtre supprimÃ©e car trop peu traduite (20.38 %)
    La langue 'ru' devrait Ãªtre supprimÃ©e car trop peu traduite (25.03 %)
    La langue 'sq' devrait Ãªtre supprimÃ©e car trop peu traduite (20.13 %)
    La langue 'uk' devrait Ãªtre supprimÃ©e car trop peu traduite (24.91 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@110098 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/trunk/lang/escal_sq.php
+++ escal/trunk/lang/escal_sq.php
@@ -1,227 +1,212 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/escal?lang_cible=sq
+// ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// fichier de langue albanais pour le squelette Escal - Traduit par Amilcar
+	// A
+	'a_decouvrir' => 'për të zbuluar',
+	'a_telecharger' => 'A télécharger',
+	'acces_direct' => 'qasje të drejtpërdrejtë',
+	'accueil' => 'i mirëpritur ',
+	'actus' => 'lajm',
+	'adresse_non' => 'ky autor nuk ka dhënë adresën e tij të postës elektronike',
+	'agenda' => 'ditar',
+	'alerte_javascript' => 'Kjo faqe nuk mund të funksionojë si duhet pa JavaScript. Ju lutem ri',
+	'alt_telechargements' => 'Shkarkime',
+	'annuaire' => 'drejtori',
+	'annuaire_invitation' => 'Ju përdorni ESCAL?<br />Pastaj shtoni faqen tuaj në këtë faqe.',
+	'article' => 'artikull',
+	'article_archive' => 'Mos harroni!',
+	'article_dernier' => 'Artikulli i fundit:',
+	'article_forum' => 'Forumi Neni',
+	'article_imprimer' => 'shtyp',
+	'article_licence' => 'leje :',
+	'article_mise_en_ligne' => 'Neni regjistrimit online',
+	'article_modifie' => 'Rifreskimi i fundit',
+	'article_trouve' => 'Artikulli gjetur',
+	'article_une' => 'Vendos këtë artikull për një',
+	'articles' => 'mall',
+	'articles_associes' => 'Shih artikuj në lidhje me',
+	'articles_associes_mot' => 'Artikuj që lidhen me fjalën',
+	'articles_auteur' => 'Ky autor ka shkruar :',
+	'articles_autres' => 'dhe të tjera',
+	'articles_derniers' => 'Artikujt e fundit',
+	'articles_trouves' => 'artikujt gjetur',
+	'aujourdhui' => 'sot:',
+	'auteurs' => 'autorët',
+	'avec' => 'me',
+	'avec_le_squelette' => 'me shtylla kurrizore',
 
-// *************************************
-// chaines de langue pour l'espace prive
-// *************************************
+	// B
+	'bandeau' => 'rrip',
+	'bienvenue' => 'i mirëpritur',
 
-// menu de configuration
-
-
-
-// descriptifs des mots-clés
-
-
-
-// **************************************
-// chaines de langue pour l'espace public
-// **************************************
-
-// A
-'acces_direct' => 'qasje të drejtpërdrejtë',
-'accueil' => 'i mirëpritur ',
-'actus' => 'lajm',
-'adresse_non' => 'ky autor nuk ka dhënë adresën e tij të postës elektronike',
-'agenda' => 'ditar',
-'alerte_javascript' => 'Kjo faqe nuk mund të funksionojë si duhet pa JavaScript. Ju lutem ri',
-'annuaire' => 'drejtori',
-'annuaire_invitation' => 'Ju përdorni ESCAL?<br />Pastaj shtoni faqen tuaj në këtë faqe.',
-'alt_telechargements' => 'Shkarkime',
-'article' => 'artikull',
-'articles' => 'mall',
-'article_archive' => 'Mos harroni!',
-'article_forum' => 'Forumi Neni',
-'articles_associes' => 'Shih artikuj në lidhje me',
-'articles_associes_mot' => 'Artikuj që lidhen me fjalën',
-'articles_auteur' => 'Ky autor ka shkruar :',
-'articles_autres' => 'dhe të tjera',
-'article_dernier' => 'Artikulli i fundit:',
-'articles_derniers' => 'Artikujt e fundit',
-'article_imprimer' => 'shtyp',
-'article_licence' => 'leje :',
-'article_mise_en_ligne' => 'Neni regjistrimit online' ,
-'article_modifie' => 'Rifreskimi i fundit' ,
-'article_trouve' => 'Artikulli gjetur',
-'articles_trouves' => 'artikujt gjetur',
-'article_une' => 'Vendos këtë artikull për një',
-'a_decouvrir' => 'për të zbuluar',
-'a_telecharger' => 'A télécharger',
-'aujourdhui'  => 'sot:',
-'auteurs' => 'autorët',
-'avec' => 'me',
-'avec_le_squelette' => 'me shtylla kurrizore',
-
-// B
-'bandeau' => 'rrip',
-'bienvenue' => 'i mirëpritur',
-
-// C
-'calendrier' => 'kalendar',
-'clic_telechargement' => 'Klikoni për të parë apo të pa',
-'collegues' => 'Të gjithë kolegët',
-'commentaires' => 'komente',
-'comments_derniers' => 'Latest',
-'contact' => 'kontakt',
-'contact_bienvenue' => 'Për referencë teknike, 
+	// C
+	'calendrier' => 'kalendar',
+	'clic_telechargement' => 'Klikoni për të parë apo të pa',
+	'collegues' => 'Të gjithë kolegët',
+	'commentaires' => 'komente',
+	'comments_derniers' => 'Latest',
+	'contact' => 'kontakt',
+	'contact_alerte_domaine' => 'Emri i domain është i pavlefshëm',
+	'contact_alerte_entete' => 'input juaj përmban lëshime apo gabime!',
+	'contact_alerte_interdit' => ' Mesazhi juaj përmban karaktere të paligjshme',
+	'contact_alerte_mail' => 'Adresa juaj e emailit nuk është e saktë.',
+	'contact_alerte_message' => ' Ju nuk keni dialed mesazh! Distracted (e)?',
+	'contact_bienvenue' => 'Për referencë teknike, 
                           <br />ju lutemi plotësoni të gjitha fushat në këtë formë.',
-'contact_nom' => 'Emri juaj:',
-'contact_prenom' => 'Emri:',
-'contact_mail' => 'e-mail juaj:',
-'contact_motif' => 'Arsyeja për mesazhin tuaj:',
-'contact_motif1' => 'informacion',
-'contact_motif2' => 'regjistrim',
-'contact_motif3' => 'problem teknik',
-'contact_motif4' => 'Mendimi juaj në faqen e internetit',
-'contact_motif5' => 'tjetër',
-'contact_message' => 'Mesazhi juaj:',
-'contact_interdit' => 'karaktere të ndaluara:',
-'contact_envoyer' => 'dërgoj',
-'contact_alerte_entete' => 'input juaj përmban lëshime apo gabime!',
-'contact_alerte_mail' => 'Adresa juaj e emailit nuk është e saktë.',
-'contact_alerte_domaine' => 'Emri i domain është i pavlefshëm',
-'contact_alerte_message' => ' Ju nuk keni dialed mesazh! Distracted (e)?',
-'contact_alerte_interdit' => ' Mesazhi juaj përmban karaktere të paligjshme',
-'contact_retour_commentaire' => 'Mesazhi juaj u dërgua me webmaster
+	'contact_envoyer' => 'dërgoj',
+	'contact_interdit' => 'karaktere të ndaluara:',
+	'contact_mail' => 'e-mail juaj:',
+	'contact_message' => 'Mesazhi juaj:',
+	'contact_motif' => 'Arsyeja për mesazhin tuaj:',
+	'contact_motif1' => 'informacion',
+	'contact_motif2' => 'regjistrim',
+	'contact_motif3' => 'problem teknik',
+	'contact_motif4' => 'Mendimi juaj në faqen e internetit',
+	'contact_motif5' => 'tjetër',
+	'contact_nom' => 'Emri juaj:',
+	'contact_prenom' => 'Emri:',
+	'contact_retour_commentaire' => 'Mesazhi juaj u dërgua me webmaster
 								<br/>i cili do të përgjigjet sa më shpejt të jetë e mundur në:',
-'contenu_site' => 'Kjo faqe përfshijnë: ',
-'copyright' => 'Të gjitha të drejtat e rezervuara',
+	'contenu_site' => 'Kjo faqe përfshijnë: ',
+	'copyright' => 'Të gjitha të drejtat e rezervuara',
 
-// D
-'dans_site' => 'Kjo faqe',
-// E
-'en_ligne' => 'Aktualisht Online:',
-'envoi_mail_nom' => 'Emri:',
-'envoi_mail_prenom' => 'Emri i parë:',
-'envoi_mail_motif' => 'Arsyeja:',
-'envoi_mail_message' => 'mesazh:',
-'envoyer_message' => 'Dërgo një mesazh',
-'erreur404' => 'gabim 404',
-'erreur404_message' => 'Faqja që kërkohet nuk ekziston ose më shumë në vend.
+	// D
+	'dans_site' => 'Kjo faqe',
+
+	// E
+	'en_ligne' => 'Aktualisht Online:',
+	'envoi_mail_message' => 'mesazh:',
+	'envoi_mail_motif' => 'Arsyeja:',
+	'envoi_mail_nom' => 'Emri:',
+	'envoi_mail_prenom' => 'Emri i parë:',
+	'envoyer_message' => 'Dërgo një mesazh',
+	'erreur404' => 'gabim 404',
+	'erreur404_message' => 'Faqja që kërkohet nuk ekziston ose më shumë në vend.
 						Nëse ju mendoni se ka një gabim në faqen e internetit,
 						ju mund të raportoni problemin që webmasteri duke përdorur [Kontaktoni | faqen e kontaktit -> @ url @] 
 						e kësaj faqe interneti.',
-'espace_redacteurs' => 'redaktorët zonë ',
-'escal' => 'Escal',
-'et' => 'dhe',
-'evenements' => 'Ngjarje të ardhshme ',
-'evenements_ajouter' => 'Shto ngjarje<br />(qasje të kufizuar)',
-'evenements_non' => 'Ka evenimente te ardhshme ',
+	'escal' => 'Escal',
+	'espace_redacteurs' => 'redaktorët zonë ',
+	'et' => 'dhe',
+	'evenements' => 'Ngjarje të ardhshme ',
+	'evenements_ajouter' => 'Shto ngjarje<br />(qasje të kufizuar)',
+	'evenements_non' => 'Ka evenimente te ardhshme ',
 
-// F
-'fleche' => 'shigjetë ',
-'fois' => 'kohë ',
-'form_pet_envoi_mail_confirmation' => 'Një email konfirmimi është dërguar webmasteri i faqes, në mënyrë që të vërtetoni regjistrimin tuaj. ',
-'format' => 'formati:',
-'forum' => 'Forum',
-'forum_trouve' => 'esazhi forumi gjetur',
-'forums_trouves' => 'mesazhi forum gjetur',
+	// F
+	'fleche' => 'shigjetë ',
+	'fois' => 'kohë ',
+	'form_pet_envoi_mail_confirmation' => 'Një email konfirmimi është dërguar webmasteri i faqes, në mënyrë që të vërtetoni regjistrimin tuaj. ',
+	'format' => 'formati:',
+	'forum' => 'Forum',
+	'forum_trouve' => 'esazhi forumi gjetur',
+	'forums_trouves' => 'mesazhi forum gjetur',
 
-// G
-'gros_jour' => 'ditë e më e madhe',
+	// G
+	'gros_jour' => 'ditë e më e madhe',
 
-// H
-'haut_page' => 'më i lartë',
-'hebergeur' => 'host:',
+	// H
+	'haut_page' => 'më i lartë',
+	'hebergeur' => 'host:',
 
-// I
-'identification' => 'identifikim',
+	// I
+	'identification' => 'identifikim',
 
-// J
-'jours' => 'ditë',
+	// J
+	'jours' => 'ditë',
 
-// L
-'le' => 'le',
-'lire_article' => 'Lexoni artikullin ...',
-'lire_suite' => 'Lexo më shumë ...',
+	// L
+	'le' => 'le',
+	'lire_article' => 'Lexoni artikullin ...',
+	'lire_suite' => 'Lexo më shumë ...',
 
-// M
-'menu' => 'Në menynë',
-'mentions' => 'ngulit',
-'message_non' => '(Nuk ka mesazh për momentin)',
-'meteo_titre_conditions' => 'kushtet aktuale',
-'mots_cles' => 'Keyword',
-'mots_clefs' => 'Keyword',
-'mots_clefs_associes' => 'Keywords ngjashme',
-'moyenne_visites' => 'mesatarisht që nga viti',
+	// M
+	'mentions' => 'ngulit',
+	'menu' => 'Në menynë',
+	'message_non' => '(Nuk ka mesazh për momentin)',
+	'meteo_titre_conditions' => 'kushtet aktuale',
+	'mots_clefs' => 'Keyword',
+	'mots_clefs_associes' => 'Keywords ngjashme',
+	'mots_cles' => 'Keyword',
+	'moyenne_visites' => 'mesatarisht që nga viti',
 
-// P
-'pages' => 'Faqet',
-'page_auteur' => 'Page Author' ,
-'page_contact' => 'Faqe kontakt' ,
-'page_contact2' => 'Page Kontakt' ,
-'pages_vues' => 'Numri i faqeve të vizituara:',
-'photos_hasard' => 'Disa fotografi<br />në mënyrë të rastësishme',
-'plan_site' => 'sitemap',
-'poster_message' => 'Posto një mesazh të ri',
-'premier_visiteur' => 'Ju jeni të parët!',
-'publie' => 'botuar',
-'puce' => 'patate të skuqura',
+	// P
+	'page_auteur' => 'Page Author',
+	'page_contact' => 'Faqe kontakt',
+	'page_contact2' => 'Page Kontakt',
+	'pages' => 'Faqet',
+	'pages_vues' => 'Numri i faqeve të vizituara:',
+	'photos_hasard' => 'Disa fotografi<br />në mënyrë të rastësishme',
+	'plan_site' => 'sitemap',
+	'poster_message' => 'Posto një mesazh të ri',
+	'premier_visiteur' => 'Ju jeni të parët!',
+	'publie' => 'botuar',
+	'puce' => 'patate të skuqura',
 
-// R
-'rappel_discussion' => 'Reminder e diskutimit',
-'recherche' => 'kërkim',
-'recherche_multi' => 'Kërko<br />multi-kriteret',
-'recherche_multi_resultat' => 'Rezultati i kërkimit:',
-'rechercher' => 'Kërko ...',
-'recherche_articles_non' => 'Nuk ka artikuj gjetur për këtë kërkim',
-'recherche_forum_non' => 'Asnjë mesazh i forumit gjetur për këtë kërkim',
-'recherche_resultat' => 'Rezultatet e kerkimit per',
-'recherche_rubriques_non' => 'Nuk ka gjetur për këtë kërkim seksion',
-'recherche_sites_non' => 'Asnjë faqe gjeti për këtë kërkim',
-'repondre_message' => 'Përgjigje për këtë post',
-'rubriques' => 'seksione',
-'rubrique_articles' => 'Artikujt në këtë seksion',
-'rubrique_contenu' => 'Ky seksion përmban',
-'rubrique_sites_references' => 'Faqet e përmendura në këtë seksion',
-'rubrique_sous_rubriques' => 'Nën-seksionet e këtij seksioni',
-'rubrique_trouvee' => 'tema',
-'rubriques_trouvees' => 'tema',
+	// R
+	'rappel_discussion' => 'Reminder e diskutimit',
+	'recherche' => 'kërkim',
+	'recherche_articles_non' => 'Nuk ka artikuj gjetur për këtë kërkim',
+	'recherche_forum_non' => 'Asnjë mesazh i forumit gjetur për këtë kërkim',
+	'recherche_multi' => 'Kërko<br />multi-kriteret',
+	'recherche_multi_resultat' => 'Rezultati i kërkimit:',
+	'recherche_resultat' => 'Rezultatet e kerkimit per',
+	'recherche_rubriques_non' => 'Nuk ka gjetur për këtë kërkim seksion',
+	'recherche_sites_non' => 'Asnjë faqe gjeti për këtë kërkim',
+	'rechercher' => 'Kërko ...',
+	'repondre_message' => 'Përgjigje për këtë post',
+	'rubrique_articles' => 'Artikujt në këtë seksion',
+	'rubrique_contenu' => 'Ky seksion përmban',
+	'rubrique_sites_references' => 'Faqet e përmendura në këtë seksion',
+	'rubrique_sous_rubriques' => 'Nën-seksionet e këtij seksioni',
+	'rubrique_trouvee' => 'tema',
+	'rubriques' => 'seksione',
+	'rubriques_trouvees' => 'tema',
 
-// S
-'sites' => 'Sites',
-'sites_favoris' => 'Faqe të preferuar',
-'site_realise_avec' => 'Site krijuar me',
-'site_reference' => 'faqe referohet',
-'sites_references' => 'faqet e cekura',
-'sous_rubrique' => 'titull',
-'sous_rubriques' => 'nën-kategori',
-'statistiques' => 'Statistikat e Faqes',
-'sujets' => 'tema',
-'sur_forum' => 'Në forum është:',
-'sur_web' => 'Në web',
+	// S
+	'site_realise_avec' => 'Site krijuar me',
+	'site_reference' => 'faqe referohet',
+	'sites' => 'Sites',
+	'sites_favoris' => 'Faqe të preferuar',
+	'sites_references' => 'faqet e cekura',
+	'sous_rubrique' => 'titull',
+	'sous_rubriques' => 'nën-kategori',
+	'statistiques' => 'Statistikat e Faqes',
+	'sujets' => 'tema',
+	'sur_forum' => 'Në forum është:',
+	'sur_web' => 'Në web',
 
-// T
-'taille_augmenter' => 'Rritja e madhësinë e shkronjave',
-'taille_diminuer' => 'Të ulur madhësinë e shkronjave',
-'taille_fichier' => 'Madhësia e dokumentit:',
-'telechargement' => 'Files për të shkarkuar:',
-'telechargements' => 'Files për të shkarkuar:',
-'title_articles_derniers' => 'Shkrimet e fundit ',
-'title_contact' => 'Kontakt ekspert teknik ',
-'title_espace_redacteurs' => 'Zona private të autorëve dhe administratorëve ',
-'title_haut_page' => 'shkoni krye të faqes ',
-'title_escal' => 'Link në faqen zyrtare Escal ',
-'title_mentions' => 'Site Imprint ',
-'title_w3c' => 'Site XHTML valide 1.0 Strict ',
-'top_articles' => 'opular Products ',
-'total_visites' => 'Numri total i vizitave:',
-'traductions_article' => 'Përkthime të këtij neni:',
+	// T
+	'taille_augmenter' => 'Rritja e madhësinë e shkronjave',
+	'taille_diminuer' => 'Të ulur madhësinë e shkronjave',
+	'taille_fichier' => 'Madhësia e dokumentit:',
+	'telechargement' => 'Files për të shkarkuar:',
+	'telechargements' => 'Files për të shkarkuar:',
+	'title_articles_derniers' => 'Shkrimet e fundit ',
+	'title_contact' => 'Kontakt ekspert teknik ',
+	'title_escal' => 'Link në faqen zyrtare Escal ',
+	'title_espace_redacteurs' => 'Zona private të autorëve dhe administratorëve ',
+	'title_haut_page' => 'shkoni krye të faqes ',
+	'title_mentions' => 'Site Imprint ',
+	'title_w3c' => 'Site XHTML valide 1.0 Strict ',
+	'top_articles' => 'opular Products ',
+	'total_visites' => 'Numri total i vizitave:',
+	'traductions_article' => 'Përkthime të këtij neni:',
 
-// V
-// 'video' => 'Vidéo',
-'visites_jour' => 'vizita në ditë',
-'visiteur' => 'vizitorët',
-'visiteurs' => 'vizitorët',
-'votre_nom' => 'Emri juaj?',
-'vu' => 'parë',
+	// V
+	'visites_jour' => 'vizita në ditë',
+	'visiteur' => 'vizitorët',
+	'visiteurs' => 'vizitorët',
+	'votre_nom' => 'Emri juaj?',
+	'vu' => 'parë',
 
-// W
-'webmestre' => 'Webmaster',
-
+	// W
+	'webmestre' => 'Webmaster'
 );
-?>
