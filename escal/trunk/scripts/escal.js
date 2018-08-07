917ba543e911c85a458531e9c37fefd3b30b1fff
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-07 15:04:56 +0000

    EscalV4 : oops, oubli d'un fichier
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@111277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ escal/trunk/scripts/escal.js
@@ -0,0 +1,13 @@
+<!-- flèche de retour-haut -->
+
+jQuery(function(){
+	$(function () {
+		$(window).scroll(function () { //Fonction appelée quand on descend la page
+			if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
+				$('#scrollHaut').css('right','10px'); // Replace à 10pixels de la droite l'image
+			} else { 
+				$('#scrollHaut').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+			}
+		});
+	});
+});
