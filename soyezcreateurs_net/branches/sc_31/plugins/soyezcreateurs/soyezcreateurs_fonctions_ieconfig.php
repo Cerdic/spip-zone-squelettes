4ab86dcdb30a7405bcc66fa3315cd3ab87caadc1
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-08 12:42:47 +0000

    Tant pis pour l'optimisation (cf https://core.spip.net/issues/4164) Backport de l'utilisation de ieconfig
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@111281 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/branches/sc_31/plugins/soyezcreateurs/soyezcreateurs_fonctions_ieconfig.php
+++ soyezcreateurs_net/branches/sc_31/plugins/soyezcreateurs/soyezcreateurs_fonctions_ieconfig.php
@@ -232,14 +232,144 @@ function form_export() {
 /*Type d'option : 
 /*	- 'rien' : Ne rien faire
 /*	- 'ecrasement' : Écraser complètement la configuraion actuel, les paramètres non renseigné dans le fichier d'importation sont remis à default
-/*	- 'fusion' : Fusionner la configuration actuelle avec le fichier d'importation, les paramètres non renseignés dans le fichier d'importation sont conserver
+/*	- 'fusion' : Fusionner la configuration actuelle avec le fichier d'importation, les paramètres non renseignés dans le fichier d'importation sont conservés
 /*	- 'fusion_inv' : Fusionner la configuration actuelle avec le fichier d'importation, les paramètres renseignés dans le fichier d'importation sont ignorés s'il sont présent dans la configuration actuelle
 */
 function sc_ieconfig_importer_fichier($chemin,$option) {
 	include_spip('inc/yaml');
 	$config = yaml_decode_file($chemin);
-	$config = $config['soyezcreateurs'];
+	
+	// Gestion des plugins utilisant le pipeline ieconfig_metas
+	foreach (pipeline('ieconfig_metas', array()) as $prefixe => $data) {
+		//Si on veut une importation avec écrasement
+		if (isset($config[$prefixe])) {
+			//Si on veut une importation avec écrasement
+			if ($option === 'ecrase') {
+				
+				if (isset($data['metas_brutes'])) {
+					foreach (explode(',', $data['metas_brutes']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									ecrire_config($m . '/', $v);
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							ecrire_config($meta . '/', $config[$prefixe][$meta]);
+						}
+					}
+				}
+				if (isset($data['metas_serialize'])) {
+					foreach (explode(',', $data['metas_serialize']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									ecrire_config($m . '/', serialize($v));
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							ecrire_config($meta . '/', serialize($config[$prefixe][$meta]));
+						}
+					}
+				}
+			}
+			//Si on veut une importation avec fusion
+			if ($option === 'fusion') {
+				
+				if (isset($data['metas_brutes'])) {
+					foreach (explode(',', $data['metas_brutes']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									$sc = lire_config($m . '/', $v);
+									$v = array_merge($sc,$v);
+									ecrire_config($m . '/', $v);
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							$sc = lire_config($meta . '/', $config[$prefixe][$meta]);
+							$config[$prefixe][$meta] = array_merge($sc,$config[$prefixe][$meta]);
+							ecrire_config($meta . '/', $config[$prefixe][$meta]);
+						}
+					}
+				}
+				if (isset($data['metas_serialize'])) {
+					foreach (explode(',', $data['metas_serialize']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									$sc = lire_config($m . '/', serialize($v));
+									$v = array_merge($sc,$v);
+									ecrire_config($m . '/', serialize($v));
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							$sc = lire_config($meta . '/', serialize($config[$prefixe][$meta]));
+							$config[$prefixe][$meta] = array_merge($sc,$config[$prefixe][$meta]);
+							ecrire_config($meta . '/', serialize($config[$prefixe][$meta]));
+						}
+					}
+				}
+			}
+			//Si on veut une importation avec fusion_inv
+			if ($option === 'fusion_inv') {
+				if (isset($data['metas_brutes'])) {
+					foreach (explode(',', $data['metas_brutes']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									$sc = lire_config($m . '/', $v);
+									$v = array_merge($v,$sc);
+									ecrire_config($m . '/', $v);
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							$sc = lire_config($meta . '/', $config[$prefixe][$meta]);
+							$config[$prefixe][$meta] = array_merge($config[$prefixe][$meta],$sc);
+							ecrire_config($meta . '/', $config[$prefixe][$meta]);
+						}
+					}
+				}
+				if (isset($data['metas_serialize'])) {
+					foreach (explode(',', $data['metas_serialize']) as $meta) {
+						// On teste le cas ou un prefixe est indique (dernier caractere est *)
+						if (substr($meta, -1) == '*') {
+							$p = substr($meta, 0, -1);
+							foreach ($config[$prefixe] as $m => $v) {
+								if (substr($m, 0, strlen($p)) == $p) {
+									$sc = lire_config($m . '/', serialize($v));
+									$v = array_merge($v,$sc);
+									ecrire_config($m . '/', serialize($v));
+								}
+							}
+						} elseif (isset($config[$prefixe][$meta])) {
+							$sc = lire_config($meta . '/', serialize($config[$prefixe][$meta]));
+							$config[$prefixe][$meta] = array_merge($config[$prefixe][$meta],$sc);
+							ecrire_config($meta . '/', serialize($config[$prefixe][$meta]));
+						}
+					}
+				}
+			}
+		}
+	}		
+	if ($message_erreur != '') {
+		return array('message_erreur' => $message_erreur);
+	} else {
+		return array('message_ok' => _T('ieconfig:message_ok_import'));
+	}
+	
+	
 	//On appelle la fonction d'importation des configurations
-	soyezcreateurs_importer_configuration($option,$option,$option,$option,$config);
+	return soyezcreateurs_importer_configuration($option,$option,$option,$option,$config);
 }
 
