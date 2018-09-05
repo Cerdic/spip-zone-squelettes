6b6ffc4075ca91d4a507c6cfdbb9ca6daed808ca
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-09-05 14:30:19 +0000

    Pouvoir déclarer une rubrique (et sa branche) comme Blog : affiche les articles de la branche de manière anti-chronologique
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@111491 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/soyezcreateurs_administrations.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/soyezcreateurs_administrations.php
@@ -268,10 +268,16 @@ function soyezcreateurs_upgrade($nom_meta_base_version,$version_cible){
 		if (version_compare($current_version,'3.1.53','<')) {
 			spip_log("SoyezCreateurs maj 3.1.53 Début", 'soyezcreateurs_install'._LOG_INFO_IMPORTANTE);
 			sc_noisettespardefaut();
+			ecrire_meta($nom_meta_base_version,$current_version='3.1.53','non');
+			
+		}
+		if (version_compare($current_version,'3.1.54','<')) {
+			spip_log("SoyezCreateurs maj 3.1.54 Début", 'soyezcreateurs_install'._LOG_INFO_IMPORTANTE);
+			$id_mot = create_mot("_Specialisation_Rubrique", "Blog", "Affecter ce mot clef à une rubrique pour que sa branche se comporte comme un blog.", "Avec ce mot clef : affichage des articles par ordre anti-chronologique pour tous les articles de la branche (et non de la rubrique en cours seulement).\n\nHéritage de la propriété pour les sous-rubriques.");
 			// On termine en invalidant les caches
 			include_spip('inc/invalideur');
 			suivre_invalideur("soyezcreateurs");
-			ecrire_meta($nom_meta_base_version,$current_version='3.1.53','non');
+			ecrire_meta($nom_meta_base_version,$current_version='3.1.54','non');
 			
 		}
 		
