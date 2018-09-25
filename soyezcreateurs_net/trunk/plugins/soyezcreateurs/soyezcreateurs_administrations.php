abe585bfbdb4219d7334a784d72e0df0592a1ce6
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-09-25 10:53:33 +0000

    [SoyezCréateurs] Pouvoir initialiser manuellement les noizettes du mode Cognac
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@111666 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/soyezcreateurs_administrations.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/soyezcreateurs_administrations.php
@@ -267,6 +267,7 @@ function soyezcreateurs_upgrade($nom_meta_base_version,$version_cible){
 		}
 		if (version_compare($current_version,'3.1.53','<')) {
 			spip_log("SoyezCreateurs maj 3.1.53 Début", 'soyezcreateurs_install'._LOG_INFO_IMPORTANTE);
+			include_spip('inc/soyezcreateurs_noizetier_config');
 			sc_noisettespardefaut();
 			ecrire_meta($nom_meta_base_version,$current_version='3.1.53','non');
 			
@@ -329,29 +330,4 @@ function soyezcreateurs_vider_tables($nom_meta_base_version) {
 	effacer_meta('soyezcreateurs_google');
 	effacer_meta('soyezcreateurs_layout');
 	effacer_meta($nom_meta_base_version);
-}
-
-function sc_noisettespardefaut() {
-	if (!defined('_DIR_PLUGIN_NOIZETIER')) {
-		return;
-	}
-	include_spip('inc/noizetier_conteneur');
-	include_spip('inc/config');
-	include_spip('inc/ncore_type_noisette');
-	include_spip('inc/ncore_noisette');
-	include_spip('ncore/ncore');
-	include_spip('ncore/noizetier');
-	$conteneur = array();
-	//Liste tous les noisettes de tous les conteneurs existant
-	$liste_noisettes = noizetier_noisette_lister('noizetier', $conteneur, '', $cle = 'rang_noisette');
-	if (!isset($liste_noisettes['contenu/corps_sommaire_modecognac'])) {//S'il n'y a pas de noisettes dans contenu/corps
-		//Liste des noisettes à inserer
-		$noisettes = array('sommaire_modeportailcognac_zoomsur', 'sommaire_modeportailcognac_cycloshow', 'sommaire_modeportailcognac_sc_edito', 'sommaire_modeportailcognac_quoideneuf', 'sommaire_modeportailcognac_dessousBreves', 'sommaire_modeportailcognac_goodies');
-		$conteneur = noizetier_conteneur_decomposer('contenu/corps_sommaire_modecognac');
-
-		//Insersion des noisettes
-		foreach ($noisettes as $noisette) {
-			$id_noisette = noisette_ajouter('noizetier', $noisette, $conteneur);
-		}
-	}
 }
