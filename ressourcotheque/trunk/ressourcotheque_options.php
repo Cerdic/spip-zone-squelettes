79554673a7087cac013b3792d61270f26cd67b6b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-08 10:55:14 +0000

    utiliser le méchanisme de spip pour les surlignages de recherche
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@112364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ressourcotheque/trunk/ressourcotheque_options.php
+++ ressourcotheque/trunk/ressourcotheque_options.php
@@ -8,3 +8,7 @@ define ('_INDEXERDOC_OBJETS_LIES',1);
 $GLOBALS['a2a_types_liaisons'] = array(
 	'connexe' => _T('ressourcotheque:connexe')
 );
+
+if (isset($_REQUEST['recherche'])) {
+  $_GET['var_recherche'] = $_REQUEST['recherche'];
+}
