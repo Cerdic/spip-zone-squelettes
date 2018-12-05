aceb012dab6b222524a52d3796cd6cd579b7c36a
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-05 21:50:40 +0000

    Une branche pour la v2
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@112638 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-vide/branches/v2/lang/paquet-zvide_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-zvide?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zvide_description' => 'Un esqueleto Z cuyo  conjunto de contenidos son gestionados con las  noisettes que se pueden configurar con el noiZetier. Este esqueleto se utiliza pues conjuntamente con el noiZetier y  una colección de noisettes.',
+	'zvide_slogan' => 'Esqueleto de Z vacio de contenido a rellenar con  noisettes (dejo la palabra original del francés  noisettes)'
+);
