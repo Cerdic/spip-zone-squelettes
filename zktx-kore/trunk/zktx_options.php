1840607e2826fa45f43006793554c31a5f899066
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-08 17:48:33 +0000

    bugs fixes, ménage, stabilisation, esthétique ... à poursuivre
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@112678 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zktx-kore/trunk/zktx_options.php
+++ zktx-kore/trunk/zktx_options.php
@@ -1,28 +1,8 @@
 <?php
 // blocs Z !	
 	$GLOBALS['z_blocs'] = array(
-		'content',
-		'head',
-		'head_js',
-		'header',
-		'extra1',
-		'extra2',
-		'footer',
-		'foot_js' //,
+		'content','head','head_js','header','extra1','extra2','footer','foot_js'
 	);
 
 	// activer le chargement parallele sur les blocs contenu et more
-	define('_Z_AJAX_PARALLEL_LOAD','extra1,extra2,footer');
-	
-// Titre (BTE PP)
-	$GLOBALS['debut_intertitre'] = '<h2 class=\"h2\">';
-	$GLOBALS['fin_intertitre'] = '</h2>';
-	$GLOBALS['debut_intertitre_2'] = '<h3 class=\"h3\">';
-	$GLOBALS['fin_intertitre_2'] = '</h3>';
-	$GLOBALS['debut_intertitre_3'] = '<h4 class=\"h4\">';
-	$GLOBALS['fin_intertitre_3'] = '</h4>';
-	$GLOBALS['debut_intertitre_4'] = '<h5 class=\"h5\">';
-	$GLOBALS['fin_intertitre_4'] = '</h5>';
-	$GLOBALS['debut_intertitre_5'] = '<h6 class=\"h6\">';
-	$GLOBALS['fin_intertitre_5'] = '</h6>';
-	$GLOBALS['BarreTypoEnrichie_Preserve_Header'] = true; 
+	define('_Z_AJAX_PARALLEL_LOAD','extra1,extra2,footer,foot_js');
