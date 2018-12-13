2c4b656d7595ffb14008a23621541c8bd2b56c0a
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-13 21:05:21 +0000

    EscalV4 :  correction css page evenement + affichage de la version d'Escal dans la page de configuration
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@112788 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/trunk/lang/escal_fr.php
+++ escal/trunk/lang/escal_fr.php
@@ -996,6 +996,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'une_bloc' => 'Le bloc "A la une"',
 
 	// V
+	'version' => 'Version actuelle :',
 	'video' => 'Vidéo',
 	'videos' => 'Vidéos',
 	'visites' => 'Visites',
