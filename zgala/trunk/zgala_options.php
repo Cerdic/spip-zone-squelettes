48f364e7b2e90dc672014470ff39d30301448b6c
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-30 18:26:39 +0000

    Mise au point des noisettes navbar et breadcrumb: preview améliorée et contexte limité au strict nécesaire. La noisette bienvenue devient spécifique à la page sommaire
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@113167 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zgala/trunk/zgala_options.php
+++ zgala/trunk/zgala_options.php
@@ -1,5 +1,4 @@
 <?php
-
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -7,7 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 // Définition des blocs Z utilisés par le squelette
 $GLOBALS['z_blocs'] = array('content', 'breadcrumb', 'nav', 'aside', 'extra', 'head', 'head_js', 'header', 'footer');
 
+// Forcer les pages Z à être décrites par un YAML ou un XML pour être détectée.
+define('_NOIZETIER_LISTER_PAGES_SANS_XML', false);
+
 // Forcer, lors de l'affichage des thèmes par Zen-Garden, le filtrage exclusif des thèmes compatibles avec Zgala
 define('_ZENGARDEN_FILTRE_THEMES', 'zgala');
-
-define('_NOIZETIER_LISTER_PAGES_SANS_XML', false);
