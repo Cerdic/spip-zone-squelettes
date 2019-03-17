3827423a015dee78c2cd434e9149597a58b655b6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-17 04:39:38 +0000

    [Salvatore] [source:_squelettes_/escal/trunk/lang/ escal] Export depuis http://trad.spip.net
    La langue 'al' devrait Ãªtre supprimÃ©e car trop peu traduite (0.86 %)
    La langue 'de' devrait Ãªtre supprimÃ©e car trop peu traduite (20.74 %)
    La langue 'eo' devrait Ãªtre supprimÃ©e car trop peu traduite (18.53 %)
    La langue 'es' devrait Ãªtre supprimÃ©e car trop peu traduite (20.74 %)
    La langue 'ja' devrait Ãªtre supprimÃ©e car trop peu traduite (20.74 %)
    La langue 'lv' devrait Ãªtre supprimÃ©e car trop peu traduite (20.37 %)
    La langue 'nl' devrait Ãªtre supprimÃ©e car trop peu traduite (20.74 %)
    La langue 'ru' devrait Ãªtre supprimÃ©e car trop peu traduite (25.28 %)
    La langue 'sq' devrait Ãªtre supprimÃ©e car trop peu traduite (20.49 %)
    La langue 'uk' devrait Ãªtre supprimÃ©e car trop peu traduite (25.15 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@114538 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- escal/trunk/lang/escal_al.php
+++ escal/trunk/lang/escal_al.php
@@ -13,5 +13,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'fonds_textes_comment' => '(en cas de champ non rempli par exemple)', # MODIF
 
 	// P
+	'pied_lien_mentions' => 'Lien vers la page des mentions légales', # MODIF
+	'pied_lien_mentions_article' => 'Numéro de l’article choisi', # MODIF
+	'pied_lien_mentions_choix' => 'Choix de la page à afficher', # MODIF
+	'pied_lien_mentions_choix1' => 'Celle du plugin "Mentions légales" si vous l’avez activé', # MODIF
+	'pied_lien_mentions_choix2' => 'Un article de votre choix', # MODIF
 	'pied_lien_puce' => 'Afficher la dernière puce (choisir non si aucun des liens ci-dessus ou si vous les affichez sous forme d’icones)' # MODIF
 );
