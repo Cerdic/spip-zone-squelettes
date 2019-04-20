96a0bbcb5fdf07a5744784b0ce3b825b1b415765
jack@jack31.net <jack@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-20 09:07:24 +0000

    Fichiers de lang en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115030 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ html5up_alpha/lang/alpha_en.php
@@ -0,0 +1,59 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil' => 'Home',
+	'alpha_titre' => 'Configure Alpha',
+
+	// B
+	'bouton_subscribe' => 'Subscribe',
+
+	// C
+	'contact' => 'Contact',
+
+	// E
+    'explication_articles_home' => "The sommaire page displays the first level topics. You may need to add one or more articles to this list of topics. Enter the SPIP article number(s) or unique page(s) number(s) separated by a comma. To put them in the desired order add a 1. 2. in the title of the article (the articles are sorted by title number)." ,
+    'explication_articles_home_apres' => "The sommaire page displays the first level topics. You may need to add one or more articles AFTER this list of topics. Enter the SPIP article number(s) or unique page(s) number(s) separated by a comma. To put them in the desired order add a 1. 2. in the title of the article (the articles are sorted by title number)." ,
+	'explication_config_contenu' => "The template uses the subtitles for the articles, don't forget to activate them in <a href='?exec=configurer_contenu'>configure content</a>",
+	'explication_config_titre_sommaire' => "Sometimes on the home page the title of the site doesn't look good on the image",
+	'explication_doc_home' => "The SPIP id of the document that will be used for the large top image. The original theme uses an image size <b>1800 × 905 pixels</b> ",
+	'explication_doc_home2' => "The SPIP id of the document that will be used for the image <i>featured</i>. The original theme Alpha uses an image size  <b>1200 × 393 pixels</b>",
+	'explication_facebook' => "Complete URL of the facebook account",
+	'explication_instagram' => "Complete URL of the Instagram account",
+	'explication_soundcloud' => "Complete URL of the du compte soundcloud account",
+	'explication_twitter' => "Complete URL of the twitter account",
+	'explication_vimeo' => "Complete URL of the vimeo account",
+
+	// L
+    'label_articles_home' => "Id article(s) before sectors list",
+    'label_articles_home_apres' => "Id article(s) after sectors list",
+	'label_config_contenu' => "Sub-title",
+	'label_config_titre_sommaire' => 'Title',
+	'label_doc_home' => "Id doc home",
+	'label_doc_home2' => "Id doc home featured",
+	'label_facebook' => 'Facebook',
+	'label_instagram' => 'Instagram',
+	'label_newsletter_home' => 'Display the subscription form to the newsletter',
+	'label_soundcloud' => 'Soundcloud',
+	'label_twitter' => 'Twitter',
+	'label_vimeo' => 'Vimeo',
+	'legend1' => "Explanations",
+	'legend_footer' => "Footer Parameters ",
+	'legend_home' => "Sommaire Parameters",
+
+	// R
+	'read_more' => 'Read more',
+
+	// T
+	'texte_calltoaction' => "To get regular news leave your email ",
+	'titre_calltoaction' => "Newsletter",
+	'texte_config_titre_sommaire' => "Site title shown on home page? ",
+	'titre_configurer_alpha' => 'Configure Html5up Alpha',
+
+);
---
+++ html5up_alpha/lang/paquet-alpha_en.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'alpha_description' => "This plugin allows you to use the design proposed on Html5Up.",
+    'alpha_slogan' => "Theme Alpha https://html5up.net/alpha"
+);
