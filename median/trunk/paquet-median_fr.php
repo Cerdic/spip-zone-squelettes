d3484657da36cab1e27527c4af32d55a56dbbbcc
cy.altern@gmail.com <cy.altern@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 16:18:57 +0000

    "Fournir slogan et description du squelette pour éviter l'excommunication de Contrib (merci _Eric_)"
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115185 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ median/trunk/paquet-median_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'median_description' => 'Le squelette median est un squelette qui permet de gérer les contenus à afficher dans les différents types de pages via une interface de configuration et un ensemble de mots-clés "techniques".',
+	'median_slogan' => 'Squelette généraliste et configurable',
+);
+?>
