192f0f4927d8d2a2827f540047fc5d18416b7abf
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:41:55 +0000

    [Salvatore] [source:_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/ soyezcreateurs] Export depuis http://trad.spip.net
    La langue 'it' devrait Ãªtre supprimÃ©e car trop peu traduite (0.59 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_ar.php
@@ -0,0 +1,139 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/soyezcreateurs?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accessibilite_menu' => 'الذهاب إلى القائمة',
+	'accessibilite_recherche' => 'الذهاب إلى البحث', # RELIRE
+	'accessibilite_texte' => 'الذهاب إلى النص',
+	'accueil' => 'الاستقبال', # RELIRE
+	'accueil_site' => 'عودة الى الصفحة الرئيسية', # RELIRE
+	'agenda' => 'مفكرة', # RELIRE
+	'agenda_exporter_ical' => 'حول الى صيغة iCal', # RELIRE
+	'agenda_fsd' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@ الساعة @fheure@<html>:</html>@fminutes@', # RELIRE
+	'agenda_fsd_notime' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@',
+	'agenda_sd' => '@djour_l@ @djour@ @dmois_l@ @dannee@ من @dheure@<html>:</html>@dminutes@ إلى @fheure@<html>:</html>@fminutes@', # RELIRE
+	'agenda_sd_h' => '@djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@', # RELIRE
+	'agenda_sinscrire' => 'سجل لهذا الحدث', # RELIRE
+	'agendamoisde' => 'مفكرة شهر', # RELIRE
+	'alaune' => 'أبرز الأحداث', # RELIRE
+	'archives' => 'أرشيف', # RELIRE
+	'archives_title' => 'عرض مقالات الأرشيف', # RELIRE
+	'articlesconnexes' => 'مقالات ذات صلة',
+	'auteur' => 'مؤلف', # RELIRE
+	'auteurs_liste' => 'قائمة المؤلفين', # RELIRE
+	'auteurs_site' => 'مؤلفو الموقع', # RELIRE
+
+	// C
+	'carte_monde' => 'خريطة العالم',
+	'chapitre_complet' => 'فصل كامل للطباعة', # RELIRE
+	'chapitre_complet_title' => 'عرض كامل محتويات القسم : ',
+	'clever_uns' => 'تم إلغاء الاشتراك',
+	'configurersc' => 'تعديل SoyezCréateurs', # RELIRE
+	'connexion' => 'تسجيل الدخول',
+
+	// D
+	'deconnexion' => 'تسجيل الخروج',
+	'deconnexiontitle' => 'تسجيل الخروج', # RELIRE
+	'derniereactualites' => 'آخر الأخبار',
+	'dernieremaj' => 'آخر تحديث للموقع :',
+	'derniersarticlespublies' => 'أحدث المقالات المنشورة',
+	'discus_en_cours' => 'محادثات جارية', # RELIRE
+	'docatelecharger' => 'وثائق للتحميل',
+
+	// E
+	'ecoute' => 'الاستماع إلى :',
+	'erreur' => 'خطأ !', # RELIRE
+	'erreur_documentexistepas' => 'خطأ : هذا المستند غير موجود !', # RELIRE
+	'erreur_excuses_404' => 'نحن متأسفون...',
+	'erreur_excuses_404_explications' => '<p>لا يمكن العثور على الصفحة التي تبحث عنها لأن:</p><ol><li>الصفحة لم تعد ذات صلة،</li><li>رابط الصفحة المطلوبة قديم،</li><li>تم نقل الصفحة المطلوبة.</li></ol><p>الرجاء البحث في <a href="@urlsite@">الصفحة الرئيسية : @nomsite@</a>, أو البحث عن إحدى الكلمات المفاتيح حسب الاستمارة أدناه.</p>', # RELIRE
+
+	// F
+	'forum' => 'منتدى',
+	'forum_enreponse' => 'ردا على المقال :',
+	'forum_enreponse_message' => 'ردا على الرسالة :',
+	'forum_repondre' => 'أضف شهادتك', # RELIRE
+	'forum_vosreponses' => 'شهاداتك', # RELIRE
+	'forum_votre_prenom' => 'إسمك :',
+
+	// L
+	'layout_accueil_title' => 'تصميم مخصَّص للصفحة الرئيسية', # RELIRE
+	'layout_defaut_title' => 'التصميم الافتراضي للموقع (المقالات و المواضيع)', # RELIRE
+	'liresuitede' => 'اقرأ المزيد عن :',
+	'liresuitesyndic' => 'اقرأ المزيد على الموقع الأصلي...',
+
+	// M
+	'memerubrique' => 'في نفس القسم...', # RELIRE
+	'menu_picalt' => 'انقر لـ', # RELIRE
+	'mot' => 'كلمة',
+	'mots' => 'كلمات',
+	'mots_title' => 'الكلمات المفاتيح للموقع',
+	'motsgroupe' => 'مجموعة كلمات', # RELIRE
+	'multimedia' => 'استمع',
+	'multimedia_title' => 'استمع إلى أو شاهد المقتطف (يفتح نافذة جديدة)', # RELIRE
+
+	// N
+	'newsletter' => 'النشرة الإخبارية للموقع', # RELIRE
+	'newsletter_recevoir' => 'تلقي النشرة الإخبارية للموقع', # RELIRE
+
+	// O
+	'ou_sommes_nous' => 'حدد موقعنا على الخريطة !',
+
+	// P
+	'plan_menu' => 'خريطة الموقع',
+	'plus_loin_title' => 'موقع المراجع', # RELIRE
+	'precisezrecherche' => 'حدد بحثك', # RELIRE
+	'publiele' => 'نشر بتاريخ', # RELIRE
+
+	// Q
+	'quoideneuf' => 'ما الجديد ؟',
+
+	// R
+	'recherche_infructueuse' => 'لم يتم العثور على ما يطابق بحثك.', # RELIRE
+	'recherche_label' => 'بحث على الموقع',
+	'recherche_title_ok' => 'ابدأ البحث',
+	'recherche_value' => 'بحث ؟',
+	'retoursommaire' => 'رجوع إلى الفهرس',
+	'retourtop' => 'رجوع إلى الأعلى',
+
+	// S
+	'savoirplus' => 'لمعرفة المزيد...',
+	'sommaire' => 'فهرس',
+	'soyezcreateurs_couleurs' => 'ألوان',
+
+	// W
+	'wdcalendar_apr' => 'ابريل',
+	'wdcalendar_aug' => 'غشث',
+	'wdcalendar_cancel' => 'إلغاء',
+	'wdcalendar_dec' => 'دجنبر',
+	'wdcalendar_event' => 'حدث', # RELIRE
+	'wdcalendar_feb' => 'فبراير',
+	'wdcalendar_fri' => 'الجمعة',
+	'wdcalendar_jan' => 'يناير',
+	'wdcalendar_jul' => 'يوليوز',
+	'wdcalendar_jun' => 'يونيو',
+	'wdcalendar_location' => 'مكان',
+	'wdcalendar_mar' => 'مارس',
+	'wdcalendar_may' => 'ماي',
+	'wdcalendar_mon' => 'الاثنين',
+	'wdcalendar_next_month' => 'الشهر القادم',
+	'wdcalendar_nov' => 'نونبر',
+	'wdcalendar_oct' => 'أكتوبر',
+	'wdcalendar_participant' => 'مشارك',
+	'wdcalendar_prev_month' => 'الشهر السابق',
+	'wdcalendar_sat' => 'السبت',
+	'wdcalendar_sep' => 'شتنبر',
+	'wdcalendar_sun' => 'الاحد',
+	'wdcalendar_thu' => 'الخميس',
+	'wdcalendar_tue' => 'الثلاثاء',
+	'wdcalendar_wed' => 'الاربعاء',
+	'wdcalendar_week' => 'أسبوع',
+	'wdcalendar_what' => 'موضوع'
+);
