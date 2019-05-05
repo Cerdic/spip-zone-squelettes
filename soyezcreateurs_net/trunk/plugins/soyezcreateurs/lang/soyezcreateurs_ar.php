d7305a0980ce4a30509c613da6c9652277169631
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-05 03:41:16 +0000

    [Salvatore] [source:_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/ soyezcreateurs] Export depuis http://trad.spip.net
    La langue 'it' devrait Ãªtre supprimÃ©e car trop peu traduite (0.59 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115248 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_ar.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_ar.php
@@ -13,30 +13,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'accessibilite_menu' => 'الذهاب إلى القائمة',
 	'accessibilite_recherche' => 'الذهاب إلى البحث', # RELIRE
 	'accessibilite_texte' => 'الذهاب إلى النص',
-	'accueil' => 'الاستقبال', # RELIRE
+	'accueil' => 'الصفحة الرئيسية',
 	'accueil_site' => 'عودة الى الصفحة الرئيسية', # RELIRE
 	'agenda' => 'مفكرة', # RELIRE
-	'agenda_exporter_ical' => 'حول الى صيغة iCal', # RELIRE
-	'agenda_fsd' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@ الساعة @fheure@<html>:</html>@fminutes@', # RELIRE
+	'agenda_exporter_ical' => 'حول الى صيغة iCal',
+	'agenda_fsd' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@ الساعة @fheure@<html>:</html>@fminutes@',
 	'agenda_fsd_notime' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@',
-	'agenda_sd' => '@djour_l@ @djour@ @dmois_l@ @dannee@ من @dheure@<html>:</html>@dminutes@ إلى @fheure@<html>:</html>@fminutes@', # RELIRE
+	'agenda_sd' => '@djour_l@ @djour@ @dmois_l@ @dannee@ من @dheure@<html>:</html>@dminutes@ إلى @fheure@<html>:</html>@fminutes@',
 	'agenda_sd_h' => '@djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@', # RELIRE
 	'agenda_sinscrire' => 'سجل لهذا الحدث', # RELIRE
 	'agendamoisde' => 'مفكرة شهر', # RELIRE
-	'alaune' => 'أبرز الأحداث', # RELIRE
-	'archives' => 'أرشيف', # RELIRE
-	'archives_title' => 'عرض مقالات الأرشيف', # RELIRE
+	'alaune' => 'أبرز الأحداث',
+	'archives' => 'أرشيف',
+	'archives_title' => 'عرض مقالات الأرشيف',
 	'articlesconnexes' => 'مقالات ذات صلة',
-	'auteur' => 'مؤلف', # RELIRE
-	'auteurs_liste' => 'قائمة المؤلفين', # RELIRE
+	'auteur' => 'مؤلف',
+	'auteurs_liste' => 'قائمة المؤلفين',
 	'auteurs_site' => 'مؤلفو الموقع', # RELIRE
 
 	// C
 	'carte_monde' => 'خريطة العالم',
-	'chapitre_complet' => 'فصل كامل للطباعة', # RELIRE
+	'chapitre_complet' => 'فصل كامل للطباعة',
 	'chapitre_complet_title' => 'عرض كامل محتويات القسم : ',
 	'clever_uns' => 'تم إلغاء الاشتراك',
-	'configurersc' => 'تعديل SoyezCréateurs', # RELIRE
+	'configurersc' => 'تعديل SoyezCréateurs',
 	'connexion' => 'تسجيل الدخول',
 
 	// D
@@ -53,7 +53,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur' => 'خطأ !', # RELIRE
 	'erreur_documentexistepas' => 'خطأ : هذا المستند غير موجود !', # RELIRE
 	'erreur_excuses_404' => 'نحن متأسفون...',
-	'erreur_excuses_404_explications' => '<p>لا يمكن العثور على الصفحة التي تبحث عنها لأن:</p><ol><li>الصفحة لم تعد ذات صلة،</li><li>رابط الصفحة المطلوبة قديم،</li><li>تم نقل الصفحة المطلوبة.</li></ol><p>الرجاء البحث في <a href="@urlsite@">الصفحة الرئيسية : @nomsite@</a>, أو البحث عن إحدى الكلمات المفاتيح حسب الاستمارة أدناه.</p>', # RELIRE
+	'erreur_excuses_404_explications' => '<p>لا يمكن العثور على الصفحة التي تبحث عنها لأن:</p><ol><li>الصفحة لم تعد ذات صلة،</li><li>رابط الصفحة المطلوبة قديم،</li><li>تم نقل الصفحة المطلوبة.</li></ol><p>الرجاء البحث في <a href="@urlsite@">الصفحة الرئيسية : @nomsite@</a>, أو البحث عن إحدى الكلمات المفاتيح حسب الاستمارة أدناه.</p>',
 
 	// F
 	'forum' => 'منتدى',
@@ -61,6 +61,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_enreponse_message' => 'ردا على الرسالة :',
 	'forum_repondre' => 'أضف شهادتك', # RELIRE
 	'forum_vosreponses' => 'شهاداتك', # RELIRE
+	'forum_votre_email_explication' => 'بريدك الالكتروني <strong>لن يتم نشره على الموقع</strong>. يتم نشر <strong>التعليقات</strong> فقط على الموقع. بريدك الإلكتروني ضروري حتى نتمكن من <strong>الإجابة</strong> إذا كان لديك <strong>سؤال شخصي</strong>.',
 	'forum_votre_prenom' => 'إسمك :',
 
 	// L
@@ -81,6 +82,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
 	'newsletter' => 'النشرة الإخبارية للموقع', # RELIRE
+	'newsletter_consulter' => 'اطلع على أرشيف النشرات الإخبارية',
 	'newsletter_recevoir' => 'تلقي النشرة الإخبارية للموقع', # RELIRE
 
 	// O
@@ -90,7 +92,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'plan_menu' => 'خريطة الموقع',
 	'plus_loin_title' => 'موقع المراجع', # RELIRE
 	'precisezrecherche' => 'حدد بحثك', # RELIRE
-	'publiele' => 'نشر بتاريخ', # RELIRE
+	'publiele' => 'نشر بتاريخ',
 
 	// Q
 	'quoideneuf' => 'ما الجديد ؟',
@@ -99,6 +101,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'recherche_infructueuse' => 'لم يتم العثور على ما يطابق بحثك.', # RELIRE
 	'recherche_label' => 'بحث على الموقع',
 	'recherche_title_ok' => 'ابدأ البحث',
+	'recherche_total' => 'العدد الجملي للعناصر التي تم العثور عليها',
 	'recherche_value' => 'بحث ؟',
 	'retoursommaire' => 'رجوع إلى الفهرس',
 	'retourtop' => 'رجوع إلى الأعلى',
@@ -108,30 +111,47 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sommaire' => 'فهرس',
 	'soyezcreateurs_couleurs' => 'ألوان',
 
+	// T
+	'themes' => 'المواضيع',
+
+	// V
+	'voirarticlespar' => 'شاهد المقالات بـ', # RELIRE
+	'voirdetailannee' => 'انظر تفاصيل السنة', # RELIRE
+	'voirdetailmois' => 'انظر تفاصيل شهر', # RELIRE
+	'voirimage' => ' عرض الصورة فقط',
+
 	// W
+	'wdcalendar_all_day' => 'كامل اليوم',
 	'wdcalendar_apr' => 'ابريل',
-	'wdcalendar_aug' => 'غشث',
+	'wdcalendar_aug' => 'أغسطس',
 	'wdcalendar_cancel' => 'إلغاء',
-	'wdcalendar_dec' => 'دجنبر',
+	'wdcalendar_day_letter' => 'ي',
+	'wdcalendar_day_plural' => 'أيام',
+	'wdcalendar_dec' => 'ديسمبر',
 	'wdcalendar_event' => 'حدث', # RELIRE
 	'wdcalendar_feb' => 'فبراير',
 	'wdcalendar_fri' => 'الجمعة',
 	'wdcalendar_jan' => 'يناير',
-	'wdcalendar_jul' => 'يوليوز',
+	'wdcalendar_jul' => 'يوليو',
 	'wdcalendar_jun' => 'يونيو',
+	'wdcalendar_loading_data' => 'جار تحميل البيانات...',
+	'wdcalendar_loading_fail' => 'عذرًا ، لا يمكننا تحميل بياناتك ، يرجى المحاولة لاحقًا',
 	'wdcalendar_location' => 'مكان',
 	'wdcalendar_mar' => 'مارس',
-	'wdcalendar_may' => 'ماي',
+	'wdcalendar_may' => 'مايو',
 	'wdcalendar_mon' => 'الاثنين',
+	'wdcalendar_month' => 'شهر',
 	'wdcalendar_next_month' => 'الشهر القادم',
 	'wdcalendar_nov' => 'نونبر',
 	'wdcalendar_oct' => 'أكتوبر',
 	'wdcalendar_participant' => 'مشارك',
 	'wdcalendar_prev_month' => 'الشهر السابق',
 	'wdcalendar_sat' => 'السبت',
-	'wdcalendar_sep' => 'شتنبر',
+	'wdcalendar_see_detail' => 'انظر التفاصيل', # RELIRE
+	'wdcalendar_sep' => 'سبتمبر',
 	'wdcalendar_sun' => 'الاحد',
 	'wdcalendar_thu' => 'الخميس',
+	'wdcalendar_today' => 'اليوم',
 	'wdcalendar_tue' => 'الثلاثاء',
 	'wdcalendar_wed' => 'الاربعاء',
 	'wdcalendar_week' => 'أسبوع',
