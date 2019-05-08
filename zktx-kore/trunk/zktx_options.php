18a70ba26ac9228ebc5aee3ef591b96fa18dc346
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-08 15:40:33 +0000

    commit un peu foutoir car plusieurs chantiers en cours + vieiles modifs qui n'avaient pas encore été pushées : cosmetique, re-zoning, et optis diverses (notament pour les forums) + ajout d'un répertoire d'accueil des fenêtres modales personalisées (j'en oublie surement et tout n'est pas encore pleinement fonctionnel)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115282 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zktx-kore/trunk/zktx_options.php
+++ zktx-kore/trunk/zktx_options.php
@@ -3,4 +3,4 @@
 	$GLOBALS['z_blocs'] = array('content','head','head_js','header','extra1','extra2','footer','foot_js','breadcrumb');
 
 	// activer le chargement parallele sur les blocs contenu et more
-	define('_Z_AJAX_PARALLEL_LOAD','extra1,extra2,footer,foot_js');
+	define('_Z_AJAX_PARALLEL_LOAD','extra1,extra2,footer');
