f67dc3c28738f7b5d069489c08db5bc87924ae3f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-07 03:41:17 +0000

    [Salvatore] [source:_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/ soyezcreateurs] Export depuis http://trad.spip.net
    La langue 'it' devrait Ãªtre supprimÃ©e car trop peu traduite (0.58 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115565 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_ar.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_ar.php
@@ -11,25 +11,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'accessibilite_menu' => 'الذهاب إلى القائمة',
-	'accessibilite_recherche' => 'الذهاب إلى البحث', # RELIRE
+	'accessibilite_onglets' => 'اذهب الى القائمة',
+	'accessibilite_recherche' => 'خانة البحث',
 	'accessibilite_texte' => 'الذهاب إلى النص',
 	'accueil' => 'الصفحة الرئيسية',
 	'accueil_site' => 'الرجوع الى الصفحة الرئيسية',
 	'actualite_toutes' => 'آخر المقالات المنشورة ',
+	'actualite_toutes_arbo' => 'لائحة',
+	'actualite_toutes_blocs' => 'الوسائط البصرية',
 	'agenda' => 'مفكرة',
 	'agenda_exporter_ical' => 'حول الى صيغة iCal',
 	'agenda_fsd' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@ الساعة @fheure@<html>:</html>@fminutes@',
 	'agenda_fsd_notime' => 'من @djour_l@ @djour@ @dmois_l@ @dannee@ إلى @fjour_l@ @fjour@ @fmois_l@ @fannee@',
-	'agenda_proposer_evenement' => 'اقترح مناسبة', # RELIRE
-	'agenda_proposer_evenement_explication' => 'بإمكانك اقتراح مناسبة على المشرف على الموقع الذي سيختار نشرها من عدمه.  المناسيات التي تحتوي على أكثر تفاصيل لها فرص أكبر للموافقة عليها.', # RELIRE
-	'agenda_proposer_evenement_title' => 'بإمكانك أن تقترح مناسبة على المشرف على الموقع', # RELIRE
+	'agenda_proposer_evenement' => 'اقترح حدث',
+	'agenda_proposer_evenement_explication' => 'بإمكانك اقتراح حدث على المشرف على الموقع الذي سيختار نشرها من عدمه. الأحداث التي تحتوي على أكثر تفاصيل لها فرص أكبر للموافقة عليها.',
+	'agenda_proposer_evenement_title' => 'بإمكانك أن تقترح حدثا على المشرف على الموقع',
 	'agenda_sd' => '@djour_l@ @djour@ @dmois_l@ @dannee@ من @dheure@<html>:</html>@dminutes@ إلى @fheure@<html>:</html>@fminutes@',
-	'agenda_sd_h' => '@djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@', # RELIRE
-	'agenda_sinscrire' => 'سجل لهذه المناسبة', # RELIRE
+	'agenda_sd_h' => '@djour_l@ @djour@ @dmois_l@ @dannee@ الساعة @dheure@<html>:</html>@dminutes@',
+	'agenda_sd_notime' => 'Le @djour_l@ @djour@ @dmois_l@ @dannee@',
+	'agenda_sinscrire' => 'التسجيل للحدث',
 	'agendamoisde' => 'مفكرة شهر',
+	'aidesc' => 'المساعدة',
 	'alaune' => 'أبرز الأحداث',
-	'annuaire' => 'دليل', # RELIRE
-	'annuaire_vide' => '.الدليل فارغ', # RELIRE
+	'annuaire' => 'الدليل',
+	'annuaire_consulter' => 'تصفح الدليل',
+	'annuaire_local' => 'دليل محلي',
+	'annuaire_vide' => '.الدليل فارغ',
 	'archives' => 'أرشيف',
 	'archives_title' => 'عرض مقالات الأرشيف',
 	'articlesconnexes' => 'مقالات ذات صلة',
@@ -41,51 +48,68 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'carte_monde' => 'خريطة العالم',
 	'chapitre_complet' => 'فصل كامل للطباعة',
 	'chapitre_complet_title' => 'عرض كامل محتويات القسم : ',
+	'chercheravecgoogle' => 'ابحث على Google.',
 	'clever_uns' => 'تم إلغاء الاشتراك',
-	'clevermail' => 'CleverMail', # RELIRE
+	'clevermail' => 'CleverMail',
+	'commencer' => 'ابدا',
 	'configurersc' => 'تعديل SoyezCréateurs',
 	'connexion' => 'تسجيل الدخول',
+	'connexiontitle' => 'الولوج الى الفضاء المخصص',
+	'copyright_cnil' => 'تصريح CNIL رقم:',
+	'copyright_realisation' => 'انجاز',
+	'copyright_spip' => 'copyright_spip',
 
 	// D
 	'deconnexion' => 'تسجيل الخروج',
 	'deconnexiontitle' => 'تسجيل الخروج',
+	'deposer_intention' => 'ارفع دعاء',
 	'derniereactualites' => 'آخر الأخبار',
 	'dernieremaj' => 'آخر تحديث للموقع :',
 	'derniersarticlespublies' => 'أحدث المقالات المنشورة',
-	'discus_en_cours' => 'محادثات جارية', # RELIRE
+	'descriptif' => 'الوصف',
+	'discus_en_cours' => 'المحادثات الجارية',
 	'docatelecharger' => 'وثائق للتحميل',
 
 	// E
 	'ecoute' => 'الاستماع إلى :',
-	'erreur' => 'خطأ !', # RELIRE
-	'erreur_documentexistepas' => 'خطأ : هذا المستند غير موجود !', # RELIRE
+	'editos' => 'تحرير',
+	'erreur' => 'خطأ !',
+	'erreur_documentexistepas' => 'خطأ : هذا المستند غير موجود !',
 	'erreur_excuses_404' => 'نحن متأسفون...',
 	'erreur_excuses_404_explications' => '<p>لا يمكن العثور على الصفحة التي تبحث عنها لأن:</p><ol><li>الصفحة لم تعد ذات صلة،</li><li>رابط الصفحة المطلوبة قديم،</li><li>تم نقل الصفحة المطلوبة.</li></ol><p>الرجاء البحث في <a href="@urlsite@">الصفحة الرئيسية : @nomsite@</a>, أو البحث عن إحدى الكلمات المفاتيح حسب الاستمارة أدناه.</p>',
 
 	// F
 	'forum' => 'منتدى',
 	'forum_enreponse' => 'ردا على المقال :',
+	'forum_enreponse_breve' => 'منتدى "الاجابة المختصرة":',
 	'forum_enreponse_message' => 'ردا على الرسالة :',
+	'forum_prenom' => 'عرف عن نفسك:',
 	'forum_repondre' => 'أضف شهادتك',
 	'forum_vosreponses' => 'شهاداتك',
 	'forum_votre_email_explication' => 'بريدك الالكتروني <strong>لن يتم نشره على الموقع</strong>. يتم نشر <strong>التعليقات</strong> فقط على الموقع. بريدك الإلكتروني ضروري حتى نتمكن من <strong>الإجابة</strong> إذا كان لديك <strong>سؤال شخصي</strong>.',
 	'forum_votre_prenom' => 'إسمك :',
 
 	// L
-	'layout_accueil_title' => 'تصميم مخصَّص للصفحة الرئيسية', # RELIRE
-	'layout_defaut_title' => 'التصميم الافتراضي للموقع (المقالات و المواضيع)', # RELIRE
+	'layout_accueil_title' => 'تصميم مخصَّص للصفحة الرئيسية',
+	'layout_defaut_title' => 'التصميم الافتراضي للموقع (المقالات و المواضيع)',
 	'liresuitede' => 'اقرأ المزيد عن :',
+	'liresuiteeditorial' => 'اتم القراءة',
 	'liresuitesyndic' => 'اقرأ المزيد على الموقع الأصلي...',
+	'listezones' => 'لائحة الروابط القابلة للنقر',
 
 	// M
-	'memerubrique' => 'في نفس القسم...', # RELIRE
-	'menu_picalt' => 'انقر لـ', # RELIRE
+	'memerubrique' => 'في نفس القسم...',
+	'mentions_legales_obligatoires' => 'معلومات قانونية إلزامية',
+	'menu_deplier' => 'اكشف: ',
+	'menu_picalt' => 'انقر لـ',
+	'menu_replier' => 'اطو :',
+	'menunavrwd' => 'قائمة التصفح',
 	'mot' => 'كلمة',
 	'mots' => 'كلمات',
 	'mots_title' => 'الكلمات المفاتيح للموقع',
 	'motsgroupe' => 'مجموعة كلمات',
 	'multimedia' => 'استمع',
-	'multimedia_title' => 'استمع إلى أو شاهد المقتطف (يفتح نافذة جديدة)', # RELIRE
+	'multimedia_title' => 'استمع إلى أو شاهد المقتطف (يفتح نافذة جديدة)',
 
 	// N
 	'newsletter' => 'النشرة الإخبارية للموقع',
@@ -93,10 +117,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'newsletter_recevoir' => 'تلقي النشرة الإخبارية للموقع',
 
 	// O
-	'ordreantichronologique' => 'عكس التسلسل الزمني', # RELIRE
+	'ordreantichronologique' => 'عكس التسلسل الزمني',
 	'ou_sommes_nous' => 'حدد موقعنا على الخريطة !',
 
 	// P
+	'par' => 'عبر',
+	'participez' => 'شارك في نشاط الموقع',
 	'plan_menu' => 'خريطة الموقع',
 	'plus_loin' => 'انظر أيضا',
 	'plus_loin_title' => 'موقع المراجع',
@@ -109,30 +135,39 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// R
 	'recherche_infructueuse' => 'لم يتم العثور على ما يطابق بحثك.',
 	'recherche_label' => 'بحث على الموقع',
+	'recherche_title' => 'ادخل البحث',
 	'recherche_title_ok' => 'ابدأ البحث',
 	'recherche_total' => 'العدد الجملي للعناصر التي تم العثور عليها',
 	'recherche_value' => 'بحث ؟',
 	'retoursommaire' => 'رجوع إلى الفهرس',
 	'retourtop' => 'رجوع إلى الأعلى',
+	'rubrique_securisee' => 'Accés réservé ouvert',
 
 	// S
 	'savoirplus' => 'لمعرفة المزيد...',
-	'sedna' => '(RSSبرنامج قراءة ال) Sedna ', # RELIRE
+	'savoirpluscritere' => 'تعرف اكثر على المعيار:',
+	'sedna' => '(RSSبرنامج قراءة ال) Sedna ',
 	'sommaire' => 'فهرس',
-	'soyezcreateurs' => 'SoyezCréateurs', # RELIRE
+	'soyezcreateurs' => 'كونوا مبدعين',
 	'soyezcreateurs_couleurs' => 'ألوان',
+	'soyezcreateurs_google' => 'Référencement',
+	'soyezcreateurs_layout' => 'التموقع',
+	'syndiquer_agenda' => 'تنظيم جدول الأعمال',
 
 	// T
 	'themes' => 'المواضيع',
 	'tousarticlesantichrono' => 'كل المقالات من الأحدث إلى الأقدم ',
-	'toutleplan' => 'الموقع بأكمله على صفحة', # RELIRE
+	'tout' => '(Aucun filtre)',
+	'toutleplan' => 'الموقع بأكمله على صفحة واحدة',
 
 	// V
 	'voirarticlespar' => 'شاهد المقالات بـ',
-	'voirdetailannee' => 'انظر تفاصيل السنة', # RELIRE
-	'voirdetailmois' => 'انظر تفاصيل شهر', # RELIRE
+	'voirdetailannee' => 'انظر تفاصيل السنة',
+	'voirdetailmois' => 'انظر تفاصيل شهر',
 	'voirimage' => ' عرض الصورة فقط',
-	'vous_souhaitez_etre_tenu_au_courant' => 'هل تريد أن تبقى مواكبا لما هو جديد ؟', # RELIRE
+	'voirle' => 'انظر الى',
+	'voirsitespar' => 'تصفح الموقع ب',
+	'vous_souhaitez_etre_tenu_au_courant' => 'هل تريد أن تبقى مواكبا لما هو جديد ؟',
 
 	// W
 	'wdcalendar_all_day' => 'كامل اليوم',
@@ -144,7 +179,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'wdcalendar_day_letter' => 'ي',
 	'wdcalendar_day_plural' => 'أيام',
 	'wdcalendar_dec' => 'ديسمبر',
-	'wdcalendar_event' => 'مناسبة', # RELIRE
+	'wdcalendar_event' => 'حدث',
 	'wdcalendar_feb' => 'فبراير',
 	'wdcalendar_fri' => 'الجمعة',
 	'wdcalendar_jan' => 'يناير',
@@ -160,6 +195,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'wdcalendar_next_month' => 'الشهر القادم',
 	'wdcalendar_nov' => 'نونبر',
 	'wdcalendar_oct' => 'أكتوبر',
+	'wdcalendar_others' => 'اشياء اخرى',
 	'wdcalendar_participant' => 'مشارك',
 	'wdcalendar_prev_month' => 'الشهر السابق',
 	'wdcalendar_refresh' => 'إعادة تحميل',
@@ -171,6 +207,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'wdcalendar_time' => 'ساعة',
 	'wdcalendar_today' => 'اليوم',
 	'wdcalendar_tue' => 'الثلاثاء',
+	'wdcalendar_undefined' => 'غير معرف',
 	'wdcalendar_wed' => 'الاربعاء',
 	'wdcalendar_week' => 'أسبوع',
 	'wdcalendar_what' => 'موضوع'
