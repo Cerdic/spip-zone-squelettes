6ee05d5d4c55b2ff6c7f167dd8faaa4a70383d76
cent20@gmail.com <cent20@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-14 09:21:13 +0000

    J'ai compris comment coder les fichiers de lang ! ;-D
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_spectral/lang/html5up_spectral_fr.php
+++ html5up_spectral/lang/html5up_spectral_fr.php
@@ -15,6 +15,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+	// S
+	'site_realise_avec' => 'Site réalisé avec',
+
 	// T
 	'titre_page_configurer_html5up_spectral' => 'Configuration du squelette Spectral ( HTML5UP ) pour SPIP',
+	'theme_graphique_par_html5up' => 'Thème graphique par HTML5 UP',
+	
+	
 );
