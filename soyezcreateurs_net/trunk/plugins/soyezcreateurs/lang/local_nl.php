fcff45c0f852ed2b5e66d29545f1c4a8841092ba
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-17 07:20:17 +0000

    [SoyezCréateurs] apostrophe typographique et synchro de langues
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_nl.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/local_nl.php
@@ -4,6 +4,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // Modification de l'interface d'admin
-'texte_descriptif_rapide' => 'Bulle d\'aide <i>(et chapeau si le chapeau vide)</i>',
+'texte_descriptif_rapide' => 'Bulle d’aide <i>(et chapeau si le chapeau vide)</i> + Meta Description <i>(pour les moteurs de recherche)</i>',
 
 );
