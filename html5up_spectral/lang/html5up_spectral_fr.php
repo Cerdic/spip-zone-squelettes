519b38527db358d136a43771f040f7fc87e8152e
cent20@gmail.com <cent20@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-25 06:39:24 +0000

    Mon premier formulaire. Dédicacé à Jean Marie.
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115773 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_spectral/lang/html5up_spectral_fr.php
+++ html5up_spectral/lang/html5up_spectral_fr.php
@@ -9,6 +9,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// H
 	'html5up_spectral_titre' => 'Html5Up Spectral',
+	'hero' => 'L\'article héro',
+	'hero_explications' => 'C\'est le premier article mis en avant sur la page d\'accueil.<br>Titre & descriptif affiché, un lien box',	
 
 	// C
 	'cfg_exemple' => 'Exemple',
@@ -16,21 +18,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_parametrages' => 'Paramétrages',
 	'contact' => 'Contact',
 
+
+	// L
+	'jeux_de_couleur' => 'Jeux de couleur',
+
 	// L
 	'lire_la_suite' => 'Lire la suite',
 	
+	// M
+	'major' => 'Un 1er article majeur',
+	'major_explications' => 'C\'est le second article mis en avant sur la page d\'accueil, <br>sous les rubriques au dessus des mots clés, <br>Titre & descriptif affiché. Pas de lien html',
+	'major_deux' => 'Un 2ème article majeur',
+	'major_explications_deux' => 'C\'est le troisième article mis en avant sur la page d\'accueil, <br>sous les mots clés avant le pied de page, <br>Titre & descriptif affiché. Deux liens box',
+		
+	// N
+	'nb_rubriques' => 'Nombre de rubrique',
+	'nb_rubriques_explications' => 'Nombre de rubrique affichée sur la page d\'accueil, <br>par defaut : 3 , triées par numéro',
 
+	// O
+	
+	// P
+	'page_accueil' => 'Page d\'accueil du site',
+	
 
 	// S
-	'site_realise_avec' => 'Site réalisé avec',
-	
-	
-	
+	'site_realise_avec' => 'Site réalisé avec',	
 	
 
 	// T
-	'titre_page_configurer_html5up_spectral' => 'Configuration du squelette Spectral ( HTML5UP ) pour SPIP',
+	'titre_page_configurer_html5up_spectral' => 'Configuration du squelette HTML5UP - Spectral',
 	'theme_graphique_par_html5up' => 'Thème graphique par HTML5 UP',
-	
-	
+	'titre_lien' => 'Titre du lien',
+	'titre_lien_explications_lire' => 'Texte utilisé pour le lien box qui suit l\'article précédent. <br>par defaut : Lire la suite',
+	'titre_lien_explications_contact' => 'Texte utilisé pour le lien box qui suit l\'article précédent. <br>par defaut : CONTACT',
+
 );
