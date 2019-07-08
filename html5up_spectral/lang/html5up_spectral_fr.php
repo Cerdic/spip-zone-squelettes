9bdd43d61d0bb98379078c31564b985df0474281
cent20@gmail.com <cent20@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-08 15:43:02 +0000

    v 1.0.11 divers correctifs liés au formulaire de configuration et à une meilleur intégration du plugin lié "masquer".
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@115900 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_spectral/lang/html5up_spectral_fr.php
+++ html5up_spectral/lang/html5up_spectral_fr.php
@@ -24,7 +24,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cc_by' => 'cc by',
 	'credits' => 'Crédits',
 	
-
+	// D
+	'documentation_et_developpement' => 'Documentation et développement',
 
 	// L
 	'jeux_de_couleur' => 'Jeux de couleur',
@@ -49,7 +50,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'page_accueil' => 'Page d\'accueil du site',
 	'plugin_spip' => 'plugin SPIP',
 	'porte_dev' => 'porté en',	
-	'parcourir_rubriques_explications' => 'Titre des liens vers les rubrique affichée sur la page d\'accueil, <br>La page sommaire liste les rubriques racines sauf celle commençant par un "9"<br>par defaut : Parcourir , triées par numéro, 5 au maximum',
+	'parcourir_rubriques_explications' => 'Titre des liens vers les rubrique affichée sur la page d\'accueil, <br>La page sommaire liste 5 rubriques racines maximum"<br>par defaut : Parcourir',
 	'pied_de_page' => 'Pied de page',
 	
 	
