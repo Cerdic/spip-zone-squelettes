a40c0a92af6c9e39cea2435a5dbe28dc2f1a1448
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-13 15:20:55 +0000

    sécurité, ajout de noopener et noreferrer dans l'attribut rel des liens externes cf ​https://www.mail-archive.com/spip-zone@rezo.net/msg40738.html
    (merci b_b)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@118594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sendagi/js/gui.js
+++ sendagi/js/gui.js
@@ -1,56 +1,51 @@
 /********************************
  *  GUI: interface utilisateur
  *
- *  version:  1.0
- *  date:     2014.04.30
- *
  ********************************/
  
-      
+
 $(document).ready(function(){
-    
-    // print
-    $(".print a").click(function() {
-           window.print();
-           return false;
-    }); 
-    
 
-    // Lien en spip_out s'ouvre ds une nouvelle fenetre
-    // voir aussi http://blog.gaboweb.com/2009/07/02/target_blank-automatique-jquery-respecter-normes-xhtml-strictes/   
-    $("a.spip_out").click(function(){
-          window.open(this.href);   
-          return false;       
-    });
-      
-    // clic externe nouvelle fenetre
-    $("a[rel*=external]").click(function(){
-              this.target= "_blank";
-    });
+	// print
+	$('.print a').click(function() {
+		window.print();
+		return false;
+	});
 
+	// Lien en spip_out s'ouvre ds une nouvelle fenetre
+	// voir aussi http://blog.gaboweb.com/2009/07/02/target_blank-automatique-jquery-respecter-normes-xhtml-strictes/
+	$('a.spip_out').click(function(){
+		$(this).attr('rel','external noopener noreferrer'); // securité : https://www.mail-archive.com/spip-zone@rezo.net/msg40738.html
+		window.open(this.href);
+		return false;
+	});
 
+	// clic externe nouvelle fenetre
+	$("a[rel*=external]").click(function(){
+		$(this).attr('rel','external noopener noreferrer'); // securité : https://www.mail-archive.com/spip-zone@rezo.net/msg40738.html
+		this.target= "_blank";
+	});
 
 	// carousel
 	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:10,
-	    nav:false,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
+		loop:true,
+		margin:10,
+		nav:false,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
 	})
 
 	// Go to the top
 	// http://www.jqueryscript.net/other/Minimal-Back-To-Top-Functionality-with-jQuery-CSS3.html
-
 	$(window).scroll(function(event){
 		var scroll = $(window).scrollTop();
 		if (scroll >= 800) {
@@ -68,7 +63,7 @@ $(document).ready(function(){
 	});
 
 	// scroll doux
-	$(".smooth-scroll").click (function() {
+	$('.smooth-scroll').click (function() {
 		var target = $(this).attr('href');
 		var hash = "#" + target.substring(target.indexOf('#')+1);
 		$('html, body').animate({ scrollTop:$(hash).offset().top 	}, 600);
