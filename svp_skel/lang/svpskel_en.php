1be09dd79f310a5ff95df0b9cdceb1cea44d2f18
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-07 04:40:04 +0000

    [Salvatore] [source:_squelettes_/svp_skel/lang/ svpskel] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@118943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_skel/lang/svpskel_en.php
+++ svp_skel/lang/svpskel_en.php
@@ -16,11 +16,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_sourcer' => 'Source code',
 	'bouton_telecharger' => 'Zip file',
 	'bouton_xmler' => 'XML File',
+	'bulle_aller_categorie' => 'Go to the plugins page of this category',
+	'bulle_aller_demonstration' => 'Go to the demo page',
+	'bulle_aller_depots' => 'Go to the depot home page ',
+	'bulle_aller_developpement' => 'Go to the development area',
+	'bulle_aller_plugins' => 'Go to the plugins home page',
+	'bulle_aller_prefixes' => 'Go to the prefix page',
+	'bulle_aller_sources' => 'Consult the package sources',
+	'bulle_aller_statistiques' => 'Go to the statistics page',
+	'bulle_aller_telechargements' => 'Go to the download page',
+	'bulle_filtrer_par_categorie' => 'Filter by category',
+	'bulle_filtrer_par_depot' => 'Filter on the repository',
+	'bulle_filtrer_par_spip' => 'Filter on the SPIP branch',
+	'bulle_supprimer_filtre' => 'Remove the filter',
 
 	// D
 	'derniere_maj' => 'Updated on',
+	'derniere_version' => 'Latest version',
 
 	// I
+	'info_actualisation_depot_cron' => 'The repository plugins are automatically updated every @periode@ hour(s). ',
 	'info_aucun_depot_disponible' => 'No repository available.',
 	'info_aucun_module_utilise' => 'No language module used',
 	'info_aucun_paquet_disponible' => 'No package available.',
@@ -28,6 +43,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_aucun_plugin_recherche' => 'No plugin is matching the search',
 	'info_aucun_prefixe_disponible' => 'No prefix used.',
 	'info_aucune_compatibilite_spip' => 'Unknown',
+	'info_aucune_contribution_disponible' => 'No other contributions available outside the plugins',
 	'info_compatible' => 'Compatible with : ',
 	'info_rechercher_plugin' => 'Search a plugin :',
 	'info_stats_categories' => '@nb_categories@ categories available (up to 13).',
@@ -58,7 +74,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_utilise_plugins' => 'Compatible with',
 
 	// R
-	'resume_table_autres_contributions' => 'Others Contributions',
+	'resume_table_autres_contributions' => 'Other Contributions',
 
 	// T
 	'titre_filtre_categorie' => 'Filter by category',
