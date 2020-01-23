299b93bf65832ef6c64bd3fe00fa2f3d3e16b81b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-23 20:59:52 +0000

    copie legacy du squelettes avant passage en trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@119920 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ html5up_alpha_legacy/lang/alpha_fr.php
@@ -0,0 +1,62 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil' => 'Accueil',
+	'alpha_titre' => 'Configurer Alpha',
+
+	// B
+	'bouton_subscribe' => 'S\'abonner',
+
+	// C
+	'contact' => 'Contact',
+
+	// E
+    'explication_articles_home' => "La page sommaire affiche les rubriques de premier niveau. Vous pouvez avoir besoin d'ajouter un ou des articles à cette liste de rubriques. Saisissez le (ou les) numéro(s) d'article(s) SPIP ou de page(s) unique(s) séparés par une virgule. Pour les mettre dans l'ordre voulu ajouter un 1. 2. dans le titre de l'article (les articles sont triés par num titre)." ,
+    'explication_articles_home_apres' => "La page sommaire affiche les rubriques de premier niveau. Vous pouvez avoir besoin d'ajouter un ou des articles APRES cette liste de rubriques. Saisissez le (ou les) numéro(s) d'article(s) SPIP ou de page(s) unique(s) séparés par une virgule. Pour les mettre dans l'ordre voulu ajouter un 1. 2. dans le titre de l'article (les articles sont triés par num titre)." ,
+	'explication_config_contenu' => "Le squelette utilise les sous titres pour les articles, n'oubliez pas de les activer dans <a href='?exec=configurer_contenu'>configurer contenu</a>",
+	'explication_config_titre_sommaire' => "Parfois sur la home page le titre du site fait un peu tâche sur l'image",
+	'explication_doc_home' => "L'id SPIP du document qui sera utilisé pour la grande image du haut. Le thème original utilise une image de taille <b>1800 × 905 pixels</b> ",
+	'explication_doc_home2' => "L'id SPIP du document qui sera utilisé pour l'image <i>featured</i>. Le thème Alpha original utilise une image de taille  <b>1200 × 393 pixels</b>",
+	'explication_facebook' => "URL complète du compte facebook",
+	'explication_instagram' => "URL complète du compte Instagram",
+	'explication_soundcloud' => "URL complète du compte soundcloud",
+	'explication_twitter' => "URL complète du compte twitter",
+	'explication_vimeo' => "URL complète du compte vimeo",
+	'explication_youtube' => "URL complète de la chaîne Youtube",
+
+
+	// L
+    'label_articles_home' => "Id article(s) avant liste des secteurs",
+    'label_articles_home_apres' => "Id article(s) après liste des secteurs",
+	'label_config_contenu' => "Sous-titre",
+	'label_config_titre_sommaire' => 'Titre',
+	'label_doc_home' => "Id doc home",
+	'label_doc_home2' => "Id doc home featured",
+	'label_facebook' => 'Facebook',
+	'label_instagram' => 'Instagram',
+	'label_newsletter_home' => 'Afficher le formulaire d\'inscription à la newsletter',
+	'label_soundcloud' => 'Soundcloud',
+	'label_twitter' => 'Twitter',
+	'label_vimeo' => 'Vimeo',
+	'label_youtube' => "Youtube",
+	'legend1' => "Explications",
+	'legend_footer' => "Paramétrages Pied de page ",
+	'legend_home' => "Paramétrages Sommaire ",
+
+	// R
+	'read_more' => 'Lire la suite',
+
+	// T
+	'texte_calltoaction' => "Pour avoir des nouvelles régulières, inscrivez votre mail ",
+	'titre_calltoaction' => "Newsletter",
+	'texte_config_titre_sommaire' => "Le titre du site apparait sur la page d'accueil ? ",
+	'titre_configurer_alpha' => 'Configurer Html5up Alpha',
+
+);
---
+++ html5up_alpha_legacy/lang/paquet-alpha_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'alpha_description' => "Ce plugin permet de reprendre le design Alpha proposé sur Html5Up https://html5up.net/alpha.",
+    'alpha_slogan' => "Squelette adapté du thème Alpha de HTML5 UP"
+);
