4f93d514858e5b3c003f3cf5f4045ac9939ed10e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:52:13 +0000

    [Salvatore] [source: eva] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: eva] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@121519 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- eva-web/4_0/eva_lang/lang/eva_fr.php
+++ eva-web/4_0/eva_lang/lang/eva_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/eva-web/4_0/eva_lang/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/eva-web/4_0/eva_lang/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
