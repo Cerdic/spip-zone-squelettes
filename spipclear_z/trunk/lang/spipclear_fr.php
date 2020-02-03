ce3b9462274993120e7854a5fc554cc6629768fe
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:52:34 +0000

    [Salvatore] [source: spipclear] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: spipclear] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@121549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipclear_z/trunk/lang/spipclear_fr.php
+++ spipclear_z/trunk/lang/spipclear_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/spipclear_z/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/spipclear_z/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
