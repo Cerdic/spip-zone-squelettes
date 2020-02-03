09d7ce2a95e70b3ca216d92bb5a321ba9a40135c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:52:30 +0000

    [Salvatore] [source: soyezcreateurs] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: soyezcreateurs] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@121543 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_fr.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
