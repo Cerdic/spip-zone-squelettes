cd8b727fdd984fbd87bbeac6e4ce79257f92347f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-06 11:23:21 +0000

    [Salvatore] [source:lang/ sarkaspip] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ sarkaspip] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sarkaspip/trunk/lang/sarkaspip_fr.php
+++ sarkaspip/trunk/lang/sarkaspip_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/sarkaspip/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-squelettes/sarkaspip.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
