c81a875359fb30fb62293a599eda2fa5f9466478
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-06 11:12:58 +0000

    [Salvatore] [source:lang/ paquet-aveline] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-aveline] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122187 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aveline/trunk/lang/paquet-aveline_fr.php
+++ aveline/trunk/lang/paquet-aveline_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/aveline/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-squelettes/aveline.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
