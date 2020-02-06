f8b25b13cf46d053f2a9712bd8b29cf2ad0c2c45
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-06 11:24:00 +0000

    [Salvatore] [source:lang/ zvide] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ zvide] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpip-vide/trunk/lang/zvide_fr.php
+++ zpip-vide/trunk/lang/zvide_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/zpip-vide/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-squelettes/zpip-vide.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
