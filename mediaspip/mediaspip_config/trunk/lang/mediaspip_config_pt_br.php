d479b78fea97d00678b6076c4a8a5fb9d3c7a1be
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-07 07:50:21 +0000

    [Salvatore] [source: mediaspip_config] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source: mediaspip_config] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip/mediaspip_config/trunk/lang/mediaspip_config_pt_br.php
@@ -0,0 +1,109 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/mediaspip_config?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_retablir_notices' => 'Restabelecer todos os avisos',
+	'bouton_supprimer_notice' => 'Não mostrar mais este aviso',
+	'bouton_verifier_originaux' => 'Verificar todos os arquivos faltosos',
+
+	// C
+	'configuration_auteurs' => 'Os utilizadores',
+	'configuration_avancee' => 'Configuração avançada',
+	'configuration_documents' => 'Gestão dos arquivos',
+	'configuration_interactivite' => 'Interatividade',
+	'configuration_outils' => 'Ferramentas',
+	'configuration_plugins' => 'Configuração dos outros plugins',
+	'configuration_spip' => 'Configuração geral do site',
+	'configuration_squelettes' => 'Os esqueletos',
+	'configuration_statistiques' => 'Estatísticas',
+
+	// E
+	'editer_secteur_nouveau' => 'Criar uma nova seção',
+	'erreur_config_autorisation_identifie' => 'Você deve se identificar para acessar a administração.',
+	'erreur_config_autorisation_insuffisante' => 'Você não possui um nível suficiente para acessar esta página.',
+	'erreur_saveauto_mail_fichier_lourd' => 'O arquivo de backup é muito grande para ser enviado por email. Você poder recuperá-lo a partir da interface de administração no endereço @url@',
+	'explication_accepter_visiteurs' => 'Se você deseja abrir livremente a inscrição ao site a novos visitantes, você pode ativar a opção abaixo:',
+	'explication_form_signature_auteur' => 'Seu nome ou seu pseudo',
+	'explication_plugins_conseilles' => 'Após análise rápida do site, os plugins suplementares seguintes podem ser úteis.',
+
+	// H
+	'hierarchie_configuration' => 'Configuração',
+
+	// I
+	'icone_admin_iextras' => 'Acrescentar / modificar os campos extras',
+	'icone_admin_plugin' => 'Gestão de plugins',
+	'icone_admin_spipmotion_file' => 'Fila de espera de codificação',
+	'icone_admin_themes' => 'Gestão dos temas',
+	'icone_config_diogene' => 'Configuração das máscaras de formulários',
+	'icone_config_diogene_nouveau' => 'Criar uma nova máscara de formulário',
+	'icone_config_gestion_mutu' => 'Gestão das instâncias de mutualização',
+	'icone_config_gestion_secteurs' => 'Gestão das seções',
+	'icone_config_menus' => 'Configuração dos menus',
+	'icone_docs_disparus' => 'Documentos desaparecidos',
+	'icone_forums_config' => 'Configuração dos fóruns',
+	'icone_forums_management' => 'Gerenciamento dos fóruns',
+	'icone_mediaspip' => 'Gerenciamento do template',
+	'icone_phpinfo' => 'Configuração do PHP',
+	'icone_supprimer_secteur' => 'Excluir este setor',
+	'icone_theme' => 'Configuração do tema atual',
+	'icone_visiteurs' => 'Gerenciamento dos usuários',
+	'icone_votre_site' => 'Informações do site',
+	'info_contacts_importer' => 'Importar os seus contatos',
+	'info_contacts_inviter' => 'Convidaros seus contatos a se juntarem ao site',
+	'info_plugin_non_disponible' => 'Este plugin não está disponível na sua instalação.',
+	'infos_champs_formulaire_forum' => 'Campos disponíveis no formulário',
+	'infos_inscription_visiteurs' => 'inscrição dos usuários',
+	'infos_tous_diogenes' => 'Todos os templates',
+	'infos_tous_menus' => 'Todos os menus',
+	'infos_tous_secteurs' => 'Todos os setores',
+	'infos_visiteur_choisir_modifier' => 'Alterar um usuário',
+	'infos_visiteur_creer' => 'Incluir um novo usuário',
+	'infos_visiteur_creer_modifier' => 'Incluir ou alterar um usuário',
+	'infos_visiteur_modifier' => 'Alterar o usuário «@login@»',
+
+	// L
+	'label_form_bio_auteur' => 'Quem é esta pessoa?',
+	'label_form_email_auteur' => 'Seu endereço de e-mail',
+	'label_form_nom_site_auteur' => 'O nome do seu site',
+	'label_form_url_site_auteur' => 'O endereço (URL) do seu site',
+	'label_site_background' => 'Transferir uma nova imagem de fundo:',
+	'legend_form_identification_auteur' => 'Seus dados de conexão',
+	'lien_creer_auteur' => 'Criar um novo usuário',
+
+	// M
+	'message_acceder_conf_doc2img' => 'Acessar a configuração da conversão de documentos em imagem.',
+	'message_acceder_conf_emballe_medias' => 'Acessar a configuração dos arquivos que podem ser anexados.',
+	'message_acceder_conf_plugins' => 'Acessar a interface de administração de plugins.',
+	'message_acceder_conf_spipmotion' => 'Acessar a configuração do codificador de mídias.',
+	'message_config_lang_ok' => 'A configuração de idiomas foi alterada.',
+	'message_config_site_ok' => 'As informações do site foram alteradas',
+	'message_config_visiteurs_ok' => 'A configuração da inscrição de usuários foi alterada',
+	'message_confirmation_supprimer_secteur' => 'Você quer realmente excluir este setor?',
+	'message_doc_disparu_objet' => 'Este documento foi vinculado ao objeto do tipo "@objet@" número "@id_objet@"',
+	'message_formats_doc2img' => 'Certos tipos de arquivos passíveis de serem anexados e compativeis com o conversor de arquivos em série de imagens (Doc2img) não estão presentes na configuração do conversor:',
+	'message_formats_doc2img_extensions' => '@formats@',
+	'message_formats_emballe_media_plugin' => 'Para a utilização de certos tipos de arquivos passíveis de serem anexados  (@formats@), alguns plugins são recomendados:',
+	'message_formats_spipmotion' => 'Certos tipos de arquivos passíveis de serem anexados e compatíveis com o codificador de mídias (SPIPmotion) não estão presentes na configuração do codificador:',
+	'message_formats_spipmotion_audio' => '@formats@ para os arquivos de áudio;',
+	'message_formats_spipmotion_video' => '@formats@ para os arquivos de vídeo;',
+	'message_page_config' => 'Esta parte do site permite-lhe configurar o seu funcionamento.',
+
+	// T
+	'theme_desactiver_previsu' => 'Desativar a pré-visualização',
+	'theme_previsu' => 'Você pré-visualiza atualmente',
+	'titre_autres_diogenes' => 'Os outros templates',
+	'titre_autres_menus' => 'Os outros menus',
+	'titre_autres_secteurs' => 'Os outros setores',
+	'titre_configuration' => 'Configuração do site',
+	'titre_documents_manquants' => '@nb@ documentos originais faltando',
+	'titre_plugins_conseilles' => 'Plugins suplementares recomendados',
+	'titre_site_background' => 'Imagem de fundo'
+);
