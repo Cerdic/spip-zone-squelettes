1441894359e007600d37da8df639e8fb6c7d326c
chankalan <chankalan@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-10 10:07:01 +0000

    la v2 - premier jet cf README.txt
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122333 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_spectral/html5up_spectral_administrations.php
+++ html5up_spectral/html5up_spectral_administrations.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Fichier gérant l'installation et désinstallation du plugin Html5Up Spectral
+ * Fichier gérant l'installation et désinstallation du plugin Html5up Spectral
  *
- * @plugin     Html5Up Spectral
- * @copyright  2019
- * @author     cent20
+ * @plugin     Html5up Spectral
+ * @copyright  2020
+ * @author     chankalan
  * @licence    GNU/GPL
  * @package    SPIP\Html5up_spectral\Installation
  */
@@ -15,7 +15,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 
 /**
- * Fonction d'installation et de mise à jour du plugin Html5Up Spectral.
+ * Fonction d'installation et de mise à jour du plugin Html5up Spectral .
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
@@ -25,21 +25,26 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 **/
 function html5up_spectral_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
-
+	$maj['create'] = array(
+		array('ecrire_config','html5up', array(
+			'couleur_accent' => '#ed4933',
+			'couleur_typo_entete' => '#ffffff',
+			'couleur_bkg_entete' => '#1D242A',
+		))
+	);
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 
 /**
- * Fonction de désinstallation du plugin Html5Up Spectral.
+ * Fonction de désinstallation du plugin Html5up Spectral .
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
  * @return void
 **/
 function html5up_spectral_vider_tables($nom_meta_base_version) {
-
-
 	effacer_meta($nom_meta_base_version);
+	effacer_meta('html5up');
 }
