d598562d84ec8ce5ef790a9f5f90fc2939ccbee5
yannx.spip@hotmail.fr <yannx.spip@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-09 15:46:31 +0000

    La version historique au 21/12/2011
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122921 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spippourlesnuls/branches/v1/lang/local_fr.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/branches/spip-2.1/ecrire/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'accueil_site' => 'Accueil du site',
+
+// F
+'form_forum_identifiants' => '',
+'form_forum_indiquer_nom_email' => '',
+'form_forum_indiquer_nom_email' => 'Indiquez ici votre nom et votre adresse email. Votre identifiant personnel vous parviendra rapidement, par courrier &eacute;lectronique.',
+
+'forum' => 'Forum',
+
+// N
+'navigation' => 'Navigation',
+'nom' => 'Nom',
+'nouveautes' => 'Les nouveaut&eacute;s',
+'nouveautes_web' => 'Nouveaut&eacute;s sur le Web',
+'nouveaux_articles' => 'Nouveaux articles',
+'nouvelles_breves' => 'Nouvelles br&egrave;ves',
+'voir_squelette' => 'voir le squelette de cette page'
+);
+
+?>
