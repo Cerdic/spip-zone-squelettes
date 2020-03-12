6572ba98c6213dfa70b167e7afcd0cc3b0f43605
jeanmarie <jeanmarie@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-12 13:44:18 +0000

    Revert 41a56f4bdbbcc79200ba7e99637e5cb3a8a5aa0a.
    On me dit dans l'oreillette que var_mode=css est un truc spécifique au plugin lesscss/scss donc inutisable ici
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122962 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5up_alpha/alpha_fonctions.php
+++ html5up_alpha/alpha_fonctions.php
@@ -8,12 +8,3 @@ $GLOBALS['meta']['version_html_max'] = 'html5';
 // Intertitres commençant par h2
 $GLOBALS['debut_intertitre'] = "\n<h2 class=\"spip\">\n";
 $GLOBALS['fin_intertitre'] = "</h2>\n";
-
-function alpha_formulaire_admin($flux) {
-	if (autoriser('configurer', 'alpha')) {
-		$btn = recuperer_fond('prive/bouton/calculer_css');
-		$flux['data'] = preg_replace('%(<!--extra-->)%is', $btn.'$1', $flux['data']);
-	}
-
-	return $flux;
-}
