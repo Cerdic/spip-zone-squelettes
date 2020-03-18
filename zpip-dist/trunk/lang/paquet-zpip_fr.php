a7da005dcfc5808402e9fba842136f056705eb32
Matthieu Marcillaud <Matthieu Marcillaud@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-18 13:14:40 +0000

    Ce plugin n’a pas dit son dernier mot (mais quand même, préférez spipr-dist !) : il s’affichera aussi en SPIP 3.3 (qui ne sait pas lire les plugin.xml).
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@122998 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-dist/trunk/lang/paquet-zpip_fr.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'zpip_description' => 'Squelettes par défaut pour Z-core, modulaires et exemplaires',
+	'zpip_slogan' => 'Squelettes Z pour SPIP (Zpip)',
+);
--- zpip-dist/trunk/zpip_options.php
+++ zpip-dist/trunk/zpip_options.php
@@ -1,6 +1,6 @@
 <?php
 // permettre la personalisation dans mes_options.php
-if (!isset($GLOBALS['z_blocs']) OR !$GLOBALS['z_blocs'])
+if (!isset($GLOBALS['z_blocs']) OR !$GLOBALS['z_blocs']) {
 	$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
+}
 
-?>
