ea93e5b83fb6069c24a3efe12be6356eb372c22f
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-18 17:41:50 +0000

    Je supprime plugin.xml car il ne sert plus à rien depuis qu'il y a paquet.xml et fait l'ajout d'un fichier de lang en reprenant le même texte car il y avait un multi dans plugin.xml. - Permet d'être certain que quand il y aura un z+1 concernant la version du plug, il n'y aura pas oubli de le faire à paquet.xml et plugin.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@123004 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip-dist/trunk/lang/paquet-zpip_en.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'zpip_description' => 'Skeleton like squelettes-dist of SPIP, with an optimized organization and modularity',
+	'zpip_slogan' => 'Z skeletons for SPIP (Zpip)',
+);
