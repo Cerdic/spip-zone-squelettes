41d9e84a34fe868562730e2c580543f13643dc7a
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-24 18:30:06 +0000

    On introduit un nouveau bloc "cartouche" (oui, ce terme existe en anglais !) qui reprend le cartouche des pages (ça ne devrait pas modifier les sites existants qui utiliseraient ce jeune squelette).
    Ce bloc est appelé par défaut depuis les blocs "content", mais il est possible de désactiver cela en envoyant le paramètre "cartouche=0" à l’inclusion dans body.html pour inclure le cartouche autre part.
    Exemple : `<INCLURE{fond=content/#ENV{type-page},env, cartouche=0} />`
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@123107 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-blog/trunk/spipr_blog_options.php
+++ spipr-blog/trunk/spipr_blog_options.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($GLOBALS['z_blocs']))
-	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb','cartouche');
 
 define('_ZENGARDEN_FILTRE_THEMES','spiprv2');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
