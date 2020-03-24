aec12a8636e19e727f5d7450cf0291e2c205d5c2
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-24 18:30:06 +0000

    Il faut un filtre zengarden different car les themes spipr v1 ne sont plus compatibles + ajouter un procure pour le futur
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@123106 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-blog/trunk/spipr_blog_options.php
+++ spipr-blog/trunk/spipr_blog_options.php
@@ -2,8 +2,6 @@
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
 
-define('_ZENGARDEN_FILTRE_THEMES','spipr');
+define('_ZENGARDEN_FILTRE_THEMES','spiprv2');
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
-
-?>
