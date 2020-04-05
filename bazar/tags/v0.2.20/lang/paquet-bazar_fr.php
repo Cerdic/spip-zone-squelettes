2f3154564e4eabd30d8e4d455d4c8ef147a8c925
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.2.20' created from /trunk:109618
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124036 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bazar/tags/v0.2.20/lang/paquet-bazar_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// F
+	'bazar' => 'Squelettes HTML5 qui propose un affichage de la page d\'accueil modulable et paramétrable depuis l\'espace d\'administration.',
+	'bazar_nom' => 'Bazar',
+	'bazar_slogan' => 'Afficher tout son bazar sur l\'accueil du site.'
+);
+
