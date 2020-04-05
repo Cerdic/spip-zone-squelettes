ac0cc1eeb4dc1f405fffb27cd8ce0308e8422d1a
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/_tags_/html5up_spectral/v1.0.20' created from /html5up_spectral:122381
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124043 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _tags_/html5up_spectral/v1.0.20/lang/paquet-html5up_spectral_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// H
+	'html5up_spectral_description' => 'Portage du thème graphique Spectral du site internet html5up.net
+Fully Responsive / Built on intelligent HTML5 + CSS3 
+Creative Commons Licence',
+	'html5up_spectral_nom' => 'Html5Up Spectral',
+	'html5up_spectral_slogan' => '',
+);
