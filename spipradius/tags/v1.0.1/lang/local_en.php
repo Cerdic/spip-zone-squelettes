705dea5083296df38fdb9e6b0fbd61b70cf75770
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.1' created from /trunk:103869
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124058 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipradius/tags/v1.0.1/lang/local_en.php
@@ -0,0 +1,20 @@
+<?php
+//  Fichier langue de SPIP propre au squelette SPIPRADIUS
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'au_hasard' => 'At random',
+	'anonyme' => 'Anonymous',
+	'les_plus_vues' => 'Most viewed',
+	'les_plus_commentés' => 'Most commented',
+    'mots_clef' => 'Tag',
+	'mots_clefs' => 'Tags',
+	'mots_clefs_meme_groupe' => 'Tags in the same group',
+	'messages_forum' => 'Comments',
+	'photos' => 'photos'
+		
+		);
+
+?>
---
+++ spipradius/tags/v1.0.1/lang/local_es.php
@@ -0,0 +1,20 @@
+<?php
+//  Fichier langue de SPIP propre au squelette SPIPRADIUS
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'au_hasard' => 'Azar',
+	'anonyme' => 'Anónimo',
+	'les_plus_vues' => 'Los más vistos',
+	'les_plus_commentés' => 'Mas Comentarios',
+    'mots_clef' => 'Etiqueta',
+	'mots_clefs' => 'Etiquetas',
+	'mots_clefs_meme_groupe' => 'Etiquetas en el mismo grupo',
+	'messages_forum' => 'Comentario',
+	'photos' => 'fotos'
+		
+		);
+
+?>
