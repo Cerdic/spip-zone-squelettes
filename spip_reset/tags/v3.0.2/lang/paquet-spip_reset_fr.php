c179da873cf04e75949074acff404d0a619c6939
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v3.0.2' created from /trunk:107264
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124059 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_reset/tags/v3.0.2/lang/paquet-spip_reset_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/autorite/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // A
+    'spip_reset_description' => 'Squelettes pour ne rien publier. Ce plugin annule tous les squelettes par défaut de SPIP, ce qui permet d’éviter la fuite involontaire d’informations d’un site.',
+    'spip_reset_nom' => 'SPIP reset',
+    'spip_reset_slogan' => 'Evitez les diffusions involontaires !'
+);
