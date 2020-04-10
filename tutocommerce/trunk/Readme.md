f5e433894c16273413f99954dc2f8b4401c691c8
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-10 14:31:29 +0000

    Simplifions la description
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124122 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tutocommerce/trunk/Readme.md
+++ tutocommerce/trunk/Readme.md
@@ -1,5 +1,5 @@
 # Plugin TutoCommerce
 
-Plugin tutoriel qui explique les bases pour faire de la vente en ligne sous SPIP, et montre par l’exemple comment implémenter un tunnel de commande simple.
+Plugin tutoriel qui montre par l’exemple les bases pour faire de la vente en ligne sous SPIP, et comment implémenter un tunnel de commande simple.
 
 Rendez-vous sur la page de démo à l’URL spip.php?page=demo/tutocommerce et laissez vous guider.
--- tutocommerce/trunk/lang/paquet-tutocommerce_fr.php
+++ tutocommerce/trunk/lang/paquet-tutocommerce_fr.php
@@ -6,10 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'tutocommerce_description' => '<p>Sous SPIP, il existe un framework permettant de faire du ecommerce. Il est complètement fonctionnel, modulaire, extensible, et permet de faire de la vente dans le confort de notre CMS favori.</p><p>Il se compose d\'une suite de plugins complémentaires, qui prennent en charge chacun un aspect : gestion des produits, des commandes, des paniers, des transactions, etc.</p><p>Le présent plugin s\'adresse aux gens qui écrivent des squelettes, il a un objectif didactique : expliquer et montrer <em>les bases</em> de l\'implémentation du framework. C\'est relativement simple : en 3 squelettes, au moyen de quelques boutons d\'actions et formulaires, vous avez déjà le minimum nécessaire pour vendre des choses sur votre site.</p>
-<p>Rendez-vous sur la page de démo à l\'URL <strong><code>spip.php?page=demo/tutocommerce</code></strong> et laissez vous guider.</p>',
-	'tutocommerce_nom' => 'Tuto-commerce',
+	'tutocommerce_description' => 'Plugin tutoriel qui montre par l’exemple les bases pour faire de la vente en ligne sous SPIP, et comment implémenter un tunnel de commande simple. Rendez-vous sur la page de démo à l’URL spip.php?page=demo/tutocommerce et laissez vous guider.',
+	'tutocommerce_nom' => 'Tuto Commerce',
 	'tutocommerce_slogan' => 'Tutoriel pour implémenter le framework ecommerce dans vos squelettes.',
 );
 
-?>
