7e6cbe765cfe3487dea2e2136e5ede670a6316fb
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-17 23:05:49 +0000

    Tag '/tags/2.1.0' created from /trunk:124230
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ressourcotheque/tags/2.1.0/lang/ressourcotheque_fr.php
@@ -0,0 +1,57 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-squelettes/ressourcotheque.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_resultat' => 'Aucun résultat',
+
+	// C
+	'connexe' => 'Ressource connexe',
+	'connexes' => 'Ressources connexes',
+	'contacter' => 'Contacter',
+	'contenu_documents' => 'Contenu des documents liés',
+	'contenu_documents_explication' => 'Mettez ici le contenu des documents liés si votre serveur ne dispose pas de la fonction d’indexation des documents.
+	Ce contenu apparaîtra dans les recherches, mais pas sur la fiche.',
+
+	// E
+	'explication_recherche' => 'Commençez par une recherche libre à travers le formulaire.
+
+	Par défaut, l’opérateur booléen ET est utilisé entre les mots. Vous pouvez utiliser :
+- <code>-</code> pour l’opérateur booléen NON ;
+- <code>|</code> pour l’opérateur booléen OU ;
+- <code>()</code> pour combiner des termes et faire une recherche complexe.
+
+	Vous pourrez ensuite filtrer les résultats par mots-clé.
+
+	Vous pouvez également parcourir directement les ressources pour un mot-clé spécifique.
+',
+	'explication_recherche_facette' => 'Vous pouvez maintenant affiner les résultats en filtrant par mot-clé.',
+
+	// F
+	'filtres' => 'Filtres :',
+
+	// G
+	'guide_redaction' => 'Guide de rédaction',
+	'guide_redaction_explication' => 'Mettre ici un guide de rédaction pour ces ressources',
+
+	// M
+	'modifier_mon_profil' => 'Modifier mon profil',
+	'modifier_profil' => 'Modifier le profil',
+
+	// P
+	'par_auteur' => 'Mise en ligne par',
+
+	// S
+	'supprimer_filtre' => 'Supprimer le filtre @txt@',
+
+	// T
+	'types_ressources' => 'Types de ressources disponibles',
+
+	// Z
+	'zip' => 'Zip de l’ensemble des documents joints'
+);
---
+++ ressourcotheque/tags/2.1.0/ressourcotheque_pipelines.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Pipeline pour choisir quels champs sont vérifier par linkcheck
+ * @param array $flux paramètres d'entrée
+ * @return array $flux
+**/
+function ressourcotheque_linkcheck_champs_a_traiter($flux) {
+	if ($flux['args']['table'] == 'articles') {
+		unset ($flux['data']['contenu_documents']);
+	}
+	return $flux;
+}
