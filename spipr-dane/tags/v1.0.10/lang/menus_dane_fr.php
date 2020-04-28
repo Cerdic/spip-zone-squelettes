49bcff873ace520b385d833ee2a3852d21b833db
Johan Pustoch <Johan Pustoch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-28 08:53:00 +0000

    Tag '/tags/v1.0.10' created from /trunk:124363
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipr-dane/tags/v1.0.10/lang/menus_dane_fr.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+ 	// D
+	'description_menu_accueil_icone' =>'Icône pointant vers la page d\'accueil.',
+	'description_menu_agenda_icone' =>'Icône pointant vers la page Agenda.',
+	'description_menu_contact_icone' =>'Icône pointant vers la page Contact.',
+	'description_menu_map_icone' =>'Icône pointant vers la page de localisation.',
+	'description_menu_separateur_vertical' => 'Séparateur vertical (utile à la suite d\'une série d\'items picto).',
+	'description_menu_picto' => 'Icône picto à choisir pointant vers une page à choisir.',
+	'description_menu_sites_icone' => 'Icône pointant vers la page Sites web.',
+    'description_menu_liste_sites_logo' => 'Logos d\'une liste de sites web référencés.',
+
+
+    // E
+	'entree_nom_fa' => 'Entrez ici le nom du pictogramme choisi sur https://fontawesome.com/v4.7.0/icons/ (obligatoire)',
+	'entree_lien' => 'Entrez ici l\'url vers laquelle le lien doit pointer. S\'il s\'agit d\'un objet Spip, saisissez  "objet|xx". Exemple article|23, rubrique|5 (obligatoire)',
+	'entree_page' => 'Vous pouvez entrer le nom d\'une page exixtante (contact par exemple) ou de la page d\'un auteur ou d\'un article, sous la forme auteurN ou articleN, où N est le numéro (id) de l\'article ou de l\'auteur.',
+    'entree_sites' => 'Identifiants des sites référencés à afficher, séparés par une virgule',
+    'entree_largeur_logo' => 'Largeur du logo. Laisser vide si la hauteur est prioritaire',
+    'entree_hauteur_logo' => 'hauteur du logo. Laisser vide si la largeur est prioritaire',
+
+    // L
+    'label_menu_alt' =>'Texte au survol du lien',
+
+    // M
+	'menu_accueil_icone' => 'Icône Accueil',
+	'menu_accueil_title' => 'Accueil',
+	'menu_agenda_icone' => 'Icône Agenda',
+	'menu_agenda_title' => 'Agenda',
+	'menu_contact_icone' => 'Icône Contact',
+	'menu_contact_title' => 'Nous contacter',
+	'menu_map_icone' => 'Icône Localisation',
+	'menu_map_title' => 'Nous situer',
+	'menu_picto' => 'Pictograme',
+	'menu_separateur_vertical' => 'Séparateur vertical',
+	'menu_sites_icone' => 'Icône Sites web',
+	'menu_sites_title' => 'Sites web',
+    'menu_liste_sites_logo' => 'Liste de logos de sites',
+
+
+);
---
+++ spipr-dane/tags/v1.0.10/spipr_dane_options.php
@@ -0,0 +1,14 @@
+<?php
+$GLOBALS['oembed_providers'] = array(
+    'http://webtv.ac-versailles.fr/restauration/*' => 'http://webtv.ac-versailles.fr/restauration/oembed.api',
+    'http://webtv.ac-versailles.fr/*' => 'http://webtv.ac-versailles.fr/oembed.api',
+    'https://www.audio-lingua.eu/*' => 'https://www.audio-lingua.eu/oembed.api',
+    'http://scolawebtv.crdp-versailles.fr/*' => 'http://scolawebtv.crdp-versailles.fr/oembed.api/',
+    'http://scolawebtv.ac-versailles.fr/*' => 'http://scolawebtv.ac-versailles.fr/oembed.api/',
+    'http://pod:8080/video*' => 'http://pod:8080/oembed/',
+    'https://pod.ac-caen.fr/video*' => 'https://pod.ac-caen.fr/oembed/',
+    'https://learningapps.org/*'=> 'https://learningapps.org/oembed.php',
+);
+
+define('_SURLIGNE_RECHERCHE_REFERERS',true);
+
