10ff0c3c09b9b946f1e7635cf49b9e3315ffda16
Jacques Pyrat <Jacques Pyrat@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-01 21:44:39 +0000

    La feuille de style d'affichage des flux RSS passe en dynamique avec chaînes de langues.
    Nota : le xslt est repris et modifié depuis celui de DotClear
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@124902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_fr.php
+++ soyezcreateurs_net/trunk/plugins/soyezcreateurs/lang/soyezcreateurs_fr.php
@@ -139,6 +139,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'recherche_value' => 'Recherche ?',
 	'retoursommaire' => 'Retour Sommaire',
 	'retourtop' => 'Revenir en haut',
+	'rss_abonnement' => 'Copiez l’URL suivante dans votre agrégateur :',
+	'rss_abonnement_titre' => 'S’abonner',
+	'rss_abonnement_titre_page' => 'S’abonner :',
+	'rss_explication' => 'Un flux RSS est un résumé automatique et gratuit des actualités d’un site. Il fournit le contenu ou un extrait du contenu, accompagné de liens vers le contenu complet. Les dernières actualités publiées peuvent être lues dans votre <a href="https://fr.wikipedia.org/wiki/Agr%C3%A9gateur">agrégateur RSS</a> de prédilection.',
+	'rss_explication_titre' => 'Qu’est-ce qu’un flux RSS ?',
 	'rubrique_securisee' => 'Accés réservé ouvert',
 
 	// S
