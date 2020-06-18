bfb68a7cea738f6e6afdcbb12d285f25c61edec9
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-18 10:33:18 +0000

    Tag '/tags/v2.1.0' created from /trunk:124230
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_@125247 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ressourcotheque/tags/v2.1.0/base/ressourcotheque.php
@@ -0,0 +1,33 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function ressourcotheque_declarer_champs_extras($champs = array()) {
+  $champs['spip_articles']['contenu_documents'] = array(
+      'saisie' => 'textarea',//Type du champ (voir plugin Saisies)
+      'options' => array(
+            'nom' => 'contenu_documents',
+						'label' => _T('ressourcotheque:contenu_documents'),
+						'conteneur_class'=>'pleine_largeur',
+						'explication' => _T('ressourcotheque:contenu_documents_explication'),
+						'sql' => "LONGTEXT NOT NULL DEFAULT ''",
+						'versionner' => true,
+						'defaut' => '',// Valeur par défaut
+						'rechercher' => true
+      ),
+	);
+  $champs['spip_rubriques']['guide_redaction'] = array(
+      'saisie' => 'textarea',//Type du champ (voir plugin Saisies)
+      'options' => array(
+            'nom' => 'guide_redaction',
+						'label' => _T('ressourcotheque:guide_redaction'),
+						'conteneur_class'=>'pleine_largeur',
+						'explication' => _T('ressourcotheque:guide_redaction_explication'),
+            'sql' => "LONGTEXT NOT NULL DEFAULT ''",
+						'versionner' => true,
+						'defaut' => '',// Valeur par défaut
+						'rechercher' => true,
+						'traitements' => '_TRAITEMENT_RACCOURCIS',
+      ),
+  );
+  return $champs;
+}
---
+++ ressourcotheque/tags/v2.1.0/lang/paquet-ressourcotheque_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-squelettes/ressourcotheque.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'ressourcotheque_description' => 'Ce squelette permet de classer et rechercher des ressources documentaires, en proposant une recherche par facette et de mettre certains documents en accès restreint',
+	'ressourcotheque_slogan' => 'Classer et retrouver des ressources documentaires'
+);
